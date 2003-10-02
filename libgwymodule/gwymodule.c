f774e7588dc3670b76d889d0b85cd15a33c005f8
yeti <yeti@25697fe4-d644-49b8-9d30-be5465f2adf3>
2003-10-02 17:42:31 +0000

    started real implementation
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@87 25697fe4-d644-49b8-9d30-be5465f2adf3

--- libgwymodule/gwymodule.c
+++ libgwymodule/gwymodule.c
@@ -1,70 +1,113 @@
 /* @(#) $Id$ */
 
-/* XXX: This file is to be replaced. */
 #include <string.h>
-#include <glib-object.h>
 #include <gmodule.h>
+#include <libgwyddion/gwymacros.h>
 
 #include "gwymodule.h"
 
-#define GWY_MODULE_TYPE_NAME "GwyModule"
+static void gwy_load_modules_in_dir(GDir *gdir, GHashTable *modules);
 
+static GHashTable *modules = NULL;
 
-static void gwy_module_class_init(GwyModuleClass *klass);
-static void gwy_module_init(GwyModule *module);
-
-
-GType
-gwy_module_get_type(void)
+void
+gwy_modules_init(void)
 {
-    static GType gwy_module_type = 0;
-
-    if (!gwy_module_type) {
-        static const GTypeInfo gwy_module_info = {
-            sizeof(GwyModuleClass),
-            NULL,
-            NULL,
-            (GClassInitFunc)gwy_module_class_init,
-            NULL,
-            NULL,
-            sizeof(GwyModule),
-            0,
-            (GInstanceInitFunc)gwy_module_init,
-            NULL,
-        };
-
-        #ifdef DEBUG
-        g_log(GWY_LOG_DOMAIN, G_LOG_LEVEL_DEBUG, "%s", __FUNCTION__);
-        #endif
-        gwy_module_type = g_type_register_static(G_TYPE_OBJECT,
-                                                 GWY_MODULE_TYPE_NAME,
-                                                 &gwy_module_info,
-                                                 0);
+    /* Check whether modules are supported. */
+    if (!g_module_supported()) {
+        g_error("Cannot initialize modules: not supported on this platform.\n");
     }
+}
 
-    return gwy_module_type;
+void
+gwy_register_modules(const gchar **paths)
+{
+    const gchar *dir;
+
+    if (!paths)
+        return;
+
+    if (!modules)
+        modules = g_hash_table_new(g_str_hash, g_str_equal);
+
+    for (dir = *paths; dir; dir = *(++paths)) {
+        GDir *gdir;
+        GError *err = NULL;
+
+        gdir = g_dir_open(dir, 0, &err);
+        if (err) {
+            g_warning("Cannot open module directory %s", dir);
+            g_clear_error(&err);
+            continue;
+        }
+
+        gwy_load_modules_in_dir(gdir, modules);
+        g_dir_close(gdir);
+    }
 }
 
 static void
-gwy_module_class_init(GwyModuleClass *klass)
+gwy_load_modules_in_dir(GDir *gdir, GHashTable *modules)
 {
-    GObjectClass *gobject_class = G_OBJECT_CLASS(klass);
+    const gchar *modulename;
 
-    #ifdef DEBUG
-    g_log(GWY_LOG_DOMAIN, G_LOG_LEVEL_DEBUG, "%s", __FUNCTION__);
-    #endif
-}
+    while ((modulename = g_dir_read_name(gdir))) {
+        GModule *mod;
+        GwyModuleRegisterFunc register_func;
+        gboolean ok;
+        GwyModuleInfo *mod_info;
 
-static void
-gwy_module_init(GwyModule *module)
-{
-    #ifdef DEBUG
-    g_log(GWY_LOG_DOMAIN, G_LOG_LEVEL_DEBUG, "%s", __FUNCTION__);
-    #endif
+        gwy_debug("Trying to load module %s.\n", modulename);
+        mod = g_module_open(modulename, G_MODULE_BIND_LAZY);
 
-    module->abi_version = GWY_MODULE_ABI_VERSION;
-    module->module_info = NULL;
-    /* TODO */
+        if (!mod) {
+            g_warning("Cannot open module %s: %s\n",
+                      modulename, g_module_error());
+            continue;
+        }
+        gwy_debug("Module loaded successfully as %s.\n", g_module_name(mod));
+
+        ok = TRUE;
+        if (!g_module_symbol(mod, GWY_MODULE_REGISTER_FUNC_NAME,
+                             (gpointer)&register_func)) {
+            g_warning("No registration function in module %s", modulename);
+            ok = FALSE;
+        }
+
+        if (ok) {
+            mod_info = register_func(mod);
+            ok = mod_info != NULL;
+            if (!ok)
+                g_warning("Module %s returned empty info", modulename);
+        }
+
+        if (ok) {
+            ok = mod_info->abi_version != GWY_MODULE_ABI_VERSION;
+            if (!ok)
+                g_warning("Module %s ABI version %d is different from %d",
+                          modulename, mod_info->abi_version,
+                          GWY_MODULE_ABI_VERSION);
+        }
+
+        if (ok) {
+            ok = !g_hash_table_lookup(modules, mod_info->name);
+            if (!ok)
+                g_warning("Duplicate module %s, keeping only the first one",
+                          mod_info->name);
+        }
+
+        if (ok) {
+            g_hash_table_insert(modules, (gpointer)mod_info->name, mod_info);
+            gwy_debug("Making module %s resident.\n", modulename);
+            g_module_make_resident(mod);
+        }
+        else {
+            if (!g_module_close(mod))
+                g_critical("Cannot unload module %s: %s\n",
+                           modulename, g_module_error());
+        }
+
+    }
 }
 
 /* vim: set cin et ts=4 sw=4 cino=>1s,e0,n0,f0,{0,}0,^0,\:1s,=0,g1s,h0,t0,+1s,c3,(0,u0 : */
--- libgwymodule/gwymodule.h
+++ libgwymodule/gwymodule.h
@@ -1,53 +1,47 @@
 /* @(#) $Id$ */
 
-/* XXX: This file is to be replaced. */
 #ifndef __GWY_MODULE_H__
 #define __GWY_MODULE_H__
-
-#include <glib-object.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif /* __cplusplus */
 
-#define GWY_TYPE_MODULE                  (gwy_module_get_type())
-#define GWY_MODULE(obj)                  (G_TYPE_CHECK_INSTANCE_CAST((obj), GWY_TYPE_MODULE, GwyModule))
-#define GWY_MODULE_CLASS(klass)          (G_TYPE_CHECK_INSTANCE_CAST((klass), GWY_TYPE_MODULE, GwyModuleClass))
-#define GWY_IS_MODULE(obj)               (G_TYPE_CHECK_INSTANCE_TYPE((obj), GWY_TYPE_MODULE))
-#define GWY_IS_MODULE_CLASS(klass)       (G_TYPE_CHECK_INSTANCE_TYPE((klass), GWY_TYPE_MODULE))
-#define GWY_MODULE_GET_CLASS(obj)        (G_TYPE_INSTANCE_GET_INTERFACE((obj), GWY_TYPE_MODULE, GwyModuleClass))
-
 #define GWY_MODULE_ABI_VERSION 0
 
+#define GWY_MODULE_REGISTER_FUNC gwy_module_register
+#define GWY_MODULE_REGISTER_FUNC_NAME G_STRINGIFY(GWY_MODULE_REGISTER_FUNC)
+
 typedef struct _GwyModuleInfo GwyModuleInfo;
+typedef struct _GwyModuleFuncInfo GwyModuleFuncInfo;
+
+typedef enum {
+    GWY_FUNC_ROLE_NONE,
+    GWY_FUNC_ROLE_CLASS,
+    GWY_FUNC_ROLE_PROCESS,
+    GWY_FUNC_ROLE_LOAD,
+    GWY_FUNC_ROLE_SAVE
+} GwyModuleFuncRole;
+
+struct _GwyModuleFuncInfo {
+    const gchar *name;
+    const gchar *blurb;
+    GwyModuleFuncRole role;
+};
 
 struct _GwyModuleInfo {
+    guint32 abi_version;
     const gchar *name;
     const gchar *blurb;
     const gchar *author;
     const gchar *version;
     const gchar *copyright;
     const gchar *date;
+    gsize nfuncs;
+    GwyModuleFuncInfo *func_info;
 };
 
-typedef struct _GwyModule GwyModule;
-typedef struct _GwyModuleClass GwyModuleClass;
-
-struct _GwyModule {
-    GObject parent_instance;
-
-    guint32 abi_version;
-    GwyModuleInfo *module_info;
-};
-
-struct _GwyModuleClass {
-    GObjectClass parent_class;
-
-    /* TODO */
-};
-
-GType      gwy_module_get_type          (void) G_GNUC_CONST;
-
+typedef GwyModuleInfo* (*GwyModuleRegisterFunc)(GModule *mod);
 
 #ifdef __cplusplus
 }
