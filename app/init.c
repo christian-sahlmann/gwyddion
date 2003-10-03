78819bd154a12bf1e3bacdd93b49af9196368bd3
yeti <yeti@25697fe4-d644-49b8-9d30-be5465f2adf3>
2003-10-03 11:17:26 +0000

    added files, just a skeleton now
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@92 25697fe4-d644-49b8-9d30-be5465f2adf3

---
+++ app/init.c
@@ -0,0 +1,26 @@
+/* @(#) $Id$ */
+
+#include <libgwyddion/gwyddion.h>
+#include <libgwymodule/gwymodule.h>
+#include <libgwydgets/gwydgets.h>
+#include <libprocess/libgwyprocess.h>
+
+/**
+ * gwy_type_init:
+ *
+ * Initializes all Gwyddion data types, i.e. types that may appear in
+ * serialized data. GObject has to know about them when g_type_from_name()
+ * is called.
+ **/
+void
+gwy_type_init(void)
+{
+    gwy_sphere_coords_get_type();
+    gwy_data_field_get_type();
+    gwy_data_line_get_type();
+    gwy_palette_get_type();
+    gwy_palette_def_get_type();
+    gwy_container_get_type();
+}
+
+/* vim: set cin et ts=4 sw=4 cino=>1s,e0,n0,f0,{0,}0,^0,\:1s,=0,g1s,h0,t0,+1s,c3,(0,u0 : */
---
+++ app/init.h
@@ -0,0 +1,18 @@
+/* @(#) $Id$ */
+
+#ifndef __GWY_INIT_H__
+#define __GWY_INIT_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+void gwy_type_init(void);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* __GWY_INIT_H__ */
+
+/* vim: set cin et ts=4 sw=4 cino=>1s,e0,n0,f0,{0,}0,^0,\:1s,=0,g1s,h0,t0,+1s,c3,(0,u0 : */
