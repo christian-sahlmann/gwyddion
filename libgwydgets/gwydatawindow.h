77bbab8c86a1b69263c910f3b39085ce3be84cf8
yeti <yeti@25697fe4-d644-49b8-9d30-be5465f2adf3>
2003-10-16 06:42:30 +0000

    fixed zoom1000 to izoom to match gwy_data_window_set_zoom() documentation
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@158 25697fe4-d644-49b8-9d30-be5465f2adf3

--- libgwydgets/gwydatawindow.h
+++ libgwydgets/gwydatawindow.h
@@ -45,7 +45,7 @@ GtkWidget*       gwy_data_window_new              (GwyDataView *data_view);
 GType            gwy_data_window_get_type         (void) G_GNUC_CONST;
 GtkWidget*       gwy_data_window_get_data_view    (GwyDataWindow *data_window);
 void             gwy_data_window_set_zoom         (GwyDataWindow *data_window,
-                                                   gint zoom1000);
+                                                   gint izoom);
 
 #ifdef __cplusplus
 }
