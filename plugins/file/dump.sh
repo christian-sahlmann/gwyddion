3c96285975c7dc069a96d041f9fd43085063185b
yeti <yeti@25697fe4-d644-49b8-9d30-be5465f2adf3>
2003-11-20 23:32:13 +0000

    - added dump file plug-in
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@397 25697fe4-d644-49b8-9d30-be5465f2adf3

---
+++ plugins/file/dump.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+# @(#) $Id$
+# An extremely simple Gwyddion plug-in example in shell.  Saves and loads
+# the data exactly as got from plugin-proxy.
+# Written by Yeti <yeti@physics.muni.cz>.
+# Public domain.
+case "$1" in
+    register)
+    # Plug-in info.
+    echo "dump"
+    echo "Plug-in proxy format dump (.dump)"
+    echo "*.dump"
+    echo "load save"
+    ;;
+
+    load)
+    dumpfile=$2
+    externfile=$3
+    cat $externfile >$dumpfile
+    ;;
+
+    save)
+    dumpfile=$2
+    externfile=$3
+    cat $dumpfile >$externfile
+    ;;
+
+    *)
+    echo "*** Error: plug-in has to be called from Gwyddion plugin-proxy." 1>&2
+    exit 1
+    ;;
+esac
+
