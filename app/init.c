da6c736b086e52b5ebba4decdf6c06de3ee1dfe5
yeti <yeti@25697fe4-d644-49b8-9d30-be5465f2adf3>
2003-11-24 13:20:43 +0000

    - set critical errors to cause core dump to ease debugging
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@432 25697fe4-d644-49b8-9d30-be5465f2adf3

--- app/init.c
+++ app/init.c
@@ -47,6 +47,7 @@ gwy_type_init(void)
     g_set_application_name(_(PACKAGE_NAME));
     gwy_palette_def_setup_presets();
     gwy_stock_register_stock_items();
+    g_log_set_always_fatal(G_LOG_LEVEL_CRITICAL);
 }
 
 /* vim: set cin et ts=4 sw=4 cino=>1s,e0,n0,f0,{0,}0,^0,\:1s,=0,g1s,h0,t0,+1s,c3,(0,u0 : */
