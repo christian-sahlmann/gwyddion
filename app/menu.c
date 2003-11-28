2c95cbb87fc0e0cf602b3a8a886a1358f9ecbb7f
yeti <yeti@25697fe4-d644-49b8-9d30-be5465f2adf3>
2003-11-28 14:11:43 +0000

    - made kill mask and kill presentation undoable
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@476 25697fe4-d644-49b8-9d30-be5465f2adf3

--- app/menu.c
+++ app/menu.c
@@ -386,8 +386,11 @@ gwy_app_kill_mask_cb(void)
     g_return_if_fail(GWY_IS_DATA_WINDOW(data_window));
     data_view = gwy_data_window_get_data_view(data_window);
     data = gwy_data_view_get_data(GWY_DATA_VIEW(data_view));
-    if (gwy_container_remove_by_name(data, "/0/mask"))
+    if (gwy_container_contains_by_name(data, "/0/mask")) {
+        gwy_app_undo_checkpoint(data, "/0/mask");
+        gwy_container_remove_by_name(data, "/0/mask");
         gwy_app_data_view_update(data_view);
+    }
 }
 
 static void
@@ -401,8 +404,11 @@ gwy_app_kill_show_cb(void)
     g_return_if_fail(GWY_IS_DATA_WINDOW(data_window));
     data_view = gwy_data_window_get_data_view(data_window);
     data = gwy_data_view_get_data(GWY_DATA_VIEW(data_view));
-    if (gwy_container_remove_by_name(data, "/0/show"))
-        gwy_data_view_update(GWY_DATA_VIEW(data_view));
+    if (gwy_container_contains_by_name(data, "/0/show")) {
+        gwy_app_undo_checkpoint(data, "/0/show");
+        gwy_container_remove_by_name(data, "/0/show");
+        gwy_data_view_update(data_view);
+    }
 }
 
 /* vim: set cin et ts=4 sw=4 cino=>1s,e0,n0,f0,{0,}0,^0,\:1s,=0,g1s,h0,t0,+1s,c3,(0,u0 : */
