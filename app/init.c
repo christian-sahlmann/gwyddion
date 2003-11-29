30f8ebb1feccc628db75ee94a70c94bc75a3dc59
yeti <yeti@25697fe4-d644-49b8-9d30-be5465f2adf3>
2003-11-29 21:02:07 +0000

    - reference all palettes at program start so they are not resampled again and   again
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@487 25697fe4-d644-49b8-9d30-be5465f2adf3

--- app/init.c
+++ app/init.c
@@ -26,17 +26,26 @@
 #include <libprocess/libgwyprocess.h>
 
 static GType optimization_fooler = 0;
+static GSList *palettes = NULL;
 
+static void ref_palette      (const gchar *name,
+                              GwyPaletteDef *pdef);
+static void unref_palettes   (void);
 /**
  * gwy_type_init:
  *
  * Initializes all Gwyddion data types, i.e. types that may appear in
  * serialized data. GObject has to know about them when g_type_from_name()
  * is called.
+ *
+ * XXX: This function does much more. It registeres stock items, setups
+ * palette presets, and similar things.
  **/
 void
 gwy_type_init(void)
 {
+    g_assert(palettes == NULL);
+
     optimization_fooler += gwy_sphere_coords_get_type();
     optimization_fooler += gwy_data_field_get_type();
     optimization_fooler += gwy_data_line_get_type();
@@ -46,8 +55,30 @@ gwy_type_init(void)
 
     g_set_application_name(_(PACKAGE_NAME));
     gwy_palette_def_setup_presets();
+    gwy_palette_def_foreach((GwyPaletteDefFunc)ref_palette, NULL);
+    g_atexit(unref_palettes);
     gwy_stock_register_stock_items();
     g_log_set_always_fatal(G_LOG_LEVEL_CRITICAL);
+}
+
+static void
+ref_palette(const gchar *name,
+            GwyPaletteDef *pdef)
+{
+    GwyPalette *palette;
+
+    palette = gwy_palette_new(pdef);
+    palettes = g_slist_prepend(palettes, palette);
+}
+
+static void
+unref_palettes(void)
+{
+    GSList *l;
+
+    for (l = palettes; l; l = g_slist_next(l))
+        gwy_object_unref(l->data);
+    g_slist_free(palettes);
 }
 
 /* vim: set cin et ts=4 sw=4 cino=>1s,e0,n0,f0,{0,}0,^0,\:1s,=0,g1s,h0,t0,+1s,c3,(0,u0 : */
