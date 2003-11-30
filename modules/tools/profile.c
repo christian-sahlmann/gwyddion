b68790e4b2f97c10e793d0216f8745280ee79e28
klapetek <klapetek@25697fe4-d644-49b8-9d30-be5465f2adf3>
2003-11-30 21:15:43 +0000

    Profile plugin basic functionality (still very bad)
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@499 25697fe4-d644-49b8-9d30-be5465f2adf3

--- modules/tools/profile.c
+++ modules/tools/profile.c
@@ -19,6 +19,7 @@
  */
 
 #include <math.h>
+#include <glib.h>
 #include <libgwyddion/gwymacros.h>
 #include <libgwyddion/gwymath.h>
 #include <libgwyddion/gwycontainer.h>
@@ -30,10 +31,10 @@
 
 typedef struct {
     gboolean is_visible; 
-    GtkWidget *startx;
-    GtkWidget *starty;
-    GtkWidget *endx;
-    GtkWidget *endy;
+    GPtrArray *positions;
+    GtkWidget *graph;
+    GtkWidget *interpolation;
+    GtkWidget *separation;
     gint interp;
     gboolean separate;
 } ProfileControls;
@@ -51,12 +52,17 @@ static ProfileControls controls;
 static gulong updated_id = 0;
 static gulong response_id = 0;
 static GwyDataViewLayer *select_layer = NULL;
+static GwyDataField *datafield = NULL;
+static GPtrArray *dtl = NULL;
+static GPtrArray *str = NULL;
+
 
 void
 gwy_tool_profile_use(GwyDataWindow *data_window)
 {
     GwyDataViewLayer *layer;
     GwyDataView *data_view;
+    gint i;
 
     gwy_debug("%s: %p", __FUNCTION__, data_window);
 
@@ -81,6 +87,23 @@ gwy_tool_profile_use(GwyDataWindow *data_window)
     if (!dialog)
         dialog = profile_dialog_create(data_view);
 
+    
+    if (!dtl)
+    {
+        dtl = g_ptr_array_new(); 
+        for (i=0; i<5; i++) g_ptr_array_add(dtl, (gpointer)gwy_data_line_new(100, 100, 0));
+    }
+    if (!str)
+    {
+        str = g_ptr_array_new();
+        g_ptr_array_add(str, (gpointer)g_string_new("Profile 1"));
+        g_ptr_array_add(str, (gpointer)g_string_new("Profile 2"));
+        g_ptr_array_add(str, (gpointer)g_string_new("Profile 3"));
+        g_ptr_array_add(str, (gpointer)g_string_new("Profile 4"));
+        g_ptr_array_add(str, (gpointer)g_string_new("Profile 5"));
+
+    }
+
     updated_id = g_signal_connect(select_layer, "updated",
                                    G_CALLBACK(profile_selection_updated_cb),
                                    NULL);
@@ -103,13 +126,6 @@ profile_do(void)
     gwy_app_clean_up_data(data);
     dfield = GWY_DATA_FIELD(gwy_container_get_object_by_name(data, "/0/data"));
 
-    /*
-    x0 = gwy_data_field_rtoj(dfield, x0);
-    y0 = gwy_data_field_rtoi(dfield, y0);
-    x1 = gwy_data_field_rtoj(dfield, x1) + 1;
-    y1 = gwy_data_field_rtoi(dfield, y1) + 1;
-    */
-    
     gwy_data_view_update(GWY_DATA_VIEW(select_layer->parent));
     gwy_debug("%s: %d %d", __FUNCTION__,
               gwy_data_field_get_xres(dfield), gwy_data_field_get_yres(dfield));
@@ -136,14 +152,15 @@ profile_dialog_create(GwyDataView *data_view)
 {
     GwyContainer *data;
     GwyDataField *dfield;
-    GtkWidget *dialog, *table, *label, *graph;
+    GtkWidget *dialog, *table, *label, *vbox;
     gdouble xreal, yreal, max, unit;
+    gint i;
 
     gwy_debug("%s", __FUNCTION__);
     data = gwy_data_view_get_data(data_view);
-    dfield = GWY_DATA_FIELD(gwy_container_get_object_by_name(data, "/0/data"));
-    xreal = gwy_data_field_get_xreal(dfield);
-    yreal = gwy_data_field_get_yreal(dfield);
+    datafield = GWY_DATA_FIELD(gwy_container_get_object_by_name(data, "/0/data"));
+    xreal = gwy_data_field_get_xreal(datafield);
+    yreal = gwy_data_field_get_yreal(datafield);
 
     dialog = gtk_dialog_new_with_buttons(_("Extract profile"),
                                          NULL,
@@ -159,14 +176,63 @@ profile_dialog_create(GwyDataView *data_view)
     gtk_container_set_border_width(GTK_CONTAINER(table), 4);
     gtk_container_add(GTK_CONTAINER(GTK_DIALOG(dialog)->vbox), table);
 
+    vbox = gtk_vbox_new(0,0);
+    
     label = gtk_label_new(NULL);
-    gtk_label_set_markup(GTK_LABEL(label), _("<b>Profile position</b>"));
+    gtk_label_set_markup(GTK_LABEL(label), _("<b>Profile positions</b>"));
     gtk_misc_set_alignment(GTK_MISC(label), 0.0, 0.5);
-    gtk_table_attach(GTK_TABLE(table), label, 0, 1, 0, 1, GTK_FILL, 0, 2, 2);
+    gtk_box_pack_start(vbox, label, 0, 0, 0);
+   
     
-    graph = gwy_graph_new();
-    gtk_table_attach(GTK_TABLE(table), graph, 1, 2, 0, 1, GTK_FILL, 0, 2, 2);
+    controls.positions = g_ptr_array_new();
+    g_ptr_array_add(controls.positions, (gpointer)gtk_label_new("x1 = 0, y1 = 0"));
+    g_ptr_array_add(controls.positions, (gpointer)gtk_label_new("x2 = 0, y2 = 0"));
+    g_ptr_array_add(controls.positions, (gpointer)gtk_label_new("x1 = 0, y1 = 0"));
+    g_ptr_array_add(controls.positions, (gpointer)gtk_label_new("x2 = 0, y2 = 0"));
+    g_ptr_array_add(controls.positions, (gpointer)gtk_label_new("x1 = 0, y1 = 0"));
+    g_ptr_array_add(controls.positions, (gpointer)gtk_label_new("x2 = 0, y2 = 0"));
+
+    gtk_misc_set_alignment(GTK_MISC(controls.positions->pdata[0]), 0.0, 0.5);
+    gtk_misc_set_alignment(GTK_MISC(controls.positions->pdata[1]), 0.0, 0.5);
+    gtk_misc_set_alignment(GTK_MISC(controls.positions->pdata[2]), 0.0, 0.5);
+    gtk_misc_set_alignment(GTK_MISC(controls.positions->pdata[3]), 0.0, 0.5);
+    gtk_misc_set_alignment(GTK_MISC(controls.positions->pdata[4]), 0.0, 0.5);
+    gtk_misc_set_alignment(GTK_MISC(controls.positions->pdata[5]), 0.0, 0.5);
     
+    label = gtk_label_new(NULL);
+    gtk_label_set_markup(GTK_LABEL(label), _("Profile 1:"));
+    gtk_misc_set_alignment(GTK_MISC(label), 0.0, 0.5);
+    gtk_box_pack_start(vbox, label, 0, 0, 5);
+    gtk_box_pack_start(vbox, GTK_WIDGET(controls.positions->pdata[0]), 0, 0, 0);
+    gtk_box_pack_start(vbox, GTK_WIDGET(controls.positions->pdata[1]), 0, 0, 0);
+    
+    label = gtk_label_new(NULL);
+    gtk_label_set_markup(GTK_LABEL(label), _("Profile 2:"));
+    gtk_misc_set_alignment(GTK_MISC(label), 0.0, 0.5);
+    gtk_box_pack_start(vbox, label, 0, 0, 5);     
+    gtk_box_pack_start(vbox, GTK_WIDGET(controls.positions->pdata[2]), 0, 0, 0);
+    gtk_box_pack_start(vbox, GTK_WIDGET(controls.positions->pdata[3]), 0, 0, 0);
+    
+    label = gtk_label_new(NULL);
+    gtk_label_set_markup(GTK_LABEL(label), _("Profile 3:"));
+    gtk_misc_set_alignment(GTK_MISC(label), 0.0, 0.5);
+    gtk_box_pack_start(vbox, label, 0, 0, 5);     
+    gtk_box_pack_start(vbox, GTK_WIDGET(controls.positions->pdata[4]), 0, 0, 0);
+    gtk_box_pack_start(vbox, GTK_WIDGET(controls.positions->pdata[5]), 0, 0, 0);
+    
+    label = gtk_label_new(NULL);
+    gtk_label_set_markup(GTK_LABEL(label), _("<b>Module parameters</b>"));
+    gtk_misc_set_alignment(GTK_MISC(label), 0.0, 0.5);
+    gtk_box_pack_start(vbox, label, 0, 0, 10);
+
+    controls.separation = gtk_check_button_new_with_label("separate profiles");
+    controls.interpolation;
+    
+  
+    gtk_table_attach(GTK_TABLE(table), vbox, 0, 1, 0, 1, GTK_FILL, 0, 2, 2);
+    
+    controls.graph = gwy_graph_new();
+    gtk_table_attach(GTK_TABLE(table), controls.graph, 1, 2, 0, 1, GTK_FILL, 0, 2, 2);
     
     
     /*
@@ -223,6 +289,11 @@ profile_selection_updated_cb(void)
 {
     gdouble lines[12];
     gboolean is_visible, is_selected;
+    gint ilines[12];
+    GString *lab1, *lab2, *lab3;
+    gint i, xres, j;
+    GwyGraphAutoProperties prop;
+    GwyGraph *gr;
 
     gwy_debug("%s", __FUNCTION__);
     /*XXX: seems broken
@@ -233,22 +304,45 @@ profile_selection_updated_cb(void)
                                                  
     if (!is_visible && !is_selected)
         return;
-    /*
+
+/*    for (i=0; i<300; i++) dtl->data[i]=3.0*sin(i/100);*/
+    gwy_graph_get_autoproperties(controls.graph, &prop);
+    prop.is_point = 0;
+    prop.is_line = 1;
+    gwy_graph_set_autoproperties(controls.graph, &prop);
+   
     if (is_selected) {
-        update_label(controls.x, MIN(x0, x1));
+
+        gwy_graph_clear(controls.graph);
+
+        j = 0;
+        for (i=0; i<is_selected; i++)
+        {
+            gwy_data_field_get_data_line(datafield, dtl->pdata[i], 
+                                     (gint)(gwy_data_field_rtoj(datafield, lines[j++])),
+                                     (gint)(gwy_data_field_rtoj(datafield, lines[j++])),
+                                     (gint)(gwy_data_field_rtoj(datafield, lines[j++])),
+                                     (gint)(gwy_data_field_rtoj(datafield, lines[j++])),
+                                     100,
+                                     GWY_INTERPOLATION_BILINEAR
+                                      );
+            gwy_graph_add_dataline(controls.graph, dtl->pdata[i],
+                               0, str->pdata[i], NULL);
+        }
+
+
+        
+        gtk_widget_queue_draw(GTK_WIDGET(controls.graph));
+        
+/*        update_label(controls.x, MIN(x0, x1));
         update_label(controls.y, MIN(y0, y1));
         update_label(controls.w, fabs(x1 - x0));
-        update_label(controls.h, fabs(y1 - y0));
+        update_label(controls.h, fabs(y1 - y0));*/
     }
-    else {
-        gtk_label_set_text(GTK_LABEL(controls.x), "");
-        gtk_label_set_text(GTK_LABEL(controls.y), "");
-        gtk_label_set_text(GTK_LABEL(controls.w), "");
-        gtk_label_set_text(GTK_LABEL(controls.h), "");
-    }*/
+    
     if (!is_visible)
         profile_dialog_set_visible(TRUE);
-        
+    
 }
 
 static void
