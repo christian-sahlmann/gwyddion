3740f6d61e3b4d8bc7a23617724693c6f980464a
yeti <yeti@25697fe4-d644-49b8-9d30-be5465f2adf3>
2003-12-01 13:41:05 +0000

    - added gwy_none stock item
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@507 25697fe4-d644-49b8-9d30-be5465f2adf3

--- libgwydgets/gwystock.h
+++ libgwydgets/gwystock.h
@@ -25,24 +25,25 @@
 extern "C" {
 #endif /* __cplusplus */
 
-#define GWY_STOCK_ZOOM_IN         "gwy_zoom_in"
-#define GWY_STOCK_ZOOM_OUT        "gwy_zoom_out"
-#define GWY_STOCK_ZOOM_1_1        "gwy_zoom_1_1"
-#define GWY_STOCK_ZOOM_FIT        "gwy_zoom_fit"
+#define GWY_STOCK_BOLD            "gwy_bold"
+#define GWY_STOCK_CROP            "gwy_crop"
 #define GWY_STOCK_FIT_PLANE       "gwy_fit_plane"
 #define GWY_STOCK_FIT_TRIANGLE    "gwy_fit_triangle"
 #define GWY_STOCK_GRAPH           "gwy_graph"
 #define GWY_STOCK_GWYDDION        "gwy_gwyddion"
-#define GWY_STOCK_CROP            "gwy_crop"
-#define GWY_STOCK_SCALE           "gwy_scale"
-#define GWY_STOCK_ROTATE          "gwy_rotate"
-#define GWY_STOCK_SHADER          "gwy_shader"
-#define GWY_STOCK_BOLD            "gwy_bold"
 #define GWY_STOCK_ITALIC          "gwy_italic"
-#define GWY_STOCK_SUBSCRIPT       "gwy_subscript"
-#define GWY_STOCK_SUPERSCRIPT     "gwy_superscript"
+#define GWY_STOCK_NONE            "gwy_none"
 #define GWY_STOCK_POINTER         "gwy_pointer"
 #define GWY_STOCK_POINTER_MEASURE "gwy_pointer_measure"
+#define GWY_STOCK_ROTATE          "gwy_rotate"
+#define GWY_STOCK_SCALE           "gwy_scale"
+#define GWY_STOCK_SHADER          "gwy_shader"
+#define GWY_STOCK_SUBSCRIPT       "gwy_subscript"
+#define GWY_STOCK_SUPERSCRIPT     "gwy_superscript"
+#define GWY_STOCK_ZOOM_1_1        "gwy_zoom_1_1"
+#define GWY_STOCK_ZOOM_FIT        "gwy_zoom_fit"
+#define GWY_STOCK_ZOOM_IN         "gwy_zoom_in"
+#define GWY_STOCK_ZOOM_OUT        "gwy_zoom_out"
 
 void gwy_stock_register_stock_items(void);
 
