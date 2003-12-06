48ead6515d37441bbcd145960f4cfb9e9aa92434
yeti <yeti@25697fe4-d644-49b8-9d30-be5465f2adf3>
2003-12-06 21:47:16 +0000

    - made the dialog singleton
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@565 25697fe4-d644-49b8-9d30-be5465f2adf3

--- app/about.c
+++ app/about.c
@@ -25,26 +25,34 @@
 #include "about.h"
 #include "app.h"
 
+static void about_close (void);
+
+static GtkWidget *about = NULL;
+
 void
 gwy_app_about(void)
 {
-    GtkWidget *dialog, *vbox, *hbox, *widget;
+    GtkWidget *vbox, *hbox, *widget;
     gchar *s;
 
+    if (about) {
+        gtk_window_present(about);
+        return;
+    }
     s = g_strdup_printf(_("About %s"), g_get_application_name());
-    dialog = gtk_dialog_new_with_buttons(s, GTK_WINDOW(gwy_app_main_window),
+    about = gtk_dialog_new_with_buttons(s, GTK_WINDOW(gwy_app_main_window),
                                          GTK_DIALOG_NO_SEPARATOR
                                          | GTK_DIALOG_DESTROY_WITH_PARENT,
                                          GTK_STOCK_CLOSE, GTK_RESPONSE_CLOSE,
                                          NULL);
-    gtk_container_set_border_width(GTK_CONTAINER(dialog), 6);
-    gtk_window_set_resizable(GTK_WINDOW(dialog), FALSE);
-    gtk_window_set_transient_for(GTK_WINDOW(dialog),
+    gtk_container_set_border_width(GTK_CONTAINER(about), 6);
+    gtk_window_set_resizable(GTK_WINDOW(about), FALSE);
+    gtk_window_set_transient_for(GTK_WINDOW(about),
                                  GTK_WINDOW(gwy_app_main_window));
-    gtk_window_set_position(GTK_WINDOW(dialog), GTK_WIN_POS_CENTER_ON_PARENT);
+    gtk_window_set_position(GTK_WINDOW(about), GTK_WIN_POS_CENTER_ON_PARENT);
     g_free(s);
 
-    vbox = GTK_DIALOG(dialog)->vbox;
+    vbox = GTK_DIALOG(about)->vbox;
     gtk_box_set_spacing(GTK_BOX(vbox), 12);
 
     hbox = gtk_hbox_new(FALSE, 6);
@@ -84,11 +92,18 @@ gwy_app_about(void)
     gtk_label_set_line_wrap(GTK_LABEL(widget), TRUE);
     gtk_label_set_selectable(GTK_LABEL(widget), TRUE);
 
-    g_signal_connect(dialog, "delete_event",
-                     G_CALLBACK(gtk_widget_destroy), NULL);
-    g_signal_connect(dialog, "response",
-                     G_CALLBACK(gtk_widget_destroy), NULL);
-    gtk_widget_show_all(dialog);
+    g_signal_connect(about, "delete_event",
+                     G_CALLBACK(about_close), NULL);
+    g_signal_connect(about, "response",
+                     G_CALLBACK(about_close), NULL);
+    gtk_widget_show_all(about);
+}
+
+static void
+about_close(void)
+{
+    gtk_widget_destroy(about);
+    about = NULL;
 }
 
 /* vim: set cin et ts=4 sw=4 cino=>1s,e0,n0,f0,{0,}0,^0,\:1s,=0,g1s,h0,t0,+1s,c3,(0,u0 : */
