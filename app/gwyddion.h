d67385df773a8c86986702f2281ca60a675aea23
yeti <yeti@25697fe4-d644-49b8-9d30-be5465f2adf3>
2003-12-18 16:16:59 +0000

    - added forgotten gwyddion.h
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@664 25697fe4-d644-49b8-9d30-be5465f2adf3

---
+++ app/gwyddion.h
@@ -0,0 +1,43 @@
+/*
+ *  @(#) $Id$
+ *  Copyright (C) 2003 David Necas (Yeti), Petr Klapetek.
+ *  E-mail: yeti@physics.muni.cz, klapetek@physics.muni.cz.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111 USA
+ */
+
+#ifndef __GWYDDION_GWYDDION_H__
+#define __GWYDDION_GWYDDION_H__
+
+#include <gtk/gtkwidget.h>
+#include <libgwydgets/gwydatawindow.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+GtkWidget* gwy_app_toolbox_create   (void);
+void       gwy_app_data_arith       (void);
+void       gwy_app_about            (void);
+void       gwy_app_metadata_browser (GwyDataWindow *data_window);
+void       gwy_app_init             (void);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* __GWYDDION_GWYDDION_H__ */
+
+/* vim: set cin et ts=4 sw=4 cino=>1s,e0,n0,f0,{0,}0,^0,\:1s,=0,g1s,h0,t0,+1s,c3,(0,u0 : */
