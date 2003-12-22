3a19d8e8a6bb3da822e97c838266767546bd1e00
yeti <yeti@25697fe4-d644-49b8-9d30-be5465f2adf3>
2003-12-22 23:36:59 +0000

    - somewhat refactored and unified with enca's gendoc.pl (but not fully)
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@683 25697fe4-d644-49b8-9d30-be5465f2adf3

--- gendoc.pl
+++ gendoc.pl
@@ -5,10 +5,13 @@ use strict;
 use HTML::Entities;
 use POSIX qw(getcwd);
 
+my $man2html = '/usr/libexec/yelp-man2html';
+my $tidy = 'tidy -asxhtml -q';
 my $unsafe_chars = "<>\"&";
 my $base = $ENV{'HOME'} . "/PHP/software/gwyddion";
 my $NEWS = "$base/NEWS.xhtml";
 my $APIDOCS = "$base";
+my $pkgname = "Gwyddion";
 
 undef $/;
 
@@ -28,12 +31,37 @@ my $footer =
 </small></p>
 ";
 
-#open FH, "<NEWS" or die; $_ = <FH>; close FH;
-#print "NEWS\n";
-#s/.*?\n\n//s;
-#$_ = encode_entities($_, $unsafe_chars);
-#open FH, ">$NEWS" or die;
-#print FH "<h1>Enca News</h1>\n\n<pre class=\"main\">$_</pre>"; close FH;
+sub gen_plaintext {
+  my $filename = $_[0];
+  my $output = $_[1];
+  my $name = $_[2] ? $_[2] : $_[0];
+  open FH, "<$filename" or die; $_ = <FH>; close FH;
+  print "$filename\n";
+  s/.*?\n\n//s;
+  $_ = encode_entities($_, $unsafe_chars);
+  open FH, ">$output" or die;
+  print FH "<h1>$pkgname $name</h1>\n\n<pre class=\"main\">$_</pre>"; close FH;
+}
+
+sub gen_manpage {
+  my $filename = $_[0];
+  my $output = $_[1];
+  $_ = qx(cat $filename | $man2html | $tidy 2>/dev/null);
+  print "$filename\n";
+  s#(<[^>]*>)#\L$1\E#sg;
+  s#<head>.*</head>##s;
+  s#\007##sg;
+  s#</?html[^>]*>##sg;
+  s#</?body[^>]*>##sg;
+  s#<!doctype[^>]*>##sg;
+  s#\nSection:#\n<p>Section:#s;
+  s#(Index</a>)#$1</p>#s;
+  s#\sname="lb.."##sg;
+  s#\sname="index"##sg;
+  s#\scompact=".*?"##sg;
+  s#^\s+##s;
+  open FH, ">$output" or die; print FH $_; close FH;
+}
 
 chdir "devel-docs";
 foreach my $dir (glob "*") {
@@ -44,7 +72,7 @@ foreach my $dir (glob "*") {
     foreach my $f (glob "html/*.html") {
         print "$dir/$f\n";
         $ok = 1;
-        $_ = qx(cat $f | sed -e 's:</*gtkdoc[^>]*>::gi' | tidy -asxhtml -q 2>/dev/null);
+        $_ = qx(cat $f | sed -e 's:</*gtkdoc[^>]*>::gi' | $tidy 2>/dev/null);
         s#((?:class|rel)=".*?")#\L$1\E#sg;
         s#<body[^>]*>#<body>#s;
         s#(<.*?)(>[^<]*)<a( id=".*?").*?>(.*?)</a>#$1$3$2$4#sg;
