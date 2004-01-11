f015beac5c2636cbe682f8c8c90f35af7675b023
yeti <yeti@25697fe4-d644-49b8-9d30-be5465f2adf3>
2004-01-11 14:10:42 +0000

    - process twice to converge more
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@861 25697fe4-d644-49b8-9d30-be5465f2adf3

--- modules/process/facet-level.c
+++ modules/process/facet-level.c
@@ -69,14 +69,19 @@ facet_level(GwyContainer *data, GwyRunType run)
 {
     GwyDataField *dfield;
     gdouble c, bx, by;
+    gint i;
 
     g_return_val_if_fail(run & LEVEL_RUN_MODES, FALSE);
     dfield = GWY_DATA_FIELD(gwy_container_get_object_by_name(data, "/0/data"));
     gwy_app_undo_checkpoint(data, "/0/data");
-    facet_level_coeffs(dfield, &bx, &by);
-    c = -0.5*(bx*gwy_data_field_get_xreal(dfield)
-              + by*gwy_data_field_get_yreal(dfield));
-    gwy_data_field_plane_level(dfield, c, bx, by);
+
+    /* to converge, do it twice */
+    for (i = 0; i < 2; i++) {
+        facet_level_coeffs(dfield, &bx, &by);
+        c = -0.5*(bx*gwy_data_field_get_xreal(dfield)
+                  + by*gwy_data_field_get_yreal(dfield));
+        gwy_data_field_plane_level(dfield, c, bx, by);
+    }
 
     return TRUE;
 }
