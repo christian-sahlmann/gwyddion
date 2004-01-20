2e2bdbf552c74037ed98de2fc95d7d1cdea6c288
yeti <yeti@25697fe4-d644-49b8-9d30-be5465f2adf3>
2004-01-20 10:27:44 +0000

    - added slightly modified msvc-recommended-pragmas.h from GLib
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@952 25697fe4-d644-49b8-9d30-be5465f2adf3

--- libgwyddion/gwymacros.h
+++ libgwyddion/gwymacros.h
@@ -98,6 +98,31 @@ void gwy_debug_gnu(const gchar *domain,
 }
 #endif /* __cplusplus */
 
+#ifdef _MSC_VER
+/* Make MSVC more pedantic, this is a recommended pragma list
+ * from _Win32_Programming_ by Rector and Newcomer.
+ */
+#pragma warning(error:4002) /* too many actual parameters for macro */
+#pragma warning(error:4003) /* not enough actual parameters for macro */
+#pragma warning(1:4010)     /* single-line comment contains line-continuation character */
+#pragma warning(error:4013) /* 'function' undefined; assuming extern returning int */
+#pragma warning(error:4016) /* no function return type; using int as default */
+#pragma warning(error:4020) /* too many actual parameters */
+#pragma warning(error:4021) /* too few actual parameters */
+#pragma warning(error:4027) /* function declared without formal parameter list */
+#pragma warning(error:4029) /* declared formal parameter list different from definition */
+#pragma warning(error:4033) /* 'function' must return a value */
+#pragma warning(error:4035) /* 'function' : no return value */
+#pragma warning(error:4045) /* array bounds overflow */
+#pragma warning(error:4047) /* different levels of indirection */
+#pragma warning(error:4049) /* terminating line number emission */
+#pragma warning(error:4053) /* An expression of type void was used as an operand */
+#pragma warning(error:4071) /* no function prototype given */
+
+#pragma warning(disable:4244) /* No possible loss of data warnings */
+#pragma warning(disable:4305)   /* No truncation from int to char warnings */
+#endif /* _MSC_VER */
+
 #endif /* __GWY_GWYMACROS_H__ */
 
 /* vim: set cin et ts=4 sw=4 cino=>1s,e0,n0,f0,{0,}0,^0,\:1s,=0,g1s,h0,t0,+1s,c3,(0,u0 : */
