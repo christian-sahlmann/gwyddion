df98fd0ae378707b8d7704cae85a2f132f3870c2
yeti <yeti@25697fe4-d644-49b8-9d30-be5465f2adf3>
2004-02-15 21:05:47 +0000

    - added ROUND() docs
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@1130 25697fe4-d644-49b8-9d30-be5465f2adf3

--- libgwyddion/gwymath.c
+++ libgwyddion/gwymath.c
@@ -299,4 +299,13 @@ gwy_math_lin_solve_rewrite(gint n, gdouble *matrix,
     return result;
 }
 
+/************************** Documentation ****************************/
+
+/**
+ * ROUND:
+ * @x: A double value.
+ *
+ * Rounds a number to nearest integer.
+ **/
+
 /* vim: set cin et ts=4 sw=4 cino=>1s,e0,n0,f0,{0,}0,^0,\:1s,=0,g1s,h0,t0,+1s,c3,(0,u0 : */
