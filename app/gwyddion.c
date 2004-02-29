19680d5342025e6b1909d016213d32b6b9b548f5
yeti <yeti@25697fe4-d644-49b8-9d30-be5465f2adf3>
2004-02-29 08:28:09 +0000

    - flushing log file after each message
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@1284 25697fe4-d644-49b8-9d30-be5465f2adf3

--- app/gwyddion.c
+++ app/gwyddion.c
@@ -126,6 +126,7 @@ logger(const gchar *log_domain,
     if (!logfile)
         return;
     fprintf(logfile, "%s: %s\n", log_domain, message);
+    fflush(logfile);
 }
 
 /* vim: set cin et ts=4 sw=4 cino=>1s,e0,n0,f0,{0,}0,^0,\:1s,=0,g1s,h0,t0,+1s,c3,(0,u0 : */
