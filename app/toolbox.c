f93b324250537b9732ca720f8e3579e433e375e8
klapetek <klapetek@25697fe4-d644-49b8-9d30-be5465f2adf3>
2004-03-28 07:57:22 +0000

    Mask by correlation facility GUI roughly introduced.
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@1415 25697fe4-d644-49b8-9d30-be5465f2adf3

--- app/toolbox.c
+++ app/toolbox.c
@@ -409,7 +409,8 @@ gwy_app_menu_create_edit_menu(GtkAccelGroup *accel_group)
         { "/Edit/Change Mask _Color", NULL, gwy_app_change_mask_color_cb, 0, NULL, NULL },
         { "/Edit/Change Default Mask _Color", NULL, gwy_app_change_mask_color_cb, 1, NULL, NULL },
         { "/Edit/---", NULL, NULL, 0, "<Separator>", NULL },
-        { "/Edit/Data _Cross-correlation", NULL, gwy_app_data_croscor, 0, NULL, NULL },
+        { "/Edit/Mask by c_orrelation", NULL, gwy_app_data_maskcor, 0, NULL, NULL },
+        { "/Edit/Data Cro_ss-correlation", NULL, gwy_app_data_croscor, 0, NULL, NULL },
     };
     static const gchar *items_need_data[] = {
         "/Edit/Duplicate", "/Edit/Data Arithmetic", NULL
