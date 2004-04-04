c41627b040b66e06ef8934a4fdc46baa1455cdb7
yeti <yeti@25697fe4-d644-49b8-9d30-be5465f2adf3>
2004-04-04 22:12:09 +0000

    - clean-up
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@1457 25697fe4-d644-49b8-9d30-be5465f2adf3

--- utils/vim-syn-gen.py
+++ utils/vim-syn-gen.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python
-import re, glob, time
+# vim-syn-gen.py -- Generate vim syntax highligting from gtk-doc documentation
+# Written by Yeti <yeti@physics.muni.cz>
+# This file is in the public domain.
+import re, glob, time, sys
 
+# Highlight deprecated symbols too?
+export_deprecated = True
+# Vim syntax name (used for group prefix)
 syntax_name = 'gwyddion'
+# Files to scan (all will be merged to one output file)
+file_glob = 'devel-docs/*/*-decl.txt'
+# Regular expression enum constants should match
+enum_re = 'GWY_[A-Z0-9_]+'
+# Syntax file metadata
+description = 'C Gwyddion'
+maintainer = 'David Ne\\v{c}as (Yeti) <yeti@physics.muni.cz>'
+url_base = 'http://trific.ath.cx/Ftp/vim/syntax'
 
+# Default highlighting
 types = {
     'CONSTANT': 'Constant',
     'MACRO': 'Macro',
@@ -13,6 +28,7 @@ types = {
     'USER_FUNCTION': 'Type'
 }
 
+#### Normally you don't want to change anything below
 normalize = lambda x: x.title().replace('_', '')
 
 hi_link_line = '  HiLink %s%s %s'
@@ -21,11 +37,12 @@ hi_links = '\n'.join([hi_link_line % (syntax_name, normalize(k), v)
 
 header = """\
 " Vim syntax file
-" Language: C Gwyddion
-" Maintainer: David Ne\\v{c}as (Yeti) <yeti@physics.muni.cz>
+" Language: %s
+" Maintainer: %s
 " Last Change: %s
-" URL: http://trific.ath.cx/Ftp/vim/syntax/%s.vim
-""" % (time.strftime('%Y-%m-%d'), syntax_name)
+" URL: %s/%s.vim
+""" % (description, maintainer, time.strftime('%Y-%m-%d'),
+       url_base, syntax_name)
 
 footer = """
 " Default highlighting
@@ -46,7 +63,7 @@ re_decl = re.compile(r'<(?P<typ>' + r'|'.join(types.keys()) + r')>\n'
                      + r'(?P<body>.*?)'
                      + r'</(?P=typ)>\n',
                      re.S)
-re_enum = re.compile(r'^\s+(?P<ident>GWY_[A-Z0-9_]+)\s*=', re.M)
+re_enum = re.compile(r'^\s+(?P<ident>' + enum_re + ')\s*=', re.M)
 
 def restruct(match):
     """Given a match object, create an object whose attributes are the named
@@ -62,13 +79,15 @@ def restruct(match):
         return None
 
 decls = dict([(x, {}) for x in types])
-for filename in glob.glob('devel-docs/*/*-decl.txt'):
+for filename in glob.glob(file_glob):
     fh = file(filename, 'r')
     text = fh.read()
     fh.close()
     for d in re_decl.finditer(text):
         d = restruct(d)
         if d.ident.startswith('_'):
+            continue
+        if not export_deprecated and d.body.find('<DEPRECATED/>'):
             continue
         decls[d.typ][d.ident] = 1
         if d.typ == 'ENUM':
