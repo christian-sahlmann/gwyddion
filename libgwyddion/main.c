34be6a2a699ef8eeb9a66af2989ff66d735a2d85
yeti <yeti@25697fe4-d644-49b8-9d30-be5465f2adf3>
2004-04-11 09:44:22 +0000

    - modularized tests
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@1512 25697fe4-d644-49b8-9d30-be5465f2adf3

--- libgwyddion/main.c
+++ libgwyddion/main.c
@@ -21,9 +21,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define _GWY_TEST_TO_TEXT
 #include "gwyddion.h"
 #include "gwytestser.h"
+
+guchar*
+gwy_serializable_serialize_to_text(GObject *serializable);
 
 #define FILENAME "testser.object"
 
@@ -34,135 +36,14 @@
               gwy_test_ser_get_radius(GWY_TEST_SER(obj))); \
     gwy_test_ser_print_history(GWY_TEST_SER(obj))
 
-#define pent(txt) \
-    { \
-        gchar *_t = gwy_entities_text_to_utf8(txt); \
-        printf("%s-Message: `%s' -> `%s'\n", G_LOG_DOMAIN, txt, _t); \
-        g_free(_t); \
-    }
-
-#define linprint(x,e,s) \
-    { \
-        gsize _i; \
-        printf("%s-Message: %s [", G_LOG_DOMAIN, s); \
-        for (_i = 0; _i < G_N_ELEMENTS(e); _i++) \
-            printf("%s%g", _i == 0 ? "" : " ", x[_i]); \
-        printf("] == ["); \
-        for (_i = 0; _i < G_N_ELEMENTS(e); _i++) \
-            printf("%s%g", _i == 0 ? "" : " ", e[_i]); \
-        printf("]\n"); \
-    }
-
-#define linsolv(m,b,x,e) \
-    x = gwy_math_lin_solve(G_N_ELEMENTS(b), m, b, NULL); \
-    linprint(x,e,"ro") \
-    gwy_math_lin_solve_rewrite(G_N_ELEMENTS(b), m, b, x); \
-    linprint(x,e,"rw") \
-    g_free(x)
-
-void
-foo_callback(gpointer obj, gpointer data __attribute__((unused)))
+static void
+test_serializable_iface(void)
 {
-    g_message("FOO! %s", g_type_name(G_TYPE_FROM_INSTANCE(obj)));
-}
-
-void
-bar_callback(gpointer hkey, GValue *value, gchar *user_data)
-{
-    g_message("`%s' -> %s (%s)",
-              g_quark_to_string(GPOINTER_TO_UINT(hkey)),
-              G_VALUE_TYPE_NAME(value),
-              user_data);
-}
-
-gdouble
-gauss(gdouble x, G_GNUC_UNUSED gint n_par, gdouble *b,
-      G_GNUC_UNUSED gpointer user_data, gboolean *fres)
-{
-    gdouble c;
-
-    if (b[2] == 0) {
-        *fres = FALSE;
-        return 0;
-    }
-    *fres = TRUE;
-    c = (x - b[1])/b[2];
-
-    return b[0] * exp(-c*c/2);
-}
-
-gdouble
-grand(void)
-{
-    gdouble c = 0;
-    gint i;
-
-    for (i = 0; i < 20; i++)
-        c += random()/(gdouble)RAND_MAX;
-    c /= 10.0;
-    c -= 1.0;
-    return c;
-}
-
-typedef enum {
-    GWY_FILE_NONE   = 0,
-    GWY_FILE_LOAD   = 1 << 0,
-    GWY_FILE_SAVE   = 1 << 1,
-    GWY_FILE_DETECT = 1 << 2,
-    GWY_FILE_MASK   = 0x07
-} GwyFileOperation;
-
-typedef enum {
-    GWY_RUN_NONE           = 0,
-    GWY_RUN_WITH_DEFAULTS  = 1 << 0,
-    GWY_RUN_NONINTERACTIVE = 1 << 1,
-    GWY_RUN_MODAL          = 1 << 2,
-    GWY_RUN_INTERACTIVE    = 1 << 3,
-    GWY_RUN_MASK           = 0x0f
-} GwyRunType;
-
-static const GwyEnum run_mode_names[] = {
-    { "interactive",    GWY_RUN_INTERACTIVE },
-    { "noninteractive", GWY_RUN_NONINTERACTIVE },
-    { "modal",          GWY_RUN_MODAL },
-    { "with_defaults",  GWY_RUN_WITH_DEFAULTS },
-    { NULL,             -1 }
-};
-
-static const GwyEnum file_op_names[] = {
-    { "load", GWY_FILE_LOAD },
-    { "save", GWY_FILE_SAVE },
-    { NULL,   -1 }
-};
-
-int
-main(void)
-{
-    enum { count = 24 };
     GObject *ser;
     gsize size, pos;
     guchar *buffer;
     FILE *fh;
     GError *err = NULL;
-    GValue val, *p;
-    GwyContainer *container;
-    GObject *obj;
-    GQuark q;
-    gulong id;
-    gdouble *x;
-    gdouble y;
-    gboolean ok;
-    GwyNLFitter *ms;
-    gdouble xmq[count + 1];
-    gdouble ymq[count + 1];
-    gdouble vmq[count + 1];
-    gdouble param[3];
-    gboolean bb;
-    gdouble xx, summ;
-    gint i, j;
-    gchar *s, *s2;
-
-    g_type_init();
 
     /* create, write and free an object */
     ser = gwy_test_ser_new(0.42, 1.001);
@@ -213,7 +94,24 @@ main(void)
 
     gwy_serializable_serialize_to_text(ser);
 
+    g_object_unref(ser);
+}
+
+void
+foo_callback(gpointer obj, gpointer data __attribute__((unused)))
+{
+    g_message("FOO! %s", g_type_name(G_TYPE_FROM_INSTANCE(obj)));
+}
+
+static void
+test_watchable_iface(void)
+{
+    GObject *ser;
+    gulong id;
+
     g_message("===== WATCHABLE INTERFACE ===========================");
+    ser = gwy_test_ser_new(1.618, 0.33333333);
+
     id = g_signal_connect(ser, "value_changed", G_CALLBACK(foo_callback), NULL);
     gwy_watchable_value_changed(ser);
     gwy_test_ser_set_theta(GWY_TEST_SER(ser), 0.1111);
@@ -221,7 +119,29 @@ main(void)
     gwy_watchable_value_changed(ser);
     g_signal_handler_disconnect(ser, id);
 
+    g_object_unref(ser);
+}
+
+void
+bar_callback(gpointer hkey, GValue *value, gchar *user_data)
+{
+    g_message("`%s' -> %s (%s)",
+              g_quark_to_string(GPOINTER_TO_UINT(hkey)),
+              G_VALUE_TYPE_NAME(value),
+              user_data);
+}
+
+static void
+test_container(void)
+{
+    GValue val, *p;
+    gdouble y;
+    GwyContainer *container;
+    GQuark q;
+    gboolean ok;
+
     g_message("====== CONTAINER ====================================");
+
     container = GWY_CONTAINER(gwy_container_new());
     p = g_new0(GValue, 1);
     g_value_init(p, G_TYPE_INT);
@@ -260,7 +180,31 @@ main(void)
     g_message("'pdfoo': %s", gwy_container_contains_by_name(container, "pdfoo")
                              ? "PRESENT" : "REMOVED");
 
+}
+
+static void
+test_container_serialization(void)
+{
+    GValue *p;
+    GQuark q;
+    GObject *ser;
+    gsize size, pos;
+    guchar *buffer;
+    GwyContainer *container;
+    FILE *fh;
+    GError *err = NULL;
+
     g_message("====== CONTAINER SERIALIZATION ======================");
+    ser = gwy_test_ser_new(1.618, 0.33333333);
+    container = GWY_CONTAINER(gwy_container_new());
+    p = g_new0(GValue, 1);
+    g_value_init(p, G_TYPE_INT);
+    g_value_set_int(p, 1133);
+    q = g_quark_from_string("foobar");
+    gwy_container_set_value_by_name(container, "foobar", p, NULL);
+    g_value_set_int(p, -100);
+    gwy_container_set_value_by_name(container, "foobar", p, NULL);
+
     gwy_container_set_double_by_name(container, "pdf", 0.5227);
     gwy_container_set_double_by_name(container, "pdf/f", 1.4142);
     gwy_container_set_int64_by_name(container, "x64", 64LL);
@@ -308,9 +252,20 @@ main(void)
     g_object_unref(container);
     g_message("restoring the empty container");
     container = GWY_CONTAINER(gwy_serializable_deserialize(buffer, size, &pos));
+}
 
+static void
+test_duplication(void)
+{
+    GObject *obj, *ser;
+    GwyContainer *container;
 
     g_message("====== DUPLICATION ======================");
+    container = GWY_CONTAINER(gwy_container_new());
+
+    gwy_container_set_double_by_name(container, "pdf", 0.5227);
+    gwy_container_set_double_by_name(container, "pdf/f", 1.4142);
+    gwy_container_set_int64_by_name(container, "x64", 64LL);
 
     gwy_container_set_double_by_name(container, "dbl", 3.141592);
     ser = gwy_test_ser_new(0.88, 0.99);
@@ -325,7 +280,20 @@ main(void)
     ser = gwy_container_get_object_by_name(GWY_CONTAINER(obj), "ser");
     print(ser, "restored");
 
+}
+
+#define pent(txt) \
+    { \
+        gchar *_t = gwy_entities_text_to_utf8(txt); \
+        fprintf(stderr, "`%s' -> `%s'\n", txt, _t); \
+        g_free(_t); \
+    }
+
+static void
+test_entities(void)
+{
     g_message("====== ENTITIES ======================");
+
     pent("foo");
     pent("&alpha;");
     pent("&alpha;&beta;");
@@ -334,7 +302,44 @@ main(void)
     pent("&alphabeta;&alpha&beta;");
     pent("<a link=\"foo&lt;&quot;&gt;\">&amp;</a>");
 
+}
+
+typedef enum {
+    GWY_FILE_NONE   = 0,
+    GWY_FILE_LOAD   = 1 << 0,
+    GWY_FILE_SAVE   = 1 << 1,
+    GWY_FILE_DETECT = 1 << 2,
+    GWY_FILE_MASK   = 0x07
+} GwyFileOperation;
+
+typedef enum {
+    GWY_RUN_NONE           = 0,
+    GWY_RUN_WITH_DEFAULTS  = 1 << 0,
+    GWY_RUN_NONINTERACTIVE = 1 << 1,
+    GWY_RUN_MODAL          = 1 << 2,
+    GWY_RUN_INTERACTIVE    = 1 << 3,
+    GWY_RUN_MASK           = 0x0f
+} GwyRunType;
+
+static const GwyEnum run_mode_names[] = {
+    { "interactive",    GWY_RUN_INTERACTIVE },
+    { "noninteractive", GWY_RUN_NONINTERACTIVE },
+    { "modal",          GWY_RUN_MODAL },
+    { "with_defaults",  GWY_RUN_WITH_DEFAULTS },
+    { NULL,             -1 }
+};
+
+static const GwyEnum file_op_names[] = {
+    { "load", GWY_FILE_LOAD },
+    { "save", GWY_FILE_SAVE },
+    { NULL,   -1 }
+};
+
+static void
+test_enums(void)
+{
     g_message("====== ENUMS ======================");
+
     g_message(gwy_enum_to_string(GWY_FILE_LOAD, file_op_names, -1));
     g_message(gwy_enum_to_string(GWY_FILE_SAVE, file_op_names,
                                  G_N_ELEMENTS(file_op_names)));
@@ -357,8 +362,34 @@ main(void)
     g_message("<noninteractive-interactive> = %d",
               gwy_string_to_flags("noninteractive-interactive",
                                   run_mode_names, -1, "-"));
+}
+
+#define linprint(x,e,s) \
+    { \
+        gsize _i; \
+        fprintf(stderr, "%s [", s); \
+        for (_i = 0; _i < G_N_ELEMENTS(e); _i++) \
+            fprintf(stderr, "%s%g", _i == 0 ? "" : " ", x[_i]); \
+        fprintf(stderr, "] == ["); \
+        for (_i = 0; _i < G_N_ELEMENTS(e); _i++) \
+            fprintf(stderr, "%s%g", _i == 0 ? "" : " ", e[_i]); \
+        fprintf(stderr, "]\n"); \
+    }
+
+#define linsolv(m,b,x,e) \
+    x = gwy_math_lin_solve(G_N_ELEMENTS(b), m, b, NULL); \
+    linprint(x,e,"ro") \
+    gwy_math_lin_solve_rewrite(G_N_ELEMENTS(b), m, b, x); \
+    linprint(x,e,"rw") \
+    g_free(x)
+
+static void
+test_math(void)
+{
+    gdouble *x;
 
     g_message("====== MATH ======================");
+
     {
         gdouble m[] = { 1, 2, 3, 4 };
         gdouble b[] = { 5, 6 };
@@ -378,8 +409,15 @@ main(void)
         gdouble e[] = { -1e6, 1e6 };
         linsolv(m, b, x, e);
     }
+}
+
+static void
+test_string_utils(void)
+{
+    gchar *s, *s2;
 
     g_message("====== STRING UTILS ======================");
+
     s = g_strdup("gwy_math_nlfit_new()");
     s2 = gwy_strkill(g_strdup(s), "");
     g_message("kill <%s> in <%s> -> <%s>", "", s, s2);
@@ -401,7 +439,52 @@ main(void)
     g_message("replace 1 <%s> in <%s> with <%s> -> <%s>", "_", s, "", s2);
     g_free(s2);
 
+}
+
+gdouble
+gauss(gdouble x, G_GNUC_UNUSED gint n_par, gdouble *b,
+      G_GNUC_UNUSED gpointer user_data, gboolean *fres)
+{
+    gdouble c;
+
+    if (b[2] == 0) {
+        *fres = FALSE;
+        return 0;
+    }
+    *fres = TRUE;
+    c = (x - b[1])/b[2];
+
+    return b[0] * exp(-c*c/2);
+}
+
+gdouble
+grand(void)
+{
+    gdouble c = 0;
+    gint i;
+
+    for (i = 0; i < 20; i++)
+        c += random()/(gdouble)RAND_MAX;
+    c /= 10.0;
+    c -= 1.0;
+    return c;
+}
+
+static void
+test_nlfit(void)
+{
+    enum { count = 24 };
+    GwyNLFitter *ms;
+    gdouble xmq[count + 1];
+    gdouble ymq[count + 1];
+    gdouble vmq[count + 1];
+    gdouble param[3];
+    gboolean bb;
+    gdouble xx, summ;
+    gint i, j;
+
     g_message("====== NLFIT ======================");
+
     ms = gwy_math_nlfit_new(gauss, gwy_math_nlfit_derive);
     param[0] = 10;                  /*amplituda*/
     param[1] = 10;                  /*stred*/
@@ -419,22 +502,56 @@ main(void)
     param[2] = 0.5;
     summ = gwy_math_nlfit_fit(ms, count, xmq, ymq, vmq, 3, param, NULL);
 
-    printf("Evaluated: %d\n", ms->eval);
-    printf("Suma: %f\n", summ);
+    fprintf(stderr, "Evaluated: %d\n", ms->eval);
+    fprintf(stderr, "Suma: %f\n", summ);
     for (i = 0; i < 3; i++)
-        printf("Par[%d] = %f +- %f\n",
-               i, param[i], gwy_math_nlfit_get_sigma(ms, i));
-    puts("");
+        fprintf(stderr, "Par[%d] = %f +- %f\n",
+                i, param[i], gwy_math_nlfit_get_sigma(ms, i));
+    fputs("\n", stderr);
     for (i = 0; i < count; i++)
-        printf("%3d: %.4f\t%.4f\t%.4f\t%.4f\n",
-               i, xmq[i], ymq[i], vmq[i], gauss(xmq[i], 3, param, NULL, &bb));
-    puts("");
+        fprintf(stderr, "%3d: %.4f\t%.4f\t%.4f\t%.4f\n",
+                i, xmq[i], ymq[i], vmq[i], gauss(xmq[i], 3, param, NULL, &bb));
+    fputs("\n", stderr);
     for (i = 0; i < 3; i++) {
         for (j = 0; j <= i; j++)
-            printf("%.4f\t", gwy_math_nlfit_get_correlations(ms, i, j));
-        puts("");
+            fprintf(stderr, "%.4f\t",
+                    gwy_math_nlfit_get_correlations(ms, i, j));
+        fputs("\n", stderr);
     }
     gwy_math_nlfit_free(ms);
+}
+
+static void
+test_all(void)
+{
+    test_serializable_iface();
+    test_watchable_iface();
+    test_container();
+    test_container_serialization();
+    test_duplication();
+    test_entities();
+    test_enums();
+    test_math();
+    test_string_utils();
+    test_nlfit();
+}
+
+static void
+log_handler(G_GNUC_UNUSED const gchar *log_domain,
+            G_GNUC_UNUSED GLogLevelFlags log_level,
+            const gchar *message,
+            G_GNUC_UNUSED gpointer user_data)
+{
+    fputs(message, stderr);
+    fputc('\n', stderr);
+}
+
+int
+main(void)
+{
+    g_type_init();
+    g_log_set_handler(G_LOG_DOMAIN, G_LOG_LEVEL_MESSAGE, log_handler, NULL);
+    test_all();
 
     return 0;
 }
