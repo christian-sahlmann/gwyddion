b1e30217a8fb0f2e98935d088c61aa073b6c44b2
yeti <yeti@25697fe4-d644-49b8-9d30-be5465f2adf3>
2004-04-21 12:58:48 +0000

    - using horner schemas for weight computation (and fixed them first)
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@1588 25697fe4-d644-49b8-9d30-be5465f2adf3

--- libprocess/interpolation.c
+++ libprocess/interpolation.c
@@ -71,8 +71,8 @@ gwy_interpolation_get_dval(gdouble x,
  * @interpolation: interpolation type
  *
  * Function computes interpolateed value bettween 2 or 4 
- * equidistant values. For using GWY_INTERPOLATION_NONE,
- * GWY_INTERPOLATION_ROUND or GWY_INTERPOLATION_BILINEAR
+ * equidistant values. For using %GWY_INTERPOLATION_NONE,
+ * %GWY_INTERPOLATION_ROUND or %GWY_INTERPOLATION_BILINEAR
  * it is enough to use @data in format {0, data[i], data[i+1], 0}
  * and function computes value at data[i+x]. For four value
  * interpolations you have to prapare @data as 
@@ -116,51 +116,67 @@ gwy_interpolation_get_dval_of_equidists(gdouble x,
         break;
     }
 
-    w1 = rest + 1;
-    w2 = rest;
-    w3 = 1 - rest;
-    w4 = 2 - rest;
     switch (interpolation) {
         case GWY_INTERPOLATION_KEY:
-        /* horner schema (by maple) 35 -> 21
-        w1 = (-0.5 + (1.0 - rest/2.0)*rest)*rest;
-        w2 = 1.0 + rest*rest*(2.5 + 1.5*rest);
-        w3 = (0.5 + (2.0 - 1.5*rest)*rest)*rest;
-        w4 = 8.0 + (-12.0 + (5.5 - rest/2.0)*rest)*rest;
-        */
+        /*
+        w1 = rest + 1.0;
+        w2 = rest;
+        w3 = 1.0 - rest;
+        w4 = 2.0 - rest;
         w1 = -0.5*w1*w1*w1 + 2.5*w1*w1 - 4*w1 + 2;
         w2 = 1.5*w2*w2*w2 - 2.5*w2*w2 + 1;
         w3 = 1.5*w3*w3*w3 - 2.5*w3*w3 + 1;
         w4 = -0.5*w4*w4*w4 + 2.5*w4*w4 - 4*w4 + 2;
+        */
+        /* horner schema (by maple) */
+        w1 = (-0.5 + (1.0 - rest/2.0)*rest)*rest;
+        w2 = 1.0 + (-2.5 + 1.5*rest)*rest*rest;
+        w3 = (0.5 + (2.0 - 1.5*rest)*rest)*rest;
+        w4 = (0.5 + (1.0 + rest/2.0)*rest)*rest;
         break;
 
         case GWY_INTERPOLATION_BSPLINE:
-        /* horner schema (by maple) 27 -> 20
-        w1 = (1.0 - rest)*(1.0 - rest)*(1.0 - rest)/6.0;
-        w2 = 2.0/3.0 - 0.5*rest*rest*(2.0 - rest);
-        w3 = 2.0/3.0 - 0.5*(1.0 - rest)*(1.0 - rest*rest);
-        w4 = rest*rest*rest/6.0;
-        */
+        /*
+        w1 = rest + 1.0;
+        w2 = rest;
+        w3 = 1.0 - rest;
+        w4 = 2.0 - rest;
         w1 = (2-w1)*(2-w1)*(2-w1)/6;
         w2 = 0.6666667-0.5*w2*w2*(2-w2);
         w3 = 0.6666667-0.5*w3*w3*(2-w3);
         w4 = (2-w4)*(2-w4)*(2-w4)/6;
+        */
+        /* horner schema (by maple) */
+        w1 = 1.0-rest;
+        w1 = w1*w1*w1/6.0;
+        w2 = 2.0/3.0 + (-1.0 + rest/2.0)*rest*rest;
+        w3 = 1.0/6.0 + (0.5 + (0.5 - rest/2.0)*rest)*rest;
+        w4 = rest*rest*rest/6.0;
         break;
 
         case GWY_INTERPOLATION_OMOMS:
-        /* horner schema (by maple) 37 -> 22
-        w1 = 4.0/21.0 + (11.0/21.0 + (0.5 + rest/6.0)*rest)*rest;
+        /*
+        w1 = rest + 1.0;
+        w2 = rest;
+        w3 = 1.0 - rest;
+        w4 = 2.0 - rest;
+        w1 = -w1*w1*w1/6 + w1*w1-85*w1/42 + 1.3809523;
+        w2 = w2*w2*w2/2-w2*w2 + w2/14 + 0.6190476;
+        w3 = w3*w3*w3/2-w3*w3 + w3/14 + 0.6190476;
+        w4 = -w4*w4*w4/6 + w4*w4-85*w4/42 + 1.3809523;
+        */
+        /* horner schema (by maple) */
+        w1 = 4.0/21.0 + (-11.0/21.0 + (0.5 - rest/6.0)*rest)*rest;
         w2 = 13.0/21.0 + (1.0/14.0 + (-1.0 + rest/2.0)*rest)*rest;
         w3 = 4.0/21.0 + (3.0/7.0 + (0.5 - rest/2.0)*rest)*rest;
         w4 = (1.0/42.0 + rest*rest/6.0)*rest;
-        */
-        w1 = -w1*w1*w1/6+w1*w1-85*w1/42+1.3809523;
-        w2 = w2*w2*w2/2-w2*w2+w2/14+0.6190476;
-        w3 = w3*w3*w3/2-w3*w3+w3/14+0.6190476;
-        w4 = -w4*w4*w4/6+w4*w4-85*w4/42+1.3809523;
         break;
 
         case GWY_INTERPOLATION_NNA:
+        w1 = rest + 1.0;
+        w2 = rest;
+        w3 = 1.0 - rest;
+        w4 = 2.0 - rest;
         w1 = 1/(w1*w1*w1*w1);
         w2 = 1/(w2*w2*w2*w2);
         w3 = 1/(w3*w3*w3*w3);
@@ -175,5 +191,37 @@ gwy_interpolation_get_dval_of_equidists(gdouble x,
     return w1*data[l-1] + w2*data[l] + w3*data[l+1] + w4*data[l+2];
 }
 
+/*
+{VERSION 5 0 "IBM INTEL LINUX" "5.0" }
+{USTYLETAB {CSTYLE "Maple Input" -1 0 "Courier" 0 1 255 0 0 1 0 1 0 0 
+1 0 0 0 0 1 }{PSTYLE "Normal" -1 0 1 {CSTYLE "" -1 -1 "" 0 1 0 0 0 0 
+0 0 0 0 0 0 0 0 0 0 }0 0 0 -1 -1 -1 0 0 0 0 0 0 -1 0 }}
+{SECT 0 {EXCHG {PARA 0 "> " 0 "" {MPLTEXT 1 0 23 "restart; with(codege
+n):" }}}{EXCHG {PARA 0 "" 0 "" {TEXT -1 17 "Key interpolation" }}}
+{EXCHG {PARA 0 "> " 0 "" {MPLTEXT 1 0 146 "intp := [ -1/2*w1*w1*w1 + 5
+/2*w1*w1 - 4*w1 + 2, 3/2*w2*w2*w2 - 5/2*w2*w2 + 1, 3/2*w3*w3*w3 - 5/2*
+w3*w3 + 1, -1/2*w4*w4*w4 + 5/2*w4*w4 - 4*w4 + 2];" }}{PARA 0 "" 0 "" 
+{TEXT -1 0 "" }}}{EXCHG {PARA 0 "> " 0 "" {MPLTEXT 1 0 88 "hintp := co
+nvert(subs(w1 = rest+1, w2 = rest, w3 = 1-rest, w4 = 2-rest, intp), ho
+rner); " }}}{EXCHG {PARA 0 "> " 0 "" {MPLTEXT 1 0 67 "C([ w1 = hintp[1
+], w2 = hintp[2], w3 = hintp[3], w4 = hintp[4] ] );" }}}{EXCHG {PARA 
+0 "" 0 "" {TEXT -1 21 "Bspline interpolation" }}}{EXCHG {PARA 0 "> " 
+0 "" {MPLTEXT 1 0 107 "intp := [ (2-w1)*(2-w1)*(2-w1)/6, 2/3 - 1/2*w2*
+w2*(2-w2), 2/3 - 1/2*w3*w3*(2-w3), (2-w4)*(2-w4)*(2-w4)/6 ];" }}}
+{EXCHG {PARA 0 "> " 0 "" {MPLTEXT 1 0 88 "hintp := convert(subs(w1 = r
+est+1, w2 = rest, w3 = 1-rest, w4 = 2-rest, intp), horner); " }}}
+{EXCHG {PARA 0 "> " 0 "" {MPLTEXT 1 0 67 "C([ w1 = hintp[1], w2 = hint
+p[2], w3 = hintp[3], w4 = hintp[4] ] );" }}}{EXCHG {PARA 0 "" 0 "" 
+{TEXT -1 19 "OMOMS interpolation" }}}{EXCHG {PARA 0 "> " 0 "" 
+{MPLTEXT 1 0 172 "intp := [ -1/6*w1*w1*w1 + w1*w1 - 85/42*w1 + 29/21, \+
+1/2*w2*w2*w2 - w2*w2 + w2/14 + 13/21, 1/2*w3*w3*w3 - w3*w3 + w3/14 + 1
+3/21, -1/6*w4*w4*w4 + w4*w4 - 85/42*w4 + 29/21 ]; " }}}{EXCHG {PARA 0 
+"> " 0 "" {MPLTEXT 1 0 88 "hintp := convert(subs(w1 = rest+1, w2 = res
+t, w3 = 1-rest, w4 = 2-rest, intp), horner); " }}}{EXCHG {PARA 0 "> " 
+0 "" {MPLTEXT 1 0 67 "C([ w1 = hintp[1], w2 = hintp[2], w3 = hintp[3],
+ w4 = hintp[4] ] );" }}}{EXCHG {PARA 0 "> " 0 "" {MPLTEXT 1 0 0 "" }}}
+}{MARK "14" 0 }{VIEWOPTS 1 1 0 1 1 1803 1 1 1 1 }{PAGENUMBERS 0 1 2 
+33 1 1 }
+*/
 
 /* vim: set cin et ts=4 sw=4 cino=>1s,e0,n0,f0,{0,}0,^0,\:1s,=0,g1s,h0,t0,+1s,c3,(0,u0 : */
