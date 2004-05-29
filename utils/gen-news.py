63856f65e82924da4e83a16142ee31bc6ca3f1c7
yeti <yeti@25697fe4-d644-49b8-9d30-be5465f2adf3>
2004-05-29 11:21:14 +0000

    - generator understands introductory paragraphs - NEWS updated
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@1833 25697fe4-d644-49b8-9d30-be5465f2adf3

--- utils/gen-news.py
+++ utils/gen-news.py
@@ -3,6 +3,7 @@ import re, sys
 
 in_list = False
 in_item = False
+in_para = False
 for line in sys.stdin.readlines():
     line = line.rstrip()
     m = re.match(r'^(?P<version>\d[0-9a-z.]*)$', line)
@@ -11,15 +12,17 @@ for line in sys.stdin.readlines():
         continue
     m = re.match(r'^(?P<component>[A-Z].*):$', line)
     if m:
-        print '<b>%s</b>\n<ul>' % m.group('component')
+        if in_para:
+            print '</p>'
+        in_para = False
+        print '<p><b>%s</b>\n<ul>' % m.group('component')
         in_list = True
         continue
-    m = re.match(r'^\s*$', line)
-    if m:
+    if re.match(r'^\s*$', line):
         if in_item:
             print '</li>'
         if in_list:
-            print '</ul>'
+            print '</ul></p>'
         in_item = False
         in_list = False
         continue
@@ -31,6 +34,14 @@ for line in sys.stdin.readlines():
             print '</li>'
         print '<li>%s' % line[2:]
         in_item = True
+        continue
+    if not in_para and re.match(r'^[A-Z]', line):
+        in_para = True
+        print '<p>'
+        print line
+        continue
+    if in_para:
+        print line
         continue
     assert line.startswith('  ')
     assert in_item
