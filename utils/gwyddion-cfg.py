b428bdccdfdf128e860fcee127c4057380b16f54
yeti <yeti@25697fe4-d644-49b8-9d30-be5465f2adf3>
2004-05-31 13:57:15 +0000

    - using UTF-8
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@1854 25697fe4-d644-49b8-9d30-be5465f2adf3

--- utils/gwyddion-cfg.py
+++ utils/gwyddion-cfg.py
@@ -1,4 +1,4 @@
 file_glob = '/home/yeti/Projects/Gwyddion/gwyddion/devel-docs/*/*-decl.txt'
-maintainer = 'David NeËas (Yeti) <yeti@physics.muni.cz>'
+maintainer = 'David Neƒças (Yeti) <yeti@physics.muni.cz>'
 syntax_name = 'gwyddion'
 url = 'http://trific.ath.cx/Ftp/vim/syntax/gwyddion.vim'
