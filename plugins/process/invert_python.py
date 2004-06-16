31f2ec421cde7cc41c582d9ea652262949bc65b1
yeti <yeti@25697fe4-d644-49b8-9d30-be5465f2adf3>
2004-06-16 20:25:04 +0000

    - added invert_python.py
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@1883 25697fe4-d644-49b8-9d30-be5465f2adf3

---
+++ plugins/process/invert_python.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+# @(#) $Id$
+# A very simple Gwyddion plug-in example in Python.
+# Written by Yeti <yeti@gwyddion.net>.  Public domain.
+import sys, Gwyddion
+
+# Plug-in information.
+run_modes = 'noninteractive', 'with_defaults'
+
+plugin_info = """\
+invert_python
+/_Test/Value Invert (Python)
+""" + ' '.join(run_modes)
+
+def register(args):
+    print plugin_info
+
+def run(args):
+    run_mode = args.pop(0)
+    assert run_mode in run_modes
+
+    filename = args.pop(0)
+    data = Gwyddion.dump.read(filename)
+    dfield = data['/0/data']['data']
+
+    mean_value = 0.0
+    n = len(dfield)
+    for i in range(n):
+        mean_value += dfield[i]
+    mean_value /= n
+    for i in range(n):
+        dfield[i] = mean_value - dfield[i]
+    Gwyddion.dump.write(data, filename)
+
+try:
+    args = sys.argv[1:]
+    function = globals()[args.pop(0)]
+    assert callable(function)
+except (IndexError, KeyError, AssertionError):
+    raise "Plug-in has to be called from Gwyddion plugin-proxy."
+
+function(args)
