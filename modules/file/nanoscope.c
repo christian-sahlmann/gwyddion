f4fcfa029229dd9ac77e67302b39ab8c150e12e2
yeti <yeti@25697fe4-d644-49b8-9d30-be5465f2adf3>
2004-07-05 21:14:33 +0000

    - added preview to selection
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@1943 25697fe4-d644-49b8-9d30-be5465f2adf3

--- modules/file/nanoscope.c
+++ modules/file/nanoscope.c
@@ -51,6 +51,12 @@ typedef struct {
     GwyDataField *data_field;
 } NanoscopeData;
 
+typedef struct {
+    GtkWidget *data_view;
+    GwyContainer *data;
+    GList *list;
+} NanoscopeDialogControls;
+
 static gboolean       module_register     (const gchar *name);
 static gint           nanoscope_detect    (const gchar *filename,
                                            gboolean only_name);
@@ -85,7 +91,7 @@ static GwyModuleInfo module_info = {
     "nanoscope",
     "Load Nanoscope data.",
     "Yeti <yeti@gwyddion.net>",
-    "0.4",
+    "0.5",
     "David Nečas (Yeti) & Petr Klapetek",
     "2004",
 };
@@ -245,7 +251,7 @@ get_value_scales(GHashTable *hash,
     /* current sensitivity */
     if (!(s = g_hash_table_lookup(hash, "@Sens. Current"))) {
         g_warning("`@Sens. Current' not found");
-        return;
+         return;
     }
     if (s[0] != 'V' || s[1] != ' '
         || sscanf(s+2, "%lf %5s", curscale, un) != 2) {
@@ -418,15 +424,41 @@ hash_to_data_field(GHashTable *hash,
     return dfield;
 }
 
+static void
+selection_changed(GtkWidget *button,
+                  NanoscopeDialogControls *controls)
+{
+    gsize i;
+    GList *l;
+    GwyDataField *dfield;
+
+    if (!gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(button)))
+        return;
+
+    i = gwy_radio_buttons_get_current_from_widget(button, "data");
+    gwy_debug("%u", i);
+    g_assert(i != (gsize)-1);
+    l = g_list_nth(controls->list, i);
+    dfield = ((NanoscopeData*)l->data)->data_field;
+    gwy_container_set_object_by_name(controls->data, "/0/data",
+                                     G_OBJECT(dfield));
+    gwy_data_view_update(GWY_DATA_VIEW(controls->data_view));
+}
+
 static NanoscopeData*
 select_which_data(GList *list)
 {
     NanoscopeData *ndata, *ndata0;
-    GtkWidget *dialog, *label, *table;
+    NanoscopeDialogControls controls;
+    GtkObject *layer;
+    GwyDataField *dfield;
+    GtkWidget *dialog, *label, *hbox, *vbox, *align;
     GwyEnum *choices;
     GList *l;
     GSList *radio, *rl;
     gint i, count, response;
+    gint xres, yres;
+    gdouble zoomval;
 
     count = 0;
     ndata0 = NULL;
@@ -441,15 +473,15 @@ select_which_data(GList *list)
         }
         list = g_list_next(list);
     }
-    list = l;
+    controls.list = l;
     if (count == 0 || count == 1) {
-        g_list_free(list);
+        g_list_free(controls.list);
         return ndata0;
     }
 
     choices = g_new(GwyEnum, count);
     i = 0;
-    for (l = list; l; l = g_list_next(l)) {
+    for (l = controls.list; l; l = g_list_next(l)) {
         ndata = (NanoscopeData*)l->data;
         choices[i].name = g_hash_table_lookup(ndata->hash, "@2:Image Data");
         if (choices[i].name[0] && choices[i].name[1])
@@ -462,20 +494,47 @@ select_which_data(GList *list)
                                          GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
                                          GTK_STOCK_OK, GTK_RESPONSE_OK,
                                          NULL);
-    table = gtk_table_new(count+1, 1, FALSE);
-    gtk_container_set_border_width(GTK_CONTAINER(table), 6);
-    gtk_box_pack_start(GTK_BOX(GTK_DIALOG(dialog)->vbox), table, TRUE, TRUE, 0);
+
+    hbox = gtk_hbox_new(FALSE, 20);
+    gtk_container_set_border_width(GTK_CONTAINER(hbox), 12);
+    gtk_box_pack_start(GTK_BOX(GTK_DIALOG(dialog)->vbox), hbox, TRUE, TRUE, 0);
+
+    align = gtk_alignment_new(0.0, 0.0, 0.0, 0.0);
+    gtk_box_pack_start(GTK_BOX(hbox), align, TRUE, TRUE, 0);
+
+    vbox = gtk_vbox_new(TRUE, 0);
+    gtk_container_add(GTK_CONTAINER(align), vbox);
 
     label = gtk_label_new(_("Data to load:"));
     gtk_misc_set_alignment(GTK_MISC(label), 0.0, 0.5);
-    gtk_table_attach(GTK_TABLE(table), label, 0, 1, 0, 1,
-                     GTK_EXPAND | GTK_FILL, 0, 2, 2);
+    gtk_box_pack_start(GTK_BOX(vbox), label, TRUE, TRUE, 0);
 
-    radio = gwy_radio_buttons_create(choices, count, "data", NULL, NULL, 0);
+    radio = gwy_radio_buttons_create(choices, count, "data",
+                                     G_CALLBACK(selection_changed), &controls,
+                                     0);
     for (i = 0, rl = radio; rl; i++, rl = g_slist_next(rl))
-        gtk_table_attach(GTK_TABLE(table), GTK_WIDGET(rl->data),
-                         0, 1, i+1, i+2,
-                         GTK_EXPAND | GTK_FILL, 0, 2, 2);
+        gtk_box_pack_start(GTK_BOX(vbox), GTK_WIDGET(rl->data), TRUE, TRUE, 0);
+
+    /* preview */
+    align = gtk_alignment_new(1.0, 0.0, 0.0, 0.0);
+    gtk_box_pack_start(GTK_BOX(hbox), align, TRUE, TRUE, 0);
+
+    l = g_list_nth(controls.list, 0);
+    dfield = ((NanoscopeData*)l->data)->data_field;
+    controls.data = GWY_CONTAINER(gwy_container_new());
+    gwy_container_set_object_by_name(controls.data, "/0/data",
+                                     G_OBJECT(dfield));
+    xres = gwy_data_field_get_xres(GWY_DATA_FIELD(dfield));
+    yres = gwy_data_field_get_yres(GWY_DATA_FIELD(dfield));
+    zoomval = 120.0/MAX(xres, yres);
+
+    controls.data_view = gwy_data_view_new(controls.data);
+    g_object_unref(controls.data);
+    gwy_data_view_set_zoom(GWY_DATA_VIEW(controls.data_view), zoomval);
+    layer = gwy_layer_basic_new();
+    gwy_data_view_set_base_layer(GWY_DATA_VIEW(controls.data_view),
+                                 GWY_PIXMAP_LAYER(layer));
+    gtk_container_add(GTK_CONTAINER(align), controls.data_view);
 
     gtk_widget_show_all(dialog);
     gtk_window_present(GTK_WINDOW(dialog));
@@ -503,11 +562,11 @@ select_which_data(GList *list)
     response = GPOINTER_TO_INT(gwy_radio_buttons_get_current(radio, "data"));
     gtk_widget_destroy(dialog);
 
-    l = g_list_nth(list, response);
+    l = g_list_nth(controls.list, response);
     ndata0 = (NanoscopeData*)l->data;
 
     g_free(choices);
-    g_list_free(list);
+    g_list_free(controls.list);
 
     return ndata0;
 }
