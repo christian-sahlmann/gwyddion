e1e1be065f8e054fefbf7520d93845d2ab90f1b7
yeti <yeti@25697fe4-d644-49b8-9d30-be5465f2adf3>
2004-07-22 18:25:21 +0000

    - fixed HTML validity - added id attributes and version list - filtering out CVS versions
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@2044 25697fe4-d644-49b8-9d30-be5465f2adf3

--- utils/gen-news.py
+++ utils/gen-news.py
@@ -4,50 +4,71 @@ import re, sys
 in_list = False
 in_item = False
 in_para = False
+version_list = []
+text = []
 for line in sys.stdin.readlines():
     line = line.rstrip()
+    # Version
     m = re.match(r'^(?P<version>\d[0-9a-z.]*)$', line)
     if m:
-        print '\n<h2>Version %s</h2>' % m.group('version')
+        ver = m.group('version')
+        if ver.find('cvs') > -1:
+            continue
+        text.append('\n<h2 id="v%s">Version %s</h2>' % (ver, ver))
+        version_list.append(ver)
         continue
+    # Component
     m = re.match(r'^(?P<component>[A-Z].*):$', line)
     if m:
         if in_para:
-            print '</p>'
+            text.append('</p>')
         in_para = False
-        print '<p><b>%s</b>\n<ul>' % m.group('component')
+        text.append('<p><b>%s</b></p>\n<ul>' % m.group('component'))
         in_list = True
         continue
+    # End of list
     if re.match(r'^\s*$', line):
         if in_item:
-            print '</li>'
+            text.append('</li>')
         if in_list:
-            print '</ul></p>'
+            text.append('</ul>')
         in_item = False
         in_list = False
         continue
+    # Begin of list/item
     if line.startswith('- '):
         if not in_list:
-            print '<ul>'
+            if in_para:
+                text.append('</p>')
+                in_para = False
+            text.append('<ul>')
             in_list = True
         if in_item:
-            print '</li>'
-        print '<li>%s' % line[2:]
+            text.append('</li>')
+        text.append('<li>%s' % line[2:])
         in_item = True
         continue
+    # Begin of paragraph
     if not in_para and re.match(r'^[A-Z]', line):
         in_para = True
-        print '<p>'
-        print line
+        text.append('<p>')
+        text.append(line)
         continue
     if in_para:
-        print line
+        text.append(line)
         continue
     assert line.startswith('  ')
     assert in_item
-    print line[2:]
+    text.append(line[2:])
 
 if in_item:
-    print '</li>'
+    text.append('</li>')
 if in_list:
-    print '</ul>'
+    text.append('</ul>')
+if in_para:
+    text.append('</p>')
+
+print '<p>News for version:'
+print ',\n'.join(['<a href="#v%s">%s</a>' % (x, x) for x in version_list]) + '.'
+print '</p>'
+print '\n'.join(text)
