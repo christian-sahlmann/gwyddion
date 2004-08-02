3573e62196c60687b7e8137e199143e8f5b9b81a
yeti <yeti@25697fe4-d644-49b8-9d30-be5465f2adf3>
2004-08-02 19:44:04 +0000

    - addded GWY_STOCK_GRAPH_RULER
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@2095 25697fe4-d644-49b8-9d30-be5465f2adf3

--- libgwydgets/gwystock.h
+++ libgwydgets/gwystock.h
@@ -44,6 +44,7 @@ G_BEGIN_DECLS
 #define GWY_STOCK_GRAPH_MEASURE   "gwy_graph_measure"
 #define GWY_STOCK_GRAPH_PALETTE   "gwy_graph_palette"
 #define GWY_STOCK_GRAPH_POINTER   "gwy_graph_pointer"
+#define GWY_STOCK_GRAPH_RULER     "gwy_graph_ruler"
 #define GWY_STOCK_GRAPH_ZOOM_FIT  "gwy_graph_zoom_fit"
 #define GWY_STOCK_GRAPH_ZOOM_IN   "gwy_graph_zoom_in"
 #define GWY_STOCK_GRAPH_ZOOM_OUT  "gwy_graph_zoom_out"
