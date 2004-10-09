c0489b859e077f0abbd3dab4ab98ba25722ec990
yeti <yeti@25697fe4-d644-49b8-9d30-be5465f2adf3>
2004-10-09 17:01:14 +0000

    - added Invert Value in Ruby
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@2550 25697fe4-d644-49b8-9d30-be5465f2adf3

---
+++ plugins/process/invert_ruby.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/ruby
+
+# A very simple Gwyddion plug-in example in Ruby.
+
+# Written by Nenad Ocelic <ocelic _at_ biochem.mpg.de>.
+# Public domain.
+
+require "gwyddion/dump"
+include Gwyddion
+
+# Plug-in information.
+RUN_MODES= 'noninteractive', 'with_defaults'
+PLUGIN_INFO= [ "invert_ruby2", "/_Test/Value Invert (Ruby)"]
+
+def register(args)
+	puts PLUGIN_INFO, RUN_MODES
+end 
+
+def run( args)
+	run_mode= args.shift
+	RUN_MODES.member? run_mode or raise "Invalid run mode"
+	
+	dump= Dump.new args.shift
+	a= dump[ '/0/data']
+	
+	n= a.length
+	mirror= a.min + a.max
+	for i in (0 ... n)
+		a[ i]= mirror- a[ i]
+	end
+	dump.write # filename optional if not changed
+end 
+
+fn= ARGV.shift
+send fn.intern, ARGV
