2060005031c1c366e62e20b3d44b1a9f1ca96e13
yeti <yeti@25697fe4-d644-49b8-9d30-be5465f2adf3>
2004-10-10 16:54:24 +0000

    - Ruby plug-in update from Nenad Ocelic - added Ruby NArray plug-ins - fixed Makefile to install only plug-ins for present languages, but to   always include all in distribution
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@2561 25697fe4-d644-49b8-9d30-be5465f2adf3

--- plugins/process/invert_narray.rb
+++ plugins/process/invert_narray.rb
@@ -5,12 +5,17 @@
 # Written by Nenad Ocelic <ocelic _at_ biochem.mpg.de>.
 # Public domain.
 
+begin
+	require "narray"
+rescue LoadError
+	exit 1
+end
 require "gwyddion/dump"
 include Gwyddion
 
 # Plug-in information.
 RUN_MODES= 'noninteractive', 'with_defaults'
-PLUGIN_INFO= [ "invert_ruby2", "/_Test/Value Invert (Ruby)"]
+PLUGIN_INFO= [ "invert_narray", "/_Test/Value Invert (Ruby+NArray)"]
 
 def register(args)
 	puts PLUGIN_INFO, RUN_MODES
@@ -20,14 +25,10 @@ def run( args)
 	run_mode= args.shift
 	RUN_MODES.member? run_mode or raise "Invalid run mode"
 	
-	dump= Dump.new args.shift
+	dump= DumpNArray.new args.shift
 	a= dump[ '/0/data']
-	
-	n= a.length
 	mirror= a.min + a.max
-	for i in (0 ... n)
-		a[ i]= mirror- a[ i]
-	end
+	dump[ '/0/data']= mirror - a 
 	dump.write # filename optional unless changed
 end 
 
