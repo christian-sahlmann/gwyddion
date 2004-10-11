13f3c04371aa3a107b09cbdd5bdd9c577249ddca
yeti <yeti@25697fe4-d644-49b8-9d30-be5465f2adf3>
2004-10-11 13:36:13 +0000

    - changed Averaging radius mnemonics
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@2581 25697fe4-d644-49b8-9d30-be5465f2adf3

--- modules/tools/pointer.c
+++ modules/tools/pointer.c
@@ -174,7 +174,7 @@ dialog_create(GwyUnitoolState *state)
     else
         radius = 1;
     controls->radius = gtk_adjustment_new((gdouble)radius, 1, 16, 1, 5, 16);
-    gwy_table_attach_spinbutton(table, 9, _("Averaging _radius:"), _("px"),
+    gwy_table_attach_spinbutton(table, 9, _("_Averaging radius:"), _("px"),
                                 controls->radius);
     g_signal_connect_swapped(controls->radius, "value_changed",
                              G_CALLBACK(dialog_update), state);
