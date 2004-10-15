bcb4c059db9b295ef0ed0e4ce38e8c0c9ce1c981
yeti <yeti@25697fe4-d644-49b8-9d30-be5465f2adf3>
2004-10-15 14:41:19 +0000

    - changed GC function to GDK_XOR
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@2629 25697fe4-d644-49b8-9d30-be5465f2adf3

--- libgwydgets/gwyvectorlayer.c
+++ libgwydgets/gwyvectorlayer.c
@@ -588,7 +588,7 @@ gwy_vector_layer_setup_gc(GwyVectorLayer *layer)
 
     if (!layer->gc)
         layer->gc = gdk_gc_new(parent->window);
-    gdk_gc_set_function(layer->gc, GDK_INVERT);
+    gdk_gc_set_function(layer->gc, GDK_XOR);
     fg.pixel = 0xFFFFFFFF;
     bg.pixel = 0x00000000;
     gdk_gc_set_foreground(layer->gc, &fg);
