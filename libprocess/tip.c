87615ab512b86edabd54c0249eb9e24eef3869a2
yeti <yeti@25697fe4-d644-49b8-9d30-be5465f2adf3>
2004-10-21 15:15:48 +0000

    - switched off debugging
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@2698 25697fe4-d644-49b8-9d30-be5465f2adf3

--- libprocess/tip.c
+++ libprocess/tip.c
@@ -17,7 +17,7 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111 USA
  */
-#define DEBUG 1
+
 #include <string.h>
 #include <glib.h>
 #include <libgwyddion/gwymacros.h>
