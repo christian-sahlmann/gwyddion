f4db6b761720439206a4881fee871319447a6d37
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2004-12-23 00:05:08 +0000

    - created directory data for miscellaneous data - moved gwyddion.vim and gwyddion-cfg.py to data - fixed hardcoded my home in gwyddion-cfg.py - improved make rules for data/gwyddion.vim - updated desktop files according to latest xdg standards and moved them to   data - updated spec file
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@3145 25697fe4-d644-49b8-9d30-be5465f2adf3

--- data/gwyddion-cfg.py
+++ data/gwyddion-cfg.py
@@ -1,5 +1,6 @@
 # coding: utf-8
-file_glob = '/home/yeti/Projects/Gwyddion/gwyddion/devel-docs/*/*-decl.txt'
+from os import getcwd as _getcwd
+file_glob = _getcwd() + '/devel-docs/*/*-decl.txt'
 maintainer = 'David Nečas (Yeti) <yeti@physics.muni.cz>'
 syntax_name = 'gwyddion'
 url = 'http://trific.ath.cx/Ftp/vim/syntax/gwyddion.vim'
