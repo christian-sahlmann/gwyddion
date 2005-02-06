c7b493345d2c3d9c1889104701f1c55055df0a12
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2005-02-06 20:46:16 +0000

    - added basic XML dumping functionality
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@3328 25697fe4-d644-49b8-9d30-be5465f2adf3

--- utils/dump-modules.c
+++ utils/dump-modules.c
@@ -26,11 +26,75 @@
 #include <stdlib.h>
 #include <string.h>
 
+static GSList *tag_stack = NULL;
+
+/* For module list sorting */
+static gint
+compare_modules(gconstpointer a,
+                gconstpointer b)
+{
+    return strcmp(((const GwyModuleInfo*)a)->name,
+                  ((const GwyModuleInfo*)b)->name);
+}
+
+/* For finding module of given name */
+static gint
+find_module(gconstpointer a,
+            gconstpointer b)
+{
+    return strcmp(((const GwyModuleInfo*)a)->name,
+                  (const gchar*)b);
+}
+
+/* Print a single tag with content, handling sensitive characters */
+static void
+tag_print(const gchar *tag, const gchar *contents)
+{
+    gchar *s;
+
+    if (!contents || !*contents) {
+        printf("<%s/>\n", tag);
+        return;
+    }
+
+    s = g_markup_escape_text(contents, strlen(contents));
+    printf("<%s>%s</%s>\n", tag, s, tag);
+    g_free(s);
+}
+
+/* Print an opening tag and push it onto stack */
+static void
+tag_open(const gchar *tag)
+{
+    printf("<%s>\n", tag);
+    tag_stack = g_slist_prepend(tag_stack, g_strdup(tag));
+}
+
+/* Pop currently open tag and close it */
+static void
+tag_close(void)
+{
+    gchar *s;
+
+    g_return_if_fail(tag_stack);
+    s = tag_stack->data;
+    printf("</%s>\n", s);
+    tag_stack = g_slist_delete_link(tag_stack, tag_stack);
+    g_free(s);
+}
+
+/* Main */
 int
 main(G_GNUC_UNUSED int argc,
      G_GNUC_UNUSED char *argv[])
 {
+    const gchar *filter_modules[] = {
+        "plugin-proxy", "threshold-example",
+    };
+
     gchar **module_dirs;
+    GSList *m, *f, *modules = NULL;
+    gsize i;
 
 #ifdef G_OS_WIN32
     gwy_find_self_set_argv0(argv[0]);
@@ -40,7 +104,44 @@ main(G_GNUC_UNUSED int argc,
 
     module_dirs = gwy_app_settings_get_module_dirs();
     gwy_module_register_modules((const gchar**)module_dirs);
+    gwy_module_foreach(gwy_hash_table_to_slist_cb, &modules);
+    modules = g_slist_sort(modules, &compare_modules);
 
+    for (i = 0; i < G_N_ELEMENTS(filter_modules); i++) {
+        m = g_slist_find_custom(modules, filter_modules[i], find_module);
+        if (!m)
+            continue;
+        modules = g_slist_delete_link(modules, m);
+    }
+
+    puts("<?xml version=\"1.0\"?>");
+    tag_open("modulelist");
+    for (m = modules; m; m = g_slist_next(m)) {
+        const GwyModuleInfo *mod_info = (const GwyModuleInfo*)m->data;
+
+        tag_open("module");
+        tag_print("name", mod_info->name);
+        tag_print("version", mod_info->version);
+        tag_print("author", mod_info->author);
+        tag_print("copyright", mod_info->copyright);
+        tag_print("date", mod_info->date);
+        tag_print("description", mod_info->blurb);
+        tag_open("funclist");
+        for (f = gwy_module_get_functions(mod_info->name);
+             f;
+             f = g_slist_next(f)) {
+            const gchar *name = (const gchar*)f->data;
+
+            tag_open("func");
+            tag_print("name", name);
+            tag_close();
+        }
+        tag_close();
+        tag_close();
+    }
+    tag_close();
+
+    g_slist_free(modules);
     g_strfreev(module_dirs);
 
     return 0;
