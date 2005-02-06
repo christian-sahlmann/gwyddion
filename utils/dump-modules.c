505de334f3aab9046340dcac25523a3b4eac993d
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2005-02-06 19:36:44 +0000

    - dedicated utils its own Makefile - added dummy dump-modules.c
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@3327 25697fe4-d644-49b8-9d30-be5465f2adf3

---
+++ utils/dump-modules.c
@@ -0,0 +1,50 @@
+/*
+ *  @(#) $Id$
+ *  Copyright (C) 2003,2004 David Necas (Yeti), Petr Klapetek.
+ *  E-mail: yeti@gwyddion.net, klapetek@gwyddion.net.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111 USA
+ */
+
+#include <libgwyddion/gwymacros.h>
+#include <libgwymodule/gwymodule.h>
+#include <libgwyddion/gwyutils.h>
+#include <app/settings.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int
+main(G_GNUC_UNUSED int argc,
+     G_GNUC_UNUSED char *argv[])
+{
+    gchar **module_dirs;
+
+#ifdef G_OS_WIN32
+    gwy_find_self_set_argv0(argv[0]);
+#endif  /* G_OS_WIN32 */
+
+    g_type_init();
+
+    module_dirs = gwy_app_settings_get_module_dirs();
+    gwy_module_register_modules((const gchar**)module_dirs);
+
+    g_strfreev(module_dirs);
+
+    return 0;
+}
+
+/* vim: set cin et ts=4 sw=4 cino=>1s,e0,n0,f0,{0,}0,^0,\:1s,=0,g1s,h0,t0,+1s,c3,(0,u0 : */
+
