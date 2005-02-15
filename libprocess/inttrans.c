e72c5ffac31804d40dca1cb6192b9e7210afd8e5
klapetek <klapetek@25697fe4-d644-49b8-9d30-be5465f2adf3>
2005-02-15 13:54:41 +0000

    FFT filtering windowing bug fixed even more...
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@3370 25697fe4-d644-49b8-9d30-be5465f2adf3

--- libprocess/inttrans.c
+++ libprocess/inttrans.c
@@ -515,7 +515,7 @@ gwy_data_field_fft_filter_1d(GwyDataField *data_field,
     
     gwy_data_field_xfft(data_field, result_field,
                     hlp_dfield, hlp_idfield,
-                    gwy_data_line_fft_hum, GWY_WINDOWING_RECT,
+                    gwy_data_line_fft_hum, GWY_WINDOWING_NONE,
                     1, interpolation,
                     FALSE, FALSE);
 
