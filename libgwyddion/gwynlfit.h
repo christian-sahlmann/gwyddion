878ec25610a45b92a2b9f6a42def339f4e0195fd
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2005-05-09 08:25:41 +0000

    - fixed missing G_END_DECLS
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@3791 25697fe4-d644-49b8-9d30-be5465f2adf3

--- libgwyddion/gwynlfit.h
+++ libgwyddion/gwynlfit.h
@@ -163,6 +163,8 @@ GwyNLFitter*    gwy_math_nlfit_fit_preset        (const GwyNLFitPreset* preset,
                                                   const gboolean *fixed_param,
                                                   gpointer user_data);
 
+G_END_DECLS
+
 #endif /* __GWY_NFLIT_H__ */
 
 /* vim: set cin et ts=4 sw=4 cino=>1s,e0,n0,f0,{0,}0,^0,\:1s,=0,g1s,h0,t0,+1s,c3,(0,u0 : */
