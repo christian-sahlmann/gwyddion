ae7ac31bf488dac5c534cc2997588180d13b4e21
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2005-08-17 07:44:19 +0000

    - added Returns documentation to gwy_gradients_get_gradient()
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@4311 25697fe4-d644-49b8-9d30-be5465f2adf3

--- libdraw/gwygradient.c
+++ libdraw/gwygradient.c
@@ -1282,6 +1282,9 @@ gwy_gradients(void)
  * @name: Gradient name.  May be %NULL to get default gradient.
  *
  * Convenience macro to get a gradient from gwy_gradients() by name.
+ *
+ * Returns: Gradient identified by @name or default gradient if it does not
+ *          exist.
  **/
 
 /* vim: set cin et ts=4 sw=4 cino=>1s,e0,n0,f0,{0,}0,^0,\:1s,=0,g1s,h0,t0,+1s,c3,(0,u0 : */
