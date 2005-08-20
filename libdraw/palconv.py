8faeb13c0717d92347b43739273d565777ddffa8
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2005-08-20 23:41:24 +0000

    - updated output from C code to GwyGradient resource format
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@4355 25697fe4-d644-49b8-9d30-be5465f2adf3

--- libdraw/palconv.py
+++ libdraw/palconv.py
@@ -2,14 +2,17 @@
 import sys
 
 lines = sys.stdin.readlines()
-print lines[1].strip()
+name = lines[1].strip().split(':')[1].strip()
+print name
+fh = file(name, 'w')
 
-fmt = '{ %s, { %s, %s, %s, %s } },'
+fmt = '%s %s %s %s %s\n'
 end = None
+fh.write('Gwyddion resource GwyGradient\n')
 for line in lines[3:]:
     fields = line.split(' ')
     start = fields[0:1] + fields[3:7]
     assert not end or start == end
     end = fields[2:3] + fields[7:11]
-    print fmt % tuple(start)
-print fmt % tuple(end)
+    fh.write(fmt % tuple(start))
+fh.write(fmt % tuple(end))
