3061beae85c942091bd9b0b644932850fa28007b
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2005-10-24 10:35:55 +0000

    - marked morph_lib functions as G_GNUC_INTERNAL
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@4764 25697fe4-d644-49b8-9d30-be5465f2adf3

--- libprocess/morph_lib.h
+++ libprocess/morph_lib.h
@@ -5,23 +5,31 @@
 
 #include <libprocess/tip.h>
 
+G_BEGIN_DECLS
+
+G_GNUC_INTERNAL
 gint **_gwy_morph_lib_iallocmatrix(gint ysiz,
                                    gint xsiz);
 
+G_GNUC_INTERNAL
 void _gwy_morph_lib_ifreematrix(gint **mptr,
                                 gint ysiz);
 
+G_GNUC_INTERNAL
 gdouble **_gwy_morph_lib_dallocmatrix(gint ysiz,
                                       gint xsiz);
 
+G_GNUC_INTERNAL
 void _gwy_morph_lib_dfreematrix(gdouble **mptr,
                                 gint ysiz);
 
 /*simple routines - integer arithmetics*/
+G_GNUC_INTERNAL
 gint **_gwy_morph_lib_ireflect(gint **surface,
                                gint surf_xsiz,
                                gint surf_ysiz);
 
+G_GNUC_INTERNAL
 gint **_gwy_morph_lib_idilation(gint **surface,
                                 gint surf_xsiz,
                                 gint surf_ysiz,
@@ -33,6 +41,7 @@ gint **_gwy_morph_lib_idilation(gint **surface,
                                 GwySetFractionFunc set_fraction,
                                 GwySetMessageFunc set_message);
 
+G_GNUC_INTERNAL
 gint **_gwy_morph_lib_ierosion(gint **image,
                                gint im_xsiz,
                                gint im_ysiz,
@@ -44,6 +53,7 @@ gint **_gwy_morph_lib_ierosion(gint **image,
                                GwySetFractionFunc set_fraction,
                                GwySetMessageFunc set_message);
 
+G_GNUC_INTERNAL
 gint **_gwy_morph_lib_icmap(gint **image,
                             gint im_xsiz,
                             gint im_ysiz,
@@ -57,10 +67,12 @@ gint **_gwy_morph_lib_icmap(gint **image,
                             GwySetMessageFunc set_message);
 
 /*simple routines - double arithmetics (can be very slow)*/
+G_GNUC_INTERNAL
 gdouble **_gwy_morph_lib_dreflect(gdouble **surface,
                                   gint surf_xsiz,
                                   gint surf_ysiz);
 
+G_GNUC_INTERNAL
 gdouble **_gwy_morph_lib_ddilation(gdouble **surface,
                                    gint surf_xsiz,
                                    gint surf_ysiz,
@@ -72,6 +84,7 @@ gdouble **_gwy_morph_lib_ddilation(gdouble **surface,
                                    GwySetFractionFunc set_fraction,
                                    GwySetMessageFunc set_message);
 
+G_GNUC_INTERNAL
 gdouble **_gwy_morph_lib_derosion(gdouble **image,
                                   gint im_xsiz,
                                   gint im_ysiz,
@@ -84,6 +97,7 @@ gdouble **_gwy_morph_lib_derosion(gdouble **image,
                                   GwySetMessageFunc set_message);
 
 /*tip estimation routines - all in integer artithmetics*/
+G_GNUC_INTERNAL
 gint _gwy_morph_lib_itip_estimate(gint **image,
                                   gint im_xsiz,
                                   gint im_ysiz,
@@ -97,6 +111,7 @@ gint _gwy_morph_lib_itip_estimate(gint **image,
                                   GwySetFractionFunc set_fraction,
                                   GwySetMessageFunc set_message);
 
+G_GNUC_INTERNAL
 gint _gwy_morph_lib_itip_estimate0(gint **image,
                                    gint im_xsiz,
                                    gint im_ysiz,
@@ -111,6 +126,7 @@ gint _gwy_morph_lib_itip_estimate0(gint **image,
                                    GwySetMessageFunc set_message);
 
 
+G_END_DECLS
 
 #endif
 
