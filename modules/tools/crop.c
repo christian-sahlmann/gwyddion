12b4d7f940765f9dd4e93abea826f76549e4754f
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2005-11-03 21:14:10 +0000

    - added possibility to keep lateral offsets on cropped data
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@4901 25697fe4-d644-49b8-9d30-be5465f2adf3

--- modules/tools/crop.c
+++ modules/tools/crop.c
@@ -27,24 +27,33 @@
 #include <libprocess/datafield.h>
 #include <libgwydgets/gwydgets.h>
 #include <app/app.h>
+#include <app/settings.h>
 #include <app/unitool.h>
 
 #define CHECK_LAYER_TYPE(l) \
     (G_TYPE_CHECK_INSTANCE_TYPE((l), func_slots.layer_type))
 
 typedef struct {
+    gboolean keep_offsets;
+    GtkWidget *ckeep_offsets;
     GwyUnitoolRectLabels labels;
 } ToolControls;
 
-static gboolean   module_register  (const gchar *name);
-static gboolean   use              (GwyDataWindow *data_window,
-                                    GwyToolSwitchEvent reason);
-static void       layer_setup      (GwyUnitoolState *state);
-static GtkWidget* dialog_create    (GwyUnitoolState *state);
-static void       dialog_update    (GwyUnitoolState *state,
-                                    GwyUnitoolUpdateType reason);
-static void       dialog_abandon   (GwyUnitoolState *state);
-static void       apply            (GwyUnitoolState *state);
+static gboolean   module_register     (const gchar *name);
+static gboolean   use                 (GwyDataWindow *data_window,
+                                       GwyToolSwitchEvent reason);
+static void       layer_setup         (GwyUnitoolState *state);
+static GtkWidget* dialog_create       (GwyUnitoolState *state);
+static void       dialog_update       (GwyUnitoolState *state,
+                                       GwyUnitoolUpdateType reason);
+static void       dialog_abandon      (GwyUnitoolState *state);
+static void       apply               (GwyUnitoolState *state);
+static void       keep_offsets_toggled(GwyUnitoolState *state,
+                                       GtkWidget *toggle);
+static void       load_args           (GwyContainer *container,
+                                       ToolControls *controls);
+static void       save_args           (GwyContainer *container,
+                                       ToolControls *controls);
 
 /* The module info. */
 static GwyModuleInfo module_info = {
@@ -52,7 +61,7 @@ static GwyModuleInfo module_info = {
     &module_register,
     N_("Crop tool, crops data to smaller size."),
     "Yeti <yeti@gwyddion.net>",
-    "1.5",
+    "1.6",
     "David Nečas (Yeti) & Petr Klapetek",
     "2003",
 };
@@ -122,9 +131,11 @@ dialog_create(GwyUnitoolState *state)
 {
     ToolControls *controls;
     GtkWidget *dialog, *table, *frame;
+    gint row;
 
     gwy_debug("");
     controls = (ToolControls*)state->user_data;
+    load_args(gwy_app_settings_get(), controls);
 
     dialog = gtk_dialog_new_with_buttons(_("Crop"), NULL, 0, NULL);
     gwy_unitool_dialog_add_button_hide(dialog);
@@ -134,11 +145,22 @@ dialog_create(GwyUnitoolState *state)
     gtk_box_pack_start(GTK_BOX(GTK_DIALOG(dialog)->vbox), frame,
                        FALSE, FALSE, 0);
 
-    table = gtk_table_new(6, 4, FALSE);
+    table = gtk_table_new(7, 4, FALSE);
     gtk_container_set_border_width(GTK_CONTAINER(table), 4);
     gtk_container_add(GTK_CONTAINER(GTK_DIALOG(dialog)->vbox), table);
-    gwy_unitool_rect_info_table_setup(&controls->labels,
-                                      GTK_TABLE(table), 0, 0);
+    row = gwy_unitool_rect_info_table_setup(&controls->labels,
+                                            GTK_TABLE(table), 0, 0);
+    gtk_table_set_row_spacing(GTK_TABLE(table), row-1, 8);
+
+    controls->ckeep_offsets
+        = gtk_check_button_new_with_mnemonic(_("Keep lateral offsets"));
+    gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(controls->ckeep_offsets),
+                                 controls->keep_offsets);
+    gtk_table_attach(GTK_TABLE(table), controls->ckeep_offsets,
+                     0, 4, row, row+1, GTK_EXPAND | GTK_FILL, 0, 2, 2);
+    g_signal_connect_swapped(controls->ckeep_offsets, "toggled",
+                             G_CALLBACK(keep_offsets_toggled), state);
+
     return dialog;
 }
 
@@ -166,13 +188,29 @@ dialog_update(GwyUnitoolState *state,
 static void
 dialog_abandon(GwyUnitoolState *state)
 {
+    ToolControls *controls;
+
+    controls = (ToolControls*)state->user_data;
+    save_args(gwy_app_settings_get(), controls);
     memset(state->user_data, 0, sizeof(ToolControls));
+}
+
+static void
+keep_offsets_toggled(GwyUnitoolState *state,
+                     GtkWidget *toggle)
+{
+    ToolControls *controls;
+
+    controls = (ToolControls*)state->user_data;
+    controls->keep_offsets
+        = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(toggle));
 }
 
 static void
 apply(GwyUnitoolState *state)
 {
     static const gchar *field_names[] = { "/0/data", "/0/mask", "/0/show" };
+    ToolControls *controls;
     GwySelection *selection;
     GtkWidget *data_window;
     GwyDataView *data_view;
@@ -182,6 +220,7 @@ apply(GwyUnitoolState *state)
     gdouble sel[4];
     gsize i;
 
+    controls = (ToolControls*)state->user_data;
     selection = gwy_vector_layer_get_selection(state->layer);
     if (!gwy_selection_get_object(selection, 0, sel))
         return;
@@ -199,11 +238,36 @@ apply(GwyUnitoolState *state)
         ximax = gwy_data_field_rtoj(dfield, sel[2]) + 1;
         yimax = gwy_data_field_rtoi(dfield, sel[3]) + 1;
         gwy_data_field_resize(dfield, ximin, yimin, ximax, yimax);
+        if (controls->keep_offsets) {
+            gwy_data_field_set_xoffset(dfield, sel[0]);
+            gwy_data_field_set_yoffset(dfield, sel[1]);
+        }
     }
     data_window = gwy_app_data_window_create(data);
     gwy_app_data_window_set_untitled(GWY_DATA_WINDOW(data_window), NULL);
     gwy_debug("%d %d",
               gwy_data_field_get_xres(dfield), gwy_data_field_get_yres(dfield));
+}
+
+static const gchar *keep_offsets_key = "/tool/crop/keep_offsets";
+
+static void
+save_args(GwyContainer *container, ToolControls *controls)
+{
+    gwy_container_set_boolean_by_name(container, keep_offsets_key,
+                                      controls->keep_offsets);
+}
+
+static void
+load_args(GwyContainer *container, ToolControls *controls)
+{
+    controls->keep_offsets = FALSE;
+
+    gwy_container_gis_boolean_by_name(container, keep_offsets_key,
+                                      &controls->keep_offsets);
+
+    /* sanitize */
+    controls->keep_offsets = !!controls->keep_offsets;
 }
 
 /* vim: set cin et ts=4 sw=4 cino=>1s,e0,n0,f0,{0,}0,^0,\:1s,=0,g1s,h0,t0,+1s,c3,(0,u0 : */
