0f09cc3cd1359f0d8ca52ff7c851e87cf31540d1
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2006-01-12 22:13:46 +0000

    - scan header files too
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@5280 25697fe4-d644-49b8-9d30-be5465f2adf3

--- utils/update-potfiles.sh
+++ utils/update-potfiles.sh
@@ -7,6 +7,6 @@ echo '# This is a GENERATED file.'
 for dir in libgwyddion libprocess libdraw libgwydgets libgwymodule app modules; do
   echo
   echo "# $dir"
-  find $dir -name \*.c | xargs grep -E -l '\<N?_\(|\<gwy_sgettext\(' \
+  find $dir -name \*.\[ch\] | xargs grep -E -l '\<N?_\(|\<gwy_sgettext\(' \
     | grep -v '/\(main\|test\)\.c' | sort
 done
