4533bfa400356dfca4dca6b2a575f0d53339fe86
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2006-01-23 15:47:38 +0000

    - removed old graph menu construction code - got rid of internal GraphFuncInfo that was useful only for that
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@5371 25697fe4-d644-49b8-9d30-be5465f2adf3

--- libgwymodule/gwymodule-graph.c
+++ libgwymodule/gwymodule-graph.c
@@ -30,19 +30,11 @@
 #include "gwymodule-graph.h"
 
 typedef struct {
-    GwyGraphFuncInfo info;
-    const gchar *menu_path_translated;
-    gchar *menu_path_factory;
-} GraphFuncInfo;
-
-typedef struct {
     GFunc function;
     gpointer user_data;
 } GraphFuncForeachData;
 
 static void gwy_graph_func_info_free   (gpointer data);
-static gint graph_menu_entry_compare   (GraphFuncInfo *a,
-                                        GraphFuncInfo *b);
 
 static GHashTable *graph_funcs = NULL;
 static void (*func_register_callback)(const gchar *fullname) = NULL;
@@ -64,7 +56,7 @@ gwy_graph_func_register(const gchar *modname,
                         GwyGraphFuncInfo *func_info)
 {
     _GwyModuleInfoInternal *iinfo;
-    GraphFuncInfo *gfinfo;
+    GwyGraphFuncInfo *gfinfo;
     gchar *canon_name;
 
     gwy_debug("");
@@ -86,16 +78,12 @@ gwy_graph_func_register(const gchar *modname,
         return FALSE;
     }
 
-    gfinfo = g_new0(GraphFuncInfo, 1);
-    gfinfo->info = *func_info;
-    gfinfo->info.name = g_strdup(func_info->name);
-    gfinfo->info.menu_path = g_strdup(func_info->menu_path);
-    gfinfo->menu_path_translated = _(func_info->menu_path);
-    gfinfo->menu_path_factory
-        = gwy_strkill(g_strdup(gfinfo->menu_path_translated), "_");
+    gfinfo = g_memdup(func_info, sizeof(GwyGraphFuncInfo));
+    gfinfo->name = g_strdup(func_info->name);
+    gfinfo->menu_path = g_strdup(func_info->menu_path);
 
-    g_hash_table_insert(graph_funcs, (gpointer)gfinfo->info.name, gfinfo);
-    canon_name = g_strconcat(GWY_MODULE_PREFIX_GRAPH, gfinfo->info.name, NULL);
+    g_hash_table_insert(graph_funcs, (gpointer)gfinfo->name, gfinfo);
+    canon_name = g_strconcat(GWY_MODULE_PREFIX_GRAPH, gfinfo->name, NULL);
     iinfo->funcs = g_slist_append(iinfo->funcs, canon_name);
     if (func_register_callback)
         func_register_callback(canon_name);
@@ -112,11 +100,10 @@ _gwy_graph_func_set_register_callback(void (*callback)(const gchar *fullname))
 static void
 gwy_graph_func_info_free(gpointer data)
 {
-    GraphFuncInfo *gfinfo = (GraphFuncInfo*)data;
+    GwyGraphFuncInfo *gfinfo = (GwyGraphFuncInfo*)data;
 
-    g_free((gpointer)gfinfo->info.name);
-    g_free((gpointer)gfinfo->info.menu_path);
-    g_free((gpointer)gfinfo->menu_path_factory);
+    g_free((gpointer)gfinfo->name);
+    g_free((gpointer)gfinfo->menu_path);
     g_free(gfinfo);
 }
 
@@ -133,7 +120,7 @@ gboolean
 gwy_graph_func_run(const guchar *name,
                    GwyGraph *graph)
 {
-    GraphFuncInfo *func_info;
+    GwyGraphFuncInfo *func_info;
     gboolean status;
 
     func_info = g_hash_table_lookup(graph_funcs, name);
@@ -141,128 +128,11 @@ gwy_graph_func_run(const guchar *name,
     g_return_val_if_fail(GWY_IS_GRAPH(graph), FALSE);
     g_object_ref(graph);
     _gwy_module_watch_settings(GWY_MODULE_PREFIX_GRAPH, name);
-    status = func_info->info.graph(graph, name);
+    status = func_info->graph(graph, name);
     _gwy_module_unwatch_settings();
     g_object_unref(graph);
 
     return status;
-}
-
-/**
- * gwy_graph_func_build_menu:
- * @item_factory: A #GtkItemFactory to add items to.
- * @prefix: Where to add the menu items to the factory.
- * @item_callback: A #GtkItemFactoryCallback1 called when an item from the
- *                 menu is selected.
- *
- * Creates #GtkItemFactory for a graph menu with all registered graph
- * functions.
- *
- * Returns: The menu item factory as a #GtkObject.
- **/
-GtkObject*
-gwy_graph_func_build_menu(GtkObject *item_factory,
-                          const gchar *prefix,
-                          GCallback item_callback)
-{
-    GtkItemFactoryEntry branch = { NULL, NULL, NULL, 0, "<Branch>", NULL };
-    GtkItemFactoryEntry tearoff = { NULL, NULL, NULL, 0, "<Tearoff>", NULL };
-    GtkItemFactoryEntry item = { NULL, NULL, item_callback, 0, "<Item>", NULL };
-    GtkItemFactory *factory;
-    GString *current, *prev;
-    const gchar *mpath;
-    GSList *l, *entries = NULL;
-    gint i, dp_len;
-
-    g_return_val_if_fail(GTK_IS_ITEM_FACTORY(item_factory), NULL);
-    factory = GTK_ITEM_FACTORY(item_factory);
-
-    if (!graph_funcs) {
-        g_warning("No graph function present to build menu of");
-        entries = NULL;
-    }
-    else
-        g_hash_table_foreach(graph_funcs, gwy_hash_table_to_slist_cb,
-                             &entries);
-    entries = g_slist_sort(entries, (GCompareFunc)graph_menu_entry_compare);
-
-    dp_len = strlen(prefix);
-
-    /* the root branch */
-    current = g_string_new(prefix);
-
-    /* the root tearoff */
-    prev = g_string_new(prefix);
-    g_string_append(prev, "/---");
-    tearoff.path = prev->str;
-    gtk_item_factory_create_item(factory, &tearoff, NULL, 1);
-
-    /* create missing branches
-     * XXX: Gtk+ essentially can do this itself
-     * but this way we can e. g. put a tearoff at the top of each branch... */
-    for (l = entries; l; l = g_slist_next(l)) {
-        GraphFuncInfo *func_info = (GraphFuncInfo*)l->data;
-
-        mpath = func_info->menu_path_translated;
-        if (!mpath || !*mpath)
-            continue;
-        if (mpath[0] != '/') {
-            g_warning("Menu path `%s' doesn't start with a slash", mpath);
-            continue;
-        }
-
-        g_string_truncate(current, dp_len);
-        g_string_append(current, mpath);
-        /* find where the paths differ */
-        i = gwy_strdiffpos(current->str + dp_len, prev->str + dp_len);
-        if (!current->str[i] && !prev->str[i])
-            g_warning("Duplicate menu entry `%s'", mpath);
-        else {
-            /* find where the next / is  */
-            do {
-                i++;
-            } while (current->str[i] && current->str[i] != '/');
-        }
-
-        while (current->str[i]) {
-            /* create a branch with a tearoff */
-            current->str[i] = '\0';
-            branch.path = current->str;
-            gtk_item_factory_create_item(factory, &branch, NULL, 1);
-
-            g_string_assign(prev, current->str);
-            g_string_append(prev, "/---");
-            tearoff.path = prev->str;
-            gtk_item_factory_create_item(factory, &tearoff, NULL, 1);
-            current->str[i] = '/';
-
-            /* find where the next / is  */
-            do {
-                i++;
-            } while (current->str[i] && current->str[i] != '/');
-        }
-
-        /* XXX: passing directly func_info->name may be a little dangerous,
-         * OTOH who would eventually free a newly allocated string? */
-        item.path = current->str;
-        gtk_item_factory_create_item(factory, &item,
-                                     (gpointer)func_info->info.name, 1);
-
-        GWY_SWAP(GString*, current, prev);
-    }
-
-    g_string_free(prev, TRUE);
-    g_string_free(current, TRUE);
-    g_slist_free(entries);
-
-    return item_factory;
-}
-
-static gint
-graph_menu_entry_compare(GraphFuncInfo *a,
-                         GraphFuncInfo *b)
-{
-    return g_utf8_collate(a->menu_path_factory, b->menu_path_factory);
 }
 
 static void
@@ -327,11 +197,12 @@ gwy_graph_func_exists(const gchar *name)
 const gchar*
 gwy_graph_func_get_menu_path(const gchar *name)
 {
-    GraphFuncInfo *func_info;
+    GwyGraphFuncInfo *func_info;
 
     func_info = g_hash_table_lookup(graph_funcs, name);
-    g_return_val_if_fail(func_info, 0);
-    return func_info->menu_path_translated;
+    g_return_val_if_fail(func_info, NULL);
+
+    return func_info->menu_path;
 }
 
 /**
--- libgwymodule/gwymodule-graph.h
+++ libgwymodule/gwymodule-graph.h
@@ -43,9 +43,6 @@ gboolean     gwy_graph_func_run           (const guchar *name,
                                            GwyGraph *graph);
 gboolean     gwy_graph_func_exists        (const gchar *name);
 const gchar* gwy_graph_func_get_menu_path (const gchar *name);
-GtkObject*   gwy_graph_func_build_menu    (GtkObject *item_factory,
-                                           const gchar *prefix,
-                                           GCallback item_callback);
 guint        gwy_graph_func_get_sensitivity_flags (const gchar *name);
 void         gwy_graph_func_foreach       (GFunc function,
                                            gpointer user_data);
