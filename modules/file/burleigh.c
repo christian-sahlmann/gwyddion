a4e2bc2fba45a01b620e995239c84cf0c4477e85
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2006-03-12 22:39:01 +0000

    - refactored a bit - v2.1 is importable now, although the physical scales remain uncertain
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@5612 25697fe4-d644-49b8-9d30-be5465f2adf3

--- modules/file/burleigh.c
+++ modules/file/burleigh.c
@@ -38,13 +38,25 @@
 
 #define HEADER_SIZE_MIN 42
 
+#define HEADER_SIZE_V21 8
+#define FOOTER_SIZE_V21 40
+#define TOTAL_SIZE_V21 (HEADER_SIZE_V21 + FOOTER_SIZE_V21)
+
 #define Angstrom (1e-10)
+
+enum {
+    BURLEIGH_CURRENT = 0,
+    BURLEIGH_TOPOGRAPHY = 1
+};
 
 typedef struct {
     gdouble version;
-    guint32 header_size;
+    guint version_int;
+    guint32 header_size;  /* Not in v2.1 */
     guint32 xres;
     guint32 yres;
+
+    /* In v2.1, this in in the footer */
     guint32 xrangemax;
     guint32 yrangemax;
     guint32 zrangemax;
@@ -52,11 +64,17 @@ typedef struct {
     guint32 yrange;
     guint32 zrange;
     guint32 data_type;
+    guint32 scan_speed;
+    gdouble z_gain;
+
+    /* Not in v2.1 */
     guint32 afm_head_id;
-    guint32 zoom_factor;
-    guint32 z_gain;
-    /* there are other fields but I have no idea how they depend on the version
-     * so just ignore them */
+    gdouble zoom_factor;
+
+    /* Not in the older version described at weizmann.ac.il */
+    guint32 zoom_level;
+    guint32 bias_volts;
+    guint32 tunneling_current;
 } IMGFile;
 
 static gboolean      module_register(void);
@@ -65,11 +83,15 @@ static gint          burleigh_detect(const GwyFileDetectInfo *fileinfo,
 static GwyContainer* burleigh_load  (const gchar *filename,
                                      GwyRunType mode,
                                      GError **error);
+static gint16* burleigh_load_v21(IMGFile *imgfile,
+                                 const guchar *buffer,
+                                 gsize size,
+                                 GError **error);
 
 static GwyModuleInfo module_info = {
     GWY_MODULE_ABI_VERSION,
     &module_register,
-    N_("Imports Burleigh IMG data files version 2.x."),
+    N_("Imports Burleigh IMG data files version 2.1."),
     "Yeti <yeti@gwyddion.net>",
     "0.1",
     "David Nečas (Yeti) & Petr Klapetek",
@@ -82,7 +104,7 @@ static gboolean
 module_register(void)
 {
     gwy_file_func_register("burleigh",
-                           N_("Burleigh 2.x files (.img)"),
+                           N_("Burleigh 2.1 files (.img)"),
                            (GwyFileDetectFunc)&burleigh_detect,
                            (GwyFileLoadFunc)&burleigh_load,
                            NULL,
@@ -96,40 +118,29 @@ burleigh_detect(const GwyFileDetectInfo *fileinfo,
                 gboolean only_name)
 {
     const guchar *buffer;
-    gdouble version;
-    guint xres, yres, hsize;
-    gint score = 0;
+    guint version, xres, yres;
 
     if (only_name)
         return g_str_has_suffix(fileinfo->name_lowercase, EXTENSION) ? 10 : 0;
 
-    if (fileinfo->buffer_len < HEADER_SIZE_MIN + 2)
+    if (fileinfo->buffer_len < 4)
         return 0;
 
     buffer = fileinfo->buffer;
-    version = get_FLOAT(&buffer);
-    if (version < 0.0 || version >= 3.0)
+    version = ROUND(10*get_FLOAT(&buffer));
+
+    if (version == 21) {
+        if (fileinfo->file_size < TOTAL_SIZE_V21 + 2)
+            return 0;
+
+        xres = get_WORD(&buffer);
+        yres = get_WORD(&buffer);
+        if (fileinfo->file_size == TOTAL_SIZE_V21 + 2*xres*yres)
+            return 95;
         return 0;
+    }
 
-    if (ROUND(10*version) == 21)
-        hsize = 48;
-    else
-        hsize = get_WORD(&buffer);
-
-    xres = get_WORD(&buffer);
-    yres = get_WORD(&buffer);
-    gwy_debug("version: %g", version);
-    gwy_debug("header size: %u", hsize);
-    gwy_debug("xres: %u", xres);
-    gwy_debug("yres: %u", yres);
-    gwy_debug("file size: %u", fileinfo->file_size);
-    gwy_debug("expected size: %u", 2*xres*yres + hsize);
-    if (hsize >= HEADER_SIZE_MIN
-        && version < 3
-        && fileinfo->file_size == 2*xres*yres + hsize)
-        score = 95;
-
-    return score;
+    return 0;
 }
 
 static GwyContainer*
@@ -161,28 +172,21 @@ burleigh_load(const gchar *filename,
         return NULL;
     }
 
+    memset(&imgfile, 0, sizeof(imgfile));
     p = buffer;
     imgfile.version = get_FLOAT(&p);
-    if (ROUND(10*imgfile.version) == 21)
-        imgfile.header_size = 48;
-    else
-        imgfile.header_size = get_WORD(&p);
-    imgfile.xres = get_WORD(&p);
-    imgfile.yres = get_WORD(&p);
-    imgfile.xrangemax = get_DWORD(&p);
-    imgfile.yrangemax = get_DWORD(&p);
-    imgfile.zrangemax = get_DWORD(&p);
-    imgfile.xrange = get_DWORD(&p);
-    imgfile.yrange = get_DWORD(&p);
-    imgfile.zrange = get_DWORD(&p);
-    imgfile.data_type = get_WORD(&p);
-    imgfile.afm_head_id = get_WORD(&p);
-    imgfile.zoom_factor = get_WORD(&p);
-    imgfile.z_gain = get_WORD(&p);
-    if (size != 2*imgfile.xres*imgfile.yres + imgfile.header_size) {
-        err_SIZE_MISMATCH(error,
-                          2*imgfile.xres*imgfile.yres + imgfile.header_size,
-                          size);
+    imgfile.version_int = ROUND(10*imgfile.version);
+    if (imgfile.version_int == 21) {
+        d = burleigh_load_v21(&imgfile, buffer, size, error);
+        if (!d) {
+            gwy_file_abandon_contents(buffer, size, NULL);
+            return NULL;
+        }
+    }
+    else {
+        g_set_error(error, GWY_MODULE_FILE_ERROR, GWY_MODULE_FILE_ERROR_DATA,
+                    _("File format version %.1f is not supported."),
+                    imgfile.version);
         gwy_file_abandon_contents(buffer, size, NULL);
         return NULL;
     }
@@ -192,8 +196,7 @@ burleigh_load(const gchar *filename,
                                 Angstrom*imgfile.yrange,
                                 FALSE);
     data = gwy_data_field_get_data(dfield);
-    d = (gint16*)(buffer + imgfile.header_size);
-    scale = imgfile.z_gain * imgfile.zrange;
+    scale = Angstrom * imgfile.z_gain * imgfile.zrange;
     for (i = 0; i < imgfile.xres*imgfile.yres; i++)
         data[i] = scale * GINT16_FROM_LE(d[i]);
 
@@ -203,15 +206,66 @@ burleigh_load(const gchar *filename,
     gwy_data_field_set_si_unit_xy(dfield, unit);
     g_object_unref(unit);
 
-    unit = gwy_si_unit_duplicate(unit);
+    container = gwy_container_new();
+    switch (imgfile.data_type) {
+        case BURLEIGH_CURRENT:
+        unit = gwy_si_unit_new("A");
+        gwy_container_set_string_by_name(container, "/0/data/title",
+                                         g_strdup("Current"));
+        break;
+
+        case BURLEIGH_TOPOGRAPHY:
+        unit = gwy_si_unit_new("m");
+        gwy_container_set_string_by_name(container, "/0/data/title",
+                                         g_strdup("Topography"));
+        break;
+
+        default:
+        unit = gwy_si_unit_new("m");
+        break;
+    }
     gwy_data_field_set_si_unit_z(dfield, unit);
     g_object_unref(unit);
 
-    container = gwy_container_new();
     gwy_container_set_object_by_name(container, "/0/data", dfield);
     g_object_unref(dfield);
 
     return container;
+}
+
+static gint16*
+burleigh_load_v21(IMGFile *imgfile,
+                  const guchar *buffer,
+                  gsize size,
+                  GError **error)
+{
+    const guchar *p = buffer + 4; /* size of version */
+    guint32 n;
+
+    /* Header */
+    imgfile->xres = get_WORD(&p);
+    imgfile->yres = get_WORD(&p);
+    n = imgfile->xres * imgfile->yres;
+    if (size != 2*n + TOTAL_SIZE_V21) {
+        err_SIZE_MISMATCH(error, 2*n + TOTAL_SIZE_V21, size);
+        return NULL;
+    }
+    /* Skip to footer */
+    p += 2*n;
+    imgfile->xrangemax = get_DWORD(&p);
+    imgfile->yrangemax = get_DWORD(&p);
+    imgfile->zrangemax = get_DWORD(&p);
+    imgfile->xrange = get_DWORD(&p);
+    imgfile->yrange = get_DWORD(&p);
+    imgfile->zrange = get_DWORD(&p);
+    imgfile->scan_speed = get_WORD(&p);
+    imgfile->zoom_level = get_WORD(&p);
+    imgfile->data_type = get_WORD(&p);
+    imgfile->z_gain = get_WORD(&p);
+    imgfile->bias_volts = get_FLOAT(&p);
+    imgfile->tunneling_current = get_FLOAT(&p);
+
+    return (gint16*)(buffer + HEADER_SIZE_V21);
 }
 
 /* vim: set cin et ts=4 sw=4 cino=>1s,e0,n0,f0,{0,}0,^0,\:1s,=0,g1s,h0,t0,+1s,c3,(0,u0 : */
