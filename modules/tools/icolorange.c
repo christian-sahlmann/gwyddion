5b075f1bfc7435cb425d3f84968ba3fd091a0c3d
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2006-05-03 23:32:04 +0000

    - working on tool logic
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@5918 25697fe4-d644-49b8-9d30-be5465f2adf3

--- modules/tools/icolorange.c
+++ modules/tools/icolorange.c
@@ -54,14 +54,15 @@ struct _GwyToolColorRange {
     GwyGraph *histogram;
     GwyGraphModel *histogram_model;
     GwyDataLine *heightdist;
+    GwySelection *graph_selection;
 
-    GtkWidget *min;
-    GtkWidget *max;
-    GtkWidget *datamin;
-    GtkWidget *datamax;
+    GtkLabel *min;
+    GtkLabel *max;
+    GtkLabel *datamin;
+    GtkLabel *datamax;
 
     ColorRangeSource range_source;
-    gboolean in_update;
+    gboolean programmatic_update;
     GSList *modelist;
 
     GQuark key_min;
@@ -95,8 +96,9 @@ static void   gwy_tool_color_range_set_range_type   (GwyToolColorRange *tool,
                                                      GwyLayerBasicRangeType range_type);
 static void   gwy_tool_color_range_get_min_max      (GwyToolColorRange *tool,
                                                      gdouble *selection);
-static void   gwy_tool_color_range_set_min_max      (GwyToolColorRange *tool,
-                                                     const gdouble *selection);
+static void   gwy_tool_color_range_set_min_max      (GwyToolColorRange *tool);
+static void   gwy_tool_color_range_update_fullrange (GwyToolColorRange *tool);
+static void   gwy_tool_color_range_update_histogram (GwyToolColorRange *tool);
 
 static GwyModuleInfo module_info = {
     GWY_MODULE_ABI_VERSION,
@@ -163,6 +165,8 @@ gwy_tool_color_range_init(GwyToolColorRange *tool)
     if (!tool->layer_type_rect)
         return;
 
+    plain_tool->unit_style = GWY_SI_UNIT_FORMAT_VFMARKUP;
+
     gwy_plain_tool_connect_selection(plain_tool, tool->layer_type_rect,
                                      "rectangle");
 
@@ -195,7 +199,6 @@ gwy_tool_color_range_init_dialog(GwyToolColorRange *tool)
     GtkDialog *dialog;
     GwyGraphCurveModel *cmodel;
     GwyGraphArea *garea;
-    GwySelection *selection;
     GSList *modelist, *l;
     gint row;
 
@@ -225,10 +228,13 @@ gwy_tool_color_range_init_dialog(GwyToolColorRange *tool)
     tool->histogram_model = gwy_graph_model_new();
     gwy_graph_model_add_curve(tool->histogram_model, cmodel);
     tool->histogram = GWY_GRAPH(gwy_graph_new(tool->histogram_model));
+    gwy_graph_set_status(tool->histogram, GWY_GRAPH_STATUS_XSEL);
     garea = GWY_GRAPH_AREA(gwy_graph_get_area(tool->histogram));
-    selection = gwy_graph_area_get_selection(garea, GWY_GRAPH_STATUS_XSEL);
-    gwy_selection_set_max_objects(selection, 1);
-    g_signal_connect(selection, "changed",
+    tool->graph_selection = gwy_graph_area_get_selection(garea,
+                                                         GWY_GRAPH_STATUS_XSEL);
+    g_return_if_fail(GWY_IS_SELECTION(tool->graph_selection));
+    gwy_selection_set_max_objects(tool->graph_selection, 1);
+    g_signal_connect(tool->graph_selection, "changed",
                      G_CALLBACK(gwy_tool_color_range_xsel_changed), tool);
 
     gwy_graph_model_set_label_visible(tool->histogram_model, FALSE);
@@ -236,8 +242,8 @@ gwy_tool_color_range_init_dialog(GwyToolColorRange *tool)
     gwy_graph_set_axis_visible(tool->histogram, GTK_POS_BOTTOM, FALSE);
     gwy_graph_set_axis_visible(tool->histogram, GTK_POS_LEFT, FALSE);
     gwy_graph_set_axis_visible(tool->histogram, GTK_POS_RIGHT, FALSE);
-    gwy_graph_set_status(tool->histogram, GWY_GRAPH_STATUS_XSEL);
     gwy_graph_enable_user_input(tool->histogram, FALSE);
+    gtk_widget_set_sensitive(GTK_WIDGET(tool->histogram), FALSE);
     gtk_box_pack_start(GTK_BOX(dialog->vbox), GTK_WIDGET(tool->histogram),
                        TRUE, TRUE, 2);
 
@@ -248,30 +254,30 @@ gwy_tool_color_range_init_dialog(GwyToolColorRange *tool)
                        FALSE, FALSE, 0);
     row = 0;
 
-    tool->min = gtk_label_new("");
+    tool->min = GTK_LABEL(gtk_label_new(NULL));
     gtk_misc_set_alignment(GTK_MISC(tool->min), 0.0, 0.5);
-    gtk_table_attach(GTK_TABLE(table), tool->min, 0, 1, row, row+1,
-                     GTK_EXPAND | GTK_FILL, 0, 2, 2);
+    gtk_table_attach(GTK_TABLE(table), GTK_WIDGET(tool->min),
+                     0, 1, row, row+1, GTK_EXPAND | GTK_FILL, 0, 2, 2);
 
-    tool->max = gtk_label_new("");
+    tool->max = GTK_LABEL(gtk_label_new(NULL));
     gtk_misc_set_alignment(GTK_MISC(tool->max), 1.0, 0.5);
-    gtk_table_attach(GTK_TABLE(table), tool->max, 3, 4, row, row+1,
-                     GTK_EXPAND | GTK_FILL, 0, 2, 2);
+    gtk_table_attach(GTK_TABLE(table), GTK_WIDGET(tool->max),
+                     3, 4, row, row+1, GTK_EXPAND | GTK_FILL, 0, 2, 2);
 
     label = gtk_label_new(_("Range"));
     gtk_table_attach(GTK_TABLE(table), label, 1, 3, row, row+1,
                      GTK_EXPAND | GTK_FILL, 0, 2, 2);
     row++;
 
-    tool->datamin = gtk_label_new("");
+    tool->datamin = GTK_LABEL(gtk_label_new(NULL));
     gtk_misc_set_alignment(GTK_MISC(tool->datamin), 0.0, 0.5);
-    gtk_table_attach(GTK_TABLE(table), tool->datamin, 0, 1, row, row+1,
-                     GTK_EXPAND | GTK_FILL, 0, 2, 2);
+    gtk_table_attach(GTK_TABLE(table), GTK_WIDGET(tool->datamin),
+                     0, 1, row, row+1, GTK_EXPAND | GTK_FILL, 0, 2, 2);
 
-    tool->datamax = gtk_label_new("");
+    tool->datamax = GTK_LABEL(gtk_label_new(NULL));
     gtk_misc_set_alignment(GTK_MISC(tool->datamax), 1.0, 0.5);
-    gtk_table_attach(GTK_TABLE(table), tool->datamax, 3, 4, row, row+1,
-                     GTK_EXPAND | GTK_FILL, 0, 2, 2);
+    gtk_table_attach(GTK_TABLE(table), GTK_WIDGET(tool->datamax),
+                     3, 4, row, row+1, GTK_EXPAND | GTK_FILL, 0, 2, 2);
 
     label = gtk_label_new(_("Full"));
     gtk_table_attach(GTK_TABLE(table), label, 1, 3, row, row+1,
@@ -329,9 +335,14 @@ gwy_tool_color_range_data_switched(GwyTool *gwytool,
         tool->key_min = tool->key_max = 0;
     }
 
+    /* TODO: make sure we do update when switching to a FIXED data (even from
+     * another FIXED) */
     tool = GWY_TOOL_COLOR_RANGE(gwytool);
     range_type = gwy_tool_color_range_get_range_type(tool);
     gwy_radio_buttons_set_current(tool->modelist, "range-type", range_type);
+    gwy_tool_color_range_update_histogram(tool);
+    gwy_tool_color_range_update_fullrange(tool);
+    gwy_tool_color_range_set_min_max(tool);
 }
 
 static void
@@ -341,6 +352,7 @@ gwy_tool_color_range_data_changed(GwyPlainTool *plain_tool)
                                    plain_tool->selection,
                                    plain_tool->data_field,
                                    NULL, NULL);
+    gwy_tool_color_range_update_histogram(GWY_TOOL_COLOR_RANGE(plain_tool));
     gwy_tool_color_range_selection_changed(plain_tool, 0);
 }
 
@@ -350,14 +362,14 @@ gwy_tool_color_range_selection_changed(GwyPlainTool *plain_tool,
 {
     GwyToolColorRange *tool;
     GwyLayerBasicRangeType range_type;
-    gint n = 0;
+    gboolean is_selected = FALSE;
+    gdouble range[4];
 
     tool = GWY_TOOL_COLOR_RANGE(plain_tool);
     g_return_if_fail(hint <= 0);
 
     if (plain_tool->selection) {
-        n = gwy_selection_get_data(plain_tool->selection, NULL);
-        g_return_if_fail(n == 0 || n == 1);
+        is_selected = gwy_selection_get_data(plain_tool->selection, range);
         gwy_rect_selection_labels_fill(tool->rlabels,
                                        plain_tool->selection,
                                        plain_tool->data_field,
@@ -370,13 +382,24 @@ gwy_tool_color_range_selection_changed(GwyPlainTool *plain_tool,
     if (range_type != GWY_LAYER_BASIC_RANGE_FIXED)
         return;
 
-    /*
-    gwy_data_field_area_get_min_max(dfield,
-                                    isel[0], isel[1],
-                                    isel[2] - isel[0],
-                                    isel[3] - isel[1],
-                                    &selection[0], &selection[1]);
-                                    */
+    if (!tool->programmatic_update)
+        tool->range_source = USE_SELECTION;
+
+    gwy_tool_color_range_set_min_max(tool);
+    if (!tool->programmatic_update) {
+        tool->programmatic_update = TRUE;
+        if (is_selected) {
+            gwy_tool_color_range_get_min_max(tool, range);
+            /* XXX XXX XXX */
+            range[2] = range[1];
+            range[1] = 0.0;
+            range[3] = 1000.0;
+            gwy_selection_set_object(tool->graph_selection, 0, range);
+        }
+        else
+            gwy_selection_clear(tool->graph_selection);
+        tool->programmatic_update = FALSE;
+    }
 }
 
 static void
@@ -384,23 +407,32 @@ gwy_tool_color_range_xsel_changed(GwySelection *selection,
                                   gint hint,
                                   GwyToolColorRange *tool)
 {
-    gdouble range[2];
-
     g_return_if_fail(hint <= 0);
 
-    if (gwy_selection_get_object(selection, 0, range))
-        gwy_tool_color_range_set_min_max(tool, range);
-    else
+    if (tool->programmatic_update)
+        return;
+
+    if (gwy_selection_get_data(selection, NULL)) {
+        tool->range_source = USE_HISTOGRAM;
+        gwy_tool_color_range_set_min_max(tool);
+    }
+    else {
+        tool->range_source = USE_SELECTION;
+        tool->programmatic_update = TRUE;
         gwy_tool_color_range_selection_changed(GWY_PLAIN_TOOL(tool), -1);
+        tool->programmatic_update = FALSE;
+    }
 }
 
+/* TODO: this is not enough, we need to restore range from container.
+ * add USE_CONTAINER source type? */
 static void
 gwy_tool_color_range_type_changed(GObject *item,
                                   GwyToolColorRange *tool)
 {
     GwyLayerBasicRangeType range_type, old_mode;
     GwyPlainTool *plain_tool;
-    gboolean sensitive;
+    gboolean fixed;
 
     plain_tool = GWY_PLAIN_TOOL(tool);
     if (!plain_tool->container)
@@ -411,10 +443,12 @@ gwy_tool_color_range_type_changed(GObject *item,
     if (old_mode == range_type)
         return;
 
-    sensitive = (range_type == GWY_LAYER_BASIC_RANGE_FIXED);
-    gtk_widget_set_sensitive(GTK_WIDGET(tool->histogram), sensitive);
+    fixed = (range_type == GWY_LAYER_BASIC_RANGE_FIXED);
+    gtk_widget_set_sensitive(GTK_WIDGET(tool->histogram), fixed);
 
     gwy_tool_color_range_set_range_type(tool, range_type);
+    if (fixed)
+        gwy_tool_color_range_set_min_max(tool);
 }
 
 static GwyLayerBasicRangeType
@@ -482,24 +516,124 @@ gwy_tool_color_range_get_min_max(GwyToolColorRange *tool,
 }
 
 static void
-gwy_tool_color_range_set_min_max(GwyToolColorRange *tool,
-                                 const gdouble *selection)
+gwy_tool_color_range_set_min_max(GwyToolColorRange *tool)
 {
     GwyPlainTool *plain_tool;
+    const GwySIValueFormat *vf;
+    gboolean clear = FALSE;
+    gchar buf[40];
+    gdouble sel[4];
+    gint isel[4];
 
     plain_tool = GWY_PLAIN_TOOL(tool);
-    g_return_if_fail(plain_tool->container);
-
-    if (selection) {
-        gwy_container_set_double(plain_tool->container,
-                                 tool->key_min, selection[0]);
-        gwy_container_set_double(plain_tool->container,
-                                 tool->key_max, selection[1]);
+    if (!plain_tool->container) {
+        gtk_label_set_text(tool->min, NULL);
+        gtk_label_set_text(tool->max, NULL);
+        return;
     }
-    else {
+
+    switch (tool->range_source) {
+        case USE_SELECTION:
+        if (!plain_tool->selection
+            || !gwy_selection_get_object(plain_tool->selection, 0, sel)
+            || sel[0] == sel[2] || sel[1] == sel[3]) {
+            clear = TRUE;
+            break;
+        }
+        isel[0] = gwy_data_field_rtoj(plain_tool->data_field, sel[0]);
+        isel[1] = gwy_data_field_rtoi(plain_tool->data_field, sel[1]);
+        isel[2] = gwy_data_field_rtoj(plain_tool->data_field, sel[2]) + 1;
+        isel[3] = gwy_data_field_rtoi(plain_tool->data_field, sel[3]) + 1;
+
+        gwy_data_field_area_get_min_max(plain_tool->data_field,
+                                        MIN(isel[0], isel[2]),
+                                        MIN(isel[1], isel[3]),
+                                        ABS(isel[2] - isel[0]),
+                                        ABS(isel[3] - isel[1]),
+                                        &sel[0], &sel[1]);
+        break;
+
+        case USE_HISTOGRAM:
+        /* XXX XXX XXX */
+        if (!gwy_selection_get_object(tool->graph_selection, 0, sel)
+            || sel[0] == sel[2])
+            clear = TRUE;
+        else
+            sel[1] = sel[2];
+        break;
+
+        default:
+        g_return_if_reached();
+        break;
+    }
+
+    if (clear) {
         gwy_container_remove(plain_tool->container, tool->key_min);
         gwy_container_remove(plain_tool->container, tool->key_max);
+        gwy_data_field_get_min_max(plain_tool->data_field, &sel[0], &sel[1]);
     }
+    else {
+        gwy_container_set_double(plain_tool->container, tool->key_min, sel[0]);
+        gwy_container_set_double(plain_tool->container, tool->key_max, sel[1]);
+    }
+
+    vf = plain_tool->value_format;
+    g_snprintf(buf, sizeof(buf), "%.*f%s%s",
+               vf->precision, sel[0]/vf->magnitude,
+               *vf->units ? " " : "", vf->units);
+    gtk_label_set_markup(tool->min, buf);
+    g_snprintf(buf, sizeof(buf), "%.*f%s%s",
+               vf->precision, sel[1]/vf->magnitude,
+               *vf->units ? " " : "", vf->units);
+    gtk_label_set_markup(tool->max, buf);
+}
+
+static void
+gwy_tool_color_range_update_fullrange(GwyToolColorRange *tool)
+{
+    GwyPlainTool *plain_tool;
+    const GwySIValueFormat *vf;
+    gdouble min, max;
+    gchar buf[40];
+
+    plain_tool = GWY_PLAIN_TOOL(tool);
+    if (!plain_tool->container) {
+        gtk_label_set_text(tool->datamin, NULL);
+        gtk_label_set_text(tool->datamax, NULL);
+        return;
+    }
+
+    gwy_data_field_get_min_max(plain_tool->data_field, &min, &max);
+
+    vf = plain_tool->value_format;
+    g_snprintf(buf, sizeof(buf), "%.*f%s%s",
+               vf->precision, min/vf->magnitude,
+               *vf->units ? " " : "", vf->units);
+    gtk_label_set_markup(tool->datamin, buf);
+    g_snprintf(buf, sizeof(buf), "%.*f%s%s",
+               vf->precision, max/vf->magnitude,
+               *vf->units ? " " : "", vf->units);
+    gtk_label_set_markup(tool->datamax, buf);
+}
+
+static void
+gwy_tool_color_range_update_histogram(GwyToolColorRange *tool)
+{
+    GwyPlainTool *plain_tool;
+    GwyGraphCurveModel *cmodel;
+
+    plain_tool = GWY_PLAIN_TOOL(tool);
+    cmodel = gwy_graph_model_get_curve_by_index(tool->histogram_model, 0);
+    if (!plain_tool->data_field) {
+        gdouble data[2] = { 0.0, 0.0 };
+
+        gwy_graph_curve_model_set_data(cmodel, data, data, G_N_ELEMENTS(data));
+        return;
+    }
+
+    gwy_data_field_dh(plain_tool->data_field, tool->heightdist, 0);
+    gwy_graph_curve_model_set_data_from_dataline(cmodel, tool->heightdist,
+                                                 0, 0);
 }
 
 #if 0
