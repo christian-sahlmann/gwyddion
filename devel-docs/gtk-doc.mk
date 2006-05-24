b4d2908829f41f2fc016b0a5b7ef841eacced273
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2006-05-24 19:59:38 +0000

    - made gtk-doc.mk more similar to the current official gtk-doc.make - moved a few common bits from Makefile.am to gtk-doc.mk
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@6050 25697fe4-d644-49b8-9d30-be5465f2adf3

--- devel-docs/gtk-doc.mk
+++ devel-docs/gtk-doc.mk
@@ -3,33 +3,53 @@
 # Included in all API-docs Makefile.am's, if you change anything here, it's
 # affects all subdirs.
 
+if GTK_DOC_USE_LIBTOOL
+GTKDOC_CC = $(LIBTOOL) --mode=compile $(CC) $(INCLUDES) $(AM_CFLAGS) $(CFLAGS)
+GTKDOC_LD = $(LIBTOOL) --mode=link $(CC) $(AM_CFLAGS) $(CFLAGS) $(LDFLAGS)
+else
+GTKDOC_CC = $(CC) $(INCLUDES) $(AM_CFLAGS) $(CFLAGS)
+GTKDOC_LD = $(CC) $(AM_CFLAGS) $(CFLAGS) $(LDFLAGS)
+endif
+
+# We set GPATH here; this gives us semantics for GNU make
+# which are more like other make's VPATH, when it comes to
+# whether a source that is a target of one rule is then
+# searched for in VPATH/GPATH.
+#
+GPATH = $(srcdir)
+
 TARGET_DIR=$(HTML_DIR)/$(DOC_MODULE)
 
 EXTRA_DIST = \
 	$(content_files) \
-	$(extra_files) \
+	makefile.msc \
 	$(HTML_IMAGES) \
 	$(DOC_MAIN_SGML_FILE) \
 	$(DOC_MODULE)-sections.txt \
 	$(DOC_MODULE)-decl.txt \
 	$(DOC_MODULE).types \
-	$(DOC_OVERRIDES)
+	$(DOC_MODULE)-overrides.txt
 
 DOC_STAMPS = \
 	scan-build.stamp \
 	template-build.stamp \
-	sgml-build.stamp \
+	xml-build.stamp \
 	html-build.stamp \
 	$(srcdir)/template.stamp \
-	$(srcdir)/sgml.stamp \
+	$(srcdir)/xml.stamp \
 	$(srcdir)/html.stamp
 
 SCANOBJ_FILES = \
 	$(DOC_MODULE).args \
 	$(DOC_MODULE).hierarchy \
-	$(DOC_MODULE).signals \
+	$(DOC_MODULE).interfaces \
 	$(DOC_MODULE).prerequisites \
-	$(DOC_MODULE).interfaces
+	$(DOC_MODULE).signals
+
+CLEANFILES = $(SCANOBJ_FILES) $(DOC_MODULE)-unused.txt $(DOC_STAMPS)
+
+HFILE_GLOB = $(DOC_SOURCE_DIR)/*.h
+CFILE_GLOB = $(DOC_SOURCE_DIR)/*.c
 
 if ENABLE_GTK_DOC
 # XXX: Uncomment following line to rebuild docs automatically
@@ -39,14 +59,15 @@ docs: html-build.stamp
 
 #### scan ####
 
-scan-build.stamp: $(HFILE_GLOB)
-	@echo '*** Scanning header files ***'
+scan-build.stamp: $(HFILE_GLOB) $(CFILE_GLOB)
+	@echo 'gtk-doc: Scanning header files'
+	@-chmod -R u+w $(srcdir)
 	if test "x$(TYPES_INCLUDE)" != x; then \
 	    echo "$(TYPES_INCLUDE)"; \
 	    IGNORE_HFILES="$(IGNORE_HFILES)" $(top_srcdir)/devel-docs/extract-types.py $(HFILE_GLOB); \
 	fi >$(srcdir)/$(DOC_MODULE).types
-	if test -s $(srcdir)/$(DOC_MODULE).types; then \
-	    CC="$(GTKDOC_CC)" LD="$(GTKDOC_LD)" CFLAGS="$(GTKDOC_CFLAGS)" LDFLAGS="$(GTKDOC_LIBS)" gtkdoc-scangobj $(SCANOBJ_OPTIONS) --module=$(DOC_MODULE) --output-dir=$(srcdir); \
+	if grep -l '^..*$$' $(srcdir)/$(DOC_MODULE).types >/dev/null 2>&1 ; then \
+	    CC="$(GTKDOC_CC)" LD="$(GTKDOC_LD)" CFLAGS="$(GTKDOC_CFLAGS)" LDFLAGS="$(GTKDOC_LIBS)" gtkdoc-scangobj $(SCANGOBJ_OPTIONS) --module=$(DOC_MODULE) --output-dir=$(srcdir); \
 	else \
 	    cd $(srcdir) ; \
 	    for i in $(SCANOBJ_FILES); do \
@@ -65,95 +86,76 @@ $(DOC_MODULE)-decl.txt $(SCANOBJ_FILES): scan-build.stamp
 
 #### templates ####
 
-template-build.stamp: $(DOC_MODULE)-decl.txt $(SCANOBJ_FILES) $(DOC_MODULE)-sections.txt $(DOC_OVERRIDES)
-	@echo '*** Rebuilding template files ***'
-	cd $(srcdir) && gtkdoc-mktmpl --module=$(DOC_MODULE) --output-dir=$(srcdir)/template
-	if test "x$(REMOVE_SECTION_TITLES)" = "xyes"; then \
-		for i in $(srcdir)/template/*.sgml; do \
-			sed '2s/.*//' "$$i" >$(DOC_MODULE).rstmpl; \
-			if diff "$$i" $(DOC_MODULE).rstmpl >/dev/null 2>&1; then :; else \
-				cat $(DOC_MODULE).rstmpl >"$$i"; \
-			fi; \
-		done; \
-		rm -f $(DOC_MODULE).rstmpl; \
-	fi; \
+template-build.stamp: $(DOC_MODULE)-decl.txt $(SCANOBJ_FILES) $(DOC_MODULE)-sections.txt $(DOC_MODULE)-overrides.txt
+	@echo 'gtk-doc: Rebuilding template files'
+	@-chmod -R u+w $(srcdir)
+	cd $(srcdir) && gtkdoc-mktmpl --module=$(DOC_MODULE) --output-dir=$(srcdir)/template $(MKTMPL_OPTIONS)
+	for i in $(srcdir)/template/*.sgml; do \
+	  sed '2s/.*//' "$$i" >$(DOC_MODULE).rstmpl; \
+	    if diff "$$i" $(DOC_MODULE).rstmpl >/dev/null 2>&1; then :; else \
+	      cat $(DOC_MODULE).rstmpl >"$$i"; \
+	    fi; \
+	  done; \
+	rm -f $(DOC_MODULE).rstmpl; \
 	touch template-build.stamp
 
 template.stamp: template-build.stamp
 	@true
 
-#### sgml ####
+#### xml ####
 
-sgml-build.stamp: template.stamp $(CFILE_GLOB) $(srcdir)/template/*.sgml
-	@echo '*** Building SGML ***'
+xml-build.stamp: template.stamp $(CFILE_GLOB) $(srcdir)/template/*.sgml $(expand_content_files)
+	@echo 'gtk-doc: Building XML'
+	@-chmod -R u+w $(srcdir)
 	cd $(srcdir) && \
-	gtkdoc-mkdb --module=$(DOC_MODULE) --tmpl-dir=$(srcdir)/template --source-dir=$(DOC_SOURCE_DIR) --sgml-mode --output-format=xml $(MKDB_OPTIONS)
-	touch sgml-build.stamp
+	gtkdoc-mkdb --module=$(DOC_MODULE) --tmpl-dir=$(srcdir)/template --source-dir=$(DOC_SOURCE_DIR) --sgml-mode --output-format=xml --expand-content-files="$(expand_content_files)" --main-sgml-file=$(DOC_MAIN_SGML_FILE) $(MKDB_OPTIONS)
+	touch xml-build.stamp
 
-sgml.stamp: sgml-build.stamp
+xml.stamp: xml-build.stamp
 	@true
 
 #### html ####
 
-html-build.stamp: sgml.stamp $(DOC_MAIN_SGML_FILE) $(content_files)
-	@echo '*** Building HTML ***'
+html-build.stamp: xml.stamp $(DOC_MAIN_SGML_FILE) $(content_files)
+	@echo 'gtk-doc: Building HTML'
+	@-chmod -R u+w $(srcdir)
 	rm -rf $(srcdir)/html
 	mkdir $(srcdir)/html
 	cd $(srcdir)/html && gtkdoc-mkhtml $(DOC_MODULE) ../$(DOC_MAIN_SGML_FILE)
 	test "x$(HTML_IMAGES)" = "x" || ( cd $(srcdir) && cp $(HTML_IMAGES) html )
-	@echo '-- Fixing Crossreferences' 
+	@echo 'gtk-doc: Fixing cross-references'
 	cd $(srcdir) && gtkdoc-fixxref --module-dir=html --html-dir=$(HTML_DIR) $(FIXXREF_OPTIONS)
 	touch html-build.stamp
+else
+docs:
 endif
 
 ##############
 
 clean-local:
-	rm -f *~ *.bak $(SCANOBJ_FILES) *-unused.txt $(DOC_STAMPS)
+	rm -f *~ *.bak
+	rm -rf .libs
 
 maintainer-clean-local: clean
-	cd $(srcdir) && rm -rf xml html $(DOC_MODULE)-decl-list.txt $(DOC_MODULE)-decl.txt $(DOC_MODULE).types $(DOC_MODULE)-sections.txt
+	cd $(srcdir) && rm -rf xml html $(DOC_MODULE)-decl-list.txt $(DOC_MODULE)-decl.txt
+	cd $(srcdir) && rm -rf $(DOC_MODULE).types $(DOC_MODULE)-sections.txt
 
 install-data-local:
-	$(mkdir_p) $(DESTDIR)$(TARGET_DIR)
-	(installfiles=`echo $(srcdir)/html/*.html`; \
-	if test "$$installfiles" = '$(srcdir)/html/*.html'; \
-	then echo '-- Nothing to install' ; \
+	installfiles=`echo $(srcdir)/html/*`; \
+	if test "$$installfiles" = '$(srcdir)/html/*'; \
+	then echo 'gtk-doc: Nothing to install' ; \
 	else \
+	  $(mkdir_p) $(DESTDIR)$(TARGET_DIR); \
 	  for i in $$installfiles; do \
-	    echo '-- Installing '$$i ; \
+	    echo 'gtk-doc: Installing '$$i ; \
 	    $(INSTALL_DATA) $$i $(DESTDIR)$(TARGET_DIR); \
 	  done; \
 	  echo '-- Installing $(srcdir)/html/index.sgml' ; \
-	  $(INSTALL_DATA) $(srcdir)/html/index.sgml $(DESTDIR)$(TARGET_DIR); \
-	fi)
-	(installfiles=`echo $(srcdir)/html/*.png`; \
-	if test "$$installfiles" != '$(srcdir)/html/*.png'; then \
-	  for i in $$installfiles; do \
-	    echo '-- Installing '$$i ; \
-	    $(INSTALL_DATA) $$i $(DESTDIR)$(TARGET_DIR); \
-	  done; \
-	fi)
+	  $(INSTALL_DATA) $(srcdir)/html/index.sgml $(DESTDIR)$(TARGET_DIR) || :; \
+	fi
 
 uninstall-local:
-	(installfiles=`cd $(srcdir)/html >/dev/null && echo *.html`; \
-	if test "$$installfiles" = '*.html'; \
-	then echo '-- Nothing to uninstall' ; \
-	else \
-	  for i in $$installfiles; do \
-	    echo '-- Removing '$$i ; \
-	    rm -f $(DESTDIR)$(TARGET_DIR)/$$i; \
-	  done; \
-	  echo '-- Removing index.sgml' ; \
-	  rm -f $(DESTDIR)$(TARGET_DIR)/index.sgml; \
-	fi)
-	(installfiles=`cd $(srcdir)/html >/dev/null && echo *.png`; \
-	if test "$$installfiles" != '*.png'; then \
-	  for i in $$installfiles; do \
-	    echo '-- Removing '$$i ; \
-	    rm -f $(DESTDIR)$(TARGET_DIR)/$$i; \
-	  done; \
-	fi)
+	rm -f $(DESTDIR)$(TARGET_DIR)/*
 
 #
 # Require gtk-doc when making dist
@@ -172,9 +174,9 @@ dist-hook: dist-check-gtkdoc dist-hook-local
 	mkdir $(distdir)/html
 	-cp $(srcdir)/template/*.sgml $(distdir)/template
 	-cp $(srcdir)/xml/*.xml $(distdir)/xml
-	-cp $(srcdir)/html/index.sgml $(distdir)/html
-	-cp $(srcdir)/html/*.html $(srcdir)/html/*.css $(distdir)/html
-	-cp $(srcdir)/html/*.png $(distdir)/html
+	-cp $(srcdir)/html/* $(distdir)/html
+	if test -f $(srcdir)/$(DOC_MODULE).types; then \
+	  cp $(srcdir)/$(DOC_MODULE).types $(distdir)/$(DOC_MODULE).types; \
+	fi
 
-.PHONY : dist-hook-local
-
+.PHONY: docs dist-hook-local
