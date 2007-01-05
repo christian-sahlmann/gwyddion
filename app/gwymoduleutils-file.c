fff516cda489219777bdfb7646e61083f7447603
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2007-01-05 14:14:21 +0000

    - include "config.h"
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@7300 25697fe4-d644-49b8-9d30-be5465f2adf3

--- app/gwymoduleutils-file.c
+++ app/gwymoduleutils-file.c
@@ -18,6 +18,7 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111 USA
  */
 
+#include "config.h"
 #include <libprocess/datafield.h>
 #include <app/data-browser.h>
 #include <app/gwymoduleutils-file.h>
