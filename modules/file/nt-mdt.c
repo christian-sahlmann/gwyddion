dfc5b9379aba970bfb0794b54007bb7b48897439
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2007-01-05 12:57:49 +0000

    - using the new binary data getter functions
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@7298 25697fe4-d644-49b8-9d30-be5465f2adf3

--- modules/file/nt-mdt.c
+++ modules/file/nt-mdt.c
@@ -451,9 +451,9 @@ mdt_read_axis_scales(const guchar *p,
                      MDTAxisScale *y_scale,
                      MDTAxisScale *z_scale)
 {
-    x_scale->offset = get_FLOAT_LE(&p);
-    x_scale->step = get_FLOAT_LE(&p);
-    x_scale->unit = (gint16)get_WORD_LE(&p);
+    x_scale->offset = gwy_get_gfloat_le(&p);
+    x_scale->step = gwy_get_gfloat_le(&p);
+    x_scale->unit = (gint16)gwy_get_guint16_le(&p);
     gwy_debug("x: *%g +%g [%d:%s]",
               x_scale->step, x_scale->offset, x_scale->unit,
               gwy_enum_to_string(x_scale->unit,
@@ -464,9 +464,9 @@ mdt_read_axis_scales(const guchar *p,
         x_scale->step = 1.0;
     }
 
-    y_scale->offset = get_FLOAT_LE(&p);
-    y_scale->step = get_FLOAT_LE(&p);
-    y_scale->unit = (gint16)get_WORD_LE(&p);
+    y_scale->offset = gwy_get_gfloat_le(&p);
+    y_scale->step = gwy_get_gfloat_le(&p);
+    y_scale->unit = (gint16)gwy_get_guint16_le(&p);
     gwy_debug("y: *%g +%g [%d:%s]",
               y_scale->step, y_scale->offset, y_scale->unit,
               gwy_enum_to_string(y_scale->unit,
@@ -477,9 +477,9 @@ mdt_read_axis_scales(const guchar *p,
         y_scale->step = 1.0;
     }
 
-    z_scale->offset = get_FLOAT_LE(&p);
-    z_scale->step = get_FLOAT_LE(&p);
-    z_scale->unit = (gint16)get_WORD_LE(&p);
+    z_scale->offset = gwy_get_gfloat_le(&p);
+    z_scale->step = gwy_get_gfloat_le(&p);
+    z_scale->unit = (gint16)gwy_get_guint16_le(&p);
     gwy_debug("z: *%g +%g [%d:%s]",
               z_scale->step, z_scale->offset, z_scale->unit,
               gwy_enum_to_string(z_scale->unit,
@@ -503,26 +503,26 @@ mdt_scanned_data_vars(const guchar *p,
 
     frame->channel_index = (gint)(*p++);
     frame->mode = (gint)(*p++);
-    frame->xres = get_WORD_LE(&p);
-    frame->yres = get_WORD_LE(&p);
+    frame->xres = gwy_get_guint16_le(&p);
+    frame->yres = gwy_get_guint16_le(&p);
     gwy_debug("channel_index = %d, mode = %d, xres = %d, yres = %d",
               frame->channel_index, frame->mode, frame->xres, frame->yres);
-    frame->ndacq = get_WORD_LE(&p);
-    frame->step_length = Angstrom*get_FLOAT_LE(&p);
-    frame->adt = get_WORD_LE(&p);
+    frame->ndacq = gwy_get_guint16_le(&p);
+    frame->step_length = Angstrom*gwy_get_gfloat_le(&p);
+    frame->adt = gwy_get_guint16_le(&p);
     frame->adc_gain_amp_log10 = (guint)(*p++);
     frame->adc_index = (guint)(*p++);
     frame->s16.version = (guint)(*p++);
     frame->s17.pass_num = (guint)(*p++);
     frame->scan_dir = (guint)(*p++);
     frame->power_of_2 = (gboolean)(*p++);
-    frame->velocity = Angstrom*get_FLOAT_LE(&p);
-    frame->setpoint = Nano*get_FLOAT_LE(&p);
-    frame->bias_voltage = get_FLOAT_LE(&p);
+    frame->velocity = Angstrom*gwy_get_gfloat_le(&p);
+    frame->setpoint = Nano*gwy_get_gfloat_le(&p);
+    frame->bias_voltage = gwy_get_gfloat_le(&p);
     frame->draw = (gboolean)(*p++);
     p++;
-    frame->xoff = get_DWORD_LE(&p);  /* FIXME: sign? */
-    frame->yoff = get_DWORD_LE(&p);  /* FIXME: sign? */
+    frame->xoff = gwy_get_gint32_le(&p);  /* FIXME: sign? */
+    frame->yoff = gwy_get_gint32_le(&p);  /* FIXME: sign? */
     frame->nl_corr = (gboolean)(*p++);
 
     p = fstart + FRAME_HEADER_SIZE + vars_size;
@@ -531,10 +531,10 @@ mdt_scanned_data_vars(const guchar *p,
                     _("Frame is too short for Frame Mode."));
         return FALSE;
     }
-    frame->fm_mode = get_WORD_LE(&p);
-    frame->fm_xres = get_WORD_LE(&p);
-    frame->fm_yres = get_WORD_LE(&p);
-    frame->fm_ndots = get_WORD_LE(&p);
+    frame->fm_mode = gwy_get_guint16_le(&p);
+    frame->fm_xres = gwy_get_guint16_le(&p);
+    frame->fm_yres = gwy_get_guint16_le(&p);
+    frame->fm_ndots = gwy_get_guint16_le(&p);
     gwy_debug("mode = %u, xres = %u, yres = %u, ndots = %u",
               frame->fm_mode, frame->fm_xres, frame->fm_yres, frame->fm_ndots);
 
@@ -559,7 +559,7 @@ mdt_scanned_data_vars(const guchar *p,
 
     /* Title */
     if ((frame_size - (p - fstart)) > 4) {
-        frame->title_len = get_DWORD_LE(&p);
+        frame->title_len = gwy_get_guint32_le(&p);
         if (frame->title_len
             && (guint)(frame_size - (p - fstart)) >= frame->title_len) {
             frame->title = p;
@@ -603,10 +603,10 @@ mdt_real_load(const guchar *buffer,
         return FALSE;
     }
     p = buffer + 4;  /* magic header */
-    mdtfile->size = get_DWORD_LE(&p);
+    mdtfile->size = gwy_get_guint32_le(&p);
     gwy_debug("File size (w/o header): %u", mdtfile->size);
     p += 4;  /* reserved */
-    mdtfile->last_frame = get_WORD_LE(&p);
+    mdtfile->last_frame = gwy_get_guint16_le(&p);
     gwy_debug("Last frame: %u", mdtfile->last_frame);
     p += 18;  /* reserved */
     /* XXX: documentation specifies 32 bytes long header, but zeroth frame
@@ -630,7 +630,7 @@ mdt_real_load(const guchar *buffer,
                         _("End of file reached in frame header #%u."), i);
             return FALSE;
         }
-        frame->size = get_DWORD_LE(&p);
+        frame->size = gwy_get_guint32_le(&p);
         gwy_debug("Frame #%u size: %u", i, frame->size);
         if ((guint)(p - buffer) + frame->size - 4 > size) {
             g_set_error(error, GWY_MODULE_FILE_ERROR,
@@ -638,7 +638,7 @@ mdt_real_load(const guchar *buffer,
                         _("End of file reached in frame data #%u."), i);
             return FALSE;
         }
-        frame->type = get_WORD_LE(&p);
+        frame->type = gwy_get_guint16_le(&p);
         gwy_debug("Frame #%u type: %s", i,
                   gwy_enum_to_string(frame->type,
                                      frame_types, G_N_ELEMENTS(frame_types)));
@@ -646,16 +646,16 @@ mdt_real_load(const guchar *buffer,
         p += 2;
         gwy_debug("Frame #%u version: %d.%d",
                   i, frame->version/0x100, frame->version % 0x100);
-        frame->year = get_WORD_LE(&p);
-        frame->month = get_WORD_LE(&p);
-        frame->day = get_WORD_LE(&p);
-        frame->hour = get_WORD_LE(&p);
-        frame->min = get_WORD_LE(&p);
-        frame->sec = get_WORD_LE(&p);
+        frame->year = gwy_get_guint16_le(&p);
+        frame->month = gwy_get_guint16_le(&p);
+        frame->day = gwy_get_guint16_le(&p);
+        frame->hour = gwy_get_guint16_le(&p);
+        frame->min = gwy_get_guint16_le(&p);
+        frame->sec = gwy_get_guint16_le(&p);
         gwy_debug("Frame #%u datetime: %d-%02d-%02d %02d:%02d:%02d",
                   i, frame->year, frame->month, frame->day,
                   frame->hour, frame->min, frame->sec);
-        frame->var_size = get_WORD_LE(&p);
+        frame->var_size = gwy_get_guint16_le(&p);
         gwy_debug("Frame #%u var size: %u", i, frame->var_size);
         if (frame->var_size + FRAME_HEADER_SIZE > frame->size) {
             err_SIZE_MISMATCH(error, frame->var_size + FRAME_HEADER_SIZE,
