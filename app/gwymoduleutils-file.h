9cdb81d6b22b34fe18b4ddb401548e0b869bf11b
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2007-01-05 12:57:27 +0000

    - generated expanded versions of the getter functions with gcc -E
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@7297 25697fe4-d644-49b8-9d30-be5465f2adf3

--- app/gwymoduleutils-file.h
+++ app/gwymoduleutils-file.h
@@ -25,34 +25,149 @@
 
 G_BEGIN_DECLS
 
-#define GWY_DEFINE_BUFFER_GETTER(lt, ut, mt, lb, ub) \
-    static inline lt \
-    gwy_get_ ## lt ## _ ## lb(const guchar **ppv) \
-    { \
-        const mt *pv = *(const mt**)ppv; \
-        mt v = ut ## _FROM_ ## ub(*pv); \
-        *ppv += sizeof(mt); \
-        return *(lt*)&v; \
-    }
+static inline gint16
+gwy_get_gint16_le(const guchar **ppv)
+{
+    const gint16 *pv = *(const gint16**)ppv;
+    gint16 v = GINT16_FROM_LE(*pv);
+    *ppv += sizeof(gint16);
+    return v;
+}
 
-GWY_DEFINE_BUFFER_GETTER(gint16,  GINT16,  gint16,  le, LE)
-GWY_DEFINE_BUFFER_GETTER(gint16,  GINT16,  gint16,  be, BE)
-GWY_DEFINE_BUFFER_GETTER(guint16, GUINT16, guint16, le, LE)
-GWY_DEFINE_BUFFER_GETTER(guint16, GUINT16, guint16, be, BE)
-GWY_DEFINE_BUFFER_GETTER(gint32,  GINT32,  gint32,  le, LE)
-GWY_DEFINE_BUFFER_GETTER(gint32,  GINT32,  gint32,  be, BE)
-GWY_DEFINE_BUFFER_GETTER(guint32, GUINT32, guint32, le, LE)
-GWY_DEFINE_BUFFER_GETTER(guint32, GUINT32, guint32, be, BE)
-GWY_DEFINE_BUFFER_GETTER(gint64,  GINT64,  gint64,  le, LE)
-GWY_DEFINE_BUFFER_GETTER(gint64,  GINT64,  gint64,  be, BE)
-GWY_DEFINE_BUFFER_GETTER(guint64, GUINT64, guint64, le, LE)
-GWY_DEFINE_BUFFER_GETTER(guint64, GUINT64, guint64, be, BE)
-GWY_DEFINE_BUFFER_GETTER(gfloat,  GUINT32, guint32, le, LE)
-GWY_DEFINE_BUFFER_GETTER(gfloat,  GUINT32, guint32, be, BE)
-GWY_DEFINE_BUFFER_GETTER(gdouble, GUINT64, guint64, le, LE)
-GWY_DEFINE_BUFFER_GETTER(gdouble, GUINT64, guint64, be, BE)
+static inline gint16
+gwy_get_gint16_be(const guchar **ppv)
+{
+    const gint16 *pv = *(const gint16**)ppv;
+    gint16 v = GINT16_FROM_BE(*pv);
+    *ppv += sizeof(gint16);
+    return v;
+}
 
-#undef GWY_DEFINE_BUFFER_GETTER
+static inline guint16
+gwy_get_guint16_le(const guchar **ppv)
+{
+    const guint16 *pv = *(const guint16**)ppv;
+    guint16 v = GUINT16_FROM_LE(*pv);
+    *ppv += sizeof(guint16);
+    return v;
+}
+
+static inline guint16
+gwy_get_guint16_be(const guchar **ppv)
+{
+    const guint16 *pv = *(const guint16**)ppv;
+    guint16 v = GUINT16_FROM_BE(*pv);
+    *ppv += sizeof(guint16);
+    return v;
+}
+
+static inline gint32
+gwy_get_gint32_le(const guchar **ppv)
+{
+    const gint32 *pv = *(const gint32**)ppv;
+    gint32 v = GINT32_FROM_LE(*pv);
+    *ppv += sizeof(gint32);
+    return v;
+}
+
+static inline gint32
+gwy_get_gint32_be(const guchar **ppv)
+{
+    const gint32 *pv = *(const gint32**)ppv;
+    gint32 v = GINT32_FROM_BE(*pv);
+    *ppv += sizeof(gint32);
+    return v;
+}
+
+static inline guint32
+gwy_get_guint32_le(const guchar **ppv)
+{
+    const guint32 *pv = *(const guint32**)ppv;
+    guint32 v = GUINT32_FROM_LE(*pv);
+    *ppv += sizeof(guint32);
+    return v;
+}
+
+static inline guint32
+gwy_get_guint32_be(const guchar **ppv)
+{
+    const guint32 *pv = *(const guint32**)ppv;
+    guint32 v = GUINT32_FROM_BE(*pv);
+    *ppv += sizeof(guint32);
+    return v;
+}
+
+static inline gint64
+gwy_get_gint64_le(const guchar **ppv)
+{
+    const gint64 *pv = *(const gint64**)ppv;
+    gint64 v = GINT64_FROM_LE(*pv);
+    *ppv += sizeof(gint64);
+    return v;
+}
+
+static inline gint64
+gwy_get_gint64_be(const guchar **ppv)
+{
+    const gint64 *pv = *(const gint64**)ppv;
+    gint64 v = GINT64_FROM_BE(*pv);
+    *ppv += sizeof(gint64);
+    return v;
+}
+
+static inline guint64
+gwy_get_guint64_le(const guchar **ppv)
+{
+    const guint64 *pv = *(const guint64**)ppv;
+    guint64 v = GUINT64_FROM_LE(*pv);
+    *ppv += sizeof(guint64);
+    return v;
+}
+
+static inline guint64
+gwy_get_guint64_be(const guchar **ppv)
+{
+    const guint64 *pv = *(const guint64**)ppv;
+    guint64 v = GUINT64_FROM_BE(*pv);
+    *ppv += sizeof(guint64);
+    return v;
+}
+
+static inline gfloat
+gwy_get_gfloat_le(const guchar **ppv)
+{
+    const guint32 *pv = *(const guint32**)ppv;
+    guint32 v = GUINT32_FROM_LE(*pv);
+    *ppv += sizeof(guint32);
+    return *(gfloat*)&v;
+}
+
+static inline gfloat
+gwy_get_gfloat_be(const guchar **ppv)
+{
+    const guint32 *pv = *(const guint32**)ppv;
+    guint32 v = GUINT32_FROM_BE(*pv);
+    *ppv += sizeof(guint32);
+    return *(gfloat*)&v;
+}
+
+static inline gdouble
+gwy_get_gdouble_le(const guchar **ppv)
+{
+    const guint64 *pv = *(const guint64**)ppv;
+    guint64 v = GUINT64_FROM_LE(*pv);
+    *ppv += sizeof(guint64);
+    return *(gdouble*)&v;
+}
+
+static inline gdouble
+gwy_get_gdouble_be(const guchar **ppv)
+{
+    const guint64 *pv = *(const guint64**)ppv;
+    guint64 v = GUINT64_FROM_BE(*pv);
+    *ppv += sizeof(guint64);
+    return *(gdouble*)&v;
+}
 
 gboolean gwy_app_channel_check_nonsquare(GwyContainer *data,
                                          gint id);
