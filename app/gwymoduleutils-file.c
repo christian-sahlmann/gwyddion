fa191009b8115684968706362d900523bc42fdc0
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2007-01-07 22:06:24 +0000

    - fixed function name in gwy_app_channel_title_fall_back() documentation
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@7315 25697fe4-d644-49b8-9d30-be5465f2adf3

--- app/gwymoduleutils-file.c
+++ app/gwymoduleutils-file.c
@@ -72,7 +72,7 @@ gwy_app_channel_check_nonsquare(GwyContainer *data,
 }
 
 /**
- * guess_channel_type:
+ * gwy_app_channel_title_fall_back:
  * @data: A data container.
  * @id: Data channel id.
  *
@@ -139,5 +139,11 @@ gwy_app_channel_title_fall_back(GwyContainer *data,
 
     return FALSE;
 }
+
+/**
+ * SECTION:gwymoduleutils-file
+ * @title: file module utils
+ * @short_description: Utility functions for file modules
+ **/
 
 /* vim: set cin et ts=4 sw=4 cino=>1s,e0,n0,f0,{0,}0,^0,\:1s,=0,g1s,h0,t0,+1s,c3,(0,u0 : */
