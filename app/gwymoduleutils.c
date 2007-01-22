e1af1df47cbe8618d9a398160268ad5d625f6c8c
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2007-01-22 08:02:13 +0000

    - include unistd.h for unlink(2)
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@7420 25697fe4-d644-49b8-9d30-be5465f2adf3

--- app/gwymoduleutils.c
+++ app/gwymoduleutils.c
@@ -21,6 +21,11 @@
 #include "config.h"
 #include <stdio.h>
 #include <errno.h>
+
+#ifdef HAVE_UNISTD_H
+#include <unistd.h>
+#endif
+
 #include <glib/gstdio.h>
 #include <gtk/gtk.h>
 #include <libgwyddion/gwymacros.h>
