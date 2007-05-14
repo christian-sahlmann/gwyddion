15190fb55167ee7bcba247d47b414e5a14fa8be3
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2007-05-14 18:48:40 +0000

    - updated paths of changed File menu items in sensitivity setup
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@8045 25697fe4-d644-49b8-9d30-be5465f2adf3

--- app/toolbox.c
+++ app/toolbox.c
@@ -631,8 +631,8 @@ gwy_app_menu_create_file_menu(GtkAccelGroup *accel_group)
 
     set_sensitivity(item_factory,
                     "<file>/Save",         GWY_MENU_FLAG_FILE,
-                    "<file>/Save As",      GWY_MENU_FLAG_FILE,
-                    "<file>/Merge",        GWY_MENU_FLAG_FILE,
+                    "<file>/Save As...",   GWY_MENU_FLAG_FILE,
+                    "<file>/Merge...",     GWY_MENU_FLAG_FILE,
                     NULL);
 
     item = gtk_item_factory_get_item(item_factory, "<file>/Open Recent");
