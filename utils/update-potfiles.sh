101c3d5fb83e1353bda3914256949ae71e424299
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2007-06-09 09:48:08 +0000

    - added a note what generated the output to the output file
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@8157 25697fe4-d644-49b8-9d30-be5465f2adf3

--- utils/update-potfiles.sh
+++ utils/update-potfiles.sh
@@ -3,7 +3,7 @@ exec >'po/POTFILES.in'
 
 # Keep the `GENERATED' string quoted to prevent match here
 echo '# List of source files containing translatable strings.'
-echo '# This is a 'GENERATED' file.'
+echo '# This is a 'GENERATED' file, by utils/update-potfiles.sh.'
 
 for dir in libgwyddion libprocess libdraw libgwydgets libgwymodule app modules; do
   echo
