b1bea47d10649f718ead66b59a8f6a70438bdb04
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2007-07-27 10:13:41 +0000

    - use an universal glib-mkenum header template instead of having one in each   directory
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@8413 25697fe4-d644-49b8-9d30-be5465f2adf3

--- utils/mkenum.mk
+++ utils/mkenum.mk
@@ -3,6 +3,7 @@
 #
 # Parameters:
 # MKENUM_NAME -- output base name
+# MKENUM_ID -- name to use for #ifndef __...__ header wrapping
 # MKENUM_HFILES -- input header files
 #
 # Adds to common variables (must set before, at least to empty):
@@ -14,22 +15,20 @@
 # TODO: Detect
 GLIB_MKENUMS = glib-mkenums
 
-mkenum_built_sources = \
-	$(MKENUM_NAME).h \
-	$(MKENUM_NAME).c
-
-EXTRA_DIST += \
-	$(mkenum_built_sources) \
-	$(MKENUM_NAME).h.template
-
-CLEANFILES += \
-	$(MKENUM_NAME).c.xgen \
-	$(MKENUM_NAME).h.xgen
+mkenum_built_sources = $(MKENUM_NAME).h $(MKENUM_NAME).c
+EXTRA_DIST += $(mkenum_built_sources)
+CLEANFILES += $(MKENUM_NAME).c.xgen $(MKENUM_NAME).h.xgen
 
 if MAINTAINER_MODE
 mkenum_stamp_files = $(MKENUM_NAME).h.stamp
 mkenum_self = $(top_srcdir)/utils/mkenum.mk
 mkenum_c_template = $(top_srcdir)/utils/mkenum.c.template
+mkenum_h_template = $(top_srcdir)/utils/mkenum.h.template
+# Keep the `GENERATED' string quoted to prevent match here
+mkenum_fix_output = \
+	| sed -e 's/_\([123]\)_D/_\1D_/g' \
+	      -e "s/@ID@/$(MKENUM_ID)/g" \
+	      -e '1s:.*:/* This is a 'GENERATED' file. */:'
 
 MAINTAINERCLEANFILES += $(mkenum_built_sources) $(mkenum_stamp_files)
 
@@ -38,25 +37,19 @@ BUILT_SOURCES += $(mkenum_built_sources)
 $(MKENUM_NAME).h: $(MKENUM_NAME).h.stamp
 	@true
 
-# Keep the `GENERATED' string quoted to prevent match here
-$(MKENUM_NAME).h.stamp: $(MKENUM_HFILES) $(MKENUM_NAME).h.template $(mkenum_self)
-	echo '/* This is a 'GENERATED' file. */' >$(MKENUM_NAME).h.xgen \
-	&& $(GLIB_MKENUMS) --template $(srcdir)/$(MKENUM_NAME).h.template \
-		$(MKENUM_HFILES) \
-		| sed -e 's/_\([123]\)_D/_\1D_/g' \
-		>>$(MKENUM_NAME).h.xgen \
+$(MKENUM_NAME).h.stamp: $(MKENUM_HFILES) $(mkenum_h_template) $(mkenum_self)
+	$(GLIB_MKENUMS) --template $(mkenum_h_template) $(MKENUM_HFILES) \
+		$(mkenum_fix_output) \
+		>$(MKENUM_NAME).h.xgen \
 	&& ( cmp -s $(MKENUM_NAME).h.xgen $(MKENUM_NAME).h \
 		|| cp $(MKENUM_NAME).h.xgen $(MKENUM_NAME).h ) \
 	&& rm -f $(MKENUM_NAME).h.xgen \
 	&& echo timestamp >$(MKENUM_NAME).h.stamp
 
-# Keep the `GENERATED' string quoted to prevent match here
 $(MKENUM_NAME).c: $(MKENUM_HFILES) $(mkenum_c_template) $(mkenum_self)
-	echo '/* This is a 'GENERATED' file. */' >$(MKENUM_NAME).c.xgen \
-	&& $(GLIB_MKENUMS) --template $(mkenum_c_template) \
-		$(MKENUM_HFILES) \
-		| sed -e 's/_\([123]\)_D/_\1D_/g' \
-		>>$(MKENUM_NAME).c.xgen \
+	$(GLIB_MKENUMS) --template $(mkenum_c_template) $(MKENUM_HFILES) \
+		$(mkenum_fix_output) \
+		>$(MKENUM_NAME).c.xgen \
 	&& cp $(MKENUM_NAME).c.xgen $(MKENUM_NAME).c  \
 	&& rm -f $(MKENUM_NAME).c.xgen
 endif
