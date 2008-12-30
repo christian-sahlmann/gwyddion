205f06c36eb79915d2a6d0279ac2baeecee02b8d
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2008-12-30 12:44:24 +0000

    - rewritten completely botched module
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@8913 25697fe4-d644-49b8-9d30-be5465f2adf3

--- modules/file/burleigh_bii.c
+++ modules/file/burleigh_bii.c
@@ -1,8 +1,7 @@
 /*
  *  @(#) $Id: burleigh_bii.c 8625 2007-10-10 20:03:12Z yeti-dn $
- *  Copyright (C) 2006 David Necas (Yeti), Petr Klapetek, Hans-Peter Doerr.
- *  E-mail: yeti@gwyddion.net, klapetek@gwyddion.net,
- *          doerr@cip.physik.uni-freiburg.de.
+ *  Copyright (C) 2008 David Necas (Yeti), Petr Klapetek, Jan Horak.
+ *  E-mail: yeti@gwyddion.net, klapetek@gwyddion.net, xhorak@gmail.com.
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -18,13 +17,15 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111 USA
  */
-//TODO: filemagic
+
+/* XXX: Unfortunately, BM6 is Windows 3.x bitmap and we cannot claim
+ * ownership of that. */
 /**
  * [FILE-MAGIC-FREEDESKTOP]
  * <mime-type type="application/x-burleigh-bii-spm">
- *   <comment>Burleigh binary SPM data</comment>
- *   <magic priority="50">
- *     <match type="string" offset="0" value="\xff\x06@\x00"/>
+ *   <comment>Burleigh BII SPM data</comment>
+ *   <magic priority="30">
+ *     <match type="string" offset="0" value="BM6"/>
  *   </magic>
  *   <glob pattern="*.bii"/>
  *   <glob pattern="*.BII"/>
@@ -45,28 +46,29 @@
 #include "err.h"
 
 #define EXTENSION "bii"
-#define FILE_START "BM6"
-#define FILE_IMG_WIDTH_POS 0x12
-#define FILE_IMG_HEIGHT_POS 0x16
+#define MAGIC "BM6"
+#define MAGIC_SIZE (sizeof(MAGIC)-1)
+#define SIGNATURE "Burleigh Instruments"
+
+#define FILE_IMG_SIZE_POS 0x12
 #define FILE_IMG_DATA_START_POS 0x36
-#define FILE_IMG_X_SCALE_POS 0x74
-#define FILE_IMG_Y_SCALE_POS 0x7C
+#define FILE_IMG_XY_SCALE_POS 0x74
 #define FILE_IMG_Z_SCALE_POS 0xCC
 
-static gboolean      module_register  (void);
-static gint          burleigh_bii_detect  (const GwyFileDetectInfo *fileinfo,
-                                       gboolean only_name);
-static GwyContainer* burleigh_bii_load    (const gchar *filename,
-                                       GwyRunType mode,
-                                       GError **error);
+static gboolean      module_register    (void);
+static gint          burleigh_bii_detect(const GwyFileDetectInfo *fileinfo,
+                                         gboolean only_name);
+static GwyContainer* burleigh_bii_load  (const gchar *filename,
+                                         GwyRunType mode,
+                                         GError **error);
 
 static GwyModuleInfo module_info = {
     GWY_MODULE_ABI_VERSION,
     &module_register,
     N_("Imports Burleigh BII binary data files."),
-    "Jan Hořák <xhorak@gmail.com>",
-    "0.1",
-    "David Nečas (Yeti) & Petr Klapetek & Hans-Peter Doerr",
+    "Jan Hořák <xhorak@gmail.com>, Yeti <yeti@gwyddion.net>",
+    "0.2",
+    "Jan Hořák & David Nečas (Yeti) & Petr Klapetek",
     "2008",
 };
 
@@ -92,11 +94,18 @@ burleigh_bii_detect(const GwyFileDetectInfo *fileinfo,
     if (only_name)
         return g_str_has_suffix(fileinfo->name_lowercase, EXTENSION) ? 10 : 0;
 
-    if (fileinfo->buffer_len > 3 && strncmp(fileinfo->head, FILE_START, 3) == 0 ) {
-        return 100;     
-    }
+    if (fileinfo->file_size < FILE_IMG_DATA_START_POS
+        || fileinfo->buffer_len < MAGIC_SIZE
+        || strncmp(fileinfo->head, MAGIC, MAGIC_SIZE) != 0)
+        return 0;
 
-    return 0;
+    /* We are fairly sure if Burleigh claims the file is theirs. */
+    if (gwy_memmem(fileinfo->tail, fileinfo->buffer_len,
+                   SIGNATURE, sizeof(SIGNATURE)-1))
+            return 95;
+
+    /* It might be.  Note 30 is lower than the generic pixmap loader score. */
+    return 30;
 }
 
 static GwyContainer*
@@ -104,71 +113,84 @@ burleigh_bii_load(const gchar *filename,
                   G_GNUC_UNUSED GwyRunType mode,
                   GError **error)
 {
-   gchar *buffer = NULL;
-   gint size = 0;
-   GError *err = NULL;
-   gint width, height, data_end_offset, i, j, power10;
-   gint16 *values;
-   gdouble x_scale, y_scale, z_scale;
-   GwyDataField *d;
-   GwyContainer *container;
-   GwySIUnit *units = NULL;
+    gchar *buffer = NULL;
+    const guchar *p;
+    gsize size = 0;
+    GError *err = NULL;
+    gint width, height, data_end_offset, i, j, power10;
+    gint16 *values;
+    gdouble x_scale, y_scale, z_scale, q;
+    GwyDataField *dfield;
+    GwyContainer *container;
+    GwySIUnit *units;
+    gdouble *data;
 
-   if (!g_file_get_contents(filename, &buffer, &size, &err)) {
-      err_GET_FILE_CONTENTS(error, &err);
-      return NULL;      
-   }
-   if (size > FILE_IMG_DATA_START_POS) {
-      // get datafield dimensions from file
-      memcpy(&width, buffer + FILE_IMG_WIDTH_POS, 4);
-      memcpy(&height, buffer + FILE_IMG_HEIGHT_POS, 4);
-      // get file offset where datafield ends
-      data_end_offset = width * height * 2 + FILE_IMG_DATA_START_POS;      
-      // get scaling factors of axes
-      if (data_end_offset + FILE_IMG_Z_SCALE_POS + 8 > size) {
-         g_warning("File is too small, expected size at least %d (size: %d)", 
-               data_end_offset + FILE_IMG_Z_SCALE_POS + 8, size);
-         return NULL;
-      }
-      memcpy(&x_scale, buffer + data_end_offset + FILE_IMG_X_SCALE_POS, 8);
-      memcpy(&y_scale, buffer + data_end_offset + FILE_IMG_Y_SCALE_POS, 8);
-      memcpy(&z_scale, buffer + data_end_offset + FILE_IMG_Z_SCALE_POS, 8);
-      
-      //FIXME: all axes always in nm?
-      units = gwy_si_unit_new_parse("nm", &power10);
-      gwy_debug("w:%d h:%d scale: x%f y%f z%f", 
-            width, height, x_scale * pow10(power10), 
-            y_scale, z_scale);
-      // create new datafield
-      d = gwy_data_field_new( 
-            width, 
-            height, 
-            x_scale * pow10(power10), 
-            y_scale * pow10(power10), 
-            TRUE);
-      gwy_data_field_set_si_unit_xy(d, units);
-      gwy_data_field_set_si_unit_z(d, units);
-      // set pointer to 16bit signed integers to start position 
-      // of data definition in file
-      values = (gint16 *)(buffer + FILE_IMG_DATA_START_POS);
-      // fill datafield
-      for (j = 0; j < height; j++)
-         for (i = 0; i < width; i++) {
-            gwy_data_field_set_val(
-                  d, 
-                  i, 
-                  j, 
-                  (gdouble)(*values * z_scale) * pow10(power10));
+    if (!g_file_get_contents(filename, &buffer, &size, &err)) {
+        err_GET_FILE_CONTENTS(error, &err);
+        return NULL;      
+    }
+    if (size < FILE_IMG_DATA_START_POS) {
+        err_TOO_SHORT(error);
+        gwy_file_abandon_contents(buffer, size, NULL);
+        return NULL;
+    }
+    /* get datafield dimensions from file */
+    p = buffer + FILE_IMG_SIZE_POS;
+    width = gwy_get_gint32_le(&p);
+    height = gwy_get_gint32_le(&p);
+    // get file offset where datafield ends
+    data_end_offset = width * height * 2 + FILE_IMG_DATA_START_POS;      
+    // get scaling factors of axes
+    if (err_SIZE_MISMATCH(error,
+                          data_end_offset + FILE_IMG_Z_SCALE_POS + 8, size,
+                          FALSE)) {
+        gwy_file_abandon_contents(buffer, size, NULL);
+        return NULL;
+    }
+    p = buffer + data_end_offset + FILE_IMG_XY_SCALE_POS;
+    x_scale = gwy_get_gdouble_le(&p);
+    y_scale = gwy_get_gdouble_le(&p);
+    p = buffer + data_end_offset + FILE_IMG_Z_SCALE_POS;
+    z_scale = gwy_get_gdouble_le(&p);
+
+    //FIXME: all axes always in nm?
+    units = gwy_si_unit_new_parse("nm", &power10);
+    gwy_debug("w:%dfield h:%dfield scale: x%f y%f z%f", 
+              width, height, x_scale * pow10(power10), 
+              y_scale, z_scale);
+    // create new datafield
+    dfield = gwy_data_field_new(width, height, 
+                           x_scale * pow10(power10), 
+                           y_scale * pow10(power10), 
+                           TRUE);
+    gwy_data_field_set_si_unit_xy(dfield, units);
+    g_object_unref(units);
+
+    units = gwy_si_unit_new_parse("nm", &power10);
+    gwy_data_field_set_si_unit_z(dfield, units);
+    g_object_unref(units);
+
+    // set pointer to 16bit signed integers to start position 
+    // of data definition in file
+    values = (gint16*)(buffer + FILE_IMG_DATA_START_POS);
+    data = gwy_data_field_get_data(dfield);
+    // fill datafield
+    q = z_scale * pow10(power10);
+    for (j = 0; j < height; j++) {
+        for (i = 0; i < width; i++) {
+            data[(height - 1 - j)*width + i] = q*GINT16_FROM_LE(*values);
             values++;
-         }
-      // fix zero
-      gwy_data_field_add(d, -gwy_data_field_get_min(d));
-      // fill container
-      container = gwy_container_new();
-      gwy_container_set_object_by_name(container, "/0/data", d);
-      return container;
-   } else {
-      g_warning("File is too small.");
-   }
-   return NULL;
+        }
+    }
+    gwy_file_abandon_contents(buffer, size, NULL);
+
+    // fill container
+    container = gwy_container_new();
+    gwy_container_set_object_by_name(container, "/0/data", dfield);
+    g_object_unref(dfield);
+    gwy_app_channel_title_fall_back(container, 0);
+
+    return container;
 }
+
+/* vim: set cin et ts=4 sw=4 cino=>1s,e0,n0,f0,{0,}0,^0,\:1s,=0,g1s,h0,t0,+1s,c3,(0,u0 : */
