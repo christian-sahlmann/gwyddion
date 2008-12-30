7581e325354e8b06e295632a4edee308fcbb23dd
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2008-12-30 12:40:31 +0000

    - do not create confusing empty masks, just remove/do not create it instead
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@8909 25697fe4-d644-49b8-9d30-be5465f2adf3

--- modules/process/outliers.c
+++ modules/process/outliers.c
@@ -1,6 +1,6 @@
 /*
  *  @(#) $Id$
- *  Copyright (C) 2004 David Necas (Yeti), Petr Klapetek.
+ *  Copyright (C) 2004,2008 David Necas (Yeti), Petr Klapetek.
  *  E-mail: yeti@gwyddion.net, klapetek@gwyddion.net.
  *
  *  This program is free software; you can redistribute it and/or modify
@@ -22,6 +22,7 @@
 #include <libgwyddion/gwymacros.h>
 #include <libgwyddion/gwymath.h>
 #include <libprocess/correct.h>
+#include <libprocess/stats.h>
 #include <libgwymodule/gwymodule-process.h>
 #include <app/gwyapp.h>
 
@@ -36,7 +37,7 @@ static GwyModuleInfo module_info = {
     &module_register,
     N_("Creates mask of outliers."),
     "Petr Klapetek <klapetek@gwyddion.net>",
-    "1.2",
+    "1.3",
     "David Nečas (Yeti) & Petr Klapetek",
     "2004",
 };
@@ -63,6 +64,8 @@ outliers(GwyContainer *data, GwyRunType run)
     GwyDataField *dfield, *maskfield;
     GQuark dquark, mquark;
     gdouble thresh = 3.0;
+    gboolean has_mask;
+    gint xres, yres, count;
 
     g_return_if_fail(run & OUTLIERS_RUN_MODES);
     gwy_app_data_browser_get_current(GWY_APP_DATA_FIELD_KEY, &dquark,
@@ -71,14 +74,41 @@ outliers(GwyContainer *data, GwyRunType run)
                                      GWY_APP_MASK_FIELD, &maskfield,
                                      0);
     g_return_if_fail(dfield && dquark);
-    gwy_app_undo_qcheckpoint(data, dquark, mquark, 0);
-    if (!maskfield) {
+    has_mask = !!maskfield;
+
+    /* Always owning a reference simplifies the logic. */
+    if (has_mask)
+        g_object_ref(maskfield);
+    else
         maskfield = gwy_data_field_new_alike(dfield, FALSE);
-        gwy_container_set_object(data, mquark, maskfield);
-        g_object_unref(maskfield);
-    }
+
     gwy_data_field_mask_outliers(dfield, maskfield, thresh);
-    gwy_data_field_data_changed(maskfield);
+
+    /* Do not create useless undo levels if there was no mask and the new
+     * mask would be empty.  And do not create empty masks at all, just remove
+     * the mask instead.  */
+    xres = gwy_data_field_get_xres(maskfield);
+    yres = gwy_data_field_get_yres(maskfield);
+    gwy_data_field_area_count_in_range(maskfield, NULL, 0, 0, xres, yres,
+                                       0.0, 0.0, &count, NULL);
+    count = xres*yres - count;
+
+    if (count || has_mask)
+        gwy_app_undo_qcheckpointv(data, 1, &mquark);
+
+    if (count) {
+        if (has_mask)
+            gwy_data_field_data_changed(maskfield);
+        else
+            gwy_container_set_object(data, mquark, maskfield);
+    }
+    else {
+        if (has_mask)
+            gwy_container_remove(data, mquark);
+        /* otherwise: do nothing */
+    }
+
+    g_object_unref(maskfield);
 }
 
 /* vim: set cin et ts=4 sw=4 cino=>1s,e0,n0,f0,{0,}0,^0,\:1s,=0,g1s,h0,t0,+1s,c3,(0,u0 : */
