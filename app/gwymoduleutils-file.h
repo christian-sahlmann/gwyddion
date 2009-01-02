1f31ac511711cecad370aba9eccbfb29e188fb7d
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2009-01-02 11:52:02 +0000

    - reformulated type-punning in a way that works with strict aliasing   (hopefully)
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@8941 25697fe4-d644-49b8-9d30-be5465f2adf3

--- app/gwymoduleutils-file.h
+++ app/gwymoduleutils-file.h
@@ -55,7 +55,7 @@ _GWY_STATIC_INLINE gdouble  gwy_get_pascal_real_be(const guchar **ppv);
 static inline gboolean
 gwy_get_gboolean8(const guchar **ppv)
 {
-    const guint8 *pv = *(const guint8**)ppv;
+    const guint8 *pv = (const guint8*)(*ppv);
     guint8 v = *pv;
     *ppv += sizeof(guint8);
     return !!v;
@@ -64,7 +64,7 @@ gwy_get_gboolean8(const guchar **ppv)
 static inline gint16
 gwy_get_gint16_le(const guchar **ppv)
 {
-    const gint16 *pv = *(const gint16**)ppv;
+    const gint16 *pv = (const gint16*)(*ppv);
     gint16 v = GINT16_FROM_LE(*pv);
     *ppv += sizeof(gint16);
     return v;
@@ -73,7 +73,7 @@ gwy_get_gint16_le(const guchar **ppv)
 static inline gint16
 gwy_get_gint16_be(const guchar **ppv)
 {
-    const gint16 *pv = *(const gint16**)ppv;
+    const gint16 *pv = (const gint16*)(*ppv);
     gint16 v = GINT16_FROM_BE(*pv);
     *ppv += sizeof(gint16);
     return v;
@@ -82,7 +82,7 @@ gwy_get_gint16_be(const guchar **ppv)
 static inline guint16
 gwy_get_guint16_le(const guchar **ppv)
 {
-    const guint16 *pv = *(const guint16**)ppv;
+    const guint16 *pv = (const guint16*)(*ppv);
     guint16 v = GUINT16_FROM_LE(*pv);
     *ppv += sizeof(guint16);
     return v;
@@ -91,7 +91,7 @@ gwy_get_guint16_le(const guchar **ppv)
 static inline guint16
 gwy_get_guint16_be(const guchar **ppv)
 {
-    const guint16 *pv = *(const guint16**)ppv;
+    const guint16 *pv = (const guint16*)(*ppv);
     guint16 v = GUINT16_FROM_BE(*pv);
     *ppv += sizeof(guint16);
     return v;
@@ -100,7 +100,7 @@ gwy_get_guint16_be(const guchar **ppv)
 static inline gint32
 gwy_get_gint32_le(const guchar **ppv)
 {
-    const gint32 *pv = *(const gint32**)ppv;
+    const gint32 *pv = (const gint32*)(*ppv);
     gint32 v = GINT32_FROM_LE(*pv);
     *ppv += sizeof(gint32);
     return v;
@@ -109,7 +109,7 @@ gwy_get_gint32_le(const guchar **ppv)
 static inline gint32
 gwy_get_gint32_be(const guchar **ppv)
 {
-    const gint32 *pv = *(const gint32**)ppv;
+    const gint32 *pv = (const gint32*)(*ppv);
     gint32 v = GINT32_FROM_BE(*pv);
     *ppv += sizeof(gint32);
     return v;
@@ -118,7 +118,7 @@ gwy_get_gint32_be(const guchar **ppv)
 static inline guint32
 gwy_get_guint32_le(const guchar **ppv)
 {
-    const guint32 *pv = *(const guint32**)ppv;
+    const guint32 *pv = (const guint32*)(*ppv);
     guint32 v = GUINT32_FROM_LE(*pv);
     *ppv += sizeof(guint32);
     return v;
@@ -127,7 +127,7 @@ gwy_get_guint32_le(const guchar **ppv)
 static inline guint32
 gwy_get_guint32_be(const guchar **ppv)
 {
-    const guint32 *pv = *(const guint32**)ppv;
+    const guint32 *pv = (const guint32*)(*ppv);
     guint32 v = GUINT32_FROM_BE(*pv);
     *ppv += sizeof(guint32);
     return v;
@@ -136,7 +136,7 @@ gwy_get_guint32_be(const guchar **ppv)
 static inline gint64
 gwy_get_gint64_le(const guchar **ppv)
 {
-    const gint64 *pv = *(const gint64**)ppv;
+    const gint64 *pv = (const gint64*)(*ppv);
     gint64 v = GINT64_FROM_LE(*pv);
     *ppv += sizeof(gint64);
     return v;
@@ -145,7 +145,7 @@ gwy_get_gint64_le(const guchar **ppv)
 static inline gint64
 gwy_get_gint64_be(const guchar **ppv)
 {
-    const gint64 *pv = *(const gint64**)ppv;
+    const gint64 *pv = (const gint64*)(*ppv);
     gint64 v = GINT64_FROM_BE(*pv);
     *ppv += sizeof(gint64);
     return v;
@@ -154,7 +154,7 @@ gwy_get_gint64_be(const guchar **ppv)
 static inline guint64
 gwy_get_guint64_le(const guchar **ppv)
 {
-    const guint64 *pv = *(const guint64**)ppv;
+    const guint64 *pv = (const guint64*)(*ppv);
     guint64 v = GUINT64_FROM_LE(*pv);
     *ppv += sizeof(guint64);
     return v;
@@ -163,7 +163,7 @@ gwy_get_guint64_le(const guchar **ppv)
 static inline guint64
 gwy_get_guint64_be(const guchar **ppv)
 {
-    const guint64 *pv = *(const guint64**)ppv;
+    const guint64 *pv = (const guint64*)(*ppv);
     guint64 v = GUINT64_FROM_BE(*pv);
     *ppv += sizeof(guint64);
     return v;
