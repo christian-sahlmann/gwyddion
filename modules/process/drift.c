b97b9454726586f1b6ad1a9e6d84f337f592d56f
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2009-01-24 17:22:34 +0000

    - made Search range and rows translatable
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@8983 25697fe4-d644-49b8-9d30-be5465f2adf3

--- modules/process/drift.c
+++ modules/process/drift.c
@@ -278,7 +278,7 @@ drift_dialog(DriftArgs *args,
     row = 0;
 
     controls.range = gtk_adjustment_new(args->range, 1.0, 50.0, 1.0, 5.0, 0);
-    spin = gwy_table_attach_hscale(table, row, "_Search range:", "rows",
+    spin = gwy_table_attach_hscale(table, row, _("_Search range:"), _("rows"),
                                    controls.range, GWY_HSCALE_DEFAULT);
     g_signal_connect(controls.range, "value-changed",
                      G_CALLBACK(drift_invalidate), &controls);
