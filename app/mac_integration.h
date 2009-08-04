405c12dcc0d08e75accba387ffa9690ed9a887dd
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2009-08-04 11:51:49 +0000

    - added GPL copyright headers
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@9551 25697fe4-d644-49b8-9d30-be5465f2adf3

--- app/mac_integration.h
+++ app/mac_integration.h
@@ -1,3 +1,22 @@
+/*
+ *  @(#) $Id: gwyddion.c 9549 2009-08-04 11:26:28Z yeti-dn $
+ *  Copyright (C) 2009 Andrey Gruzdev.
+ *  E-mail: gruzdev@ntmdt.ru.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111 USA
+ */
 #ifndef __MAC_INTEGRATION_H__
 #define __MAC_INTEGRATION_H__
 
