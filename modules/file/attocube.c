bd5cb8d11b00374ae2b4c250592ac1361d0395fe
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2009-09-24 15:09:07 +0000

    - added import module for Attocube ASC data
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@10154 25697fe4-d644-49b8-9d30-be5465f2adf3

---
+++ modules/file/attocube.c
@@ -0,0 +1,251 @@
+/*
+ *  $Id$
+ *  Copyright (C) 2009 David Necas (Yeti).
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111 USA
+ */
+
+/* The format is quite similar to SPIP-ASC, however, there are enough
+ * differences to warrant another module. */
+
+/**
+ * [FILE-MAGIC-FREEDESKTOP]
+ * <mime-type type="application/x-attocube-asc">
+ *   <comment>Attocube ASCII data</comment>
+ *   <magic priority="80">
+ *     <match type="string" offset="0" value="# Daisy frame view snapshot"/>
+ *   </magic>
+ *   <glob pattern="*.asc"/>
+ *   <glob pattern="*.ASC"/>
+ * </mime-type>
+ **/
+
+#include "config.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <libgwyddion/gwymacros.h>
+#include <libgwyddion/gwymath.h>
+#include <libgwyddion/gwyutils.h>
+#include <libprocess/stats.h>
+#include <libgwymodule/gwymodule-file.h>
+#include <app/gwymoduleutils-file.h>
+#include <app/data-browser.h>
+
+#include "err.h"
+
+#define MAGIC_BARE "# Daisy frame view snapshot"
+#define MAGIC1 MAGIC_BARE "\r\n"
+#define MAGIC2 MAGIC_BARE "\n"
+#define MAGIC1_SIZE (sizeof(MAGIC1)-1)
+#define MAGIC2_SIZE (sizeof(MAGIC2)-1)
+#define EXTENSION ".asc"
+
+static gboolean      module_register(void);
+static gint          asc_detect     (const GwyFileDetectInfo *fileinfo,
+                                     gboolean only_name);
+static GwyContainer* asc_load       (const gchar *filename,
+                                     GwyRunType mode,
+                                     GError **error);
+
+static GwyModuleInfo module_info = {
+    GWY_MODULE_ABI_VERSION,
+    &module_register,
+    N_("Imports Attocube Systems ASC files."),
+    "Yeti <yeti@gwyddion.net>",
+    "0.1",
+    "David Nečas (Yeti)",
+    "2009",
+};
+
+GWY_MODULE_QUERY(module_info)
+
+static gboolean
+module_register(void)
+{
+    gwy_file_func_register("attocube",
+                           N_("Attocube ASCII files (.asc)"),
+                           (GwyFileDetectFunc)&asc_detect,
+                           (GwyFileLoadFunc)&asc_load,
+                           NULL,
+                           NULL);
+
+    return TRUE;
+}
+
+static gint
+asc_detect(const GwyFileDetectInfo *fileinfo,
+               gboolean only_name)
+{
+    if (only_name)
+        return g_str_has_suffix(fileinfo->name_lowercase, EXTENSION) ? 10 : 0;
+
+    if (fileinfo->file_size < MAX(MAGIC1_SIZE, MAGIC2_SIZE)
+        || (memcmp(fileinfo->head, MAGIC1, MAGIC1_SIZE) != 0
+            && memcmp(fileinfo->head, MAGIC2, MAGIC2_SIZE) != 0))
+        return 0;
+
+    return 100;
+}
+
+static GwyContainer*
+asc_load(const gchar *filename,
+         G_GNUC_UNUSED GwyRunType mode,
+         GError **error)
+{
+    GwyContainer *container = NULL;
+    GwyDataField *dfield = NULL;
+    GwySIUnit *unit;
+    gchar *line, *p, *value, *buffer = NULL;
+    GHashTable *hash = NULL;
+    gsize size;
+    GError *err = NULL;
+    gdouble xreal, yreal, q;
+    guint year, month, day, hour, minute, second;
+    gint i, xres, yres, power10;
+    gdouble *data;
+
+    if (!g_file_get_contents(filename, &buffer, &size, &err)) {
+        err_GET_FILE_CONTENTS(error, &err);
+        goto fail;
+    }
+
+    p = buffer;
+    line = gwy_str_next_line(&p);
+    if (!gwy_strequal(line, MAGIC_BARE)) {
+        err_FILE_TYPE(error, "Attocube ASC");
+        goto fail;
+    }
+
+    line = gwy_str_next_line(&p);
+    if (!line
+        || sscanf(line, "# %u-%u-%uT%u:%u:%u",
+                  &year, &month, &day, &hour, &minute, &second) != 6) {
+        err_FILE_TYPE(error, "Attocube ASC");
+        goto fail;
+    }
+
+    hash = g_hash_table_new(g_str_hash, g_str_equal);
+    while ((line = gwy_str_next_line(&p))) {
+        g_strstrip(line);
+        if (!line[0])
+            continue;
+
+        if (line[0] != '#') {
+            g_warning("Strange line not starting with #");
+            continue;
+        }
+
+        do {
+            line++;
+        } while (g_ascii_isspace(line[0]));
+
+        if (gwy_strequal(line, "Start of Data:"))
+            break;
+
+        value = strchr(line, ':');
+        if (!value)
+            continue;
+
+        *value = '\0';
+        do {
+            value++;
+        } while (g_ascii_isspace(value[0]));
+
+        g_strchomp(line);
+        for (i = 0; line[i]; i++)
+            line[i] = g_ascii_tolower(line[i]);
+
+        gwy_debug("<%s> = <%s>\n", line, value);
+        g_hash_table_insert(hash, line, value);
+    }
+
+    if (!require_keys(hash, error,
+                      "x-pixels", "y-pixels", "x-length", "y-length",
+                      NULL))
+        goto fail;
+
+    xres = atoi(g_hash_table_lookup(hash, "x-pixels"));
+    yres = atoi(g_hash_table_lookup(hash, "y-pixels"));
+    if (err_DIMENSION(error, xres) || err_DIMENSION(error, yres))
+        goto fail;
+
+    xreal = g_ascii_strtod(g_hash_table_lookup(hash, "x-length"), NULL);
+    yreal = g_ascii_strtod(g_hash_table_lookup(hash, "y-length"), NULL);
+    /* Use negated positive conditions to catch NaNs */
+    if (!((xreal = fabs(xreal)) > 0)) {
+        g_warning("Real x size is 0.0, fixing to 1.0");
+        xreal = 1.0;
+    }
+    if (!((yreal = fabs(yreal)) > 0)) {
+        g_warning("Real y size is 0.0, fixing to 1.0");
+        yreal = 1.0;
+    }
+
+    dfield = gwy_data_field_new(xres, yres, xreal, yreal, FALSE);
+
+
+    if ((value = g_hash_table_lookup(hash, "x-unit"))) {
+        if ((line = g_hash_table_lookup(hash, "y-unit"))
+            && !gwy_strequal(line, value))
+            g_warning("X and Y units differ, using X");
+
+        unit = gwy_si_unit_new_parse(value, &power10);
+        gwy_data_field_set_si_unit_xy(dfield, unit);
+        g_object_unref(unit);
+
+        q = pow10(power10);
+        xreal *= q;
+        yreal *= q;
+        gwy_data_field_set_xreal(dfield, xreal);
+        gwy_data_field_set_yreal(dfield, yreal);
+    }
+    else
+        q = 1.0;
+
+    if ((value = g_hash_table_lookup(hash, "x-offset")))
+        gwy_data_field_set_xoffset(dfield, q*g_ascii_strtod(value, NULL));
+    if ((value = g_hash_table_lookup(hash, "y-offset")))
+        gwy_data_field_set_yoffset(dfield, q*g_ascii_strtod(value, NULL));
+
+    if ((value = g_hash_table_lookup(hash, "z-unit"))) {
+        unit = gwy_si_unit_new_parse(value, &power10);
+        gwy_data_field_set_si_unit_z(dfield, unit);
+        g_object_unref(unit);
+        q = pow10(power10);
+    }
+    else
+        q = 1.0;
+
+    data = gwy_data_field_get_data(dfield);
+    value = p;
+    for (i = 0; i < xres*yres; i++) {
+        data[i] = q*g_ascii_strtod(value, &p);
+        value = p;
+    }
+
+    container = gwy_container_new();
+
+    gwy_container_set_object(container, gwy_app_get_data_key_for_id(0), dfield);
+    g_object_unref(dfield);
+
+fail:
+    g_free(buffer);
+    g_hash_table_destroy(hash);
+
+    return container;
+}
+
+/* vim: set cin et ts=4 sw=4 cino=>1s,e0,n0,f0,{0,}0,^0,\:1s,=0,g1s,h0,t0,+1s,c3,(0,u0 : */
