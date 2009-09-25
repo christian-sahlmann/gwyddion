86400295819221c4e65c75f2f19ba112fc2ebd26
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2009-09-25 14:49:22 +0000

    - added precision option - made decimal dot fix work also on the informative header
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@10162 25697fe4-d644-49b8-9d30-be5465f2adf3

--- modules/file/asciiexport.c
+++ modules/file/asciiexport.c
@@ -1,7 +1,7 @@
 /*
  *  @(#) $Id$
- *  Copyright (C) 2003,2007 David Necas (Yeti), Petr Klapetek.
- *  E-mail: yeti@gwyddion.net, klapetek@gwyddion.net.
+ *  Copyright (C) 2003,2007 David Necas (Yeti).
+ *  E-mail: yeti@gwyddion.net.
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -20,6 +20,7 @@
 
 #include "config.h"
 #include <stdio.h>
+#include <locale.h>
 #include <string.h>
 #include <glib/gstdio.h>
 #include <gtk/gtk.h>
@@ -41,6 +42,7 @@
 typedef struct {
     gboolean add_comment;
     gboolean decimal_dot;
+    guint precision;
 } ASCIIExportArgs;
 
 static gboolean module_register          (void);
@@ -50,14 +52,15 @@ static gboolean asciiexport_export       (GwyContainer *data,
                                           const gchar *filename,
                                           GwyRunType mode,
                                           GError **error);
-static gboolean asciiexport_export_dialog(ASCIIExportArgs *args);
+static gboolean asciiexport_export_dialog(ASCIIExportArgs *args,
+                                          gboolean needs_decimal_dot);
 static void     asciiexport_load_args    (GwyContainer *settings,
                                           ASCIIExportArgs *args);
 static void     asciiexport_save_args    (GwyContainer *settings,
                                           ASCIIExportArgs *args);
 
 static const ASCIIExportArgs asciiexport_defaults = {
-    FALSE, TRUE
+    FALSE, TRUE, 5
 };
 
 static GwyModuleInfo module_info = {
@@ -65,8 +68,8 @@ static GwyModuleInfo module_info = {
     &module_register,
     N_("Exports data as simple ASCII matrix."),
     "Yeti <yeti@gwyddion.net>",
-    "1.2",
-    "David Nečas (Yeti) & Petr Klapetek",
+    "1.3",
+    "David Nečas (Yeti)",
     "2004",
 };
 
@@ -92,6 +95,33 @@ asciiexport_detect(const GwyFileDetectInfo *fileinfo,
     return g_str_has_suffix(fileinfo->name_lowercase, EXTENSION) ? 20 : 0;
 }
 
+static inline gint
+print_with_decimal_dot(FILE *fh,
+                       gchar *formatted_number,
+                       const gchar *decimal_dot,
+                       guint decimal_dot_len)
+{
+    gchar *pos = strstr(formatted_number, decimal_dot);
+
+    if (!pos)
+        return fputs(formatted_number, fh);
+    else {
+        pos[0] = '.';
+        if (decimal_dot_len == 1)
+            return fputs(formatted_number, fh);
+        else {
+            gint l1, l2;
+
+            pos[1] = '\0';
+            if ((l1 = fputs(formatted_number, fh)) == EOF)
+                return EOF;
+            if ((l2 = fputs(pos + decimal_dot_len, fh)) == EOF)
+                return EOF;
+            return l1 + l2;
+        }
+    }
+}
+
 static gboolean
 asciiexport_export(G_GNUC_UNUSED GwyContainer *data,
                    const gchar *filename,
@@ -101,6 +131,10 @@ asciiexport_export(G_GNUC_UNUSED GwyContainer *data,
     ASCIIExportArgs args;
     GwyDataField *dfield;
     gint xres, yres, i, id;
+    struct lconv *locale_data;
+    const gchar *decimal_dot;
+    guint precision, decimal_dot_len;
+    gboolean needs_decimal_dot;
     gdouble *d;
     gchar buf[40];
     FILE *fh;
@@ -114,8 +148,15 @@ asciiexport_export(G_GNUC_UNUSED GwyContainer *data,
     }
 
     asciiexport_load_args(gwy_app_settings_get(), &args);
+    locale_data = localeconv();
+    decimal_dot = locale_data->decimal_point;
+    g_return_val_if_fail(decimal_dot, FALSE);
+    decimal_dot_len = strlen(decimal_dot);
+    g_return_val_if_fail(decimal_dot_len, FALSE);
+    needs_decimal_dot = !gwy_strequal(decimal_dot, ".");
+
     if (mode == GWY_RUN_INTERACTIVE) {
-        if (!asciiexport_export_dialog(&args)) {
+        if (!asciiexport_export_dialog(&args, needs_decimal_dot)) {
             err_CANCELLED(error);
             return FALSE;
         }
@@ -142,12 +183,31 @@ asciiexport_export(G_GNUC_UNUSED GwyContainer *data,
         vf = gwy_data_field_get_value_format_xy(dfield,
                                                 GWY_SI_UNIT_FORMAT_VFMARKUP,
                                                 vf);
-        fprintf(fh, "# %s %.*f %s\n", _("Width:"),
-                vf->precision, gwy_data_field_get_xreal(dfield)/vf->magnitude,
-                vf->units);
-        fprintf(fh, "# %s %.*f %s\n", _("Height:"),
-                vf->precision, gwy_data_field_get_yreal(dfield)/vf->magnitude,
-                vf->units);
+        if (args.decimal_dot) {
+            fprintf(fh, "# %s ", _("Width:"));
+            g_snprintf(buf, sizeof(buf), "%.*f",
+                       vf->precision,
+                       gwy_data_field_get_xreal(dfield)/vf->magnitude);
+            print_with_decimal_dot(fh, buf, decimal_dot, decimal_dot_len);
+            fprintf(fh, " %s\n", vf->units);
+
+            fprintf(fh, "# %s ", _("Height:"));
+            g_snprintf(buf, sizeof(buf), "%.*f",
+                       vf->precision,
+                       gwy_data_field_get_yreal(dfield)/vf->magnitude);
+            print_with_decimal_dot(fh, buf, decimal_dot, decimal_dot_len);
+            fprintf(fh, " %s\n", vf->units);
+        }
+        else {
+            fprintf(fh, "# %s %.*f %s\n", _("Width:"),
+                    vf->precision,
+                    gwy_data_field_get_xreal(dfield)/vf->magnitude,
+                    vf->units);
+            fprintf(fh, "# %s %.*f %s\n", _("Height:"),
+                    vf->precision,
+                    gwy_data_field_get_yreal(dfield)/vf->magnitude,
+                    vf->units);
+        }
 
         units = gwy_data_field_get_si_unit_z(dfield);
         s = gwy_si_unit_get_string(units, GWY_SI_UNIT_FORMAT_VFMARKUP);
@@ -157,17 +217,19 @@ asciiexport_export(G_GNUC_UNUSED GwyContainer *data,
         gwy_si_unit_value_format_free(vf);
     }
 
+    precision = args.precision;
     if (args.decimal_dot) {
         for (i = 0; i < xres*yres; i++) {
-            g_ascii_formatd(buf, sizeof(buf), "%g", d[i]);
-            if (fputs(buf, fh) == EOF
-                || fputc((i + 1) % xres ? '\t' : '\n', fh) == EOF)
+            g_snprintf(buf, sizeof(buf), "%.*g%c",
+                       precision, d[i], (i + 1) % xres ? '\t' : '\n');
+            if (print_with_decimal_dot(fh, buf,
+                                       decimal_dot, decimal_dot_len) == EOF)
                 goto fail;
         }
     }
     else {
         for (i = 0; i < xres*yres; i++) {
-            if (fprintf(fh, "%g%c", d[i],
+            if (fprintf(fh, "%.*g%c", precision, d[i],
                         (i + 1) % xres ? '\t' : '\n') < 2) {
                 goto fail;
             }
@@ -186,15 +248,12 @@ fail:
 }
 
 static gboolean
-asciiexport_export_dialog(ASCIIExportArgs *args)
+asciiexport_export_dialog(ASCIIExportArgs *args,
+                          gboolean needs_decimal_dot)
 {
-    GtkWidget *dialog, *vbox, *decimal_dot, *add_comment;
-    gboolean needs_decimal_dot;
-    gchar buf[8];
+    GtkWidget *dialog, *vbox, *hbox, *label, *decimal_dot, *add_comment,
+              *precision;
     gint response;
-
-    g_snprintf(buf, sizeof(buf), "%f", G_PI);
-    needs_decimal_dot = !strchr(buf, '.');
 
     dialog = gtk_dialog_new_with_buttons(_("Export Text"), NULL, 0,
                                          GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
@@ -223,6 +282,16 @@ asciiexport_export_dialog(ASCIIExportArgs *args)
                                  args->add_comment);
     gtk_box_pack_start(GTK_BOX(vbox), add_comment, FALSE, FALSE, 0);
 
+    hbox = gtk_hbox_new(FALSE, 6);
+    gtk_box_pack_start(GTK_BOX(vbox), hbox, FALSE, FALSE, 0);
+
+    label = gtk_label_new_with_mnemonic(_("_Precision:"));
+    gtk_box_pack_start(GTK_BOX(hbox), label, FALSE, FALSE, 0);
+    precision = gtk_spin_button_new_with_range(0, 16, 1);
+    gtk_spin_button_set_value(GTK_SPIN_BUTTON(precision), args->precision);
+    gtk_label_set_mnemonic_widget(GTK_LABEL(label), precision);
+    gtk_box_pack_start(GTK_BOX(hbox), precision, FALSE, FALSE, 0);
+
     gtk_widget_show_all(dialog);
     response = gtk_dialog_run(GTK_DIALOG(dialog));
     if (response != GTK_RESPONSE_NONE) {
@@ -231,6 +300,8 @@ asciiexport_export_dialog(ASCIIExportArgs *args)
                 = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(decimal_dot));
         args->add_comment
             = gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(add_comment));
+        args->precision
+            = gtk_spin_button_get_value_as_int(GTK_SPIN_BUTTON(precision));
         asciiexport_save_args(gwy_app_settings_get(), args);
         gtk_widget_destroy(dialog);
     }
@@ -240,6 +311,7 @@ asciiexport_export_dialog(ASCIIExportArgs *args)
 
 static const gchar decimal_dot_key[] = "/module/asciiexport/decimal-dot";
 static const gchar add_comment_key[] = "/module/asciiexport/add-comment";
+static const gchar precision_key[]   = "/module/asciiexport/precision";
 
 static void
 asciiexport_load_args(GwyContainer *settings,
@@ -251,6 +323,9 @@ asciiexport_load_args(GwyContainer *settings,
                                       &args->decimal_dot);
     gwy_container_gis_boolean_by_name(settings, add_comment_key,
                                       &args->add_comment);
+    gwy_container_gis_int32_by_name(settings, precision_key, &args->precision);
+
+    args->precision = MIN(args->precision, 16);
 }
 
 static void
@@ -261,6 +336,7 @@ asciiexport_save_args(GwyContainer *settings,
                                       args->decimal_dot);
     gwy_container_set_boolean_by_name(settings, add_comment_key,
                                       args->add_comment);
+    gwy_container_set_int32_by_name(settings, precision_key, args->precision);
 }
 
 /* vim: set cin et ts=4 sw=4 cino=>1s,e0,n0,f0,{0,}0,^0,\:1s,=0,g1s,h0,t0,+1s,c3,(0,u0 : */
