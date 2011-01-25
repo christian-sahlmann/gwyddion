d678748ea8e0fcfe9cb876086265914bd2d9554f
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2011-01-25 07:11:03 +0000

    - updated dialog title to Dimensions and Units
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@11854 25697fe4-d644-49b8-9d30-be5465f2adf3

--- modules/process/calibrate.c
+++ modules/process/calibrate.c
@@ -292,7 +292,7 @@ calibrate_dialog(CalibrateArgs *args,
     CalibrateControls controls;
     gint row, response;
 
-    dialog = gtk_dialog_new_with_buttons(_("Calibrate"), NULL, 0,
+    dialog = gtk_dialog_new_with_buttons(_("Dimensions and Units"), NULL, 0,
                                          _("_Reset"), RESPONSE_RESET,
                                          GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
                                          GTK_STOCK_OK, GTK_RESPONSE_OK,
