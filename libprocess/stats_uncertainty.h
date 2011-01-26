0b500ba7480ab4f9691244197153a4fcec381380
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2011-01-26 21:51:08 +0000

    - fixed stats_uncertainty.h more, adding standard mutliple-inclusion guards   and G_BEGIN/END_DECLS
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@11880 25697fe4-d644-49b8-9d30-be5465f2adf3

--- libprocess/stats_uncertainty.h
+++ libprocess/stats_uncertainty.h
@@ -18,11 +18,13 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111 USA
  */
 
-#include "config.h"
-#include <string.h>
+#ifndef __GWY_STATS_UNCERTAINTY_H__
+#define __GWY_STATS_UNCERTAINTY_H__ 1
 
 #include <libgwyddion/gwymacros.h>
 #include <libprocess/datafield.h>
+
+G_BEGIN_DECLS
 
 gdouble gwy_data_field_get_max_uncertainty                   (GwyDataField *data_field,
                                                               GwyDataField *uncz_field);
@@ -276,5 +278,9 @@ gdouble gwy_data_field_get_yder_uncertainty                  (GwyDataField *data
                                                               GwyDataField *uncy_field,
                                                               gint col,
                                                               gint row);
+
+G_END_DECLS
+
+#endif /* __GWY_STATS_UNCERTAINTY_H__ */
 
 /* vim: set cin et ts=4 sw=4 cino=>1s,e0,n0,f0,{0,}0,^0,\:1s,=0,g1s,h0,t0,+1s,c3,(0,u0 : */
