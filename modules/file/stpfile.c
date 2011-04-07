237c82ad812da8624b8c89c2a8a8639fbe46581d
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2011-04-07 09:21:30 +0000

    - corrected vim modeline
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@12269 25697fe4-d644-49b8-9d30-be5465f2adf3

--- modules/file/stpfile.c
+++ modules/file/stpfile.c
@@ -524,6 +524,4 @@ read_data_field(GwyDataField *dfield,
     }
 }
 
-/* vim: set cin et ts=4 sw=4
-cino=>1s,e0,n0,f0,{0,}0,^0,\:1s,=0,g1s,h0,t0,+1s,c3,(0,u0 : */
-
+/* vim: set cin et ts=4 sw=4 cino=>1s,e0,n0,f0,{0,}0,^0,\:1s,=0,g1s,h0,t0,+1s,c3,(0,u0 : */
