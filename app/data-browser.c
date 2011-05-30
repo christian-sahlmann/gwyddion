892f057708986a3baf0240a8d966237cf5770721
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2011-05-30 12:58:20 +0000

    - increased flags column widths a bit as columns with width-chars of 3 do not   always display full 3 characters
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@12490 25697fe4-d644-49b8-9d30-be5465f2adf3

--- app/data-browser.c
+++ app/data-browser.c
@@ -2498,7 +2498,7 @@ gwy_app_data_browser_construct_channels(GwyAppDataBrowser *browser)
 
     /* Add the flags column */
     renderer = gtk_cell_renderer_text_new();
-    g_object_set(renderer, "width-chars", 4, NULL);
+    g_object_set(renderer, "width-chars", 5, NULL);
     column = gtk_tree_view_column_new_with_attributes("Flags", renderer,
                                                       NULL);
     gtk_tree_view_column_set_cell_data_func
@@ -3126,7 +3126,7 @@ gwy_app_data_browser_construct_graphs(GwyAppDataBrowser *browser)
 
     /* Add the flags column */
     renderer = gtk_cell_renderer_text_new();
-    g_object_set(renderer, "width-chars", 3, NULL);
+    g_object_set(renderer, "width-chars", 4, NULL);
     column = gtk_tree_view_column_new_with_attributes("Curves", renderer,
                                                       NULL);
     gtk_tree_view_column_set_cell_data_func
@@ -3389,7 +3389,7 @@ gwy_app_data_browser_construct_spectra(GwyAppDataBrowser *browser)
 
     /* Add the flags column */
     renderer = gtk_cell_renderer_text_new();
-    g_object_set(renderer, "width-chars", 3, NULL);
+    g_object_set(renderer, "width-chars", 4, NULL);
     column = gtk_tree_view_column_new_with_attributes("Points", renderer,
                                                       NULL);
     gtk_tree_view_column_set_cell_data_func
