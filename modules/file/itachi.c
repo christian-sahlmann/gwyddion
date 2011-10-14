b1ebf29472f9947bb093f9e0fc5655b4a8c97717
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2011-10-14 08:19:23 +0000

    - implemented actual data loading
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@12924 25697fe4-d644-49b8-9d30-be5465f2adf3

--- modules/file/itachi.c
+++ modules/file/itachi.c
@@ -16,7 +16,7 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-#define DEBUG 1
+
 /**
  * [FILE-MAGIC-FREEDESKTOP]
  * <mime-type type="application/x-itachi-sem">
@@ -48,6 +48,8 @@
 
 #include "err.h"
 
+#define Nanometre 1e-9
+
 #define MAGIC "[SemImageFile]"
 #define MAGIC_SIZE (sizeof(MAGIC)-1)
 #define HEADER_EXTENSION ".txt"
@@ -63,6 +65,9 @@ static GHashTable*   itachi_load_header   (const gchar *filename,
                                            GError **error);
 static gchar*        itachi_find_data_name(const gchar *header_name,
                                            const gchar *image_name);
+static void          store_meta           (gpointer key,
+                                           gpointer value,
+                                           gpointer user_data);
 
 static GwyModuleInfo module_info = {
     GWY_MODULE_ABI_VERSION,
@@ -128,147 +133,105 @@ itachi_load(const gchar *filename,
             G_GNUC_UNUSED GwyRunType mode,
             GError **error)
 {
-#if 0
     GwyContainer *container = NULL, *meta;
+    GdkPixbuf *pixbuf = NULL;
     GwyDataField *dfield = NULL;
-    GwySIUnit *unit;
-    gchar *line, *p, *value, *header, *buffer = NULL;
-    GwyTextHeaderParser parser;
+    gchar *value, *imagename = NULL, *header = NULL;
+    guchar *pixels;
     GHashTable *hash = NULL;
-    gsize size;
     GError *err = NULL;
-    gdouble xreal, yreal, q;
-    guint year, month, day, hour, minute, second;
-    gint i, xres, yres, power10;
+    gdouble dx;
+    gint pxres, pyres, hxres, hyres, rowstride, nchannels, i, j;
     gdouble *data;
 
-    if (!g_file_get_contents(filename, &buffer, &size, &err)) {
-        err_GET_FILE_CONTENTS(error, &err);
-        goto fail;
-    }
-
-    p = buffer;
-    line = gwy_str_next_line(&p);
-    if (!g_str_has_prefix(line, MAGIC)) {
-        err_FILE_TYPE(error, "Attocube ASC");
-        goto fail;
-    }
-
-    line = gwy_str_next_line(&p);
-    if (!line
-        || sscanf(line, "# %u-%u-%uT%u:%u:%u",
-                  &year, &month, &day, &hour, &minute, &second) != 6) {
-        err_FILE_TYPE(error, "Attocube ASC");
-        goto fail;
-    }
-
-    header = p;
-    p = strstr(header, DATA_MAGIC);
-    if (!p) {
-        err_FILE_TYPE(error, "Attocube ASC");
-        goto fail;
-    }
-    *p = '\0';
-    p += strlen(DATA_MAGIC);
-
-    gwy_clear(&parser, 1);
-    parser.line_prefix = "#";
-    parser.key_value_separator = ":";
-    parser.item = &lowercase_value;
-    hash = gwy_text_header_parse(header, &parser, NULL, NULL);
+    if (!(hash = itachi_load_header(filename, &header, error)))
+        return NULL;
 
     if (!require_keys(hash, error,
-                      "x-pixels", "y-pixels", "x-length", "y-length",
+                      "ImageName", "DataSize", "PixelSize",
                       NULL))
         goto fail;
+    value = g_hash_table_lookup(hash, "ImageName");
 
-    xres = atoi(g_hash_table_lookup(hash, "x-pixels"));
-    yres = atoi(g_hash_table_lookup(hash, "y-pixels"));
-    if (err_DIMENSION(error, xres) || err_DIMENSION(error, yres))
+    if (!(imagename = itachi_find_data_name(filename, value))) {
+        g_set_error(error, GWY_MODULE_FILE_ERROR, GWY_MODULE_FILE_ERROR_DATA,
+                    _("No corresponding data file was found for header file."));
+        goto fail;
+    }
+
+    if (!(pixbuf = gdk_pixbuf_new_from_file(imagename, &err))) {
+        g_set_error(error, GWY_MODULE_FILE_ERROR, GWY_MODULE_FILE_ERROR_IO,
+                    _("Cannot load image: %s"), err->message);
+        g_clear_error(&err);
+        goto fail;
+    }
+
+    /* We know the image dimensions so check them. */
+    pxres = gdk_pixbuf_get_width(pixbuf);
+    pyres = gdk_pixbuf_get_height(pixbuf);
+
+    value = g_hash_table_lookup(hash, "DataSize");
+    if (sscanf(value, "%ux%u", &hxres, &hyres) != 2) {
+        err_INVALID(error, "DataSize");
+        goto fail;
+    }
+    if (hxres != pxres || hyres != pyres) {
+        g_set_error(error, GWY_MODULE_FILE_ERROR, GWY_MODULE_FILE_ERROR_DATA,
+                    _("Field DataSize %ux%u does not match image dimensions "
+                      "%ux%u."),
+                    hxres, hyres, pxres, pyres);
+        goto fail;
+    }
+    if (err_DIMENSION(error, hxres) || err_DIMENSION(error, hyres))
         goto fail;
 
-    xreal = g_ascii_strtod(g_hash_table_lookup(hash, "x-length"), NULL);
-    yreal = g_ascii_strtod(g_hash_table_lookup(hash, "y-length"), NULL);
+    dx = g_ascii_strtod(g_hash_table_lookup(hash, "PixelSize"), NULL);
     /* Use negated positive conditions to catch NaNs */
-    if (!((xreal = fabs(xreal)) > 0)) {
-        g_warning("Real x size is 0.0, fixing to 1.0");
-        xreal = 1.0;
+    if (!((dx = fabs(dx)) > 0)) {
+        g_warning("Pixel size is 0.0, fixing to 1.0");
+        dx = 1.0;
     }
-    if (!((yreal = fabs(yreal)) > 0)) {
-        g_warning("Real y size is 0.0, fixing to 1.0");
-        yreal = 1.0;
-    }
+    dx *= Nanometre;
 
-    dfield = gwy_data_field_new(xres, yres, xreal, yreal, FALSE);
-
-
-    if ((value = g_hash_table_lookup(hash, "x-unit"))) {
-        if ((line = g_hash_table_lookup(hash, "y-unit"))
-            && !gwy_strequal(line, value))
-            g_warning("X and Y units differ, using X");
-
-        unit = gwy_si_unit_new_parse(value, &power10);
-        gwy_data_field_set_si_unit_xy(dfield, unit);
-        g_object_unref(unit);
-
-        q = pow10(power10);
-        xreal *= q;
-        yreal *= q;
-        gwy_data_field_set_xreal(dfield, xreal);
-        gwy_data_field_set_yreal(dfield, yreal);
-    }
-    else
-        q = 1.0;
-
-    if ((value = g_hash_table_lookup(hash, "x-offset")))
-        gwy_data_field_set_xoffset(dfield, q*g_ascii_strtod(value, NULL));
-    if ((value = g_hash_table_lookup(hash, "y-offset")))
-        gwy_data_field_set_yoffset(dfield, q*g_ascii_strtod(value, NULL));
-
-    if ((value = g_hash_table_lookup(hash, "z-unit"))) {
-        unit = gwy_si_unit_new_parse(value, &power10);
-        gwy_data_field_set_si_unit_z(dfield, unit);
-        g_object_unref(unit);
-        q = pow10(power10);
-    }
-    else
-        q = 1.0;
+    dfield = gwy_data_field_new(hxres, hyres, hxres*dx, hyres*dx, FALSE);
+    gwy_si_unit_set_from_string(gwy_data_field_get_si_unit_xy(dfield), "m");
 
     data = gwy_data_field_get_data(dfield);
-    value = p;
-    for (i = 0; i < xres*yres; i++) {
-        data[i] = q*g_ascii_strtod(value, &p);
-        value = p;
+    pixels = gdk_pixbuf_get_pixels(pixbuf);
+    rowstride = gdk_pixbuf_get_rowstride(pixbuf);
+    nchannels = gdk_pixbuf_get_n_channels(pixbuf);
+    for (i = 0; i < hyres; i++) {
+        gdouble *drow = data + i*hxres;
+        guchar *p = pixels + i*rowstride;
+
+        for (j = 0; j < hxres; j++, p += nchannels)
+            drow[j] = (p[0] + p[1] + p[2])/765.0;
     }
 
     container = gwy_container_new();
-
     gwy_container_set_object(container, gwy_app_get_data_key_for_id(0), dfield);
     g_object_unref(dfield);
 
-    if ((value = g_hash_table_lookup(hash, "display")))
+    if ((value = g_hash_table_lookup(hash, "SampleName"))
+        && *value)
         gwy_container_set_string_by_name(container, "/0/data/title",
                                          g_strdup(value));
+    else
+        gwy_container_set_string_by_name(container, "/0/data/title",
+                                         g_strdup("SEM"));
 
     meta = gwy_container_new();
+    g_hash_table_foreach(hash, store_meta, meta);
     gwy_container_set_object_by_name(container, "/0/meta", meta);
     g_object_unref(meta);
 
-    value = g_strdup_printf("%04u-%02u-%02u %02u:%02u:%02u",
-                            year, month, day, hour, minute, second);
-    gwy_container_set_string_by_name(meta, "Date", value);
-
-    if ((value = g_hash_table_lookup(hash, "scanspeed")))
-        gwy_container_set_string_by_name(meta, "Scan Speed", g_strdup(value));
-
 fail:
-    g_free(buffer);
+    gwy_object_unref(pixbuf);
+    g_free(imagename);
+    g_free(header);
     g_hash_table_destroy(hash);
 
     return container;
-#endif
-    err_NO_DATA(error);
-    return NULL;
 }
 
 static GHashTable*
@@ -346,6 +309,23 @@ itachi_find_data_name(const gchar *header_name,
     gwy_debug("failed");
 
     return NULL;
+}
+
+static void
+store_meta(gpointer key,
+           gpointer value,
+           gpointer user_data)
+{
+    GwyContainer *meta = (GwyContainer*)user_data;
+
+    if (g_utf8_validate(value, -1, NULL)) {
+        gwy_container_set_string_by_name(meta, key, g_strdup(value));
+    }
+    else {
+        gchar *s = g_convert(value, -1, "UTF-8", "CP1252", NULL, NULL, NULL);
+        if (s)
+            gwy_container_set_string_by_name(meta, key, s);
+    }
 }
 
 /* vim: set cin et ts=4 sw=4 cino=>1s,e0,n0,f0,{0,}0,^0,\:1s,=0,g1s,h0,t0,+1s,c3,(0,u0 : */
