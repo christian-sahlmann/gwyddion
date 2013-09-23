25aa5098c4219124cdfd2399572b178b8b1c1efd
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2013-09-23 07:51:11 +0000

    - changed _get_output_format() definition so that it compiles on Win32
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@15362 25697fe4-d644-49b8-9d30-be5465f2adf3

--- app/gwyddion.c
+++ app/gwyddion.c
@@ -713,10 +713,13 @@ gwy_app_gl_disabled(void)
 }
 
 /* http://mingw.5.n7.nabble.com/Linking-Error-with-MSVC-6-0-td18263.html */
-int
+#ifdef G_OS_WIN32
+__attribute__((dllimport))
+unsigned int
 _get_output_format(void)
 {
     return 0;
 }
+#endif
 
 /* vim: set cin et ts=4 sw=4 cino=>1s,e0,n0,f0,{0,}0,^0,\:1s,=0,g1s,h0,t0,+1s,c3,(0,u0 : */
