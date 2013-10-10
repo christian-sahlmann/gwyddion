0251662796e3fcf880f2fab745ca94a7dec08c58
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2013-10-10 20:32:09 +0000

    - data are actually read - error handling and masking still missing
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@15413 25697fe4-d644-49b8-9d30-be5465f2adf3

--- modules/file/sagfile.c
+++ modules/file/sagfile.c
@@ -28,7 +28,6 @@
 #include "config.h"
 #include <stdlib.h>
 #include <libgwyddion/gwymath.h>
-#include <libprocess/stats.h>
 #include <app/gwymoduleutils-file.h>
 #include <app/data-browser.h>
 #include "err.h"
@@ -40,13 +39,14 @@ typedef enum {
     SAG_UNIT_CM = 1,
     SAG_UNIT_IN = 2,
     SAG_UNIT_M = 3,
+    SAG_NUNITS,
 } SagUnit;
 
 typedef struct {
     guint xres;
     guint yres;
-    gdouble xreal;
-    gdouble yreal;
+    gdouble dx;
+    gdouble dy;
     SagUnit unit;
     gdouble xoff;
     gdouble yoff;
@@ -97,7 +97,7 @@ sag_detect(const GwyFileDetectInfo *fileinfo, gboolean only_name)
         return g_str_has_suffix(fileinfo->name_lowercase, EXTENSION) ? 20 : 0;
 
     if (sag_read_header(fileinfo->head, fileinfo->buffer_len, &sagfile, NULL))
-        return 80;
+        return 60;
 
     return 0;
 }
@@ -107,28 +107,101 @@ sag_load(const gchar *filename,
          G_GNUC_UNUSED GwyRunType mode,
          GError **error)
 {
-    GwyContainer *container = NULL;
+    static const gchar *titles[4] = {
+        "Height", "dz/dx", "dz/dy", "d²Z/dXdY"
+    };
 
-    err_NO_DATA(error);
+    SagFile sagfile;
+    GwyContainer *container = NULL;
+    GwyDataField *fields[5];
+    gchar *p, *line, *end, *buffer = NULL;
+    GError *err = NULL;
+    gdouble q = 1.0;
+    gsize size = 0;
+    GString *str;
+    guint len, i, k;
+
+    if (!g_file_get_contents(filename, &buffer, &size, &err)) {
+        err_GET_FILE_CONTENTS(error, &err);
+        return NULL;
+    }
+
+    gwy_clear(fields, G_N_ELEMENTS(fields));
+    if (!(len = sag_read_header(buffer, size, &sagfile, error)))
+        goto fail;
+
+    p = buffer + len;
+
+    if (sagfile.unit == SAG_UNIT_MM)
+        q = 1e-3;
+    else if (sagfile.unit == SAG_UNIT_CM)
+        q = 1e-2;
+    else if (sagfile.unit == SAG_UNIT_IN)
+        q = 0.0254;
+
+    for (i = 0; i < G_N_ELEMENTS(fields); i++) {
+        fields[i] = gwy_data_field_new(sagfile.xres,
+                                       sagfile.yres,
+                                       sagfile.xres*sagfile.dx*q,
+                                       sagfile.yres*sagfile.dy*q,
+                                       FALSE);
+    }
+
+    for (k = 0; k < sagfile.xres*sagfile.yres; k++) {
+        line = gwy_str_next_line(&p);
+        if (!line) {
+            /* TODO: Fail miserably. */
+        }
+
+        for (i = 0; i < G_N_ELEMENTS(fields); i++) {
+            fields[i]->data[k] = g_ascii_strtod(line, &end);
+            if (end == line) {
+                /* TODO: Fail miserably. */
+            }
+            line = end;
+        }
+    }
+
+    gwy_data_field_multiply(fields[0], q);
+
+    container = gwy_container_new();
+    str = g_string_new(NULL);
+    for (i = 0; i < G_N_ELEMENTS(titles); i++) {
+        GQuark quark = gwy_app_get_data_key_for_id(i);
+        gwy_container_set_object(container, quark, fields[i]);
+        g_string_assign(str, g_quark_to_string(quark));
+        g_string_append(str, "/title");
+        gwy_container_set_string_by_name(container, str->str,
+                                         g_strdup(titles[i]));
+    }
+    g_string_free(str, TRUE);
+
+fail:
+    g_free(buffer);
+    for (i = 0; i < G_N_ELEMENTS(fields); i++)
+        gwy_object_unref(fields[i]);
 
     return container;
 }
 
+/* XXX: This is a weak detection.  We basically accept anything that has seven
+ * reasonable numbers on the first line. */
 static guint
 sag_read_header(const gchar *header,
                 guint len,
                 SagFile *sagfile,
                 GError **error)
 {
-    const gchar *p = header;
-    gchar *end;
+    const gchar *p = header, *lineend;
+    gchar *end, *line = NULL;
 
     /* Weed out binary files quickly. */
     if (len < 16 || (header[0] != '!' && !g_ascii_isdigit(header[0])))
         goto fail;
 
-    /* Avoid memory allocations and stuff since this is also used in the
-     * detection path which should be fast. */
+    /* Avoid premature memory allocations and stuff since this is also used in
+     * the detection path which should be fast.  Find the first line which does
+     * not start with '!'. */
     while (header[0] == '!') {
         while (*header && *header != '\n' && *header != '\r')
             header++;
@@ -139,34 +212,69 @@ sag_read_header(const gchar *header,
             goto fail;
     }
 
+    /* Find its end. */
+    lineend = header;
+    while (*lineend && *lineend != '\n' && *lineend != '\r')
+        lineend++;
+    while (*lineend == '\n' || *lineend == '\r')
+        lineend++;
+
+    line = g_strndup(header, lineend-header);
+    header = line;
+
     sagfile->xres = strtol(header, &end, 10);
     gwy_debug("xres %u", sagfile->xres);
     if (end == header)
         goto fail;
+    if (err_DIMENSION(error, sagfile->xres)) {
+        g_free(line);
+        return 0;
+    }
     header = end;
 
     sagfile->yres = strtol(header, &end, 10);
     gwy_debug("yres %u", sagfile->yres);
     if (end == header)
         goto fail;
+    if (err_DIMENSION(error, sagfile->yres)) {
+        g_free(line);
+        return 0;
+    }
     header = end;
 
-    sagfile->xreal = g_ascii_strtod(header, &end);
-    gwy_debug("xreal %g", sagfile->xreal);
+    sagfile->dx = g_ascii_strtod(header, &end);
+    gwy_debug("dx %g", sagfile->dx);
     if (end == header)
         goto fail;
+    /* Use negated positive conditions to catch NaNs */
+    if (!((sagfile->dx = fabs(sagfile->dx)) > 0)) {
+        g_warning("Real x step is 0.0, fixing to 1.0");
+        sagfile->dx = 1.0;
+    }
     header = end;
 
-    sagfile->yreal = g_ascii_strtod(header, &end);
-    gwy_debug("yreal %g", sagfile->yreal);
+    sagfile->dy = g_ascii_strtod(header, &end);
+    gwy_debug("dy %g", sagfile->dy);
     if (end == header)
         goto fail;
+    /* Use negated positive conditions to catch NaNs */
+    if (!((sagfile->dy = fabs(sagfile->dy)) > 0)) {
+        g_warning("Real x step is 0.0, fixing to 1.0");
+        sagfile->dy = 1.0;
+    }
     header = end;
 
     sagfile->unit = strtol(header, &end, 10);
     gwy_debug("unit %u", sagfile->unit);
     if (end == header)
         goto fail;
+    if (sagfile->unit >= SAG_NUNITS) {
+        g_free(line);
+        g_set_error(error, GWY_MODULE_FILE_ERROR, GWY_MODULE_FILE_ERROR_DATA,
+                    _("Unit code %d is invalid or unsupported."),
+                    sagfile->unit);
+        return 0;
+    }
     header = end;
 
     sagfile->xoff = g_ascii_strtod(header, &end);
@@ -181,14 +289,21 @@ sag_read_header(const gchar *header,
         goto fail;
     header = end;
 
-    while (*header && *header != '\n' && *header != '\r')
-        header++;
-    while (*header == '\n' || *header == '\r')
+    while (*header && g_ascii_isspace(*header))
         header++;
 
-    return header - p;
+    if (*header) {
+        g_free(line);
+        g_set_error(error, GWY_MODULE_FILE_ERROR, GWY_MODULE_FILE_ERROR_DATA,
+                    _("The first line contrains too many items."));
+        return 0;
+    }
+
+    g_free(line);
+    return lineend - p;
 
 fail:
+    g_free(line);
     err_FILE_TYPE(error, "SAG");
     return 0;
 }
