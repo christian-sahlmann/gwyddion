ac346d7bb4fc6d972ce177496c2f3312a5254e09
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2013-11-29 14:38:06 +0000

    - calculate ACF and allow switching between data and ACF - display the selected lattice vectors
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@15684 25697fe4-d644-49b8-9d30-be5465f2adf3

--- modules/process/correct_affine.c
+++ modules/process/correct_affine.c
@@ -24,15 +24,12 @@
 #include <gtk/gtk.h>
 #include <libgwyddion/gwymacros.h>
 #include <libgwyddion/gwymath.h>
-#include <libprocess/inttrans.h>
 #include <libprocess/gwyprocesstypes.h>
-#include <libprocess/linestats.h>
+#include <libprocess/stats.h>
 #include <libgwydgets/gwydataview.h>
 #include <libgwydgets/gwylayer-basic.h>
-#include <libgwydgets/gwygraph.h>
 #include <libgwydgets/gwydgetutils.h>
-#include <libgwydgets/gwycombobox.h>
-#include <libgwydgets/gwystock.h>
+#include <libgwydgets/gwyradiobuttons.h>
 #include <libgwymodule/gwymodule-process.h>
 #include <app/gwymoduleutils.h>
 #include <app/gwyapp.h>
@@ -47,8 +44,15 @@ enum {
     RESPONSE_RESET = 1,
 };
 
+typedef enum {
+    IMAGE_DATA,
+    IMAGE_ACF,
+} ImageMode;
+
 typedef struct {
     gboolean whatever;
+
+    ImageMode image_mode;
 } AffcorArgs;
 
 typedef struct {
@@ -57,27 +61,37 @@ typedef struct {
     GtkWidget *view;
     GwySelection *selection;
     GwyContainer *mydata;
+    GSList *image_mode;
+    GwySIValueFormat *vf;
+    GtkWidget *a1;
+    GtkWidget *a2;
 } AffcorControls;
 
-static gboolean module_register     (void);
-static void     correct_affine      (GwyContainer *data,
-                                     GwyRunType run);
-static void     affcor_dialog       (AffcorArgs *args,
-                                     GwyContainer *data,
-                                     GwyDataField *dfield,
-                                     gint id);
-static void     init_selection      (GwySelection *selection,
-                                     GwyDataField *dfield);
-static void     selection_changed   (AffcorControls *controls,
-                                     gint hint);
-static void     affcor_load_args    (GwyContainer *container,
-                                     AffcorArgs *args);
-static void     affcor_save_args    (GwyContainer *container,
-                                     AffcorArgs *args);
-static void     affcor_sanitize_args(AffcorArgs *args);
+static gboolean   module_register     (void);
+static void       correct_affine      (GwyContainer *data,
+                                       GwyRunType run);
+static void       affcor_dialog       (AffcorArgs *args,
+                                       GwyContainer *data,
+                                       GwyDataField *dfield,
+                                       gint id);
+static GtkWidget* add_lattice_label   (GtkTable *table,
+                                       const gchar *name,
+                                       gint *row,
+                                       GwySIValueFormat *vf);
+static void       init_selection      (GwySelection *selection,
+                                       GwyDataField *dfield);
+static void       selection_changed   (AffcorControls *controls);
+static void       image_mode_changed  (GtkToggleButton *button,
+                                       AffcorControls *controls);
+static void       affcor_load_args    (GwyContainer *container,
+                                       AffcorArgs *args);
+static void       affcor_save_args    (GwyContainer *container,
+                                       AffcorArgs *args);
+static void       affcor_sanitize_args(AffcorArgs *args);
 
 static const AffcorArgs affcor_defaults = {
     FALSE,
+    IMAGE_DATA,
 };
 
 static GwyModuleInfo module_info = {
@@ -132,12 +146,15 @@ affcor_dialog(AffcorArgs *args,
               GwyDataField *dfield,
               gint id)
 {
-    GtkWidget *hbox, *hbox2, *vbox, *label;
+    GtkWidget *hbox, *label;
     GtkDialog *dialog;
+    GtkTable *table;
+    GwyDataField *acf;
     AffcorControls controls;
     gint response;
     GwyPixmapLayer *layer;
     GwyVectorLayer *vlayer;
+    gint row;
 
     controls.args = args;
 
@@ -160,14 +177,27 @@ affcor_dialog(AffcorArgs *args,
                        FALSE, FALSE, 4);
 
     controls.mydata = gwy_container_new();
-    gwy_container_set_object_by_name(controls.mydata, "/0/data",
-                                     dfield);
+    gwy_container_set_object_by_name(controls.mydata, "/0/data", dfield);
     gwy_app_sync_data_items(data, controls.mydata, id, 0, FALSE,
                             GWY_DATA_ITEM_RANGE_TYPE,
                             GWY_DATA_ITEM_RANGE,
                             GWY_DATA_ITEM_GRADIENT,
                             GWY_DATA_ITEM_REAL_SQUARE,
                             0);
+
+    acf = gwy_data_field_new_alike(dfield, FALSE);
+    gwy_data_field_area_2dacf(dfield, acf, 0, 0, dfield->xres, dfield->yres,
+                              dfield->xres/4, dfield->yres/4);
+
+    gwy_container_set_object_by_name(controls.mydata, "/1/data", acf);
+    g_object_unref(acf);
+    gwy_app_sync_data_items(data, controls.mydata, id, 1, FALSE,
+                            GWY_DATA_ITEM_RANGE_TYPE,
+                            GWY_DATA_ITEM_RANGE,
+                            GWY_DATA_ITEM_GRADIENT,
+                            GWY_DATA_ITEM_REAL_SQUARE,
+                            0);
+
     controls.view = gwy_data_view_new(controls.mydata);
     layer = gwy_layer_basic_new();
     g_object_set(layer,
@@ -192,7 +222,41 @@ affcor_dialog(AffcorArgs *args,
 
     gtk_box_pack_start(GTK_BOX(hbox), controls.view, FALSE, FALSE, 4);
 
+    table = GTK_TABLE(gtk_table_new(6, 3, FALSE));
+    gtk_table_set_row_spacings(GTK_TABLE(table), 2);
+    gtk_table_set_col_spacings(GTK_TABLE(table), 6);
+    gtk_container_set_border_width(GTK_CONTAINER(table), 4);
+    gtk_box_pack_start(GTK_BOX(hbox), GTK_WIDGET(table), FALSE, FALSE, 0);
+    row = 0;
 
+    /* XXX: This is not a preview, we should also have a real preview. */
+    label = gtk_label_new(_("Preview type:"));
+    gtk_misc_set_alignment(GTK_MISC(label), 0.0, 0.5);
+    gtk_table_attach(GTK_TABLE(table), label,
+                     0, 3, row, row+1, GTK_FILL, 0, 0, 0);
+    row++;
+
+    controls.image_mode
+        = gwy_radio_buttons_createl(G_CALLBACK(image_mode_changed), &controls,
+                                    args->image_mode,
+                                    _("Data"), IMAGE_DATA,
+                                    _("2D ACF"), IMAGE_ACF,
+                                    NULL);
+    row = gwy_radio_buttons_attach_to_table(controls.image_mode,
+                                            GTK_TABLE(table), 3, row);
+    gtk_table_set_row_spacing(GTK_TABLE(table), row-1, 8);
+
+    label = gwy_label_new_header(_("Lattice Vectors"));
+    gtk_table_attach(GTK_TABLE(table), label,
+                     0, 3, row, row+1, GTK_FILL, 0, 0, 0);
+    row++;
+
+    controls.vf = gwy_data_field_get_value_format_xy(dfield,
+                                                     GWY_SI_UNIT_FORMAT_VFMARKUP,
+                                                     NULL);
+    controls.a1 = add_lattice_label(table, "a<sub>1</sub>:", &row, controls.vf);
+    controls.a2 = add_lattice_label(table, "a<sub>2</sub>:", &row, controls.vf);
+    gtk_table_set_row_spacing(GTK_TABLE(table), row-1, 8);
 
     gtk_widget_show_all(controls.dialog);
     do {
@@ -220,7 +284,40 @@ affcor_dialog(AffcorArgs *args,
 
     gtk_widget_destroy(controls.dialog);
 finalize:
+    gwy_si_unit_value_format_free(controls.vf);
     g_object_unref(controls.mydata);
+}
+
+static GtkWidget*
+add_lattice_label(GtkTable *table,
+                  const gchar *name,
+                  gint *row,
+                  GwySIValueFormat *vf)
+{
+    GtkWidget *label;
+    GtkRequisition req;
+
+    label = gtk_label_new("-1234.567");
+    gtk_widget_size_request(label, &req);
+
+    gtk_label_set_markup(GTK_LABEL(label), name);
+    gtk_misc_set_alignment(GTK_MISC(label), 0.0, 0.5);
+    gtk_table_attach(table, label, 0, 1, *row, *row+1, GTK_FILL, 0, 0, 0);
+
+    label = gtk_label_new(NULL);
+    gtk_label_set_markup(GTK_LABEL(label), vf->units);
+    gtk_misc_set_alignment(GTK_MISC(label), 0.0, 0.5);
+    gtk_table_attach(table, label, 2, 3, *row, *row+1, GTK_FILL, 0, 0, 0);
+
+    label = gtk_label_new(NULL);
+    gtk_widget_set_size_request(label, req.width, -1);
+    gtk_misc_set_alignment(GTK_MISC(label), 1.0, 0.5);
+    gtk_table_attach(table, label,
+                     1, 2, *row, *row+1, GTK_EXPAND | GTK_FILL, 0, 0, 0);
+
+    (*row)++;
+
+    return label;
 }
 
 static void
@@ -235,9 +332,54 @@ init_selection(GwySelection *selection,
 }
 
 static void
-selection_changed(AffcorControls *controls,
-                  gint hint)
+selection_changed(AffcorControls *controls)
 {
+    GwySIValueFormat *vf;
+    gdouble xy[4];
+    gchar *buf;
+
+    if (!gwy_selection_get_data(controls->selection, NULL)) {
+        gtk_label_set_text(GTK_LABEL(controls->a1), "");
+        gtk_label_set_text(GTK_LABEL(controls->a2), "");
+        return;
+    }
+
+    gwy_selection_get_object(controls->selection, 0, xy);
+    vf = controls->vf;
+
+    buf = g_strdup_printf("(%.*f, %.*f)",
+                          vf->precision, xy[0]/vf->magnitude,
+                          vf->precision, xy[1]/vf->magnitude);
+    gtk_label_set_text(GTK_LABEL(controls->a1), buf);
+    g_free(buf);
+
+    buf = g_strdup_printf("(%.*f, %.*f)",
+                          vf->precision, xy[2]/vf->magnitude,
+                          vf->precision, xy[3]/vf->magnitude);
+    gtk_label_set_text(GTK_LABEL(controls->a2), buf);
+    g_free(buf);
+}
+
+static void
+image_mode_changed(G_GNUC_UNUSED GtkToggleButton *button,
+                   AffcorControls *controls)
+{
+    AffcorArgs *args = controls->args;
+    GwyPixmapLayer *layer;
+    ImageMode mode;
+
+    mode = gwy_radio_buttons_get_current(controls->image_mode);
+    if (mode == args->image_mode)
+        return;
+    args->image_mode = mode;
+    layer = gwy_data_view_get_base_layer(GWY_DATA_VIEW(controls->view));
+
+    if (args->image_mode == IMAGE_DATA)
+        gwy_pixmap_layer_set_data_key(layer, "/0/data");
+    else if (args->image_mode == IMAGE_ACF)
+        gwy_pixmap_layer_set_data_key(layer, "/1/data");
+
+    gwy_set_data_preview_size(GWY_DATA_VIEW(controls->view), PREVIEW_SIZE);
 }
 
 //static const gchar separate_key[]      = "/module/fft_profile/separate";
