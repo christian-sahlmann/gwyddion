c1bc65e089d2bd4362b06acb8f5d1e1419112663
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2013-12-05 12:11:42 +0000

    - use a collation helper which sorts cyrillic more naturally
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@15702 25697fe4-d644-49b8-9d30-be5465f2adf3

--- utils/gen-authors.py
+++ utils/gen-authors.py
@@ -1,31 +1,39 @@
 #!/usr/bin/python
+# vim: set fileencoding=utf-8 :
 # @(#) $Id$
 import re, sys, locale
 from xml.sax.saxutils import escape
 
+cyrillic_translit = u'АAБBВVГGДDЕEЁEЖZЗZИIЙJКKЛLМMНNОOПPРRСSТTУUФFХHЦCЧCШSЩSЪZЫZЬZЭEЮUЯA'
+cyrillic_map = dict((cyrillic_translit[2*i], cyrillic_translit[2*i + 1])
+                    for i in range(len(cyrillic_translit)/2))
+
 locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
 
-contrib_re = re.compile(r'(?ms)^(?P<name>\S[^<>]+?)\s+'
-                        r'<(?P<email>[^<> ]+)>\n'
-                        r'(?P<what>(?:^ +\S[^\n]+\n)+)')
+contrib_re = re.compile(ur'(?ms)^(?P<name>\S[^<>]+?)\s+'
+                        ur'<(?P<email>[^<> ]+)>\n'
+                        ur'(?P<what>(?:^ +\S[^\n]+\n)+)')
 
 def sortkey(x):
-    x = x.split()[-1] + ' ' + x
+    last = [y for y in x.split() if not y.startswith(u'(')][-1]
+    x = last + u' ' + x
+    if x[0] in cyrillic_map:
+        x = cyrillic_map[x[0]] + x
     return locale.strxfrm(x.encode('utf-8'))
 
 def parse_contributors(text):
     contributors = {}
     for m in contrib_re.finditer(text):
         name, email = m.group('name'), m.group('email')
-        what = re.sub(r'(?s)\s+', r' ', m.group('what').strip())
+        what = re.sub(ur'(?s)\s+', ur' ', m.group('what').strip())
         contributors[name] = (email, what)
     return contributors
 
 def format_list(text, section):
-    header_re = re.compile(r'(?ms)^=== ' + section + ' ===(?P<body>.*?)^$')
+    header_re = re.compile(ur'(?ms)^=== ' + section + ur' ===(?P<body>.*?)^$')
     contributors = parse_contributors(header_re.search(text).group('body'))
-    sectid = re.sub(r'[^a-zA-Z]', '', section.lower())
-    out = [u'<p id="%s">%s:</p>' % (sectid, section)]
+    sectid = re.sub(ur'[^a-zA-Z]', '', section.lower())
+    out = [u'<p id="%s"><b>%s:</b></p>' % (sectid, section)]
     out.append(u'<dl>')
     for name in sorted(contributors.keys(), key=sortkey):
         email, what = contributors[name]
