fd6710e663f6680b29c1f8f586e7af48868dd77f
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2013-12-31 13:21:22 +0000

    - disable ubuntu menuproxy
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@15796 25697fe4-d644-49b8-9d30-be5465f2adf3

--- app/gwyddion.c
+++ app/gwyddion.c
@@ -99,9 +99,10 @@ main(int argc, char *argv[])
     GTimer *timer;
 
     sneaking_thread_init();
-    // If I print a debugging messages that's because someone wants to see it
+    // If I print a debugging message that's because someone wants to see it
     // you idiots.
     g_setenv("G_MESSAGES_DEBUG", "all", TRUE);
+    g_unsetenv("UBUNTU_MENUPROXY");
     timer = g_timer_new();
     gwy_app_check_version();
 
