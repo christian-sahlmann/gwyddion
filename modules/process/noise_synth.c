48f89a09b905534b4daa90abba2daaec9bcfefea
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2014-05-30 19:50:20 +0000

    - use GwyRandGenSet
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@16233 25697fe4-d644-49b8-9d30-be5465f2adf3

--- modules/process/noise_synth.c
+++ modules/process/noise_synth.c
@@ -24,6 +24,7 @@
 #include <gtk/gtk.h>
 #include <libgwyddion/gwymacros.h>
 #include <libgwyddion/gwymath.h>
+#include <libgwyddion/gwyrandgenset.h>
 #include <libprocess/stats.h>
 #include <libprocess/arithmetic.h>
 #include <libgwydgets/gwydataview.h>
@@ -41,10 +42,9 @@
 #define NOISE_DISTRIBUTION_RUN_MODES (GWY_RUN_IMMEDIATE | GWY_RUN_INTERACTIVE)
 
 #define DECLARE_NOISE(name) \
-    static gdouble noise_##name##_both(GRand *rng, gdouble sigma); \
-    static gdouble noise_##name##_up(GRand *rng, gdouble sigma); \
-    static gdouble noise_##name##_down(GRand *rng, gdouble sigma); \
-    static gdouble rand_gen_##name(GRand *rng, gdouble sigma)
+    static gdouble noise_##name##_both(GwyRandGenSet *rng, gdouble sigma); \
+    static gdouble noise_##name##_up(GwyRandGenSet *rng, gdouble sigma); \
+    static gdouble noise_##name##_down(GwyRandGenSet *rng, gdouble sigma);
 
 enum {
     PREVIEW_SIZE = 400,
@@ -75,7 +75,7 @@ typedef enum {
     NOISE_DIRECTION_NTYPES
 } NoiseDirectionType;
 
-typedef gdouble (*PointNoiseFunc)(GRand *rng, gdouble sigma);
+typedef gdouble (*PointNoiseFunc)(GwyRandGenSet *rng, gdouble sigma);
 
 /* This scheme makes the distribution type list easily reordeable in the GUI
  * without changing the ids.  Directions need not be reordered. */
@@ -83,7 +83,6 @@ typedef struct {
     NoiseDistributionType distribution;
     const gchar *name;
     PointNoiseFunc point_noise[NOISE_DIRECTION_NTYPES];
-    PointNoiseFunc base_generator;
 } NoiseSynthGenerator;
 
 typedef struct {
@@ -184,25 +183,21 @@ static const NoiseSynthGenerator generators[] = {
         NOISE_DISTRIBUTION_GAUSSIAN,
         N_("distribution|Gaussian"),
         { &noise_gaussian_both, &noise_gaussian_up, &noise_gaussian_down, },
-        rand_gen_gaussian,
     },
     {
         NOISE_DISTRIBUTION_EXPONENTIAL,
         N_("distribution|Exponential"),
         { &noise_exp_both, &noise_exp_up, &noise_exp_down, },
-        rand_gen_exp,
     },
     {
         NOISE_DISTRIBUTION_UNIFORM,
         N_("distribution|Uniform"),
         { &noise_uniform_both, &noise_uniform_up, &noise_uniform_down, },
-        rand_gen_uniform,
     },
     {
         NOISE_DISTRIBUTION_TRIANGULAR,
         N_("distribution|Triangular"),
         { &noise_triangle_both, &noise_triangle_up, &noise_triangle_down, },
-        rand_gen_triangle,
     },
 };
 
@@ -211,7 +206,7 @@ static GwyModuleInfo module_info = {
     &module_register,
     N_("Generates uncorrelated random noise."),
     "Yeti <yeti@gwyddion.net>",
-    "1.1",
+    "1.2",
     "David Nečas (Yeti)",
     "2010",
 };
@@ -665,18 +660,16 @@ noise_synth_do(const NoiseSynthArgs *args,
 {
     const NoiseSynthGenerator *generator;
     PointNoiseFunc point_noise;
-    GRand *rng;
+    GwyRandGenSet *rngset;
     gdouble *data;
     gdouble sigma;
     gint xres, yres, i;
 
     generator = get_point_noise_generator(args->distribution);
     point_noise = generator->point_noise[args->direction];
-    /* Clear spare values possibly saved in the base generator */
-    generator->base_generator(NULL, 0.0);
 
-    rng = g_rand_new();
-    g_rand_set_seed(rng, args->seed);
+    rngset = gwy_rand_gen_set_new(1);
+    gwy_rand_gen_set_init(rngset, args->seed);
 
     xres = gwy_data_field_get_xres(dfield);
     yres = gwy_data_field_get_yres(dfield);
@@ -684,191 +677,84 @@ noise_synth_do(const NoiseSynthArgs *args,
     sigma = args->sigma * pow10(dimsargs->zpow10);
 
     for (i = 0; i < xres*yres; i++)
-        data[i] += point_noise(rng, sigma);
+        data[i] += point_noise(rngset, sigma);
 
-    g_rand_free(rng);
-}
-
-static gdouble
-rand_gen_gaussian(GRand *rng,
-                  gdouble sigma)
-{
-    static gboolean have_spare = FALSE;
-    static gdouble spare;
-
-    gdouble x, y, w;
-
-    /* Calling with NULL rng just clears the spare random value. */
-    if (have_spare || G_UNLIKELY(!rng)) {
-        have_spare = FALSE;
-        return sigma*spare;
-    }
-
-    do {
-        x = -1.0 + 2.0*g_rand_double(rng);
-        y = -1.0 + 2.0*g_rand_double(rng);
-        w = x*x + y*y;
-    } while (w >= 1.0 || G_UNLIKELY(w == 0.0));
-
-    w = sqrt(-2.0*log(w)/w);
-    spare = y*w;
-    have_spare = TRUE;
-
-    return sigma*x*w;
-}
-
-static gdouble
-rand_gen_exp(GRand *rng,
-             gdouble sigma)
-{
-    static guint spare_bits = 0;
-    static guint32 spare;
-
-    gdouble x;
-    gboolean sign;
-
-    /* Calling with NULL rng just clears the spare random value. */
-    if (G_UNLIKELY(!rng)) {
-        spare_bits = 0;
-        return 0.0;
-    }
-
-    x = g_rand_double(rng);
-    /* This is how we get exact 0.0 at least sometimes */
-    if (G_UNLIKELY(x == 0.0))
-        return 0.0;
-
-    if (!spare_bits) {
-        spare = g_rand_int(rng);
-        spare_bits = 32;
-    }
-
-    sign = spare & 1;
-    spare >>= 1;
-    spare_bits--;
-
-    if (sign)
-        return -sigma/G_SQRT2*log(x);
-    else
-        return sigma/G_SQRT2*log(x);
-}
-
-static gdouble
-rand_gen_uniform(GRand *rng,
-                 gdouble sigma)
-{
-    gdouble x;
-
-    if (G_UNLIKELY(!rng))
-        return 0.0;
-
-    do {
-        x = g_rand_double(rng);
-    } while (G_UNLIKELY(x == 0.0));
-
-    return (2.0*x - 1.0)*GWY_SQRT3*sigma;
-}
-
-static gdouble
-rand_gen_triangle(GRand *rng,
-                  gdouble sigma)
-{
-    gdouble x;
-
-    if (G_UNLIKELY(!rng))
-        return 0.0;
-
-    do {
-        x = g_rand_double(rng);
-    } while (G_UNLIKELY(x == 0.0));
-
-    return (x <= 0.5 ? sqrt(2.0*x) - 1.0 : 1.0 - sqrt(2.0*(1.0 - x)))
-           *sigma*GWY_SQRT6;
+    gwy_rand_gen_set_free(rngset);
 }
 
 /* XXX: Sometimes the generators seem unnecessarily complicated; this is to
  * make the positive and negative noise related to the symmetrical one. */
 
 static gdouble
-noise_gaussian_both(GRand *rng, gdouble sigma)
+noise_gaussian_both(GwyRandGenSet *rng, gdouble sigma)
 {
-    return rand_gen_gaussian(rng, sigma);
+    return gwy_rand_gen_set_gaussian(rng, 0, sigma);
 }
 
 static gdouble
-noise_gaussian_up(GRand *rng, gdouble sigma)
+noise_gaussian_up(GwyRandGenSet *rng, gdouble sigma)
 {
-    return fabs(rand_gen_gaussian(rng, sigma));
+    return fabs(gwy_rand_gen_set_gaussian(rng, 0, sigma));
 }
 
 static gdouble
-noise_gaussian_down(GRand *rng, gdouble sigma)
+noise_gaussian_down(GwyRandGenSet *rng, gdouble sigma)
 {
-    return -fabs(rand_gen_gaussian(rng, sigma));
+    return -fabs(gwy_rand_gen_set_gaussian(rng, 0, sigma));
 }
 
 static gdouble
-noise_exp_both(GRand *rng, gdouble sigma)
+noise_exp_both(GwyRandGenSet *rng, gdouble sigma)
 {
-    return rand_gen_exp(rng, sigma);
+    return gwy_rand_gen_set_exponential(rng, 0, sigma);
 }
 
 static gdouble
-noise_exp_up(GRand *rng, gdouble sigma)
+noise_exp_up(GwyRandGenSet *rng, gdouble sigma)
 {
-    gdouble x = g_rand_double(rng);
-
-    if (G_UNLIKELY(x == 0.0))
-        return 0.0;
-
-    return -sigma/G_SQRT2*log(x);
+    return fabs(gwy_rand_gen_set_exponential(rng, 0, sigma));
 }
 
 static gdouble
-noise_exp_down(GRand *rng, gdouble sigma)
+noise_exp_down(GwyRandGenSet *rng, gdouble sigma)
 {
-    gdouble x = g_rand_double(rng);
-
-    if (G_UNLIKELY(x == 0.0))
-        return 0.0;
-
-    return sigma/G_SQRT2*log(x);
+    return -fabs(gwy_rand_gen_set_exponential(rng, 0, sigma));
 }
 
 static gdouble
-noise_uniform_both(GRand *rng, gdouble sigma)
+noise_uniform_both(GwyRandGenSet *rng, gdouble sigma)
 {
-    return rand_gen_uniform(rng, sigma);
+    return gwy_rand_gen_set_uniform(rng, 0, sigma);
 }
 
 static gdouble
-noise_uniform_up(GRand *rng, gdouble sigma)
+noise_uniform_up(GwyRandGenSet *rng, gdouble sigma)
 {
-    return fabs(rand_gen_uniform(rng, sigma));
+    return fabs(gwy_rand_gen_set_uniform(rng, 0, sigma));
 }
 
 static gdouble
-noise_uniform_down(GRand *rng, gdouble sigma)
+noise_uniform_down(GwyRandGenSet *rng, gdouble sigma)
 {
-    return -fabs(rand_gen_uniform(rng, sigma));
+    return -fabs(gwy_rand_gen_set_uniform(rng, 0, sigma));
 }
 
 static gdouble
-noise_triangle_both(GRand *rng, gdouble sigma)
+noise_triangle_both(GwyRandGenSet *rng, gdouble sigma)
 {
-    return rand_gen_triangle(rng, sigma);
+    return gwy_rand_gen_set_triangular(rng, 0, sigma);
 }
 
 static gdouble
-noise_triangle_up(GRand *rng, gdouble sigma)
+noise_triangle_up(GwyRandGenSet *rng, gdouble sigma)
 {
-    return fabs(rand_gen_triangle(rng, sigma));
+    return fabs(gwy_rand_gen_set_triangular(rng, 0, sigma));
 }
 
 static gdouble
-noise_triangle_down(GRand *rng, gdouble sigma)
+noise_triangle_down(GwyRandGenSet *rng, gdouble sigma)
 {
-    return -fabs(rand_gen_triangle(rng, sigma));
+    return -fabs(gwy_rand_gen_set_triangular(rng, 0, sigma));
 }
 
 static const gchar prefix[]           = "/module/noise_synth";
