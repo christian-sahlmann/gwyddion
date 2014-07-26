e84944576012cbef9f7fa20c256316767a0bd92b
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2014-07-26 13:10:13 +0000

    - started JEOL JSPM file import module - seems to be a development of the format loaded by jeol.c but hard to tell
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@16452 25697fe4-d644-49b8-9d30-be5465f2adf3

---
+++ modules/file/jspmfile.c
@@ -0,0 +1,361 @@
+/*
+ *  @(#) $Id$
+ *  Copyright (C) 2014 David Necas (Yeti).
+ *  E-mail: yeti@gwyddion.net.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ *  Boston, MA 02110-1301, USA.
+ */
+
+/* This format seems similar in spirit to jeol.c, but newer, more complicated
+ * and, above all, lacking proper documentation. */
+
+/**
+ * [FILE-MAGIC-FREEDESKTOP]
+ * <mime-type type="application/x-jeol-jspm">
+ *   <comment>JEOL JSPM data</comment>
+ *   <magic priority="80">
+ *     <match type="string" offset="0" value="II\x2a\x00">
+ *       <match type="string" offset="30" value="JEOL SPM">
+ *         <match type="string" offset="62" value="WinSPM "/>
+ *       </match>
+ *     </match>
+ *   </magic>
+ * </mime-type>
+ **/
+
+/**
+ * [FILE-MAGIC-FILEMAGIC]
+ * # JEOL JSPM data (new, complex)
+ * 0 string II\x2a\x00
+ * >30 string JEOL\x20SPM
+ * >>62 string WinSPM JEOL JSPM data,
+ * >>>10 leshort x version %d
+ **/
+
+/**
+ * [FILE-MAGIC-USERGUIDE]
+ * JEOL JSPM
+ * .tif
+ * Read
+ **/
+#define DEBUG 1
+#include "config.h"
+#include <string.h>
+#include <stdarg.h>
+#include <libgwyddion/gwymacros.h>
+#include <libgwyddion/gwymath.h>
+#include <libgwyddion/gwyutils.h>
+#include <libprocess/stats.h>
+#include <libgwymodule/gwymodule-file.h>
+#include <app/gwymoduleutils-file.h>
+
+#include "err.h"
+#include "get.h"
+
+#define MAGIC      "II\x2a\x00"
+#define MAGIC_SIZE (sizeof(MAGIC) - 1)
+
+#define JEOL_MAGIC1 "JEOL SPM"
+#define JEOL_MAGIC1_SIZE (sizeof(JEOL_MAGIC1) - 1)
+
+#define JEOL_MAGIC2 "WinSPM "
+#define JEOL_MAGIC2_SIZE (sizeof(JEOL_MAGIC2) - 1)
+
+#define Nanometer (1e-9)
+#define Picometer (1e-12)
+
+enum {
+    TIFF_HEADER_SIZE = 0x000a,
+};
+
+typedef struct {
+    guint offset;
+    guint len;
+} JSPMHeaderBlock;
+
+typedef struct {
+    GArray *blocks;
+    /* Useful information we are actually able to extract from the blocks. */
+    guint winspm_version;
+    guint xres;
+    guint yres;
+    gdouble xreal;
+    gdouble yreal;
+    guint data_offset;
+} JSPMFile;
+
+static gboolean      module_register        (void);
+static gint          jspm_detect            (const GwyFileDetectInfo *fileinfo,
+                                             gboolean only_name);
+static GwyContainer* jspm_load              (const gchar *filename,
+                                             GwyRunType mode,
+                                             GError **error);
+static gboolean      meas_header_seems_ok   (const gchar *buffer);
+static gboolean      jspm_read_headers      (JSPMFile *jspmfile,
+                                             const guchar *buffer,
+                                             gsize size,
+                                             GError **error);
+static gboolean      read_image_header_block(JSPMFile *jspmfile,
+                                             const guchar *buffer,
+                                             gsize size,
+                                             GError **error);
+static GwyDataField* jspm_read_data_field   (const JSPMFile *jspmfile,
+                                             const guchar *buffer);
+
+static GwyModuleInfo module_info = {
+    GWY_MODULE_ABI_VERSION,
+    module_register,
+    N_("Imports JEOL JSPM data files."),
+    "Yeti <yeti@gwyddion.net>",
+    "0.1",
+    "David Nečas (Yeti)",
+    "2014",
+};
+
+GWY_MODULE_QUERY(module_info)
+
+static gboolean
+module_register(void)
+{
+    gwy_file_func_register("jspmfile",
+                           N_("JEOL JSPM data files (.tif)"),
+                           (GwyFileDetectFunc)&jspm_detect,
+                           (GwyFileLoadFunc)&jspm_load,
+                           NULL,
+                           NULL);
+
+    return TRUE;
+}
+
+static gint
+jspm_detect(const GwyFileDetectInfo *fileinfo, gboolean only_name)
+{
+    if (only_name)
+        return 0;
+
+    if (fileinfo->buffer_len <= MAGIC_SIZE
+        || memcmp(fileinfo->head, MAGIC, MAGIC_SIZE) != 0)
+        return 0;
+
+    if (fileinfo->buffer_len < 0x48
+        || !meas_header_seems_ok(fileinfo->head))
+        return 0;
+
+    return 100;
+}
+
+static GwyContainer*
+jspm_load(const gchar *filename,
+          G_GNUC_UNUSED GwyRunType mode,
+          GError **error)
+{
+    JSPMFile jspmfile;
+    GwyContainer *container = NULL;
+    guchar *buffer = NULL;
+    gsize size = 0;
+    GError *err = NULL;
+    GwyDataField *dfield = NULL;
+
+    gwy_clear(&jspmfile, 1);
+
+    if (!gwy_file_get_contents(filename, &buffer, &size, &err)) {
+        err_GET_FILE_CONTENTS(error, &err);
+        return NULL;
+    }
+    if (size < 0x48) {
+        err_TOO_SHORT(error);
+        goto fail;
+    }
+    if (!meas_header_seems_ok(buffer)) {
+        err_FILE_TYPE(error, "JEOL JSPM");
+        goto fail;
+    }
+
+    if (!jspm_read_headers(&jspmfile, buffer, size, error))
+        goto fail;
+
+    container = gwy_container_new();
+
+    dfield = jspm_read_data_field(&jspmfile, buffer);
+    gwy_container_set_object_by_name(container, "/0/data", dfield);
+    g_object_unref(dfield);
+
+    gwy_app_channel_title_fall_back(container, 0);
+    gwy_file_channel_import_log_add(container, 0, NULL, filename);
+
+fail:
+    if (jspmfile.blocks)
+        g_array_free(jspmfile.blocks, TRUE);
+    gwy_file_abandon_contents(buffer, size, NULL);
+    return container;
+}
+
+static gboolean
+meas_header_seems_ok(const gchar *buffer)
+{
+    return (memcmp(buffer + 0x1e, JEOL_MAGIC1, JEOL_MAGIC1_SIZE) == 0
+            && memcmp(buffer + 0x3e, JEOL_MAGIC2, JEOL_MAGIC2_SIZE) == 0);
+}
+
+static void
+err_JSPM_BLOCK(GError **error, guint i)
+{
+    g_set_error(error, GWY_MODULE_FILE_ERROR, GWY_MODULE_FILE_ERROR_DATA,
+                _("Header block %u has invalid position or size."), i+1);
+}
+
+/* We kind of know how to walk through the physical structure of the file
+ * headers.  We are at a loss as to what to do with their content though. */
+static gboolean
+jspm_read_headers(JSPMFile *jspmfile,
+                  const guchar *buffer,
+                  gsize size,
+                  GError **error)
+{
+    JSPMHeaderBlock block;
+    const guchar *p = buffer;
+
+    block.offset = TIFF_HEADER_SIZE;
+    p += block.offset;
+    /* Normally next and block size are bytes 4-8 and 9-10.  But the version
+     * at the begining of the first block seems extra so we have to look
+     * two bytes later. */
+    jspmfile->winspm_version = gwy_get_guint16_le(&p);
+    gwy_debug("version: %u", jspmfile->winspm_version);
+
+    jspmfile->blocks = g_array_new(FALSE, FALSE, sizeof(JSPMHeaderBlock));
+    do {
+        guint next;
+
+        if (block.offset >= size - (4 + 4 + 2)) {
+            err_JSPM_BLOCK(error, jspmfile->blocks->len);
+            return FALSE;
+        }
+
+        gwy_debug("block #%u intro %02x %02x %02x %02x",
+                  jspmfile->blocks->len+1, p[0], p[1], p[2], p[3]);
+
+        p += 4;
+        next = gwy_get_guint32_le(&p);
+        block.len = gwy_get_guint16_le(&p);
+        gwy_debug("block #%u: 0x%x bytes at 0x%x",
+                  jspmfile->blocks->len+1, block.len, block.offset);
+        if (block.offset + block.len > size) {
+            err_JSPM_BLOCK(error, jspmfile->blocks->len);
+            return FALSE;
+        }
+        g_array_append_val(jspmfile->blocks, block);
+
+        if (next && next < block.offset + block.len) {
+            err_JSPM_BLOCK(error, jspmfile->blocks->len);
+            return FALSE;
+        }
+
+        block.offset = next;
+        p = buffer + next;
+    } while (block.offset);
+
+    if (!read_image_header_block(jspmfile, buffer, size, error))
+        return FALSE;
+
+    return TRUE;
+}
+
+static gboolean
+read_image_header_block(JSPMFile *jspmfile, const guchar *buffer, gsize size,
+                        GError **error)
+{
+    enum {
+        DATAPOS_OFFSET = 0x0a,
+        RES_OFFSET = 0x18,
+        REAL_OFFSET = 0x1c,
+    };
+
+    JSPMHeaderBlock *block;
+    gsize expected_size;
+    const guchar *p;
+
+    if (jspmfile->blocks->len < 2) {
+        g_set_error(error, GWY_MODULE_FILE_ERROR, GWY_MODULE_FILE_ERROR_DATA,
+                    _("Cannot find image header block."));
+        return FALSE;
+    }
+
+    block = &g_array_index(jspmfile->blocks, JSPMHeaderBlock, 1);
+    p = buffer + block->offset;
+    if (p[0] != 0x0a || p[1] != 0x00 || block->len != 0x0bc) {
+        g_set_error(error, GWY_MODULE_FILE_ERROR, GWY_MODULE_FILE_ERROR_DATA,
+                    _("Cannot find image header block."));
+        return FALSE;
+    }
+
+    p = buffer + block->offset + DATAPOS_OFFSET;
+    jspmfile->data_offset = gwy_get_guint32_le(&p);
+
+    p = buffer + block->offset + RES_OFFSET;
+    jspmfile->xres = gwy_get_guint16_le(&p);
+    jspmfile->yres = gwy_get_guint16_le(&p);
+
+    p = buffer + block->offset + REAL_OFFSET;
+    jspmfile->xreal = gwy_get_gfloat_le(&p);
+    jspmfile->yreal = gwy_get_gfloat_le(&p);
+
+    if (err_DIMENSION(error, jspmfile->xres)
+        || err_DIMENSION(error, jspmfile->yres))
+        return FALSE;
+
+    expected_size = jspmfile->xres*jspmfile->yres*sizeof(guint32);
+    if (err_SIZE_MISMATCH(error, jspmfile->data_offset + expected_size, size,
+                          FALSE))
+        return FALSE;
+
+    /* Use negated positive conditions to catch NaNs */
+    if (!((jspmfile->xreal = fabs(jspmfile->xreal)) > 0)) {
+        g_warning("Real x size is 0.0, fixing to 1.0");
+        jspmfile->xreal = 1.0;
+    }
+    if (!((jspmfile->yreal = fabs(jspmfile->yreal)) > 0)) {
+        g_warning("Real y size is 0.0, fixing to 1.0");
+        jspmfile->yreal = 1.0;
+    }
+
+    return TRUE;
+}
+
+static GwyDataField*
+jspm_read_data_field(const JSPMFile *jspmfile,
+                     const guchar *buffer)
+
+{
+    GwyDataField *dfield;
+
+    dfield = gwy_data_field_new(jspmfile->xres, jspmfile->yres,
+                                Nanometer*jspmfile->xreal,
+                                Nanometer*jspmfile->yreal,
+                                FALSE);
+
+    gwy_si_unit_set_from_string(gwy_data_field_get_si_unit_xy(dfield), "m");
+    gwy_si_unit_set_from_string(gwy_data_field_get_si_unit_z(dfield), "m");
+
+    gwy_convert_raw_data(buffer + jspmfile->data_offset,
+                         jspmfile->xres*jspmfile->yres, 1,
+                         GWY_RAW_DATA_UINT32, GWY_BYTE_ORDER_LITTLE_ENDIAN,
+                         gwy_data_field_get_data(dfield), Picometer, 0.0);
+
+    return dfield;
+}
+
+/* vim: set cin et ts=4 sw=4 cino=>1s,e0,n0,f0,{0,}0,^0,\:1s,=0,g1s,h0,t0,+1s,c3,(0,u0 : */
