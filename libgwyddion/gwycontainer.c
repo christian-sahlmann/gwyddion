christian-sahlmann/gwyddion

commit 5eab17ef397dcd3362e44c75c1a81e14c2684ba7

- always do g_value_copy() in gwy_container_try_set_one() to the in-Container   GValue - this means ownership rules for generic GValues are consistent - updated gwy_container_set_string() to free the string passed to avoid a leak   here
git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@16496 25697fe4-d644-49b8-9d30-be5465f2adf3

libgwyddion/gwycontainer.c
@@ -1412,10 +1412,7 @@ gwy_container_try_set_one(GwyContainer *container,
         changed = TRUE;
     }
     g_value_init(old, G_VALUE_TYPE(value));
-    if (G_VALUE_HOLDS_STRING(value))
-        g_value_take_string(old, g_value_peek_pointer(value));
-    else
-        g_value_copy(value, old);
+    g_value_copy(value, old);
 
     if (changed && !container->in_construction)
         g_signal_emit(container, container_signals[ITEM_CHANGED], key, key);
@@ -1751,6 +1748,9 @@ gwy_container_set_string(GwyContainer *container,
     g_value_init(&gvalue, G_TYPE_STRING);
     g_value_take_string(&gvalue, (gchar*)value);
     gwy_container_try_set_one(container, key, &gvalue, TRUE, TRUE);
+    /* This is necessary because we do g_value_copy() in
+     * gwy_container_try_set_one(). */
+    g_free((gchar*)value);
 }
 
 /**

commit 69f4560397aede743f2af84aea935eb43e43e273

- special-cased gwy_container_set_string() implementation to keep the passed   string alive because code depends on it - fixed weird problems caused by r16496
git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@16536 25697fe4-d644-49b8-9d30-be5465f2adf3

libgwyddion/gwycontainer.c
@@ -1720,15 +1720,41 @@ gwy_container_set_string(GwyContainer *container,
                          GQuark key,
                          const guchar *value)
 {
-    GValue gvalue;
+    gboolean changed = FALSE;
+    GValue *gvalue;
+    gpointer pkey;
+    const gchar *oldstring;
 
-    gwy_clear(&gvalue, 1);
-    g_value_init(&gvalue, G_TYPE_STRING);
-    g_value_take_string(&gvalue, (gchar*)value);
-    gwy_container_try_set_one(container, key, &gvalue);
-    /* This is necessary because we do g_value_copy() in
-     * gwy_container_try_set_one(). */
-    g_free((gchar*)value);
+    g_return_if_fail(GWY_IS_CONTAINER(container));
+    g_return_if_fail(key);
+
+    pkey = GUINT_TO_POINTER(key);
+    gvalue = (GValue*)g_hash_table_lookup(container->values, pkey);
+    if (gvalue) {
+        g_assert(G_IS_VALUE(gvalue));
+        if (!G_VALUE_HOLDS_STRING(gvalue)) {
+            g_value_unset(gvalue);
+            g_value_init(gvalue, G_TYPE_STRING);
+            changed = TRUE;
+        }
+        oldstring = g_value_get_string(gvalue);
+        if ((oldstring && !value)
+            || (!oldstring && value)
+            || (oldstring && value && !gwy_strequal(oldstring, value)))
+            changed = TRUE;
+    }
+    else {
+        gvalue = g_new0(GValue, 1);
+        g_value_init(gvalue, G_TYPE_STRING);
+        g_hash_table_insert(container->values, pkey, gvalue);
+        changed = TRUE;
+    }
+    /* XXX: We MUST do this.  There is code relying on @value being consumed
+     * and kept alive, not just immediately freed by this function. */
+    g_value_take_string(gvalue, (gchar*)value);
+
+    if (changed && !container->in_construction)
+        g_signal_emit(container, container_signals[ITEM_CHANGED], key, key);
 }
 
 /**