8acc0f9fa29cb7548c28c028c5ada1b1e9a1c999
dn2010 <dn2010@25697fe4-d644-49b8-9d30-be5465f2adf3>
2014-09-28 12:30:38 +0000

    - Some more debug data
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@16619 25697fe4-d644-49b8-9d30-be5465f2adf3

--- modules/file/renishaw.c
+++ modules/file/renishaw.c
@@ -76,48 +76,107 @@ enum {
 };
 
 typedef enum {
-	WDF_BLOCKID_FILE           = 0x31464457UL, /* "WDF1" */
-	WDF_BLOCKID_DATA           = 0x41544144UL, /* "DATA" */
-	WDF_BLOCKID_YLIST          = 0x54534C59UL, /* "YLST" */ 
-	WDF_BLOCKID_XLIST          = 0x54534C58UL, /* "XLST" */
-	WDF_BLOCKID_ORIGIN         = 0x4E47524FUL, /* "ORGN" */
-	WDF_BLOCKID_COMMENT        = 0x54584554UL, /* "TEXT" */
-	WDF_BLOCKID_WIREDATA       = 0x41445857UL, /* "WXDA" */
-	WDF_BLOCKID_DATASETDATA    = 0x42445857UL, /* "WXDB" */
-	WDF_BLOCKID_MEASUREMENT    = 0x4d445857UL, /* "WXDM" */
-	WDF_BLOCKID_CALIBRATION    = 0x53435857UL, /* "WXCS" */
-	WDF_BLOCKID_INSTRUMENT     = 0x53495857UL, /* "WXIS" */
-	WDF_BLOCKID_MAPAREA        = 0x50414D57UL, /* "WMAP" */
-	WDF_BLOCKID_WHITELIGHT     = 0x4C544857UL, /* "WHTL" */
-	WDF_BLOCKID_THUMBNAIL      = 0x4C49414EUL, /* "NAIL" */
-	WDF_BLOCKID_MAP            = 0x2050414DUL, /* "MAP " */
-	WDF_BLOCKID_CURVEFIT       = 0x52414643UL, /* "CFAR" */
-	WDF_BLOCKID_COMPONENT      = 0x534C4344UL, /* "DCLS" */
-	WDF_BLOCKID_PCA            = 0x52414350UL, /* "PCAR" */
-	WDF_BLOCKID_EM             = 0x4552434DUL, /* "MCRE" */
-	WDF_BLOCKID_ZELDAC         = 0x43444C5AUL, /* "ZLDC" */
-	WDF_BLOCKID_RESPONSECAL    = 0x4C414352UL, /* "RCAL" */
-	WDF_BLOCKID_CAP            = 0x20504143UL, /* "CAP " */
-	WDF_BLOCKID_PROCESSING     = 0x50524157UL, /* "WARP" */
-	WDF_BLOCKID_ANALYSIS       = 0x41524157UL, /* "WARA" */
-	WDF_BLOCKID_SPECTRUMLABELS = 0x4C424C57UL, /* "WLBL" */
-	WDF_BLOCKID_CHECKSUM       = 0x4B484357UL, /* "WCHK" */
-	WDF_BLOCKID_RXCALDATA      = 0x44435852UL, /* "RXCD" */
-	WDF_BLOCKID_RXCALFIT       = 0x46435852UL, /* "RXCF" */
-	WDF_BLOCKID_XCAL           = 0x4C414358UL, /* "XCAL" */
-	WDF_BLOCKID_SPECSEARCH     = 0x48435253UL, /* "SRCH" */
-	WDF_BLOCKID_TEMPPROFILE    = 0x504D4554UL, /* "TEMP" */
-	WDF_BLOCKID_UNITCONVERT    = 0x56434E55UL, /* "UNCV" */
-	WDF_BLOCKID_ARPLATE        = 0x52505241UL, /* "ARPR" */
-	WDF_BLOCKID_ELECSIGN       = 0x43454C45UL, /* "ELEC" */
-	WDF_BLOCKID_BKXLIST        = 0x4C584B42UL, /* "BKXL" */
-	WDF_BLOCKID_AUXILARYDATA   = 0x20585541UL, /* "AUX " */                 
-	WDF_BLOCKID_CHANGELOG      = 0x474C4843UL, /* "CHLG" */
-	WDF_BLOCKID_SURFACE        = 0x46525553UL, /* "SURF" */
-	WDF_BLOCKID_ANY            = 0xFFFFFFFFUL, 
-	                       /* reserved value for @ref Wdf_FindSection */ 
-	WDF_STREAM_IS_PSET         = 0x54455350UL /* "PSET" */
+    WDF_BLOCKID_FILE           = 0x31464457UL, /* "WDF1" */
+    WDF_BLOCKID_DATA           = 0x41544144UL, /* "DATA" */
+    WDF_BLOCKID_YLIST          = 0x54534C59UL, /* "YLST" */
+    WDF_BLOCKID_XLIST          = 0x54534C58UL, /* "XLST" */
+    WDF_BLOCKID_ORIGIN         = 0x4E47524FUL, /* "ORGN" */
+    WDF_BLOCKID_COMMENT        = 0x54584554UL, /* "TEXT" */
+    WDF_BLOCKID_WIREDATA       = 0x41445857UL, /* "WXDA" */
+    WDF_BLOCKID_DATASETDATA    = 0x42445857UL, /* "WXDB" */
+    WDF_BLOCKID_MEASUREMENT    = 0x4D445857UL, /* "WXDM" */
+    WDF_BLOCKID_CALIBRATION    = 0x53435857UL, /* "WXCS" */
+    WDF_BLOCKID_INSTRUMENT     = 0x53495857UL, /* "WXIS" */
+    WDF_BLOCKID_MAPAREA        = 0x50414D57UL, /* "WMAP" */
+    WDF_BLOCKID_WHITELIGHT     = 0x4C544857UL, /* "WHTL" */
+    WDF_BLOCKID_THUMBNAIL      = 0x4C49414EUL, /* "NAIL" */
+    WDF_BLOCKID_MAP            = 0x2050414DUL, /* "MAP " */
+    WDF_BLOCKID_CURVEFIT       = 0x52414643UL, /* "CFAR" */
+    WDF_BLOCKID_COMPONENT      = 0x534C4344UL, /* "DCLS" */
+    WDF_BLOCKID_PCA            = 0x52414350UL, /* "PCAR" */
+    WDF_BLOCKID_EM             = 0x4552434DUL, /* "MCRE" */
+    WDF_BLOCKID_ZELDAC         = 0x43444C5AUL, /* "ZLDC" */
+    WDF_BLOCKID_RESPONSECAL    = 0x4C414352UL, /* "RCAL" */
+    WDF_BLOCKID_CAP            = 0x20504143UL, /* "CAP " */
+    WDF_BLOCKID_PROCESSING     = 0x50524157UL, /* "WARP" */
+    WDF_BLOCKID_ANALYSIS       = 0x41524157UL, /* "WARA" */
+    WDF_BLOCKID_SPECTRUMLABELS = 0x4C424C57UL, /* "WLBL" */
+    WDF_BLOCKID_CHECKSUM       = 0x4B484357UL, /* "WCHK" */
+    WDF_BLOCKID_RXCALDATA      = 0x44435852UL, /* "RXCD" */
+    WDF_BLOCKID_RXCALFIT       = 0x46435852UL, /* "RXCF" */
+    WDF_BLOCKID_XCAL           = 0x4C414358UL, /* "XCAL" */
+    WDF_BLOCKID_SPECSEARCH     = 0x48435253UL, /* "SRCH" */
+    WDF_BLOCKID_TEMPPROFILE    = 0x504D4554UL, /* "TEMP" */
+    WDF_BLOCKID_UNITCONVERT    = 0x56434E55UL, /* "UNCV" */
+    WDF_BLOCKID_ARPLATE        = 0x52505241UL, /* "ARPR" */
+    WDF_BLOCKID_ELECSIGN       = 0x43454C45UL, /* "ELEC" */
+    WDF_BLOCKID_BKXLIST        = 0x4C584B42UL, /* "BKXL" */
+    WDF_BLOCKID_AUXILARYDATA   = 0x20585541UL, /* "AUX " */
+    WDF_BLOCKID_CHANGELOG      = 0x474C4843UL, /* "CHLG" */
+    WDF_BLOCKID_SURFACE        = 0x46525553UL, /* "SURF" */
+    WDF_BLOCKID_ANY            = 0xFFFFFFFFUL,
+                           /* reserved value for @ref Wdf_FindSection */
+    WDF_STREAM_IS_PSET         = 0x54455350UL  /* "PSET" */
 } WdfBlockIDs;
+
+typedef enum {
+    WdfDataType_Arbitrary,      /* arbitrary type */
+    WdfDataType_Spectral,       /* DEPRECATED: Use Frequency instead */
+    WdfDataType_Intensity,      /* intensity */
+    WdfDataType_Spatial_X,      /* X position */
+    WdfDataType_Spatial_Y,      /* Y axis position */
+    WdfDataType_Spatial_Z,      /* Z axis (vertical) position */
+    WdfDataType_Spatial_R,      /* rotary stage R axis position */
+    WdfDataType_Spatial_Theta,  /* rotary stage theta angle */
+    WdfDataType_Spatial_Phi,    /* rotary stage phi angle */
+    WdfDataType_Temperature,    /* temperature */
+    WdfDataType_Pressure,       /* pressure */
+    WdfDataType_Time,           /* time */
+    WdfDataType_Derived,        /* derivative type */
+    WdfDataType_Polarization,   /* polarization */
+    WdfDataType_FocusTrack,     /* focus track Z position */
+    WdfDataType_RampRate,       /* temperature ramp rate */
+    WdfDataType_Checksum,       /* spectrum data checksum */
+    WdfDataType_Flags,          /* bit flags */
+    WdfDataType_ElapsedTime,    /* elapsed time intervals */
+    WdfDataType_Frequency,      /* frequency */
+    /* Microplate mapping origins */
+    WdfDataType_Mp_Well_Spatial_X,
+    WdfDataType_Mp_Well_Spatial_Y,
+    WdfDataType_Mp_LocationIndex,
+    WdfDataType_Mp_WellReference,
+
+    WdfDataType_EndMarker       /* THIS SHOULD ALWAYS BE LAST */
+} WdfDataType;
+
+typedef enum {
+    WdfDataUnits_Arbitrary,    /* arbitrary units */
+    WdfDataUnits_RamanShift,   /* Raman shift (cm^-1) */
+    WdfDataUnits_Wavenumber,   /* wavenumber (nm) */
+    WdfDataUnits_Nanometre,    /* 10-9 metres (nm) */
+    WdfDataUnits_ElectronVolt, /* electron volts (eV) */
+    WdfDataUnits_Micron,       /* 10-6 metres (um) */
+    WdfDataUnits_Counts,       /* counts */
+    WdfDataUnits_Electrons,    /* electrons */
+    WdfDataUnits_Millimetres,  /* 10^-3 metres (mm) */
+    WdfDataUnits_Metres,       /* metres (m) */
+    WdfDataUnits_Kelvin,       /* degrees Kelvin (K) */
+    WdfDataUnits_Pascal,       /* Pascals (Pa) */
+    WdfDataUnits_Seconds,      /* seconds (s) */
+    WdfDataUnits_Milliseconds, /* 10^-3 seconds (ms) */
+    WdfDataUnits_Hours,
+    WdfDataUnits_Days,
+    WdfDataUnits_Pixels,
+    WdfDataUnits_Intensity,
+    WdfDataUnits_RelativeIntensity,
+    WdfDataUnits_Degrees,
+    WdfDataUnits_Radians,
+    WdfDataUnits_Celcius,
+    WdfDataUnits_Farenheit,
+    WdfDataUnits_KelvinPerMinute,
+    WdfDataUnits_FileTime,     /* date-time as a Windows FILETIME */
+    WdfDataUnits_EndMarker
+} WdfDataUnits;
 
 typedef struct {
     guint32 id;
@@ -142,13 +201,13 @@ typedef struct {
     guint64 nspectra;    /* number of actual spectra (capacity) */
     guint64 ncollected;  /* number of spectra written into the file */
     guint32 naccum;      /* number of accumulations per spectrum */
-    guint32 ylistcount;  /* number of elements in the y-list 
-													   (>1 for image) */
+    guint32 ylistcount;  /* number of elements in the y-list
+                                                       (>1 for image) */
     guint32 xlistcount;  /* number of elements for the x-list */
     guint32 origincount; /* number of data origin lists */
     gchar   appname[24]; /* application name (utf-8) */
-    guint16 appversion[4]; /* application version 
-										 (major, minor, patch, build) */
+    guint16 appversion[4]; /* application version
+                                         (major, minor, patch, build) */
     guint32 scantype;    /* scan type - WdfScanType */
     guint32 type;        /* measurement type - WdfType */
     guint64 time_start;  /* collection start time as FILETIME */
@@ -162,6 +221,12 @@ typedef struct {
     guint64 free[4];     /* available for third party use */
     guint64 reserved[4]; /* reserved for internal use by WiRE */
 } WdfHeader;
+
+typedef struct {
+    WdfHeader *header;
+    gfloat    *data;
+    gsize      datasize;
+} WdfFile;
 
 static gboolean       module_register        (void);
 static gint           wdf_detect             (const GwyFileDetectInfo *fileinfo,
@@ -234,6 +299,7 @@ wdf_load(const gchar *filename,
     GwyDataField *dfield;
     GwyDataLine *cal;
     gdouble *data;
+    WdfFile filedata;
     WdfHeader fileheader;
     WdfBlock block;
     const guchar *p;
@@ -252,6 +318,7 @@ wdf_load(const gchar *filename,
         goto fail;
     p += WDF_HEADER_SIZE;
     size -= WDF_HEADER_SIZE;
+    filedata.header = &header;
     gwy_debug("npoints = %d, nspectra=%" G_GUINT64_FORMAT "",
               fileheader.npoints,
               fileheader.nspectra);
@@ -259,18 +326,28 @@ wdf_load(const gchar *filename,
     while (size > 0) {
         if ((len = wdf_read_block_header(p, size, &block, error)) == 0)
             goto fail;
-		
-		if (block.id == WDF_BLOCKID_ORIGIN) {
-			gwy_debug("ORGN offset = %" G_GUINT64_FORMAT " size=%" G_GUINT64_FORMAT "",
-			          ((guint64)p - (guint64)buffer), block.size);
-			
-		}
-		else if (block.id == WDF_BLOCKID_WHITELIGHT) {
-			gwy_debug("WL offset = %" G_GUINT64_FORMAT " size=%" G_GUINT64_FORMAT "",
-			          ((guint64)p - (guint64)buffer), block.size);
-			
-		}
-				
+        if (block.id == WDF_BLOCKID_DATA) {
+            gwy_debug("DATA offset = %" G_GUINT64_FORMAT " size=%" G_GUINT64_FORMAT "",
+                      ((guint64)p - (guint64)buffer), block.size);
+            filedata.data = (gfloat *)p;
+            filedata.datasize = block.size - WDF_BLOCK_HEADER_SIZE;
+        }
+        else if (block.id == WDF_BLOCKID_XLIST) {
+            gwy_debug("XLST offset = %" G_GUINT64_FORMAT " size=%" G_GUINT64_FORMAT "",
+                      ((guint64)p - (guint64)buffer), block.size);
+
+        }
+        else if (block.id == WDF_BLOCKID_ORIGIN) {
+            gwy_debug("ORGN offset = %" G_GUINT64_FORMAT " size=%" G_GUINT64_FORMAT "",
+                      ((guint64)p - (guint64)buffer), block.size);
+
+        }
+        else if (block.id == WDF_BLOCKID_WHITELIGHT) {
+            gwy_debug("WL offset = %" G_GUINT64_FORMAT " size=%" G_GUINT64_FORMAT "",
+                      ((guint64)p - (guint64)buffer), block.size);
+
+        }
+
         p += len;
         size -= len;
     }
@@ -279,6 +356,7 @@ wdf_load(const gchar *filename,
     brick = gwy_brick_new(xres, yres, zres, xreal, yreal, zreal, TRUE);
     data = gwy_brick_get_data(brick);
 
+    /*
     p = buffer + 528;
 
     for (i = 0; i < xres; i++)
@@ -315,6 +393,8 @@ wdf_load(const gchar *filename,
     g_object_unref(brick);
 
     gwy_file_volume_import_log_add(container, 0, NULL, filename);
+    */
+
     fail:
     g_free(buffer);
 
@@ -406,7 +486,7 @@ wdf_read_block_header(const guchar *buffer,
                     _("Block header is truncated"));
         return 0;
     }
-	
+
     header->id   = gwy_get_guint32_le(&buffer);
     header->uid  = gwy_get_guint32_le(&buffer);
     header->size = gwy_get_guint64_le(&buffer);
@@ -421,8 +501,8 @@ wdf_read_block_header(const guchar *buffer,
                     _("Data block is truncated"));
         return 0;
     }
-    
-	header->data = buffer;
+
+    header->data = buffer;
 
     return header->size;
 }
