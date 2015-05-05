d8848287d649b4efdcc125450077d2c7f4a1df6b
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2015-05-05 20:21:22 +0000

    - added a thorough check for why we cannot load the splash image when it   happens
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@17123 25697fe4-d644-49b8-9d30-be5465f2adf3

--- app/splash.c
+++ app/splash.c
@@ -19,6 +19,10 @@
  *  Boston, MA 02110-1301, USA.
  */
 
+/* XXX: Temporary */
+#include <errno.h>
+#define GDK_PIXBUF_ENABLE_BACKEND 1
+
 #include "config.h"
 #include <stdlib.h>
 #include <gtk/gtk.h>
@@ -558,6 +562,181 @@ digits[] = {
     { '.', G_N_ELEMENTS(digit_dot_png), digit_dot_png, },
 };
 
+static gint
+format_check(GdkPixbufFormat *format, guchar *buffer, int size)
+{
+    int i, j, ii;
+    gchar m;
+    GdkPixbufModulePattern *pattern;
+    gboolean anchored;
+    guchar *prefix;
+    gchar *mask;
+
+    ii = 0;
+    for (pattern = format->signature; pattern->prefix; pattern++, ii++) {
+        if (pattern->mask && pattern->mask[0] == '*') {
+            prefix = (guchar *)pattern->prefix + 1;
+            mask = pattern->mask + 1;
+            anchored = FALSE;
+        }
+        else {
+            prefix = (guchar *)pattern->prefix;
+            mask = pattern->mask;
+            anchored = TRUE;
+        }
+        for (i = 0; i < size; i++) {
+            for (j = 0; i + j < size && prefix[j] != 0; j++) {
+                m = mask ? mask[j] : ' ';
+                if (m == ' ') {
+                    if (buffer[i + j] != prefix[j])
+                        break;
+                }
+                else if (m == '!') {
+                    if (buffer[i + j] == prefix[j])
+                        break;
+                }
+                else if (m == 'z') {
+                    if (buffer[i + j] != 0)
+                        break;
+                }
+                else if (m == 'n') {
+                    if (buffer[i + j] == 0)
+                        break;
+                }
+            }
+
+            if (prefix[j] == 0) {
+                g_printerr("  Pattern #%d matches the file header (relevance %d).\n",
+                           ii, pattern->relevance);
+                return pattern->relevance;
+            }
+
+            if (anchored)
+                break;
+        }
+    }
+    g_printerr("  No pattern matches the file header.\n");
+    return 0;
+}
+
+static void
+sanity_check(const gchar *filename)
+{
+    GdkPixbufLoader *loader;
+    GSList *formats, *l;
+    GError *err = NULL;
+    GdkPixbuf *pixbuf;
+    gchar *buf = NULL;
+    gsize size;
+    FILE *fh;
+    gboolean ok, fileok = TRUE;
+
+    g_printerr("Performing sanity check for %s.\n", filename);
+    fileok &= ok = g_file_test(filename, G_FILE_TEST_EXISTS);
+    g_printerr("Does it exist: %s\n", ok ? "YES" : "NO");
+    fileok &= ok = g_file_test(filename, G_FILE_TEST_IS_REGULAR);
+    g_printerr("Is it a regular file: %s\n", ok ? "YES" : "NO");
+
+    fh = fopen(filename, "rb");
+    fileok &= ok = !!fh;
+    g_printerr("Can we open it for reading: %s\n", ok ? "YES" : "NO");
+    if (fh)
+        fclose(fh);
+    else
+        g_printerr("fopen() fails with: %s\n", g_strerror(errno));
+
+    fileok &= ok = g_file_get_contents(filename, &buf, &size, &err);
+    g_printerr("Can we use g_file_get_contents(): %s\n", ok ? "YES" : "NO");
+    if (!ok) {
+        g_printerr("g_file_get_contents() fails with: %s\n", err->message);
+        g_clear_error(&err);
+    }
+
+    if (!fileok) {
+        if (buf)
+            g_free(buf);
+        g_printerr("The file does not seem OK.  No point continuing.\n");
+        return;
+    }
+    g_printerr("The file seems OK, continuing checks.\n");
+
+    pixbuf = gdk_pixbuf_new_from_file(filename, &err);
+    if (pixbuf) {
+        g_printerr("Apparently we can load the pixbuf (%dx%d) now?!\n",
+                   gdk_pixbuf_get_width(pixbuf),
+                   gdk_pixbuf_get_height(pixbuf));
+        g_printerr("What has changed?\n");
+        g_printerr("This is completely fucked up.\n");
+        g_object_unref(pixbuf);
+    }
+    else {
+        g_printerr("gdk_pixbuf_new_from_file() fails with: %s\n", err->message);
+        g_clear_error(&err);
+    }
+
+    g_printerr("Checking the pixbuf loaders.\n");
+
+    formats = gdk_pixbuf_get_formats();
+    for (l = formats; l; l = g_slist_next(l)) {
+        GdkPixbufFormat *pixbuf_format = (GdkPixbufFormat*)l->data;
+        gchar **ext;
+        gchar *fmtname, *desc, *exts;
+
+        fmtname = gdk_pixbuf_format_get_name(pixbuf_format);
+        desc = gdk_pixbuf_format_get_description(pixbuf_format);
+        ext = gdk_pixbuf_format_get_extensions(pixbuf_format);
+        exts = g_strjoinv(" ", ext);
+        g_printerr("Found format %s: %s (%s)\n", fmtname, desc, exts);
+        g_free(exts);
+        g_strfreev(ext);
+        g_free(desc);
+
+        format_check(pixbuf_format, buf, size);
+
+        loader = gdk_pixbuf_loader_new_with_type(fmtname, &err);
+        if (!loader) {
+            g_printerr("  Cannot create loader for %s: %s\n",
+                       fmtname, err->message);
+            g_clear_error(&err);
+            g_free(fmtname);
+            continue;
+        }
+
+        ok = gdk_pixbuf_loader_write(loader, buf, size, &err);
+        if (ok) {
+            g_printerr("  Loader %s accepts the file content.\n", fmtname);
+            ok = gdk_pixbuf_loader_close(loader, &err);
+            if (ok) {
+                g_printerr("  Loader %s accepts the entire file.\n", fmtname);
+                pixbuf = gdk_pixbuf_loader_get_pixbuf(loader);
+                if (pixbuf) {
+                    g_printerr("  Obtained pixbuf %dx%d from the loader.\n",
+                               gdk_pixbuf_get_width(pixbuf),
+                               gdk_pixbuf_get_height(pixbuf));
+                }
+                else {
+                    g_printerr("  Cannot obtain pixbuf from the loader.\n");
+                }
+            }
+            else {
+                g_printerr("  Loader %s fails at close(): %s.\n",
+                           fmtname, err->message);
+                g_clear_error(&err);
+            }
+        }
+        else {
+            g_printerr("  Loader %s does not accept the file content: %s\n",
+                       fmtname, err->message);
+            g_clear_error(&err);
+        }
+        g_object_unref(loader);
+
+        g_free(fmtname);
+    }
+
+    g_slist_free(formats);
+}
+
 static GdkPixbuf*
 compose_splash(const gchar *version)
 {
@@ -577,6 +756,7 @@ compose_splash(const gchar *version)
     if (!base) {
         g_warning("Cannot load base splash image: %s", err->message);
         g_clear_error(&err);
+        sanity_check(filename);
         g_free(filename);
         return NULL;
     }
