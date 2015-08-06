63dc2013c7c7aecfb6b4b674964910c8eab7603c
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2015-08-06 15:14:48 +0000

    - added the option to plot background as a graph
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@17255 25697fe4-d644-49b8-9d30-be5465f2adf3

--- modules/process/linematch.c
+++ modules/process/linematch.c
@@ -59,10 +59,12 @@ typedef struct {
     LineMatchMethod method;
     gint max_degree;
     gboolean do_extract;
+    gboolean do_plot;
     GwyMaskingType masking;
     /* Runtime state */
     GwyDataField *result;
     GwyDataField *bg;
+    GwyDataLine *shifts;
 } LineMatchArgs;
 
 typedef struct {
@@ -72,6 +74,7 @@ typedef struct {
     GSList *masking_group;
     GSList *method_group;
     GtkWidget *do_extract;
+    GtkWidget *do_plot;
     GtkWidget *dataview;
     GwyContainer *data;
     GwyDataField *dfield;
@@ -107,6 +110,8 @@ static void     degree_changed          (LineMatchControls *controls,
                                          GtkObject *adj);
 static void     do_extract_changed      (LineMatchControls *controls,
                                          GtkToggleButton *check);
+static void     do_plot_changed         (LineMatchControls *controls,
+                                         GtkToggleButton *check);
 static void     masking_changed         (GtkToggleButton *button,
                                          LineMatchControls *controls);
 static void     method_changed          (GtkToggleButton *button,
@@ -122,10 +127,10 @@ static void     sanitize_args           (LineMatchArgs *args);
 static const LineMatchArgs linematch_defaults = {
     LINE_LEVEL_MEDIAN,
     1,
-    FALSE,
+    FALSE, FALSE,
     GWY_MASK_IGNORE,
     /* Runtime state */
-    NULL, NULL,
+    NULL, NULL, NULL,
 };
 
 static GwyModuleInfo module_info = {
@@ -172,27 +177,27 @@ linematch(GwyContainer *data, GwyRunType run)
     g_return_if_fail(dfield && quark);
 
     load_args(gwy_app_settings_get(), &args);
+    args.bg = gwy_data_field_new_alike(dfield, FALSE);
+    args.shifts = gwy_data_line_new(dfield->yres, dfield->yreal, FALSE);
+    gwy_data_field_copy_units_to_data_line(dfield, args.shifts);
+
     if (run == GWY_RUN_INTERACTIVE) {
         ok = linematch_dialog(&args, data, dfield, mfield, id);
         save_args(gwy_app_settings_get(), &args);
-        if (!ok) {
-            gwy_object_unref(args.result);
-            gwy_object_unref(args.bg);
-            return;
-        }
+        if (!ok)
+            goto end;
         gwy_app_undo_qcheckpointv(data, 1, &quark);
         gwy_data_field_copy(args.result, dfield, FALSE);
     }
     else {
         gwy_app_undo_qcheckpointv(data, 1, &quark);
         args.result = g_object_ref(dfield);
-        if (args.do_extract)
-            args.bg = gwy_data_field_new_alike(dfield, FALSE);
         linematch_do(mfield, &args);
     }
 
     gwy_data_field_data_changed(dfield);
     gwy_app_channel_log_add_proc(data, id, id);
+
     if (args.do_extract) {
         newid = gwy_app_data_browser_add_data_field(args.bg, data, TRUE);
         gwy_app_sync_data_items(data, data, id, newid, FALSE,
@@ -201,7 +206,34 @@ linematch(GwyContainer *data, GwyRunType run)
         gwy_app_set_data_field_title(data, newid, _("Row background"));
         gwy_app_channel_log_add(data, id, newid, NULL, NULL);
     }
+
+    if (args.do_plot) {
+        GwyGraphModel *gmodel = gwy_graph_model_new();
+        GwyGraphCurveModel *gcmodel = gwy_graph_curve_model_new();
+
+        gwy_graph_curve_model_set_data_from_dataline(gcmodel, args.shifts,
+                                                     0, 0);
+        g_object_set(gcmodel,
+                     "description", _("Row background"),
+                     "mode", GWY_GRAPH_CURVE_LINE,
+                     "color", gwy_graph_get_preset_color(0),
+                     NULL);
+        gwy_graph_model_add_curve(gmodel, gcmodel);
+        g_object_unref(gcmodel);
+
+        g_object_set(gmodel,
+                     "title", _("Row background"),
+                     "axis-label-bottom", _("Vertical position"),
+                     "axis-label-left", _("Corrected offset"),
+                     NULL);
+        gwy_graph_model_set_units_from_data_line(gmodel, args.shifts);
+        gwy_app_data_browser_add_graph_model(gmodel, data, TRUE);
+        g_object_unref(gmodel);
+    }
+
+end:
     gwy_object_unref(args.result);
+    gwy_object_unref(args.shifts);
     gwy_object_unref(args.bg);
 }
 
@@ -240,6 +272,7 @@ linematch_do_poly(GwyDataField *mask,
                   const LineMatchArgs *args)
 {
     GwyDataField *dfield, *bg;
+    GwyDataLine *means;
     GwyMaskingType masking;
     gdouble *xpowers, *zxpowers, *matrix;
     gint xres, yres, degree, i, j, k;
@@ -249,8 +282,9 @@ linematch_do_poly(GwyDataField *mask,
 
     dfield = args->result;
     bg = args->bg;
-
+    means = args->shifts;
     masking = args->masking;
+
     degree = args->max_degree;
     xres = gwy_data_field_get_xres(dfield);
     yres = gwy_data_field_get_yres(dfield);
@@ -298,6 +332,7 @@ linematch_do_poly(GwyDataField *mask,
             gwy_clear(zxpowers, degree+1);
 
         /* Subtract. */
+        gwy_data_line_set_val(means, i, zxpowers[0]);
         for (j = 0; j < xres; j++) {
             gdouble p = 1.0, x = j - xc, z = 0.0;
 
@@ -335,6 +370,7 @@ linematch_do_median(GwyDataField *mask,
     dfield = args->result;
     bg = args->bg;
     masking = args->masking;
+    medians = args->shifts;
 
     xres = gwy_data_field_get_xres(dfield);
     yres = gwy_data_field_get_yres(dfield);
@@ -344,7 +380,6 @@ linematch_do_median(GwyDataField *mask,
     d = gwy_data_field_get_data(dfield);
     m = mask ? gwy_data_field_get_data_const(mask) : NULL;
     line = gwy_data_line_new(xres, 1.0, FALSE);
-    medians = gwy_data_line_new(yres, 1.0, FALSE);
     buf = gwy_data_line_get_data(line);
 
     if (mask) {
@@ -379,7 +414,6 @@ linematch_do_median(GwyDataField *mask,
 
     apply_row_shifts(dfield, bg, medians);
 
-    g_object_unref(medians);
     g_object_unref(line);
 }
 
@@ -398,13 +432,13 @@ linematch_do_median_diff(GwyDataField *mask,
     dfield = args->result;
     bg = args->bg;
     masking = args->masking;
+    medians = args->shifts;
 
     xres = gwy_data_field_get_xres(dfield);
     yres = gwy_data_field_get_yres(dfield);
     d = gwy_data_field_get_data(dfield);
     m = mask ? gwy_data_field_get_data_const(mask) : NULL;
     line = gwy_data_line_new(xres, 1.0, FALSE);
-    medians = gwy_data_line_new(yres, 1.0, FALSE);
     buf = gwy_data_line_get_data(line);
 
     gwy_data_line_set_val(medians, 0, 0.0);
@@ -428,7 +462,6 @@ linematch_do_median_diff(GwyDataField *mask,
 
     apply_row_shifts(dfield, bg, medians);
 
-    g_object_unref(medians);
     g_object_unref(line);
 }
 
@@ -447,6 +480,7 @@ linematch_do_modus(GwyDataField *mask,
     dfield = args->result;
     bg = args->bg;
     masking = args->masking;
+    modi = args->shifts;
 
     xres = gwy_data_field_get_xres(dfield);
     yres = gwy_data_field_get_yres(dfield);
@@ -456,7 +490,6 @@ linematch_do_modus(GwyDataField *mask,
     d = gwy_data_field_get_data(dfield);
     m = mask ? gwy_data_field_get_data_const(mask) : NULL;
     line = gwy_data_line_new(xres, 1.0, FALSE);
-    modi = gwy_data_line_new(yres, 1.0, FALSE);
     buf = gwy_data_line_get_data(line);
 
     for (i = 0; i < yres; i++) {
@@ -499,11 +532,9 @@ linematch_do_modus(GwyDataField *mask,
 
     apply_row_shifts(dfield, bg, modi);
 
-    g_object_unref(modi);
     g_object_unref(line);
 }
 
-/* TODO: Implement masking. */
 static void
 linematch_do_match(GwyDataField *mask,
                    const LineMatchArgs *args)
@@ -516,6 +547,7 @@ linematch_do_match(GwyDataField *mask,
     const gdouble *d, *m, *a, *b, *ma, *mb;
     gdouble *s, *w;
 
+    shifts = args->shifts;
     dfield = args->result;
     bg = args->bg;
     masking = args->masking;
@@ -524,7 +556,6 @@ linematch_do_match(GwyDataField *mask,
     yres = gwy_data_field_get_yres(dfield);
     d = gwy_data_field_get_data(dfield);
     m = mask ? gwy_data_field_get_data_const(mask) : NULL;
-    shifts = gwy_data_line_new(yres, 1.0, TRUE);
     s = gwy_data_line_get_data(shifts);
 
     w = g_new(gdouble, xres-1);
@@ -586,7 +617,6 @@ linematch_do_match(GwyDataField *mask,
     }
     apply_row_shifts(dfield, bg, shifts);
 
-    g_object_unref(shifts);
     g_free(w);
 }
 
@@ -645,7 +675,6 @@ linematch_dialog(LineMatchArgs *args,
     controls.data = gwy_container_new();
 
     args->result = gwy_data_field_duplicate(dfield);
-    args->bg = gwy_data_field_duplicate(dfield);
 
     gwy_container_set_object_by_name(controls.data, "/0/data", args->result);
     gwy_app_sync_data_items(data, controls.data, id, 0, FALSE,
@@ -682,7 +711,7 @@ linematch_dialog(LineMatchArgs *args,
     gwy_set_data_preview_size(GWY_DATA_VIEW(controls.dataview), PREVIEW_SIZE);
     gtk_box_pack_start(GTK_BOX(hbox), controls.dataview, FALSE, FALSE, 4);
 
-    table = gtk_table_new(4 + LINE_LEVEL_NMETHODS + (mfield ? 4 : 0), 4, FALSE);
+    table = gtk_table_new(5 + LINE_LEVEL_NMETHODS + (mfield ? 4 : 0), 4, FALSE);
     gtk_table_set_row_spacings(GTK_TABLE(table), 2);
     gtk_table_set_col_spacings(GTK_TABLE(table), 6);
     gtk_container_set_border_width(GTK_CONTAINER(table), 4);
@@ -726,7 +755,16 @@ linematch_dialog(LineMatchArgs *args,
                                  args->do_extract);
     g_signal_connect_swapped(controls.do_extract, "toggled",
                              G_CALLBACK(do_extract_changed), &controls);
-    gtk_table_set_row_spacing(GTK_TABLE(table), row, 8);
+    row++;
+
+    controls.do_plot
+        = gtk_check_button_new_with_mnemonic(_("Plot background _graph"));
+    gtk_table_attach(GTK_TABLE(table), controls.do_plot,
+                     0, 4, row, row+1, GTK_FILL, 0, 0, 0);
+    gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(controls.do_plot),
+                                 args->do_plot);
+    g_signal_connect_swapped(controls.do_plot, "toggled",
+                             G_CALLBACK(do_plot_changed), &controls);
     row++;
 
     if (mfield) {
@@ -766,9 +804,11 @@ linematch_dialog(LineMatchArgs *args,
             case RESPONSE_RESET:
             {
                 GwyDataField *result = args->result, *bg = args->bg;
+                GwyDataLine *shifts = args->shifts;
                 *args = linematch_defaults;
                 args->result = result;
                 args->bg = bg;
+                args->shifts = shifts;
                 linematch_dialog_update(&controls, args);
             }
             break;
@@ -821,6 +861,13 @@ do_extract_changed(LineMatchControls *controls,
 }
 
 static void
+do_plot_changed(LineMatchControls *controls,
+                         GtkToggleButton *check)
+{
+    controls->args->do_plot = gtk_toggle_button_get_active(check);
+}
+
+static void
 masking_changed(GtkToggleButton *button,
                 LineMatchControls *controls)
 {
@@ -869,6 +916,7 @@ update_preview(LineMatchControls *controls, LineMatchArgs *args)
 }
 
 static const gchar do_extract_key[] = "/module/linematch/do_extract";
+static const gchar do_plot_key[]    = "/module/linematch/do_plot";
 static const gchar masking_key[]    = "/module/linematch/masking";
 static const gchar method_key[]     = "/module/linematch/method";
 static const gchar max_degree_key[] = "/module/linematch/max_degree";
@@ -880,6 +928,7 @@ sanitize_args(LineMatchArgs *args)
     args->masking = MIN(args->masking, GWY_MASK_INCLUDE);
     args->method = MIN(args->method, LINE_LEVEL_NMETHODS-1);
     args->do_extract = !!args->do_extract;
+    args->do_plot = !!args->do_plot;
 }
 
 static void
@@ -894,6 +943,8 @@ load_args(GwyContainer *container,
     gwy_container_gis_enum_by_name(container, method_key, &args->method);
     gwy_container_gis_boolean_by_name(container, do_extract_key,
                                       &args->do_extract);
+    gwy_container_gis_boolean_by_name(container, do_plot_key,
+                                      &args->do_plot);
     sanitize_args(args);
 }
 
@@ -907,6 +958,7 @@ save_args(GwyContainer *container,
     gwy_container_set_enum_by_name(container, method_key, args->method);
     gwy_container_set_boolean_by_name(container, do_extract_key,
                                       args->do_extract);
+    gwy_container_set_boolean_by_name(container, do_plot_key, args->do_plot);
 }
 
 /* vim: set cin et ts=4 sw=4 cino=>1s,e0,n0,f0,{0,}0,^0,\:1s,=0,g1s,h0,t0,+1s,c3,(0,u0 : */
