5b479a66091d88bb8db6fa46a3d746d2ecd69dd3
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2015-10-25 10:08:38 +0000

    - use string.Template class instead of neverending '+pname+' string   concatenation - do not check g_malloc() return value because it aborts if it cannot allocate   memory; it cannot fail and return - use g_new() instead of g_malloc() - added wrapper for const integer array arguments
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@17649 25697fe4-d644-49b8-9d30-be5465f2adf3

--- modules/pygwy/pygwy-codegen.py
+++ modules/pygwy/pygwy-codegen.py
@@ -4,6 +4,7 @@
 
 import sys
 import re
+import string
 
 # Add codegen path to our import path
 i = 1
@@ -144,132 +145,158 @@ Wrapper.write_function_wrapper=write_function_wrapper
 
 # New argument types:
 class GwyRGBAArg(argtypes.ArgType):
-   def write_param(self, ptype, pname, pdflt, pnull, info):
-      info.varlist.add('GwyRGBA', pname)
-      info.arglist.append(pname)
-      info.add_parselist('[dddd]', ['&' + pname + '.r', '&' + pname + '.g','&' + pname + '.b' , '&' + pname + '.a'], [pname])
-   def write_return(self, ptype, ownsreturn, info):
-      info.varlist.add('GwyRGBA', 'ret');
-      info.varlist.add('PyObject', '*color_tuple');
-      info.codeafter.append('    color_tuple = PyTuple_New(4);\n')      
-      info.codeafter.append('    PyTuple_SetItem(color_tuple, 0, PyFloat_FromDouble(ret.r));\n')
-      info.codeafter.append('    PyTuple_SetItem(color_tuple, 1, PyFloat_FromDouble(ret.g));\n')
-      info.codeafter.append('    PyTuple_SetItem(color_tuple, 2, PyFloat_FromDouble(ret.b));\n')
-      info.codeafter.append('    PyTuple_SetItem(color_tuple, 3, PyFloat_FromDouble(ret.a));\n')
-      info.codeafter.append('    return color_tuple;')
+    def write_param(self, ptype, pname, pdflt, pnull, info):
+        info.varlist.add('GwyRGBA', pname)
+        info.arglist.append(pname)
+        info.add_parselist('[dddd]', ['&' + pname + '.r', '&' + pname + '.g','&' + pname + '.b' , '&' + pname + '.a'], [pname])
+    def write_return(self, ptype, ownsreturn, info):
+        info.varlist.add('GwyRGBA', 'ret');
+        info.varlist.add('PyObject', '*color_tuple');
+        codeafter_template = '''\
+    color_tuple = PyTuple_New(4);
+    PyTuple_SetItem(color_tuple, 0, PyFloat_FromDouble(ret.r));
+    PyTuple_SetItem(color_tuple, 1, PyFloat_FromDouble(ret.g));
+    PyTuple_SetItem(color_tuple, 2, PyFloat_FromDouble(ret.b));
+    PyTuple_SetItem(color_tuple, 3, PyFloat_FromDouble(ret.a));
+    return color_tuple;'''
+        info.codeafter.append(codeafter_template)
 
 class GwyRGBAArgPtr(argtypes.ArgType):
-   def write_param(self, ptype, pname, pdflt, pnull, info):
-      # info.varlist.add('GwyRGBA', '*p_' + pname + ' = ' + pname)
-      info.kwlist = ['"red"', '"green"', '"blue"' ]
-      info.varlist.add('GwyRGBA', 's_' + pname)
-      info.varlist.add('GwyRGBA', '*' + pname + ' = &s_' + pname)
-      info.arglist.append(pname)
-      info.add_parselist('dddd', ['&' + pname + '->r', '&' + pname + '->g', '&' + pname + '->b' , '&' + pname + '->a'], [pname])
-      info.codebefore.append('    gwy_debug("color: %f %f %f %f", '+pname+'->r, '+pname+'->g, '+pname+'->b, '+pname+'->a);')
-   def write_return(self, ptype, ownsreturn, info):
-      info.varlist.add('GwyRGBA', '*ret');
-      info.varlist.add('PyObject', '*color_tuple');
-      info.codeafter.append('    color_tuple = PyTuple_New(4);\n')      
-      info.codeafter.append('    PyTuple_SetItem(color_tuple, 0, PyFloat_FromDouble(ret->r));\n')
-      info.codeafter.append('    PyTuple_SetItem(color_tuple, 1, PyFloat_FromDouble(ret->g));\n')
-      info.codeafter.append('    PyTuple_SetItem(color_tuple, 2, PyFloat_FromDouble(ret->b));\n')
-      info.codeafter.append('    PyTuple_SetItem(color_tuple, 3, PyFloat_FromDouble(ret->a));\n')
-      info.codeafter.append('    return color_tuple;')
+    def write_param(self, ptype, pname, pdflt, pnull, info):
+        # info.varlist.add('GwyRGBA', '*p_' + pname + ' = ' + pname)
+        info.kwlist = ['"red"', '"green"', '"blue"' ]
+        info.varlist.add('GwyRGBA', 's_' + pname)
+        info.varlist.add('GwyRGBA', '*' + pname + ' = &s_' + pname)
+        info.arglist.append(pname)
+        info.add_parselist('dddd', ['&' + pname + '->r', '&' + pname + '->g', '&' + pname + '->b' , '&' + pname + '->a'], [pname])
+        info.codebefore.append('    gwy_debug("color: %f %f %f %f", '+pname+'->r, '+pname+'->g, '+pname+'->b, '+pname+'->a);')
+    def write_return(self, ptype, ownsreturn, info):
+        info.varlist.add('GwyRGBA', '*ret');
+        info.varlist.add('PyObject', '*color_tuple');
+        codeafter_template = '''\
+    color_tuple = PyTuple_New(4);
+    PyTuple_SetItem(color_tuple, 0, PyFloat_FromDouble(ret->r));
+    PyTuple_SetItem(color_tuple, 1, PyFloat_FromDouble(ret->g));
+    PyTuple_SetItem(color_tuple, 2, PyFloat_FromDouble(ret->b));
+    PyTuple_SetItem(color_tuple, 3, PyFloat_FromDouble(ret->a));
+    return color_tuple;'''
+        info.codeafter.append(codeafter_template)
 
 class GDoubleValue(argtypes.ArgType):
-   def write_param(self, ptype, pname, pdflt, pnull, info):
-      info.varlist.add(ptype, pname)
-      info.arglist.append('&'+pname)
-      info.codebefore.append('    '+pname+' = 0;\n')
-      info.codeafter.append('    PyTuple_SetItem(py_tuple_ret,py_tuple_index++,PyFloat_FromDouble('+pname+'));\n')
+    def write_param(self, ptype, pname, pdflt, pnull, info):
+        info.varlist.add(ptype, pname)
+        info.arglist.append('&'+pname)
+        info.codebefore.append('    '+pname+' = 0;\n')
+        info.codeafter.append('    PyTuple_SetItem(py_tuple_ret,py_tuple_index++,PyFloat_FromDouble('+pname+'));\n')
 
 class GIntValue(argtypes.ArgType):
-   def write_param(self, ptype, pname, pdflt, pnull, info):
-      info.varlist.add(ptype, pname)
-      info.arglist.append('&'+pname)
-      info.codebefore.append('    '+pname+' = 0;\n')
-      info.codeafter.append('    PyTuple_SetItem(py_tuple_ret,py_tuple_index++,PyInt_FromLong('+pname+'));\n')
+    def write_param(self, ptype, pname, pdflt, pnull, info):
+        info.varlist.add(ptype, pname)
+        info.arglist.append('&'+pname)
+        info.codebefore.append('    '+pname+' = 0;\n')
+        info.codeafter.append('    PyTuple_SetItem(py_tuple_ret,py_tuple_index++,PyInt_FromLong('+pname+'));\n')
 
 class GConstDoubleArray(argtypes.ArgType):
-   def write_param(self, ptype, pname, pdflt, pnull, info):
-      ptype = ptype.replace('*', '')
-      ptype = ptype.replace('const-', '')
-      info.varlist.add(ptype, '*'+pname);
-      info.varlist.add('int', 'i_'+pname);
-      info.varlist.add('PyObject', '*'+pname+'_pyobj')
-      info.add_parselist("O", ['&'+pname+'_pyobj'], [pname])
-      info.arglist.append(pname)
-      info.codebefore.append('    if (!PyList_Check('+pname+'_pyobj)) {\n')
-      info.codebefore.append('        PyErr_SetString(PyExc_TypeError, "Parameter \''+pname+'\' must be a list of floats");\n')
-      info.codebefore.append('        return NULL;\n')
-      info.codebefore.append('    }\n')
-      info.codebefore.append('    '+pname+' = g_new('+ptype+', PyList_Size('+pname+'_pyobj));\n')
-      info.codebefore.append('    for (i_'+pname+' = 0; i_'+pname+' < PyList_Size('+pname+'_pyobj); i_'+pname+'++) {\n')
-      info.codebefore.append('        if (!PyFloat_Check(PyList_GetItem('+pname+'_pyobj, i_'+pname+'))) {\n')
-      info.codebefore.append('            g_free('+pname+');\n')
-      info.codebefore.append('            PyErr_SetString(PyExc_TypeError, "Parameter \''+pname+'\' must be a list of floats");\n')
-      info.codebefore.append('            return NULL;\n')
-      info.codebefore.append('        }\n')
-      info.codebefore.append('        '+pname+'[i_'+pname+'] = PyFloat_AsDouble(PyList_GetItem('+pname+'_pyobj, i_'+pname+'));\n')
-      info.codebefore.append('    }\n')
-      info.codeafter.append('    g_free('+pname+');\n')
-   def write_return(self, ptype, ownsreturn, info):
-      info.codeafter.append('    PyErr_SetString(PyExc_NotImplementedError, "Return type \''+ptype+'\' not supported");\n')
-      ptype = ptype.replace('*', '')
-      ptype = ptype.replace('const-', '')
-      info.varlist.add(ptype, '*ret')
-      info.codeafter.append('    return NULL;')
+    def write_param(self, ptype, pname, pdflt, pnull, info):
+        ptype = ptype.replace('*', '')
+        ptype = ptype.replace('const-', '')
+        pdict = {'name': pname, 'type': ptype}
+        info.varlist.add(ptype, '*'+pname);
+        info.varlist.add('int', 'i_'+pname);
+        info.varlist.add('PyObject', '*'+pname+'_pyobj')
+        info.add_parselist("O", ['&'+pname+'_pyobj'], [pname])
+        info.arglist.append(pname)
+        codebefore_template = string.Template('''\
+    if (!PyList_Check(${name}_pyobj)) {
+        PyErr_SetString(PyExc_TypeError, "Parameter \'${name}\' must be a list of floats");
+        return NULL;
+    }
+    ${name} = g_new(${type}, PyList_Size(${name}_pyobj));
+    for (i_${name} = 0; i_${name} < PyList_Size(${name}_pyobj); i_${name}++) {
+        if (!PyFloat_Check(PyList_GetItem(${name}_pyobj, i_${name}))) {
+            g_free(${name});
+            PyErr_SetString(PyExc_TypeError, "Parameter \'${name}\' must be a list of floats");
+            return NULL;
+        }
+        ${name}[i_${name}] = PyFloat_AsDouble(PyList_GetItem(${name}_pyobj, i_${name}));
+    }
+''')
+        codeafter_template = string.Template('''\
+    g_free(${name});
+''')
+        info.codebefore.append(codebefore_template.substitute(pdict))
+        info.codeafter.append(codeafter_template.substitute(pdict))
+    def write_return(self, ptype, ownsreturn, info):
+        info.codeafter.append('    PyErr_SetString(PyExc_NotImplementedError, "Return type \''+ptype+'\' not supported");\n')
+        ptype = ptype.replace('*', '')
+        ptype = ptype.replace('const-', '')
+        info.varlist.add(ptype, '*ret')
+        info.codeafter.append('    return NULL;')
 
 class GArrayDouble(argtypes.ArgType):
-   def write_return(self, ptype, ownsreturn, info):
-      ptype = ptype.replace('*', '')
-      info.varlist.add(ptype, '*ret')
-      info.varlist.add("PyObject", '*py_ret')
-      info.varlist.add("gint", 'i_ret')
-      info.codeafter.append('    py_ret = PyList_New(ret->len);\n')
-      info.codeafter.append('    for (i_ret = 0; i_ret < ret->len; i_ret++) {\n')
-      info.codeafter.append('        PyList_SetItem(py_ret, i_ret, PyFloat_FromDouble(g_array_index(ret, gdouble, i_ret)));\n')
-      info.codeafter.append('    }\n')
-      info.codeafter.append('    g_array_free(ret, TRUE);\n')
-      info.codeafter.append('    return py_ret;\n')
+    def write_return(self, ptype, ownsreturn, info):
+        ptype = ptype.replace('*', '')
+        info.varlist.add(ptype, '*ret')
+        info.varlist.add("PyObject", '*py_ret')
+        info.varlist.add("gint", 'i_ret')
+        codeafter_template = '''\
+    py_ret = PyList_New(ret->len);
+    for (i_ret = 0; i_ret < ret->len; i_ret++) {
+        PyList_SetItem(py_ret, i_ret, PyFloat_FromDouble(g_array_index(ret, gdouble, i_ret)));
+    }
+    g_array_free(ret, TRUE);
+    return py_ret;
+'''
+        info.codeafter.append(codeafter_template)
 
 class GArrayInt(argtypes.ArgType):
-   def write_return(self, ptype, ownsreturn, info):
-      ptype = ptype.replace('*', '')
-      info.varlist.add(ptype, '*ret')
-      info.varlist.add("PyObject", '*py_ret')
-      info.varlist.add("gint", 'i_ret')
-      info.codeafter.append('    py_ret = PyList_New(ret->len);\n')
-      info.codeafter.append('    for (i_ret = 0; i_ret < ret->len; i_ret++) {\n')
-      info.codeafter.append('        PyList_SetItem(py_ret, i_ret, PyInt_FromLong(g_array_index(ret, gint, i_ret)));\n')
-      info.codeafter.append('    }\n')
-      info.codeafter.append('    g_array_free(ret, TRUE);\n')
-      info.codeafter.append('    return py_ret;\n')
-   def write_param(self, ptype, pname, pdflt, pnull, info):
-      ptype = ptype.replace('*', '')
-      ptype = ptype.replace('const-', '')
-      info.varlist.add(ptype, '*'+pname);
-      info.varlist.add('int', 'i_'+pname);
-      info.varlist.add('gint', pname+'_value')
-      info.varlist.add('PyObject', '*'+pname+'_pyobj')
-      info.add_parselist("O", ['&'+pname+'_pyobj'], [pname])
-      info.arglist.append(pname)
-      info.codebefore.append('    if (!PyList_Check('+pname+'_pyobj)) {\n')
-      info.codebefore.append('        PyErr_SetString(PyExc_TypeError, "Parameter \''+pname+'\' must be a list of integers");\n')
-      info.codebefore.append('        return NULL;\n')
-      info.codebefore.append('    }\n')
-      info.codebefore.append('    '+pname+' = g_array_sized_new(FALSE,FALSE,sizeof('+ptype+'),PyList_Size('+pname+'_pyobj));\n')
-      info.codebefore.append('    for (i_'+pname+' = 0; i_'+pname+' < PyList_Size('+pname+'_pyobj); i_'+pname+'++) {\n')
-      info.codebefore.append('        if (!PyInt_Check(PyList_GetItem('+pname+'_pyobj, i_'+pname+'))) {\n')
-      info.codebefore.append('            g_array_free('+pname+',TRUE);\n')
-      info.codebefore.append('            PyErr_SetString(PyExc_TypeError, "Parameter \''+pname+'\' must be a list of integers");\n')
-      info.codebefore.append('            return NULL;\n')
-      info.codebefore.append('        }\n')
-      info.codebefore.append('        '+pname+'_value = PyInt_AsLong(PyList_GetItem('+pname+'_pyobj, i_'+pname+'));\n')
-      info.codebefore.append('        g_array_append_val('+pname+','+pname+'_value);\n')
-      info.codebefore.append('    }\n')
-      info.codeafter.append('    g_array_free('+pname+',TRUE);\n')
+    def write_return(self, ptype, ownsreturn, info):
+        ptype = ptype.replace('*', '')
+        info.varlist.add(ptype, '*ret')
+        info.varlist.add("PyObject", '*py_ret')
+        info.varlist.add("gint", 'i_ret')
+        codeafter_template = '''\
+    py_ret = PyList_New(ret->len);
+    for (i_ret = 0; i_ret < ret->len; i_ret++) {
+        PyList_SetItem(py_ret, i_ret, PyInt_FromLong(g_array_index(ret, gint, i_ret)));
+    }
+    g_array_free(ret, TRUE);
+    return py_ret;
+'''
+        info.codeafter.append(codeafter_template)
+    def write_param(self, ptype, pname, pdflt, pnull, info):
+        ptype = ptype.replace('*', '')
+        ptype = ptype.replace('const-', '')
+        pdict = {'name': pname, 'type': ptype}
+        info.varlist.add(ptype, '*'+pname);
+        info.varlist.add('int', 'i_'+pname);
+        info.varlist.add('int', 'n_'+pname);
+        info.varlist.add('gint', pname+'_value')
+        info.varlist.add('PyObject', '*'+pname+'_pyobj')
+        info.add_parselist("O", ['&'+pname+'_pyobj'], [pname])
+        info.arglist.append(pname)
+        codebefore_template = string.Template('''\
+    if (!PyList_Check(${name}_pyobj)) {
+        PyErr_SetString(PyExc_TypeError, "Parameter \'${name}\' must be a list of integers");
+        return NULL;
+    }
+    n_${name} = PyList_Size(${name}_pyobj);
+    ${name} = g_array_sized_new(FALSE, FALSE, sizeof(${type}), n);
+    for (i_${name} = 0; i_${name} < n_${name}; i_${name}++) {
+        if (!PyInt_Check(PyList_GetItem(${name}_pyobj, i_${name}))) {
+            g_array_free(${name},TRUE);
+            PyErr_SetString(PyExc_TypeError, "Parameter \'${name}\' must be a list of integers");
+            return NULL;
+        }
+        ${name}_value = PyInt_AsLong(PyList_GetItem(${name}_pyobj, i_${name}));
+        g_array_append_val(${name},${name}_value);
+    }
+''')
+        codeafter_template = string.Template('''\
+    g_array_free(${name},TRUE);
+''')
+        info.codebefore.append(codebefore_template.substitute(pdict))
+        info.codeafter.append(codeafter_template.substitute(pdict))
 
 class StringPassArg(argtypes.ArgType):
     def write_param(self, ptype, pname, pdflt, pnull, info):
@@ -306,7 +333,7 @@ class StringPassArg(argtypes.ArgType):
 
 
 
-      
+
 # Extend argtypes
 arg = GArrayDouble()
 argtypes.matcher.register('GArray*', arg)
