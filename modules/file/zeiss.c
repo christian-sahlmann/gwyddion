1c98d3c97f32a47690ee6c5eefe2914b6339c465
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2015-12-03 15:01:54 +0000

    - simplified code using gwy_tiff_read_image_row_averaged()
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@17800 25697fe4-d644-49b8-9d30-be5465f2adf3

--- modules/file/zeiss.c
+++ modules/file/zeiss.c
@@ -60,7 +60,7 @@ static GwyModuleInfo module_info = {
     module_register,
     N_("Imports Carl Zeiss SEM images."),
     "Yeti <yeti@gwyddion.net>",
-    "0.5",
+    "0.6",
     "David Nečas (Yeti)",
     "2011",
 };
@@ -208,27 +208,9 @@ zeiss_load_tiff(const GwyTIFF *tiff, GError **error)
     g_object_unref(siunit);
 
     data = gwy_data_field_get_data(dfield);
-    if (reader->samples_per_pixel > 1) {
-        gdouble *datarow = g_new(gdouble, reader->width);
-        gint ch, j, spp = reader->samples_per_pixel;
-
-        gwy_data_field_clear(dfield);
-        for (i = 0; i < reader->height; i++) {
-            for (ch = 0; ch < spp; ch++) {
-                gwy_tiff_read_image_row(tiff, reader, 0, i, 1.0, 0.0, datarow);
-                for (j = 0; j < reader->width; j++)
-                    data[i*reader->width + j] += datarow[j];
-            }
-        }
-        g_free(datarow);
-        gwy_data_field_multiply(dfield, 1.0/spp);
-        gwy_data_field_invalidate(dfield);
-    }
-    else {
-        for (i = 0; i < reader->height; i++)
-            gwy_tiff_read_image_row(tiff, reader, 0, i, 1.0, 0.0,
-                                    data + i*reader->width);
-    }
+    for (i = 0; i < reader->height; i++)
+        gwy_tiff_read_image_row_averaged(tiff, reader, i, 1.0, 0.0,
+                                         data + i*reader->width);
 
     container = gwy_container_new();
     gwy_container_set_object_by_name(container, "/0/data", dfield);
