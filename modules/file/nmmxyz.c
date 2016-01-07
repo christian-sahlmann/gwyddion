944289460ceaedbbb6c4d3056daee00e9d5ea965
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2016-01-07 09:58:28 +0000

    - refactored code, preparation for three-stage loading: overview scan, GUI,   actual regularisation
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@18014 25697fe4-d644-49b8-9d30-be5465f2adf3

--- modules/file/nmmxyz.c
+++ modules/file/nmmxyz.c
@@ -43,6 +43,30 @@
 #define DASHED_LINE "------------------------------------------"
 
 typedef struct {
+    guint nfiles;
+    guint blocksize;
+    gulong ndata;
+    gdouble xmin;
+    gdouble xmax;
+    gdouble ymin;
+    gdouble ymax;
+} NMMXYZInfo;
+
+typedef struct {
+    guint nchannels;
+    gboolean *include_channel;
+    gint xres;
+    gint yres;
+} NMMXYZArgs;
+
+typedef struct {
+    NMMXYZArgs *args;
+    GtkWidget *dialog;
+    GtkWidget *info;
+    GtkWidget **include_channel;
+} NMMXYZControls;
+
+typedef struct {
     guint id;
     guint npts;
     gchar *date;
@@ -56,20 +80,32 @@ static gint          nmmxyz_detect             (const GwyFileDetectInfo *fileinf
 static GwyContainer* nmmxyz_load               (const gchar *filename,
                                                 GwyRunType mode,
                                                 GError **error);
-static void          find_data_range           (const gdouble *alldata,
-                                                guint ndata,
-                                                guint blocksize,
-                                                gdouble *xmin,
-                                                gdouble *xmax,
-                                                gdouble *ymin,
-                                                gdouble *ymax);
+static void          gather_data_files         (const gchar *filename,
+                                                NMMXYZInfo *info,
+                                                GArray *dscs,
+                                                GArray *data);
+static PointXYZ*     create_points_with_xy     (GArray *data,
+                                                guint blocksize);
+static void          create_data_field         (GwyContainer *container,
+                                                const NMMXYZInfo *info,
+                                                const NMMXYZArgs *args,
+                                                GArray *dscs,
+                                                GArray *data,
+                                                PointXYZ *points,
+                                                guint i);
+static void          find_data_range           (const PointXYZ *points,
+                                                NMMXYZInfo *info);
 static void          read_data_file            (GArray *data,
                                                 const gchar *filename,
                                                 guint nrec);
 static gboolean      profile_descriptions_match(GArray *descs1,
                                                 GArray *descs2);
-static GArray*       read_profile_description  (const gchar *filename);
-static void          free_profile_descriptions (GArray *dscs);
+static void          read_profile_description  (const gchar *filename,
+                                                GArray *dscs);
+static void          free_profile_descriptions (GArray *dscs,
+                                                gboolean free_array);
+static void          copy_profile_descriptions (GArray *source,
+                                                GArray *dest);
 
 static GwyModuleInfo module_info = {
     GWY_MODULE_ABI_VERSION,
@@ -115,127 +151,84 @@ nmmxyz_load(const gchar *filename,
             G_GNUC_UNUSED GwyRunType mode,
             GError **error)
 {
+    NMMXYZInfo info;
+    NMMXYZArgs args;
     GwyContainer *container = NULL;
     GArray *data = NULL;
-    GArray *dscs_first = NULL;
-    gchar *dirname = NULL, *basename = NULL, *s;
-    GDir *dir = NULL;
-    const gchar *fname;
-    gdouble xmin, xmax, ymin, ymax;
+    GArray *dscs = NULL;
     PointXYZ *points = NULL;
-    guint i, j, blocksize = 0, npts = 0;
-    const gdouble *d;
-    gint xres, yres;
+    guint i;
+    gdouble q;
 
-    dirname = g_path_get_dirname(filename);
-    basename = g_path_get_basename(filename);
-    if ((s = strrchr(basename, '.')))
-        *s = '\0';
-
-    dir = g_dir_open(dirname, 0, NULL);
-    if (!dir)
-        goto fail;
-
-    data = g_array_new(FALSE, FALSE, sizeof(gdouble));
-    while ((fname = g_dir_read_name(dir))) {
-        GArray *dscs;
-        guint len;
-
-        if (!g_str_has_prefix(fname, basename)
-            || !g_str_has_suffix(fname, EXTENSION))
-            continue;
-
-        s = g_build_filename(dirname, fname, NULL);
-        dscs = read_profile_description(s);
-        g_free(s);
-
-        if (!dscs)
-            continue;
-
-        gwy_debug("Found DSC file %s (%u records)", fname, (guint)dscs->len);
-        if (!dscs_first) {
-            dscs_first = dscs;
-            blocksize = dscs_first->len;
-        }
-        else {
-            if (!profile_descriptions_match(dscs_first, dscs)) {
-                gwy_debug("non-matching profile descriptions for %s", fname);
-                free_profile_descriptions(dscs);
-                continue;
-            }
-            free_profile_descriptions(dscs);
-        }
-
-        s = g_build_filename(dirname, fname, NULL);
-        len = strlen(s);
-        s[len-1] = 't';
-        s[len-2] = 'a';
-        read_data_file(data, s, blocksize);
-        g_free(s);
+    /* In principle we can load data non-interactively, but it is going to
+     * take several minutes which is not good for previews... */
+    if (mode != GWY_RUN_INTERACTIVE) {
+        g_set_error(error, GWY_MODULE_FILE_ERROR,
+                    GWY_MODULE_FILE_ERROR_INTERACTIVE,
+                    _("Nano Measuring Machine data import "
+                      "must be run as interactive."));
+        return NULL;
     }
 
-    if (!data->len) {
+    dscs = g_array_new(FALSE, FALSE, sizeof(NMMXYZProfileDescription));
+    gather_data_files(filename, &info, dscs, NULL);
+    if (!info.ndata) {
         err_NO_DATA(error);
         goto fail;
     }
 
-    npts = data->len/blocksize;
-    d = (const gdouble*)data->data;
-    find_data_range(d, npts, blocksize, &xmin, &xmax, &ymin, &ymax);
+    /* TODO: here goes the dialogue. */
 
-    points = g_new(PointXYZ, npts);
-    for (i = 0; i < npts; i++) {
-        const gdouble *xy = d + i*blocksize;
-        points[i].x = xy[0];
-        points[i].y = xy[1];
-    }
+    free_profile_descriptions(dscs, FALSE);
+    data = g_array_new(FALSE, FALSE, sizeof(gdouble));
+    gather_data_files(filename, &info, dscs, data);
+
+    points = create_points_with_xy(data, info.blocksize);
+    find_data_range(points, &info);
 
     /* XXX: Needs some safeguards.  But anyway, this will be selectable in the
      * GUI when we have it. */
-    xres = (gint)ceil(2048.0*(xmax - xmin)/(ymax - ymin));
-    yres = (gint)ceil(2048.0*(ymax - ymin)/(xmax - xmin));
+    q = sqrt((info.xmax - info.xmin)/(info.ymax - info.ymin));
+    args.xres = (gint)ceil(2048.0*q);
+    args.yres = (gint)ceil(2048.0/q);
 
     container = gwy_container_new();
-    for (j = 0; j < blocksize-2; j++) {
-        const NMMXYZProfileDescription *dsc
-            = &g_array_index(dscs_first, NMMXYZProfileDescription, i);
-
-        GwyDataField *dfield = gwy_data_field_new(xres, yres,
-                                                  xmax - xmin, ymax - ymin,
-                                                  FALSE);
-        GQuark quark;
-
-        gwy_data_field_set_xoffset(dfield, xmin);
-        gwy_data_field_set_yoffset(dfield, ymin);
-        for (i = 0; i < npts; i++)
-            points[i].z = d[i*blocksize + 2 + j];
-
-        gwy_data_field_average_xyz(dfield, NULL, points, npts);
-
-        quark = gwy_app_get_data_key_for_id(j);
-        gwy_container_set_object(container, quark, dfield);
-        g_object_unref(dfield);
-
-        quark = gwy_app_get_data_title_key_for_id(j);
-        gwy_container_set_const_string(container, quark, dsc->long_name);
+    for (i = 0; i < info.blocksize-2; i++) {
+        create_data_field(container, &info, &args, dscs, data, points, i);
     }
 
 fail:
     g_free(points);
-    free_profile_descriptions(dscs_first);
+    free_profile_descriptions(dscs, TRUE);
     if (data)
         g_array_free(data, TRUE);
-    if (dir)
-        g_dir_close(dir);
-    g_free(basename);
-    g_free(dirname);
 
     return container;
 }
 
+static PointXYZ*
+create_points_with_xy(GArray *data, guint blocksize)
+{
+    PointXYZ *points;
+    const gdouble *d;
+    gulong i, npts;
+
+    gwy_debug("data->len %u, block size %u", data->len, blocksize);
+    d = (const gdouble*)data->data;
+    npts = data->len/blocksize;
+    points = g_new(PointXYZ, npts);
+    gwy_debug("creating %lu XYZ points", npts);
+    for (i = 0; i < npts; i++) {
+        points[i].x = d[0];
+        points[i].y = d[1];
+        d += blocksize;
+    }
+
+    return points;
+}
+
 static void
-find_data_range(const gdouble *alldata, guint ndata, guint blocksize,
-                gdouble *pxmin, gdouble *pxmax, gdouble *pymin, gdouble *pymax)
+find_data_range(const PointXYZ *points, NMMXYZInfo *info)
 {
     gdouble xmin = G_MAXDOUBLE;
     gdouble ymin = G_MAXDOUBLE;
@@ -243,24 +236,167 @@ find_data_range(const gdouble *alldata, guint ndata, guint blocksize,
     gdouble ymax = -G_MAXDOUBLE;
     guint i;
 
-    gwy_debug("finding the XY range from %u records", ndata);
-    for (i = 0; i < ndata; i++) {
-        const gdouble *xy = alldata + i*blocksize;
-        if (xy[0] < xmin)
-            xmin = xy[0];
-        if (xy[0] > xmax)
-            xmax = xy[0];
-        if (xy[1] < ymin)
-            ymin = xy[1];
-        if (xy[1] > ymax)
-            ymax = xy[1];
+    gwy_debug("finding the XY range from %lu records", info->ndata);
+    for (i = 0; i < info->ndata; i++) {
+        const gdouble x = points[i].x, y = points[i].y;
+        if (x < xmin)
+            xmin = x;
+        if (x > xmax)
+            xmax = x;
+        if (y < ymin)
+            ymin = y;
+        if (y > ymax)
+            ymax = y;
     }
 
-    *pxmin = xmin;
-    *pxmax = xmax;
-    *pymin = ymin;
-    *pymax = ymax;
+    info->xmin = xmin;
+    info->xmax = xmax;
+    info->ymin = ymin;
+    info->ymax = ymax;
     gwy_debug("full data range [%g,%g]x[%g,%g]", xmin, xmax, ymin, ymax);
+}
+
+static void
+create_data_field(GwyContainer *container,
+                  const NMMXYZInfo *info,
+                  const NMMXYZArgs *args,
+                  GArray *dscs,
+                  GArray *data,
+                  PointXYZ *points,
+                  guint i)
+{
+    const NMMXYZProfileDescription *dsc
+        = &g_array_index(dscs, NMMXYZProfileDescription, i + 2);
+    GwyDataField *dfield;
+    gulong k, ndata;
+    guint blocksize;
+    const gdouble *d;
+    GQuark quark;
+
+    gwy_debug("regularising field #%u %s (%s)",
+              i, dsc->short_name, dsc->long_name);
+    dfield = gwy_data_field_new(args->xres, args->yres,
+                                info->xmax - info->xmin,
+                                info->ymax - info->ymin,
+                                FALSE);
+
+    gwy_data_field_set_xoffset(dfield, info->xmin);
+    gwy_data_field_set_yoffset(dfield, info->ymin);
+    gwy_si_unit_set_from_string(gwy_data_field_get_si_unit_xy(dfield), "m");
+
+    d = (const gdouble*)data->data + (i + 2);
+    blocksize = info->blocksize;
+    ndata = info->ndata;
+    for (k = 0; k < ndata; k++) {
+        points[k].z = *d;
+        d += blocksize;
+    }
+
+    gwy_data_field_average_xyz(dfield, NULL, points, ndata);
+
+    quark = gwy_app_get_data_key_for_id(i);
+    gwy_container_set_object(container, quark, dfield);
+    g_object_unref(dfield);
+
+    quark = gwy_app_get_data_title_key_for_id(i);
+    gwy_container_set_const_string(container, quark, dsc->long_name);
+}
+
+/* Fills nfiles, blocksize and ndata fields of @info.  When @data is %NULL
+ * the number of files and data is taken from headers.
+ *
+ * If non-NULL @data array is passed the raw data are immediately loaded.
+ *
+ * TODO: We need a progress bar.
+ * TODO: If include_channel is passed we can both save memory (by skipping
+ *       unwanted raw data) and time (by moving forward to the start of next
+ *       whitespace instead of parsing the coordinates).
+ */
+static void
+gather_data_files(const gchar *filename, NMMXYZInfo *info,
+                  GArray *dscs, GArray *data)
+{
+    const NMMXYZProfileDescription *dsc;
+    GArray *this_dscs = NULL;
+    GDir *dir = NULL;
+    const gchar *fname;
+    gchar *dirname = NULL, *basename = NULL, *s;
+    GString *str;
+
+    info->nfiles = 0;
+    info->ndata = 0;
+    info->blocksize = 0;
+
+    dirname = g_path_get_dirname(filename);
+    basename = g_path_get_basename(filename);
+    str = g_string_new(basename);
+    g_free(basename);
+    if ((s = strrchr(str->str, '.'))) {
+        g_string_truncate(str, s - str->str);
+        g_string_append(str, "_");
+    }
+    basename = g_strdup(str->str);
+
+    gwy_debug("scanning dir <%s> for files <%s*%s>",
+              dirname, basename, EXTENSION);
+    dir = g_dir_open(dirname, 0, NULL);
+    if (!dir)
+        goto fail;
+
+    this_dscs = g_array_new(FALSE, FALSE, sizeof(NMMXYZProfileDescription));
+    while ((fname = g_dir_read_name(dir))) {
+        gwy_debug("candidate file %s", fname);
+        if (!g_str_has_prefix(fname, basename)
+            || !g_str_has_suffix(fname, EXTENSION))
+            continue;
+
+        s = g_build_filename(dirname, fname, NULL);
+        free_profile_descriptions(this_dscs, FALSE);
+        read_profile_description(s, this_dscs);
+        g_free(s);
+
+        gwy_debug("found DSC file %s (%u records)", fname, this_dscs->len);
+        if (!this_dscs->len)
+            continue;
+
+        if (!info->nfiles) {
+            copy_profile_descriptions(this_dscs, dscs);
+            info->blocksize = this_dscs->len;
+        }
+        else {
+            if (!profile_descriptions_match(dscs, this_dscs)) {
+                gwy_debug("non-matching profile descriptions for %s", fname);
+                continue;
+            }
+        }
+
+        info->nfiles++;
+
+        if (data) {
+            s = g_build_filename(dirname, fname, NULL);
+            g_string_assign(str, s);
+            g_free(s);
+
+            if ((s = strrchr(str->str, '.'))) {
+                g_string_truncate(str, s - str->str);
+                g_string_append(str, ".dat");
+            }
+            read_data_file(data, str->str, info->blocksize);
+            info->ndata = data->len/info->blocksize;
+        }
+        else {
+            dsc = &g_array_index(this_dscs, NMMXYZProfileDescription, 0);
+            info->ndata += dsc->npts;
+        }
+    }
+
+fail:
+    free_profile_descriptions(this_dscs, TRUE);
+    if (dir)
+        g_dir_close(dir);
+    g_string_free(str, TRUE);
+    g_free(basename);
+    g_free(dirname);
 }
 
 static void
@@ -308,7 +444,7 @@ profile_descriptions_match(GArray *descs1, GArray *descs2)
 
     if (descs1->len != descs2->len) {
         gwy_debug("non-matching channel numbers %u vs %u",
-                  (guint)descs1->len, (guint)descs2->len);
+                  descs1->len, descs2->len);
         return FALSE;
     }
 
@@ -328,10 +464,9 @@ profile_descriptions_match(GArray *descs1, GArray *descs2)
     return TRUE;
 }
 
-static GArray*
-read_profile_description(const gchar *filename)
+static void
+read_profile_description(const gchar *filename, GArray *dscs)
 {
-    GArray *dscs = NULL;
     NMMXYZProfileDescription dsc;
     gchar *buffer = NULL, *line, *p;
     gchar **pieces = NULL;
@@ -345,7 +480,7 @@ read_profile_description(const gchar *filename)
     if (!(line = gwy_str_next_line(&p)) || !gwy_strequal(line, DASHED_LINE))
         goto fail;
 
-    dscs = g_array_new(FALSE, FALSE, sizeof(NMMXYZProfileDescription));
+    g_assert(!dscs->len);
     while ((line = gwy_str_next_line(&p))) {
         if (gwy_strequal(line, DASHED_LINE))
             break;
@@ -364,30 +499,29 @@ read_profile_description(const gchar *filename)
 
     /* The ids should match the line numbers. */
     for (i = 0; i < dscs->len; i++) {
-        if (g_array_index(dscs, NMMXYZProfileDescription, i).id != i)
+        if (g_array_index(dscs, NMMXYZProfileDescription, i).id != i) {
+            gwy_debug("non-matching channel id #%u", i);
             goto fail;
+        }
     }
 
     /* We cannot read files with different number of points for each channel. */
     for (i = 1; i < dscs->len; i++) {
         if (g_array_index(dscs, NMMXYZProfileDescription, i).npts
-            != g_array_index(dscs, NMMXYZProfileDescription, i).npts)
+            != g_array_index(dscs, NMMXYZProfileDescription, i-1).npts) {
+            gwy_debug("non-matching number of points per channel #%u", i);
             goto fail;
+        }
     }
-
-    return dscs;
 
 fail:
     g_free(buffer);
-    free_profile_descriptions(dscs);
     if (pieces)
         g_strfreev(pieces);
-
-    return NULL;
 }
 
 static void
-free_profile_descriptions(GArray *dscs)
+free_profile_descriptions(GArray *dscs, gboolean free_array)
 {
     guint i;
 
@@ -402,7 +536,31 @@ free_profile_descriptions(GArray *dscs)
         g_free(dsc->short_name);
         g_free(dsc->long_name);
     }
-    g_array_free(dscs, TRUE);
+
+    if (free_array)
+        g_array_free(dscs, TRUE);
+    else
+        g_array_set_size(dscs, 0);
+}
+
+static void
+copy_profile_descriptions(GArray *source, GArray *dest)
+{
+    guint i;
+
+    g_assert(source);
+    g_assert(dest);
+    g_assert(!dest->len);
+
+    for (i = 0; i < source->len; i++) {
+        NMMXYZProfileDescription dsc
+            = g_array_index(source, NMMXYZProfileDescription, i);
+
+        dsc.date = g_strdup(dsc.date);
+        dsc.short_name = g_strdup(dsc.short_name);
+        dsc.long_name = g_strdup(dsc.long_name);
+        g_array_append_val(dest, dsc);
+    }
 }
 
 /* vim: set cin et ts=4 sw=4 cino=>1s,e0,n0,f0,{0,}0,^0,\:1s,=0,g1s,h0,t0,+1s,c3,(0,u0 : */
