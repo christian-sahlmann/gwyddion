afaeb5038e6a3c290fd392dc76c60b4e35bb406c
dn2010 <dn2010@25697fe4-d644-49b8-9d30-be5465f2adf3>
2016-01-07 12:38:00 +0000

    - Added vim comment to the end of file
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@18018 25697fe4-d644-49b8-9d30-be5465f2adf3

--- modules/file/leica.c
+++ modules/file/leica.c
@@ -976,3 +976,5 @@ header_parse_text(G_GNUC_UNUSED GMarkupParseContext *context,
 {
     // gwy_debug("Text = %s", value);
 }
+
+/* vim: set cin et ts=4 sw=4 cino=>1s,e0,n0,f0,{0,}0,^0,\:1s,=0,g1s,h0,t0,+1s,c3,(0,u0 : */
