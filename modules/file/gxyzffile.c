bb8d6cc2db4785c3b912825c7cda6057c44c8112
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2016-02-23 19:13:36 +0000

    - import GXYZF as native XYZ data
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@18330 25697fe4-d644-49b8-9d30-be5465f2adf3

--- modules/file/gxyzffile.c
+++ modules/file/gxyzffile.c
@@ -61,15 +61,6 @@
 #define MAGIC_SIZE (sizeof(MAGIC)-1)
 #define EXTENSION ".gxyzf"
 
-typedef struct {
-    gint xres;
-    gint yres;
-    gdouble xoff;
-    gdouble yoff;
-    gdouble xreal;
-    gdouble yreal;
-} GridInfo;
-
 static gboolean      module_register    (void);
 static gint          gxyzf_detect       (const GwyFileDetectInfo *fileinfo,
                                          gboolean only_name);
@@ -80,24 +71,13 @@ static gboolean      gxyzf_export       (GwyContainer *container,
                                          const gchar *filename,
                                          GwyRunType mode,
                                          GError **error);
-static void          estimate_dimensions(GHashTable *hash,
-                                         gdouble *points,
-                                         guint pointlen,
-                                         guint npoints,
-                                         GridInfo *ginfo);
-static guint         estimate_res       (const gdouble *points,
-                                         guint pointlen,
-                                         guint npoints,
-                                         gdouble *off,
-                                         gdouble *real,
-                                         gdouble *dispersion);
 
 static GwyModuleInfo module_info = {
     GWY_MODULE_ABI_VERSION,
     &module_register,
     N_("Imports Gwyddion XYZ field files."),
     "Yeti <yeti@gwyddion.net>",
-    "1.5",
+    "2.0",
     "David Nečas (Yeti)",
     "2013",
 };
@@ -161,7 +141,6 @@ gxyzf_load(const gchar *filename,
     GwySIUnit **zunits = NULL;
     GwySIUnit *xyunit = NULL, *zunit = NULL;
     guint nchan = 0, pointlen, pointsize, npoints, i, id;
-    GridInfo ginfo;
 
     if (!g_file_get_contents(filename, (gchar**)&buffer, &size, &err)) {
         err_GET_FILE_CONTENTS(error, &err);
@@ -233,43 +212,36 @@ gxyzf_load(const gchar *filename,
         append_double(&xyzpoints[i].y, points[i*pointlen + 1]);
     }
 
-    estimate_dimensions(hash, points, pointlen, npoints, &ginfo);
-
     container = gwy_container_new();
     for (id = 0; id < nchan; id++) {
-        GwyDataField *dfield;
+        GwySurface *surface;
         GwySIUnit *unit;
-        gchar buf[32];
+        GQuark quark;
+        gchar buf[24];
 
         for (i = 0; i < npoints; i++)
             append_double(&xyzpoints[i].z, points[i*pointlen + 2+id]);
 
-        dfield = gwy_data_field_new(ginfo.xres, ginfo.yres,
-                                    ginfo.xreal, ginfo.yreal,
-                                    FALSE);
-        gwy_data_field_set_xoffset(dfield, ginfo.xoff);
-        gwy_data_field_set_yoffset(dfield, ginfo.yoff);
-
-        gwy_data_field_average_xyz(dfield, NULL, xyzpoints, npoints);
-        unit = gwy_data_field_get_si_unit_z(dfield);
+        surface = gwy_surface_new_from_data(xyzpoints, npoints);
+        unit = gwy_surface_get_si_unit_z(surface);
         if (zunit)
             gwy_serializable_clone(G_OBJECT(zunit), G_OBJECT(unit));
         else
             gwy_serializable_clone(G_OBJECT(zunits[id]), G_OBJECT(unit));
-        unit = gwy_data_field_get_si_unit_xy(dfield);
+        unit = gwy_surface_get_si_unit_xy(surface);
         gwy_serializable_clone(G_OBJECT(xyunit), G_OBJECT(unit));
 
-        gwy_container_set_object(container, gwy_app_get_data_key_for_id(id),
-                                 dfield);
-        g_object_unref(dfield);
+        quark = gwy_app_get_surface_key_for_id(id);
+        gwy_container_set_object(container, quark, surface);
+        g_object_unref(surface);
 
         g_snprintf(buf, sizeof(buf), "Title%u", id+1);
         if ((value = g_hash_table_lookup(hash, buf))) {
-            g_snprintf(buf, sizeof(buf), "/%d/data/title", id);
-            gwy_container_set_string_by_name(container, buf, g_strdup(value));
+            quark = gwy_app_get_surface_title_key_for_id(id);
+            gwy_container_set_const_string(container, quark, value);
         }
-        gwy_app_channel_check_nonsquare(container, id);
-        gwy_file_channel_import_log_add(container, id, NULL, filename);
+        // TODO
+        // gwy_file_xyz_import_log_add(container, id, NULL, filename);
     }
 
 fail:
@@ -288,8 +260,13 @@ fail:
     return container;
 }
 
-/* NB: We only export the current channel even though the format can, in
- * principle, represent multiple channels at the same coordinates. */
+/* FIXME FIXME FIXME:
+ * Once we have native XYZ data this is incorrect.  We can export either the
+ * current image or the current XYZ data – and need to know which one.
+ *
+ * Furthermore, we only export the current channel even though the format can,
+ * in principle, represent multiple channels at the same coordinates.
+ * We probably have to add an export dialogue here. */
 static gboolean
 gxyzf_export(GwyContainer *container,
              const gchar *filename,
@@ -396,112 +373,6 @@ fail:
     g_free(ddbl);
 
     return FALSE;
-}
-
-static void
-estimate_dimensions(GHashTable *hash,
-                    gdouble *points, guint pointlen, guint npoints,
-                    GridInfo *ginfo)
-{
-    guint xres, yres, estxres, estyres;
-    gdouble shx, shy;
-    const gchar *value;
-
-    /* Take resolution hints that NMM stores in the files, if possible. */
-    xres = yres = 0;
-    if ((value = g_hash_table_lookup(hash, "XRes"))) {
-        xres = atoi(value);
-        if (err_DIMENSION(NULL, xres))
-            xres = 0;
-    }
-    if ((value = g_hash_table_lookup(hash, "YRes"))) {
-        yres = atoi(value);
-        if (err_DIMENSION(NULL, yres))
-            yres = 0;
-    }
-    gwy_debug("hint xres %u, yres %u", xres, yres);
-
-    estxres = estimate_res(points, pointlen, npoints,
-                           &ginfo->xoff, &ginfo->xreal, &shx);
-    estyres = estimate_res(points+1, pointlen, npoints,
-                           &ginfo->yoff, &ginfo->yreal, &shy);
-    gwy_debug("estimated xres %u (width=%g), yres %u (width=%g)",
-              estxres, shx, estyres, shy);
-
-    if (!xres || !yres) {
-        if (shx < shy) {
-            xres = estxres;
-            yres = npoints/estxres;
-        }
-        else {
-            xres = npoints/estyres;
-            yres = estyres;
-        }
-    }
-
-    ginfo->xres = xres;
-    ginfo->yres = yres;
-    gwy_debug("final xres %u, yres %u", ginfo->xres, ginfo->yres);
-    gwy_debug("xreal %g, xoff %g", ginfo->xreal, ginfo->xoff);
-    gwy_debug("yreal %g, yoff %g", ginfo->yreal, ginfo->yoff);
-}
-
-static guint
-estimate_res(const gdouble *points, guint pointlen, guint npoints,
-             gdouble *off, gdouble *real, gdouble *dispersion)
-{
-    guint n = (guint)(sqrt(5000.0*npoints) + 1), i, res, prev;
-    gdouble min = G_MAXDOUBLE, max = G_MINDOUBLE, d, s2;
-    const gdouble *p = points;
-    guint *counts;
-
-    for (i = 0; i < npoints; i++) {
-        gdouble v = *p;
-        if (v < min)
-            min = v;
-        if (v > max)
-            max = v;
-        p += pointlen;
-    }
-    d = max - min;
-    min -= 1e-9*d;
-    max += 1e-9*d;
-
-    counts = g_new0(guint, n);
-    p = points;
-    for (i = 0; i < npoints; i++) {
-        guint j = (guint)((*p - min)/(max - min)*n);
-        counts[j]++;
-        p += pointlen;
-    }
-
-    res = 2;
-    prev = 0;
-    s2 = 0.0;
-    for (i = 1; i < n-1; i++) {
-        if (counts[i] > counts[i-1] && counts[i] > counts[i+1]) {
-            s2 += (gdouble)(i - prev)*(i - prev);
-            res++;
-            prev = i;
-        }
-    }
-    g_free(counts);
-
-    s2 += (gdouble)(n - prev)*(n - prev);
-    s2 /= (res - 1);
-    s2 -= (gdouble)n*n/(res - 1.0)/(res - 1.0);
-    s2 = sqrt(MAX(s2, 0.0));
-
-    gwy_debug("%u %g %u", res, s2, n);
-    *dispersion = s2;
-
-    *off = min;
-    *real = max - min;
-    d = *real/res;
-    *off -= 0.4999*d;
-    *real += 0.4999*d;
-
-    return res;
 }
 
 /* vim: set cin et ts=4 sw=4 cino=>1s,e0,n0,f0,{0,}0,^0,\:1s,=0,g1s,h0,t0,+1s,c3,(0,u0 : */
