de159b200e268f12173f73d790a9156d3f6bb73d
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2016-04-22 14:31:46 +0000

    - fixed brick units not being set at all - added sensible units handling and the possibility to set units arbitrarily - fixed adjustment ranges to disallow zero dimensions - removed xres, yres and zres settings because the module always resets them   anyway
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@18631 25697fe4-d644-49b8-9d30-be5465f2adf3

--- modules/process/volumize_layers.c
+++ modules/process/volumize_layers.c
@@ -45,11 +45,11 @@ typedef struct {
     gint yres;
     gint zres;
     gdouble zreal;
-    /*interface only*/
-    GwySIValueFormat *valform;
-}   VolumizeLayersArgs;
+    gchar *zunit;
+} VolumizeLayersArgs;
 
 typedef struct {
+    VolumizeLayersArgs *args;
     GtkObject *xres;
     GtkObject *yres;
     GtkObject *zres;
@@ -62,13 +62,14 @@ static void     volumize_layers              (GwyContainer *data,
 static gboolean volumize_layers_dialog       (VolumizeLayersArgs *args,
                                               gint xres,
                                               gint yres,
-                                              gint zres,
-                                              gdouble zreal);
+                                              gint zres);
+static void     change_zunits                (VolumizeLayersControls *controls);
+static void     update_unit_label            (VolumizeLayersControls *controls);
+static void     volumize_layers_dialog_update(VolumizeLayersControls *controls,
+                                              VolumizeLayersArgs *args);
 static void     volumize_layers_load_args    (GwyContainer *container,
                                               VolumizeLayersArgs *args);
 static void     volumize_layers_save_args    (GwyContainer *container,
-                                              VolumizeLayersArgs *args);
-static void     volumize_layers_dialog_update(VolumizeLayersControls *controls,
                                               VolumizeLayersArgs *args);
 
 static const VolumizeLayersArgs volumize_layers_defaults = {
@@ -84,7 +85,7 @@ static GwyModuleInfo module_info = {
     &module_register,
     N_("Converts all datafields to 3D volume data."),
     "Petr Klapetek <klapetek@gwyddion.net>",
-    "1.2",
+    "2.0",
     "David Nečas (Yeti) & Petr Klapetek",
     "2013",
 };
@@ -112,8 +113,9 @@ volumize_layers(GwyContainer *data, GwyRunType run)
     GwyDataField *dfield = NULL;
     GtkWidget *dialog;
     GwyBrick *brick;
+    GwySIUnit *siunit;
     gboolean ok = TRUE;
-    gint *ids, col, row, i, nids, xres, yres, newid;
+    gint *ids, col, row, i, nids, xres, yres, newid, power10;
     gdouble *ddata, *bdata;
 
     g_return_if_fail(run & VOLUMIZE_LAYERS_RUN_MODES);
@@ -128,11 +130,6 @@ volumize_layers(GwyContainer *data, GwyRunType run)
     xres = gwy_data_field_get_xres(dfield);
     yres = gwy_data_field_get_yres(dfield);
 
-    args.valform
-        = gwy_data_field_get_value_format_xy(dfield,
-                                             GWY_SI_UNIT_FORMAT_VFMARKUP, NULL);
-
-
     i = 0;
     while (ids[i] != -1) {
         dfield = gwy_container_get_object(data,
@@ -146,9 +143,6 @@ volumize_layers(GwyContainer *data, GwyRunType run)
         i++;
         nids++;
     }
-    args.xres = xres;
-    args.yres = yres;
-    args.zres = nids-1;
 
     if (!ok) {
         dialog = gtk_message_dialog_new(gwy_app_find_window_for_channel(data,
@@ -161,21 +155,25 @@ volumize_layers(GwyContainer *data, GwyRunType run)
                                            "them."));
         gtk_dialog_run(GTK_DIALOG(dialog));
         gtk_widget_destroy(dialog);
-        g_free(ids);
-        return;
+        goto finish;
     }
 
+    args.xres = xres;
+    args.yres = yres;
+    args.zres = nids-1;
     if (run == GWY_RUN_INTERACTIVE) {
-        ok = volumize_layers_dialog(&args, xres, yres, nids-1,
-                                    gwy_data_field_get_xreal(dfield));
+        ok = volumize_layers_dialog(&args, xres, yres, nids-1);
         volumize_layers_save_args(gwy_app_settings_get(), &args);
     }
-    gwy_si_unit_value_format_free(args.valform);
     if (!ok)
-        return;
+        goto finish;
 
-    brick = gwy_brick_new(xres, yres, nids-1, gwy_data_field_get_xreal(dfield),
-                          gwy_data_field_get_yreal(dfield), args.zreal, FALSE);
+    siunit = gwy_si_unit_new_parse(args.zunit, &power10);
+    brick = gwy_brick_new(xres, yres, nids-1,
+                          gwy_data_field_get_xreal(dfield),
+                          gwy_data_field_get_yreal(dfield),
+                          args.zreal * pow10(power10),
+                          FALSE);
     bdata = gwy_brick_get_data(brick);
     for (i = 0; i < nids-1; i++) {
         dfield = gwy_container_get_object(data,
@@ -190,18 +188,34 @@ volumize_layers(GwyContainer *data, GwyRunType run)
 
     gwy_brick_resample(brick, args.xres, args.yres, args.zres,
                        GWY_INTERPOLATION_ROUND);
+    gwy_serializable_clone(G_OBJECT(gwy_data_field_get_si_unit_xy(dfield)),
+                           G_OBJECT(gwy_brick_get_si_unit_x(brick)));
+    gwy_serializable_clone(G_OBJECT(gwy_data_field_get_si_unit_xy(dfield)),
+                           G_OBJECT(gwy_brick_get_si_unit_y(brick)));
+    gwy_serializable_clone(G_OBJECT(gwy_data_field_get_si_unit_z(dfield)),
+                           G_OBJECT(gwy_brick_get_si_unit_w(brick)));
+    gwy_brick_set_si_unit_z(brick, siunit);
+    g_object_unref(siunit);
+
     newid = gwy_app_data_browser_add_brick(brick, dfield, data, TRUE);
     g_object_unref(brick);
     gwy_app_volume_log_add(data, -1, newid, "proc::volumize_layers", NULL);
+
+finish:
+    g_free(ids);
+    g_free(args.zunit);
 }
 
 static gboolean
 volumize_layers_dialog(VolumizeLayersArgs *args,
-                       gint xres, gint yres, gint zres, gdouble zreal)
+                       gint xres, gint yres, gint zres)
 {
-    GtkWidget *dialog, *table;
+    GtkWidget *dialog, *table, *zunitbutton;
     VolumizeLayersControls controls;
     gint response, row = 0;
+
+    gwy_clear(&controls, 1);
+    controls.args = args;
 
     dialog = gtk_dialog_new_with_buttons(_("Volumize layers"), NULL, 0,
                                          _("_Reset"), RESPONSE_RESET,
@@ -211,7 +225,7 @@ volumize_layers_dialog(VolumizeLayersArgs *args,
     gtk_dialog_set_default_response(GTK_DIALOG(dialog), GTK_RESPONSE_OK);
     gwy_help_add_to_proc_dialog(GTK_DIALOG(dialog), GWY_HELP_DEFAULT);
 
-    table = gtk_table_new(4, 4, FALSE);
+    table = gtk_table_new(4, 5, FALSE);
     gtk_table_set_row_spacings(GTK_TABLE(table), 2);
     gtk_table_set_col_spacings(GTK_TABLE(table), 6);
     gtk_container_set_border_width(GTK_CONTAINER(table), 4);
@@ -221,36 +235,38 @@ volumize_layers_dialog(VolumizeLayersArgs *args,
     args->xres = xres;
     args->yres = yres;
     args->zres = zres;
-    args->zreal = zreal;
 
-    controls.xres = gtk_adjustment_new(args->xres, 0.0, 1000.0, 1, 10, 0);
+    controls.xres = gtk_adjustment_new(args->xres, 1.0, 1000.0, 1, 10, 0);
     gwy_table_attach_hscale(table, row, _("_X resolution:"), "pixels",
-                            controls.xres, GWY_HSCALE_DEFAULT);
-    g_object_set_data(G_OBJECT(controls.xres), "controls", &controls);
+                            controls.xres, GWY_HSCALE_SQRT);
     row++;
 
-    controls.yres = gtk_adjustment_new(args->yres, 0.0, 1000.0, 1, 10, 0);
+    controls.yres = gtk_adjustment_new(args->yres, 1.0, 1000.0, 1, 10, 0);
     gwy_table_attach_hscale(table, row, _("_Y resolution:"), "pixels",
-                            controls.yres, GWY_HSCALE_DEFAULT);
-    g_object_set_data(G_OBJECT(controls.yres), "controls", &controls);
+                            controls.yres, GWY_HSCALE_SQRT);
     row++;
 
-    controls.zres = gtk_adjustment_new(args->zres, 0.0, 1000.0, 1, 10, 0);
+    controls.zres = gtk_adjustment_new(args->zres, 1.0, 1000.0, 1, 10, 0);
     gwy_table_attach_hscale(table, row, _("_Z resolution:"), "pixels",
-                            controls.zres, GWY_HSCALE_DEFAULT);
-    g_object_set_data(G_OBJECT(controls.zres), "controls", &controls);
+                            controls.zres, GWY_HSCALE_SQRT);
     row++;
 
+    controls.zreal = gtk_adjustment_new(args->zreal,
+                                        0.01, 10000.0, 0.01, 1.0, 0);
+    gwy_table_attach_hscale(table, row, _("Z _range:"), args->zunit,
+                            controls.zreal, GWY_HSCALE_SQRT);
 
-
-    controls.zreal = gtk_adjustment_new(args->zreal/args->valform->magnitude,
-                                        0.0, 10000.0, 1, 10, 0);
-    gwy_table_attach_hscale(table, row, _("Z _range:"), args->valform->units,
-                            controls.zreal, GWY_HSCALE_DEFAULT);
-    g_object_set_data(G_OBJECT(controls.zreal), "controls", &controls);
+    zunitbutton = gtk_button_new_with_label(gwy_sgettext("verb|Change"));
+    gtk_table_attach(GTK_TABLE(table), zunitbutton,
+                     4, 5, row, row+2,
+                     GTK_EXPAND | GTK_FILL | GTK_SHRINK, 0, 0, 0);
+    g_signal_connect_swapped(zunitbutton, "clicked",
+                             G_CALLBACK(change_zunits), &controls);
     row++;
 
+    update_unit_label(&controls);
     gtk_widget_show_all(dialog);
+
     do {
         response = gtk_dialog_run(GTK_DIALOG(dialog));
         switch (response) {
@@ -268,7 +284,9 @@ volumize_layers_dialog(VolumizeLayersArgs *args,
             args->xres = xres;  /* go back to initially detected resolution */
             args->yres = yres;
             args->zres = zres;
-            args->zreal = zreal;
+            args->zreal = volumize_layers_defaults.zreal;
+            g_free(args->zunit);
+            args->zunit = g_strdup("");
             volumize_layers_dialog_update(&controls, args);
             break;
 
@@ -281,7 +299,7 @@ volumize_layers_dialog(VolumizeLayersArgs *args,
     args->xres = gtk_adjustment_get_value(GTK_ADJUSTMENT(controls.xres));
     args->yres = gtk_adjustment_get_value(GTK_ADJUSTMENT(controls.yres));
     args->zres = gtk_adjustment_get_value(GTK_ADJUSTMENT(controls.zres));
-    args->zreal = gtk_adjustment_get_value(GTK_ADJUSTMENT(controls.zreal))*args->valform->magnitude;
+    args->zreal = gtk_adjustment_get_value(GTK_ADJUSTMENT(controls.zreal));
 
     gtk_widget_destroy(dialog);
 
@@ -289,54 +307,99 @@ volumize_layers_dialog(VolumizeLayersArgs *args,
 }
 
 static void
-volumize_layers_dialog_update(VolumizeLayersControls *controls,
-                  VolumizeLayersArgs *args)
+change_zunits(VolumizeLayersControls *controls)
 {
-    gtk_adjustment_set_value(GTK_ADJUSTMENT(controls->xres),
-                             args->xres);
-    gtk_adjustment_set_value(GTK_ADJUSTMENT(controls->yres),
-                             args->yres);
-    gtk_adjustment_set_value(GTK_ADJUSTMENT(controls->zres),
-                             args->zres);
-    gtk_adjustment_set_value(GTK_ADJUSTMENT(controls->zreal),
-                             args->zreal);
-}
+    GtkWidget *dialog, *hbox, *label, *entry;
+    gint response;
 
-static const gchar xres_key[]  = "/module/volumize_layers/xres";
-static const gchar yres_key[]  = "/module/volumize_layers/yres";
-static const gchar zres_key[]  = "/module/volumize_layers/zres";
-static const gchar zreal_key[] = "/module/volumize_layers/zreal";
+    dialog = gtk_dialog_new_with_buttons(_("Change Units"),
+                                         NULL,
+                                         GTK_DIALOG_MODAL
+                                         | GTK_DIALOG_NO_SEPARATOR,
+                                         GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
+                                         GTK_STOCK_OK, GTK_RESPONSE_OK,
+                                         NULL);
+    gtk_dialog_set_default_response(GTK_DIALOG(dialog), GTK_RESPONSE_OK);
+
+    hbox = gtk_hbox_new(FALSE, 6);
+    gtk_container_set_border_width(GTK_CONTAINER(hbox), 4);
+    gtk_box_pack_start(GTK_BOX(GTK_DIALOG(dialog)->vbox), hbox,
+                       FALSE, FALSE, 0);
+
+    label = gtk_label_new_with_mnemonic(_("New _units:"));
+    gtk_box_pack_start(GTK_BOX(hbox), label, TRUE, TRUE, 0);
+
+    entry = gtk_entry_new();
+    gtk_entry_set_activates_default(GTK_ENTRY(entry), TRUE);
+    gtk_box_pack_start(GTK_BOX(hbox), entry, TRUE, TRUE, 0);
+
+    gtk_widget_show_all(dialog);
+    response = gtk_dialog_run(GTK_DIALOG(dialog));
+    if (response != GTK_RESPONSE_OK) {
+        gtk_widget_destroy(dialog);
+        return;
+    }
+
+    g_free(controls->args->zunit);
+    controls->args->zunit = g_strdup(gtk_entry_get_text(GTK_ENTRY(entry)));
+    gtk_widget_destroy(dialog);
+
+    update_unit_label(controls);
+}
 
 static void
-volumize_layers_sanitize_args(VolumizeLayersArgs *args)
+update_unit_label(VolumizeLayersControls *controls)
 {
-    args->xres = CLAMP(args->xres, 0, 10000);
-    args->yres = CLAMP(args->yres, 0, 10000);
-    args->zres = CLAMP(args->zres, 0, 10000);
+    GtkWidget *label;
+    GwySIUnit *siunit;
+    gint power10;
+    GwySIValueFormat *vf;
+
+    siunit = gwy_si_unit_new_parse(controls->args->zunit, &power10);
+    vf = gwy_si_unit_get_format_for_power10(siunit, GWY_SI_UNIT_FORMAT_VFMARKUP,
+                                            power10, NULL);
+    label = gwy_table_hscale_get_units(controls->zreal);
+    gtk_label_set_markup(GTK_LABEL(label), vf->units);
+    gwy_si_unit_value_format_free(vf);
+    g_object_unref(siunit);
 }
+
+static void
+volumize_layers_dialog_update(VolumizeLayersControls *controls,
+                              VolumizeLayersArgs *args)
+{
+    gtk_adjustment_set_value(GTK_ADJUSTMENT(controls->xres), args->xres);
+    gtk_adjustment_set_value(GTK_ADJUSTMENT(controls->yres), args->yres);
+    gtk_adjustment_set_value(GTK_ADJUSTMENT(controls->zres), args->zres);
+    gtk_adjustment_set_value(GTK_ADJUSTMENT(controls->zreal), args->zreal);
+    update_unit_label(controls);
+}
+
+static const gchar zreal_key[] = "/module/volumize_layers/zreal";
+static const gchar zunit_key[] = "/module/volumize_layers/zunit";
 
 static void
 volumize_layers_load_args(GwyContainer *container,
-                     VolumizeLayersArgs *args)
+                          VolumizeLayersArgs *args)
 {
+    const guchar *s;
+
     *args = volumize_layers_defaults;
 
-    gwy_container_gis_int32_by_name(container, xres_key, &args->xres);
-    gwy_container_gis_int32_by_name(container, yres_key, &args->yres);
-    gwy_container_gis_int32_by_name(container, zres_key, &args->zres);
+    args->zunit = g_strdup("");
     gwy_container_gis_double_by_name(container, zreal_key, &args->zreal);
-    volumize_layers_sanitize_args(args);
+    if (gwy_container_gis_string_by_name(container, zunit_key, &s)) {
+        g_free(args->zunit);
+        args->zunit = g_strdup(s);
+    }
 }
 
 static void
 volumize_layers_save_args(GwyContainer *container,
-                     VolumizeLayersArgs *args)
+                          VolumizeLayersArgs *args)
 {
-    gwy_container_set_int32_by_name(container, xres_key, args->xres);
-    gwy_container_set_int32_by_name(container, yres_key, args->yres);
-    gwy_container_set_int32_by_name(container, zres_key, args->zres);
     gwy_container_set_double_by_name(container, zreal_key, args->zreal);
-
+    gwy_container_set_const_string_by_name(container, zunit_key, args->zunit);
 }
 
 /* vim: set cin et ts=4 sw=4 cino=>1s,e0,n0,f0,{0,}0,^0,\:1s,=0,g1s,h0,t0,+1s,c3,(0,u0 : */
