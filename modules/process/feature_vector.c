96fa7c3e3dfd6bf95583ef402fdcea384b1e5dee
dn2010 <dn2010@25697fe4-d644-49b8-9d30-be5465f2adf3>
2016-05-03 16:50:42 +0000

    - Transform a datafield into a Brick of feature vectors (currently with different convolution operators)
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@18677 25697fe4-d644-49b8-9d30-be5465f2adf3

---
+++ modules/process/feature_vector.c
@@ -0,0 +1,207 @@
+/*
+ *  @(#) $Id$
+ *  Copyright (C) 2016 David Necas (Yeti), Petr Klapetek,
+ *  Daniil Bratashov.
+ *  E-mail: yeti@gwyddion.net, klapetek@gwyddion.net,
+ *  dn2010@gwyddion.net
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ *  Boston, MA 02110-1301, USA.
+ */
+
+#include "config.h"
+#include <gtk/gtk.h>
+#include <libgwyddion/gwymacros.h>
+#include <libgwyddion/gwymath.h>
+#include <libprocess/stats.h>
+#include <libprocess/filters.h>
+#include <libgwydgets/gwystock.h>
+#include <libgwydgets/gwydgetutils.h>
+#include <libgwymodule/gwymodule-process.h>
+#include <app/gwyapp.h>
+#include <string.h>
+
+#define FEATURE_VECTOR_RUN_MODES (GWY_RUN_IMMEDIATE | GWY_RUN_INTERACTIVE)
+
+#define FWHM2SIGMA (1.0/(2.0*sqrt(2*G_LN2)))
+
+static gboolean module_register              (void);
+static void     create_feature_vector        (GwyContainer *data,
+                                              GwyRunType run);
+
+static GwyModuleInfo module_info = {
+    GWY_MODULE_ABI_VERSION,
+    &module_register,
+    N_("Converts datafield to feature vector volume data."),
+    "Daniil Bratashov <dn2010@gwyddion.net>",
+    "0.1",
+    "David Nečas (Yeti) & Petr Klapetek & Daniil Bratashov",
+    "2016",
+};
+
+GWY_MODULE_QUERY(module_info)
+
+static gboolean
+module_register(void)
+{
+    gwy_process_func_register("feature_vector",
+                              (GwyProcessFunc)&create_feature_vector,
+                              N_("/_Statistics/Feature _Vector..."),
+                              GWY_STOCK_VOLUMIZE_LAYERS,
+                              FEATURE_VECTOR_RUN_MODES,
+                              GWY_MENU_FLAG_DATA,
+                              N_("Create feature vector from datafield"));
+
+    return TRUE;
+}
+
+static void
+create_feature_vector(GwyContainer *data, GwyRunType run)
+{
+    GwyDataField *dfield = NULL, *feature0 = NULL, *feature = NULL;
+    GwyBrick *features = NULL;
+    gdouble max, min, avg, xreal, yreal, size;
+    gdouble *fdata, *bdata;
+    gint id, newid, xres, yres, z, zres, i, ngauss;
+
+    g_return_if_fail(run & FEATURE_VECTOR_RUN_MODES);
+    gwy_app_data_browser_get_current(GWY_APP_DATA_FIELD, &dfield,
+                                     GWY_APP_DATA_FIELD_ID, &id,
+                                     0);
+    feature0 = gwy_data_field_duplicate(dfield);
+    xres = gwy_data_field_get_xres(feature0);
+    yres = gwy_data_field_get_yres(feature0);
+    xreal = gwy_data_field_get_xreal(feature0);
+    yreal = gwy_data_field_get_yreal(feature0);
+    ngauss = 5;
+    zres = 1 + 4 * ngauss;
+    z = 0;
+    max = gwy_data_field_get_max(feature0);
+    min = gwy_data_field_get_min(feature0);
+    g_return_if_fail(max - min > 0.0);
+    gwy_data_field_multiply(feature0, 1.0/(max - min));
+    avg = gwy_data_field_get_avg(feature0);
+    gwy_data_field_add(feature0, -avg);
+
+    features = gwy_brick_new(xres, yres, zres,
+                             xreal, yreal, zres, TRUE);
+    bdata = gwy_brick_get_data(features);
+    fdata = gwy_data_field_get_data(feature0);
+    memmove(bdata, fdata, xres * yres * sizeof(gdouble));
+    z++;
+
+    for (i = 0, size = 1.0; i < ngauss; i++, size *= 2.0) {
+        feature = gwy_data_field_duplicate(feature0);
+        gwy_data_field_filter_gaussian(feature, size * FWHM2SIGMA);
+        max = gwy_data_field_get_max(feature);
+        min = gwy_data_field_get_min(feature);
+        g_return_if_fail(max - min > 0.0);
+        gwy_data_field_multiply(feature, 1.0/(max - min));
+        avg = gwy_data_field_get_avg(feature);
+        gwy_data_field_add(feature, -avg);
+        fdata = gwy_data_field_get_data(feature);
+        memmove(bdata + z * xres * yres, fdata,
+                xres * yres * sizeof(gdouble));
+        z++;
+        gwy_data_field_filter_laplacian(feature);
+        max = gwy_data_field_get_max(feature);
+        min = gwy_data_field_get_min(feature);
+        g_return_if_fail(max - min > 0.0);
+        gwy_data_field_multiply(feature, 1.0/(max - min));
+        avg = gwy_data_field_get_avg(feature);
+        gwy_data_field_add(feature, -avg);
+        memmove(bdata + z * xres * yres, fdata,
+                xres * yres * sizeof(gdouble));
+        memmove(fdata, bdata + (z-1) * xres * yres,
+                xres * yres * sizeof(gdouble));
+        z++;
+        gwy_data_field_filter_sobel(feature, GWY_ORIENTATION_HORIZONTAL);
+        max = gwy_data_field_get_max(feature);
+        min = gwy_data_field_get_min(feature);
+        g_return_if_fail(max - min > 0.0);
+        gwy_data_field_multiply(feature, 1.0/(max - min));
+        avg = gwy_data_field_get_avg(feature);
+        gwy_data_field_add(feature, -avg);
+        memmove(bdata + z * xres * yres, fdata,
+                xres * yres * sizeof(gdouble));
+        memmove(fdata, bdata + (z-2) * xres * yres,
+                xres * yres * sizeof(gdouble));
+        z++;
+        gwy_data_field_filter_sobel(feature, GWY_ORIENTATION_VERTICAL);
+        max = gwy_data_field_get_max(feature);
+        min = gwy_data_field_get_min(feature);
+        g_return_if_fail(max - min > 0.0);
+        gwy_data_field_multiply(feature, 1.0/(max - min));
+        avg = gwy_data_field_get_avg(feature);
+        gwy_data_field_add(feature, -avg);
+        memmove(bdata + z * xres * yres, fdata,
+                xres * yres * sizeof(gdouble));
+        gwy_data_field_filter_sobel(feature, GWY_ORIENTATION_HORIZONTAL);
+        max = gwy_data_field_get_max(feature);
+        min = gwy_data_field_get_min(feature);
+        g_return_if_fail(max - min > 0.0);
+        gwy_data_field_multiply(feature, 1.0/(max - min));
+        avg = gwy_data_field_get_avg(feature);
+        gwy_data_field_add(feature, -avg);
+        memmove(bdata + z * xres * yres, fdata,
+                xres * yres * sizeof(gdouble));
+        memmove(fdata, bdata + (z-3) * xres * yres,
+                xres * yres * sizeof(gdouble));
+        z++;
+        g_object_unref(feature);
+    }
+
+    newid = gwy_app_data_browser_add_brick(features, feature0, data, TRUE);
+    g_object_unref(features);
+    g_object_unref(feature0);
+    gwy_app_volume_log_add(data, -1, newid, "proc::feature_vector", NULL);
+}
+
+static inline gdouble
+sigmoid(gdouble z)
+{
+    return 1.0/(1.0 + exp(-z));
+}
+
+static gdouble
+cost_function(GwyBrick *brick, GwyDataField *mask,
+              gdouble *thetas, gdouble *grad, gdouble lambda)
+{
+    gint i, j, k, m, xres, yres, zres;
+    GwyDataField *hfield;
+    gdouble sum;
+    gdouble *bp, *mp, *hp;
+
+    xres = gwy_brick_get_xres(brick);
+    yres = gwy_brick_get_yres(brick);
+    zres = gwy_brick_get_zres(brick);
+    bp = gwy_brick_get_data(brick);
+    m = xres * yres;
+
+    hfield = gwy_data_field_new_alike(mask, TRUE);
+    hp = gwy_data_field_get_data(hfield);
+
+    for (i = 0; i < yres; i++)
+        for (j = 0; j < xres; j++) {
+            sum = 0;
+            for (k = 0; k < zres; k++) {
+                sum += *(bp + k *(xres * yres) + i * xres + j)
+                                                            * thetas[k];
+            }
+            *(hp + i * xres + j) = sigmoid(sum);
+        }
+
+    return 0.0;
+}
