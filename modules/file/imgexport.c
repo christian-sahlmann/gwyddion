1e4239f60a257d5984122ee2e6d30d6300a30299
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2016-07-30 07:20:40 +0000

    - increased kilo threshold range
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@18787 25697fe4-d644-49b8-9d30-be5465f2adf3

--- modules/file/imgexport.c
+++ modules/file/imgexport.c
@@ -3745,7 +3745,7 @@ create_value_controls(ImgExportControls *controls)
     row++;
 
     controls->kilo_threshold = gtk_adjustment_new(args->kilo_threshold,
-                                                  10.0, 10000.0, 1.0, 100.0, 0);
+                                                  5.0, 50000.0, 1.0, 100.0, 0);
     gwy_table_attach_hscale(table, row, _("Fixed _kilo threshold:"), NULL,
                             controls->kilo_threshold,
                             GWY_HSCALE_CHECK | GWY_HSCALE_LOG);
