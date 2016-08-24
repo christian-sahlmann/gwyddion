f273c56bd3bc3bb4dfca172cf7551989d14c3d86
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2016-08-24 17:23:30 +0000

    - added merged flag support to graph ascii export - use gwy_save_auxiliary_data() instead of some home-baked code
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@18879 25697fe4-d644-49b8-9d30-be5465f2adf3

--- modules/graph/graph_export_ascii.c
+++ modules/graph/graph_export_ascii.c
@@ -1,6 +1,6 @@
 /*
  *  @(#) $Id$
- *  Copyright (C) 2006,2014 David Necas (Yeti), Petr Klapetek.
+ *  Copyright (C) 2006,2014-2016 David Necas (Yeti), Petr Klapetek.
  *  E-mail: yeti@gwyddion.net, klapetek@gwyddion.net.
  *
  *  This program is free software; you can redistribute it and/or modify
@@ -27,27 +27,29 @@
 #include <libgwydgets/gwystock.h>
 #include <libgwymodule/gwymodule-graph.h>
 #include <app/gwyapp.h>
+#include <app/gwymoduleutils.h>
 
 typedef struct {
     gboolean units;
     gboolean labels;
     gboolean metadata;
     gboolean posix;
+    gboolean merged_x;
     GwyGraphModelExportStyle style;
 } ExportParameters;
 
-static gboolean module_register          (void);
-static void     export                   (GwyGraph *graph);
-static void     export_dialog            (GwyGraph *graph,
-                                          ExportParameters *params);
-static void     export_dialog_choose_file(GwyGraph *graph,
-                                          ExportParameters *params);
-static void     boolean_changed_cb       (GtkToggleButton *check,
-                                          gboolean *value);
-static void     load_args                (GwyContainer *settings,
-                                          ExportParameters *params);
-static void     save_args                (GwyContainer *settings,
-                                          ExportParameters *params);
+static gboolean module_register   (void);
+static void     export            (GwyGraph *graph);
+static void     export_dialog     (GwyGraph *graph,
+                                   ExportParameters *params);
+static void     export_dialog_save(GwyGraph *graph,
+                                   ExportParameters *params);
+static void     boolean_changed   (GtkToggleButton *check,
+                                   gboolean *value);
+static void     load_args         (GwyContainer *settings,
+                                   ExportParameters *params);
+static void     save_args         (GwyContainer *settings,
+                                   ExportParameters *params);
 
 static const GwyEnum style_type[] = {
    { N_("Plain text"),             GWY_GRAPH_MODEL_EXPORT_ASCII_PLAIN,   },
@@ -58,10 +60,8 @@ static const GwyEnum style_type[] = {
 };
 
 static const ExportParameters load_defaults = {
-    TRUE,
-    TRUE,
-    TRUE,
-    FALSE,
+    TRUE, TRUE, TRUE,
+    FALSE, FALSE,
     GWY_GRAPH_MODEL_EXPORT_ASCII_PLAIN,
 };
 
@@ -70,7 +70,7 @@ static GwyModuleInfo module_info = {
     &module_register,
     N_("Exports graph data to text files."),
     "Petr Klapetek <klapetek@gwyddion.net>",
-    "1.7",
+    "2.0",
     "David Nečas (Yeti) & Petr Klapetek",
     "2006",
 };
@@ -93,10 +93,9 @@ module_register(void)
 static void
 export(GwyGraph *graph)
 {
+    GwyContainer *settings = gwy_app_settings_get();
     ExportParameters params;
-    GwyContainer *settings;
 
-    settings = gwy_app_settings_get();
     load_args(settings, &params);
     export_dialog(graph, &params);
     save_args(settings, &params);
@@ -129,25 +128,31 @@ export_dialog(GwyGraph *graph,
     gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(check), params->posix);
     gtk_box_pack_start(vbox, check, FALSE, FALSE, 0);
     g_signal_connect(check, "toggled",
-                     G_CALLBACK(boolean_changed_cb), &params->posix);
+                     G_CALLBACK(boolean_changed), &params->posix);
+
+    check = gtk_check_button_new_with_mnemonic(_("Single _merged abscissa"));
+    gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(check), params->merged_x);
+    gtk_box_pack_start(vbox, check, FALSE, FALSE, 0);
+    g_signal_connect(check, "toggled",
+                     G_CALLBACK(boolean_changed), &params->merged_x);
 
     check = gtk_check_button_new_with_mnemonic(_("Export _labels"));
     gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(check), params->labels);
     gtk_box_pack_start(vbox, check, FALSE, FALSE, 0);
     g_signal_connect(check, "toggled",
-                     G_CALLBACK(boolean_changed_cb), &params->labels);
+                     G_CALLBACK(boolean_changed), &params->labels);
 
     check = gtk_check_button_new_with_mnemonic(_("Export _units"));
     gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(check), params->units);
     gtk_box_pack_start(vbox, check, FALSE, FALSE, 0);
     g_signal_connect(check, "toggled",
-                     G_CALLBACK(boolean_changed_cb), &params->units);
+                     G_CALLBACK(boolean_changed), &params->units);
 
     check = gtk_check_button_new_with_mnemonic(_("Export _metadata"));
     gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(check), params->metadata);
     gtk_box_pack_start(vbox, check, FALSE, FALSE, 0);
     g_signal_connect(check, "toggled",
-                     G_CALLBACK(boolean_changed_cb), &params->metadata);
+                     G_CALLBACK(boolean_changed), &params->metadata);
 
     gtk_widget_show_all(dialog);
     switch (gtk_dialog_run(GTK_DIALOG(dialog))) {
@@ -159,7 +164,7 @@ export_dialog(GwyGraph *graph,
 
         case GTK_RESPONSE_OK:
         gtk_widget_destroy(dialog);
-        export_dialog_choose_file(graph, params);
+        export_dialog_save(graph, params);
         break;
 
         default:
@@ -167,76 +172,39 @@ export_dialog(GwyGraph *graph,
         break;
     }
 }
+
 static void
-export_dialog_choose_file(GwyGraph *graph,
-                          ExportParameters *params)
+export_dialog_save(GwyGraph *graph, ExportParameters *params)
 {
-    GtkWidget *dialog;
-    gchar *filename;
+    GwyGraphModelExportStyle style;
     GString *str;
-    FILE *fw;
 
-    dialog = gtk_file_chooser_dialog_new("Export to Text File", NULL,
-                                         GTK_FILE_CHOOSER_ACTION_SAVE,
-                                         GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
-                                         GTK_STOCK_SAVE, GTK_RESPONSE_OK,
-                                         NULL);
-    gtk_dialog_set_default_response(GTK_DIALOG(dialog), GTK_RESPONSE_OK);
-    gtk_file_chooser_set_current_folder(GTK_FILE_CHOOSER(dialog),
-                                        gwy_app_get_current_directory());
+    style = params->style;
+    if (params->posix)
+        style |= GWY_GRAPH_MODEL_EXPORT_ASCII_POSIX;
+    if (params->merged_x)
+        style |= GWY_GRAPH_MODEL_EXPORT_ASCII_MERGED;
 
-    switch (gtk_dialog_run(GTK_DIALOG(dialog))) {
-        case GTK_RESPONSE_CANCEL:
-        case GTK_RESPONSE_DELETE_EVENT:
-        gtk_widget_destroy(dialog);
-        case GTK_RESPONSE_NONE:
-        return;
-        break;
-
-        case GTK_RESPONSE_OK:
-        /* pass */
-        break;
-
-        default:
-        g_warning("Unhandled dialog response");
-        return;
-        break;
-    }
-
-    if (gwy_app_file_confirm_overwrite(dialog)) {
-        GwyGraphModelExportStyle style;
-
-        filename = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(dialog));
-        style = params->style | (params->posix
-                                 ? GWY_GRAPH_MODEL_EXPORT_ASCII_POSIX
-                                 : 0);
-        str = gwy_graph_model_export_ascii(graph->graph_model,
-                                           params->units,
-                                           params->labels,
-                                           params->metadata,
-                                           style,
-                                           NULL);
-
-        /* FIXME: Must check success */
-        fw = gwy_fopen(filename, "w");
-        fwrite(str->str, str->len, 1, fw);
-        fclose(fw);
-        g_string_free(str, TRUE);
-        g_free(filename);
-    }
-    gtk_widget_destroy(dialog);
+    str = gwy_graph_model_export_ascii(gwy_graph_get_model(graph),
+                                       params->units,
+                                       params->labels,
+                                       params->metadata,
+                                       style,
+                                       NULL);
+    gwy_save_auxiliary_data(_("Export to Text File"), NULL, str->len, str->str);
+    g_string_free(str, TRUE);
 }
 
 static void
-boolean_changed_cb(GtkToggleButton *check, gboolean *value)
+boolean_changed(GtkToggleButton *check, gboolean *value)
 {
     *value = gtk_toggle_button_get_active(check);
 }
 
-static const gchar style_key[]    = "/module/graph_export_ascii/style";
 static const gchar labels_key[]   = "/module/graph_export_ascii/labels";
-static const gchar units_key[]    = "/module/graph_export_ascii/units";
 static const gchar metadata_key[] = "/module/graph_export_ascii/metadata";
+static const gchar style_key[]    = "/module/graph_export_ascii/style";
+static const gchar units_key[]    = "/module/graph_export_ascii/units";
 
 static void
 load_args(GwyContainer *settings,
@@ -249,7 +217,9 @@ load_args(GwyContainer *settings,
     gwy_container_gis_enum_by_name(settings, style_key, &params->style);
 
     params->posix = params->style & GWY_GRAPH_MODEL_EXPORT_ASCII_POSIX;
-    params->style = params->style & ~GWY_GRAPH_MODEL_EXPORT_ASCII_POSIX;
+    params->merged_x = params->style & GWY_GRAPH_MODEL_EXPORT_ASCII_MERGED;
+    params->style = params->style & ~(GWY_GRAPH_MODEL_EXPORT_ASCII_POSIX
+                                      | GWY_GRAPH_MODEL_EXPORT_ASCII_MERGED);
     params->style = MIN(params->style, GWY_GRAPH_MODEL_EXPORT_ASCII_IGORPRO);
 }
 
@@ -259,9 +229,12 @@ save_args(GwyContainer *settings,
 {
     GwyGraphModelExportStyle style;
 
-    style = params->style | (params->posix
-                             ? GWY_GRAPH_MODEL_EXPORT_ASCII_POSIX
-                             : 0);
+    style = params->style;
+    if (params->posix)
+        style |= GWY_GRAPH_MODEL_EXPORT_ASCII_POSIX;
+    if (params->merged_x)
+        style |= GWY_GRAPH_MODEL_EXPORT_ASCII_MERGED;
+
     gwy_container_set_boolean_by_name(settings, labels_key, params->labels);
     gwy_container_set_boolean_by_name(settings, units_key, params->units);
     gwy_container_set_boolean_by_name(settings, metadata_key, params->metadata);
