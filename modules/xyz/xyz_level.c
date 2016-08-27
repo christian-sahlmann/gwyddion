eda97f32533e920273e8e5c8a93b29264140d510
yeti-dn <yeti-dn@25697fe4-d644-49b8-9d30-be5465f2adf3>
2016-08-27 08:49:04 +0000

    - started a simple XYZ data plane levelling module - plane subtraction implemented - GUI should work
    git-svn-id: svn://svn.code.sf.net/p/gwyddion/code/trunk/gwyddion@18893 25697fe4-d644-49b8-9d30-be5465f2adf3

---
+++ modules/xyz/xyz_level.c
@@ -0,0 +1,328 @@
+/*
+ *  $Id$
+ *  Copyright (C) 2016 David Necas (Yeti).
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ *  Boston, MA 02110-1301, USA.
+ */
+
+#include "config.h"
+#include <string.h>
+#include <gtk/gtk.h>
+#include <libgwyddion/gwymacros.h>
+#include <libgwyddion/gwymath.h>
+#include <libprocess/surface.h>
+#include <libgwydgets/gwydgetutils.h>
+#include <libgwydgets/gwyradiobuttons.h>
+#include <libgwymodule/gwymodule-xyz.h>
+#include <app/gwyapp.h>
+
+#define XYZLEVEL_RUN_MODES (GWY_RUN_INTERACTIVE | GWY_RUN_IMMEDIATE)
+
+typedef enum {
+    XYZ_LEVEL_SUBTRACT = 0,
+    XYZ_LEVEL_ROTATE = 1,
+} XYZLevelType;
+
+typedef struct {
+    XYZLevelType method;
+    gboolean update_all;
+} XYZLevelArgs;
+
+typedef struct {
+    XYZLevelArgs *args;
+    GtkWidget *dialogue;
+    GSList *method;
+    GtkWidget *update_all;
+} XYZLevelControls;
+
+static gboolean module_register   (void);
+static void     xyzlevel          (GwyContainer *data,
+                                   GwyRunType run);
+static gboolean xyzlevel_dialogue (XYZLevelArgs *arg);
+static void     method_changed    (GtkToggleButton *toggle,
+                                   XYZLevelControls *controls);
+static void     update_all_changed(GtkToggleButton *toggle,
+                                   XYZLevelControls *controls);
+static void     xyzlevel_do       (GwySurface *surface,
+                                   GwyContainer *data,
+                                   gint id,
+                                   const XYZLevelArgs *args);
+static void     find_plane_coeffs (GwySurface *surface,
+                                   gdouble *a,
+                                   gdouble *bx,
+                                   gdouble *by);
+static void     xyzlevel_load_args(GwyContainer *container,
+                                   XYZLevelArgs *args);
+static void     xyzlevel_save_args(GwyContainer *container,
+                                   XYZLevelArgs *args);
+
+static const XYZLevelArgs xyzlevel_defaults = {
+    XYZ_LEVEL_ROTATE, TRUE,
+};
+
+static GwyModuleInfo module_info = {
+    GWY_MODULE_ABI_VERSION,
+    &module_register,
+    N_("Simple XYZ data leveling."),
+    "Yeti <yeti@gwyddion.net>",
+    "1.0",
+    "David Nečas (Yeti)",
+    "2016",
+};
+
+GWY_MODULE_QUERY(module_info)
+
+static gboolean
+module_register(void)
+{
+    gwy_xyz_func_register("xyz_level",
+                          (GwyXYZFunc)&xyzlevel,
+                          N_("/Plane _Level..."),
+                          NULL,
+                          XYZLEVEL_RUN_MODES,
+                          GWY_MENU_FLAG_XYZ,
+                          N_("Level data by mean plane correction"));
+
+    return TRUE;
+}
+
+static void
+xyzlevel(GwyContainer *data, GwyRunType run)
+{
+    XYZLevelArgs args;
+
+    GwyContainer *settings;
+    GwySurface *surface = NULL;
+    gboolean ok = TRUE;
+    gint id;
+
+    g_return_if_fail(run & XYZLEVEL_RUN_MODES);
+
+    gwy_app_data_browser_get_current(GWY_APP_SURFACE, &surface,
+                                     GWY_APP_SURFACE_ID, &id,
+                                     0);
+    g_return_if_fail(GWY_IS_SURFACE(surface));
+
+    settings = gwy_app_settings_get();
+    xyzlevel_load_args(settings, &args);
+
+    if (run == GWY_RUN_INTERACTIVE)
+        ok = xyzlevel_dialogue(&args);
+
+    xyzlevel_save_args(settings, &args);
+
+    if (ok)
+        xyzlevel_do(surface, data, id, &args);
+}
+
+static gboolean
+xyzlevel_dialogue(XYZLevelArgs *args)
+{
+    static const GwyEnum methods[] = {
+        { N_("Plane subtraction"), XYZ_LEVEL_SUBTRACT, },
+        { N_("Rotation"),          XYZ_LEVEL_ROTATE,   },
+    };
+
+    GtkWidget *dialogue, *label, *check;
+    GtkTable *table;
+    XYZLevelControls controls;
+    gint row, response;
+
+    gwy_clear(&controls, 1);
+    controls.args = args;
+
+    dialogue = gtk_dialog_new_with_buttons(_("Level XYZ Data"), NULL, 0,
+                                           GTK_STOCK_CANCEL,
+                                           GTK_RESPONSE_CANCEL,
+                                           GTK_STOCK_OK,
+                                           GTK_RESPONSE_OK,
+                                           NULL);
+    gtk_dialog_set_default_response(GTK_DIALOG(dialogue), GTK_RESPONSE_OK);
+    gwy_help_add_to_xyz_dialog(GTK_DIALOG(dialogue), GWY_HELP_DEFAULT);
+    controls.dialogue = dialogue;
+
+    table = GTK_TABLE(gtk_table_new(4, 4, FALSE));
+    gtk_container_set_border_width(GTK_CONTAINER(table), 4);
+    gtk_table_set_row_spacings(table, 2);
+    gtk_table_set_col_spacings(table, 6);
+    gtk_box_pack_start(GTK_BOX(GTK_DIALOG(dialogue)->vbox), GTK_WIDGET(table),
+                       FALSE, FALSE, 0);
+    row = 0;
+
+    label = gtk_label_new_with_mnemonic(_("Method:"));
+    gtk_misc_set_alignment(GTK_MISC(label), 0.0, 0.5);
+    gtk_table_attach(table, label, 0, 1, row, row+1,
+                     GTK_EXPAND | GTK_FILL, 0, 0, 0);
+    row++;
+
+    controls.method = gwy_radio_buttons_create(methods, G_N_ELEMENTS(methods),
+                                               G_CALLBACK(method_changed),
+                                               &controls,
+                                               args->method);
+    row = gwy_radio_buttons_attach_to_table(controls.method, table, 3, row);
+
+    check = gtk_check_button_new_with_mnemonic(_("Update X and Y of _all"
+                                                 "compatible data"));
+    controls.update_all = check;
+    gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(check), args->update_all);
+    gtk_widget_set_sensitive(check, args->method == XYZ_LEVEL_ROTATE);
+    g_signal_connect(check, "toggled",
+                     G_CALLBACK(update_all_changed), &controls);
+    gtk_table_attach(table, controls.update_all, 0, 1, row, row+1,
+                     GTK_EXPAND | GTK_FILL, 0, 0, 0);
+
+    gtk_widget_show_all(dialogue);
+
+    do {
+        response = gtk_dialog_run(GTK_DIALOG(dialogue));
+        switch (response) {
+            case GTK_RESPONSE_CANCEL:
+            case GTK_RESPONSE_DELETE_EVENT:
+            gtk_widget_destroy(dialogue);
+            case GTK_RESPONSE_NONE:
+            return FALSE;
+            break;
+
+            case GTK_RESPONSE_OK:
+            break;
+
+            default:
+            g_assert_not_reached();
+            break;
+        }
+    } while (response != GTK_RESPONSE_OK);
+
+    gtk_widget_destroy(dialogue);
+
+    return TRUE;
+}
+
+static void
+method_changed(GtkToggleButton *toggle,
+               XYZLevelControls *controls)
+{
+    XYZLevelArgs *args = controls->args;
+
+    if (toggle && !gtk_toggle_button_get_active(toggle))
+        return;
+
+    args->method = gwy_radio_buttons_get_current(controls->method);
+    gtk_widget_set_sensitive(controls->update_all,
+                             args->method == XYZ_LEVEL_ROTATE);
+}
+
+static void
+update_all_changed(GtkToggleButton *toggle,
+                   XYZLevelControls *controls)
+{
+    controls->args->update_all = gtk_toggle_button_get_active(toggle);
+}
+
+static void
+xyzlevel_do(GwySurface *surface,
+            GwyContainer *data,
+            gint id,
+            const XYZLevelArgs *args)
+{
+    XYZLevelType method = args->method;
+    GwyXYZ *xyz;
+    guint k, n;
+    gdouble a, bx, by;
+
+    find_plane_coeffs(surface, &a, &bx, &by);
+
+    xyz = gwy_surface_get_data(surface);
+    n = gwy_surface_get_npoints(surface);
+    for (k = 0; k < n; k++) {
+        xyz[k].z -= a + bx*xyz[k].x + by*xyz[k].y;
+    }
+
+    if (method == XYZ_LEVEL_ROTATE) {
+        g_warning("Implement me!");
+    }
+
+    /* XXX: This does not do what one would expect because preview updates are
+     * manual.  The app should probably automatically re-render the preview
+     * when it receives "data-changed" on the Surface. */
+    gwy_surface_data_changed(surface);
+}
+
+static void
+find_plane_coeffs(GwySurface *surface, gdouble *a, gdouble *bx, gdouble *by)
+{
+    const GwyXYZ *xyz = gwy_surface_get_data_const(surface);
+    guint k, n = gwy_surface_get_npoints(surface);
+    gdouble sx, sy, sxx, sxy, syy, sz, sxz, syz, D;
+
+    sx = sy = sz = 0.0;
+    for (k = 0; k < n; k++) {
+        sx += xyz[k].x;
+        sy += xyz[k].y;
+    }
+    sx /= n;
+    sy /= n;
+
+    sxx = sxy = syy = sxz = syz = 0.0;
+    for (k = 0; k < n; k++) {
+        gdouble x = xyz[k].x - sx;
+        gdouble y = xyz[k].y - sy;
+        gdouble z = xyz[k].z;
+
+        sxx += x*x;
+        syy += y*y;
+        sxy += x*y;
+        sxz += x*z;
+        syz += y*z;
+    }
+
+    D = sxx*syy - sxy*sxy;
+    *bx = (syy*sxz - sxy*syz)/D;
+    *by = (sxx*syz - sxy*sxz)/D;
+    *a = -(sx*(*bx) + sy*(*by));
+}
+
+static const gchar method_key[]     = "/module/xyz_level/method";
+static const gchar update_all_key[] = "/module/xyz_level/update_all";
+
+static void
+xyzlevel_sanitize_args(XYZLevelArgs *args)
+{
+    args->method = CLAMP(args->method, XYZ_LEVEL_SUBTRACT, XYZ_LEVEL_ROTATE);
+    args->update_all = !!args->update_all;
+}
+
+static void
+xyzlevel_load_args(GwyContainer *container,
+                   XYZLevelArgs *args)
+{
+    *args = xyzlevel_defaults;
+
+    gwy_container_gis_enum_by_name(container, method_key, &args->method);
+    gwy_container_gis_boolean_by_name(container, update_all_key,
+                                      &args->update_all);
+    xyzlevel_sanitize_args(args);
+}
+
+static void
+xyzlevel_save_args(GwyContainer *container,
+                   XYZLevelArgs *args)
+{
+    gwy_container_set_enum_by_name(container, method_key, args->method);
+    gwy_container_set_boolean_by_name(container, update_all_key,
+                                      args->update_all);
+}
+
+/* vim: set cin et ts=4 sw=4 cino=>1s,e0,n0,f0,{0,}0,^0,\:1s,=0,g1s,h0,t0,+1s,c3,(0,u0 : */
